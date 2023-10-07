4369e88a991e550a87017fbc232030a1dc352279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:10:31 +0000

    Add package predictive-smeters/0.0.3/predictive_smeters-0.0.3-py3-none-any.whl

---
+++ packages/predictive-smeters/predictive_smeters-0.0.3-py3-none-any.whl/Predictive_smeters/Config/settings/Predictive_smeters_settings.py
@@ -0,0 +1,6 @@
+def settings():
+    return {
+        "parameters": {"dependent_var": "p"},
+        "directories.data_input": {},
+        "directories.data_output": {},
+    }
---
+++ packages/predictive-smeters/predictive_smeters-0.0.3-py3-none-any.whl/Predictive_smeters/Models/elastic_net_regression.py
@@ -0,0 +1,6 @@
+
+
+class ENR:
+    """Class to implement the Elastic Net Regression algorithm using cross validation."""
+    def __init__(self):
+        pass
