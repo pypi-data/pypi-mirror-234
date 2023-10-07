3947d3b82cd53f950c739a27c66dbcd1bca771a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:06:04 +0000

    Add package bestLibraryEver/0.0.7/bestLibraryEver-0.0.7.tar.gz

---
+++ packages/bestLibraryEver/bestLibraryEver-0.0.7.tar.gz/bestLibraryEver-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7'
+DESCRIPTION = 'A random base class package'
+LONG_DESCRIPTION = 'A random base class package long descriotion'
+
+setup(
+    name='bestLibraryEver',
+    version=VERSION,
+    author='Irakli Nozadze',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python'],
+    # install_requires=[
+    #     'package1',
+    #     'package2',
+    # ],
+)
