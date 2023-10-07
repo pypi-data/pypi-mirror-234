ab2197ae414c28d3db9aba1c9a3b6dab638520d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:04:20 +0000

    Add package predictive-smeters/0.0.1/predictive_smeters-0.0.1-py3-none-any.whl

---
+++ packages/predictive-smeters/predictive_smeters-0.0.1-py3-none-any.whl/Predictive_smeters/Config/__init__.py
@@ -0,0 +1,3 @@
+# Predictive_models.Config.__init__
+from Predictive_smeters.Config.test_data_dirs import TestDataDirs
+from Predictive_smeters.Config.test_data_files import TestDataFiles
---
+++ packages/predictive-smeters/predictive_smeters-0.0.1-py3-none-any.whl/Predictive_smeters/Config/test_data_files.py
@@ -0,0 +1,3 @@
+class TestDataFiles:
+    def create_test_data_files(self):
+        pass
