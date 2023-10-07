1cf9ad00059c1477b5694ab7be91a7c3a90d2657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:13:10 +0000

    Add package comet-llm/1.4.1/comet_llm-1.4.1-py3-none-any.whl

---
+++ packages/comet-llm/comet_llm-1.4.1-py3-none-any.whl/comet_llm/experiment_api/experiment_api.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# *******************************************************
+#   ____                     _               _
+#  / ___|___  _ __ ___   ___| |_   _ __ ___ | |
+# | |   / _ \| '_ ` _ \ / _ \ __| | '_ ` _ \| |
+# | |__| (_) | | | | | |  __/ |_ _| | | | | | |
+#  \____\___/|_| |_| |_|\___|\__(_)_| |_| |_|_|
+#
+#  Sign up for free at https://www.comet.com
+#  Copyright (C) 2015-2023 Comet ML INC
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this package.
+# *******************************************************
+
+from typing import IO, List, Optional
+
+from comet_llm.types import JSONEncodable
+
+from . import comet_api_client, request_exception_wrapper
+
+
+class ExperimentAPI:
+    @request_exception_wrapper.wrap(check_on_prem=True)
+    def __init__(
+        self,
+        api_key: str,
+        workspace: Optional[str],
+        project_name: Optional[str],
+    ):
+        self._client = comet_api_client.get(api_key)
+        self._initialize_experiment(workspace, project_name)
+
+    @property
+    def link(self) -> str:
+        return self._link
+
+    @property
+    def project_url(self) -> str:
+        return self._project_url
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    def _initialize_experiment(
+        self, workspace: Optional[str] = None, project_name: Optional[str] = None
+    ) -> None:
+        response = self._client.create_experiment("LLM", workspace, project_name)
+        self._id: str = response["experimentKey"]
+        self._initialize_links(response["link"])
+
+    def _initialize_links(self, link: str) -> None:
+        self._link = link
+        self._project_url = link[: link.rfind("/")]
+
+    @request_exception_wrapper.wrap(check_on_prem=True)
+    def log_asset_with_io(self, name: str, file: IO, asset_type: str) -> None:
+        self._client.log_experiment_asset_with_io(
+            self._id, name=name, file=file, asset_type=asset_type
+        )
+
+    @request_exception_wrapper.wrap()
+    def log_parameter(self, name: str, value: JSONEncodable) -> None:
+        self._client.log_experiment_parameter(self._id, name=name, value=value)
+
+    @request_exception_wrapper.wrap()
+    def log_metric(self, name: str, value: JSONEncodable) -> None:
+        self._client.log_experiment_metric(self._id, name=name, value=value)
+
+    @request_exception_wrapper.wrap()
+    def log_tags(self, tags: List[str]) -> None:
+        self._client.log_experiment_tags(self._id, tags=tags)
+
+    @request_exception_wrapper.wrap()
+    def log_other(self, name: str, value: JSONEncodable) -> None:
+        self._client.log_experiment_other(self._id, name=name, value=value)
