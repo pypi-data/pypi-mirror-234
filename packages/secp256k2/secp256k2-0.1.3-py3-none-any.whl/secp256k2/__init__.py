0eda87fbe87fc6caf8a80619bdc1822fb7ca0aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:12:35 +0000

    Add package secp256k2/0.1.3/secp256k2-0.1.3-py3-none-any.whl

---
+++ packages/secp256k2/secp256k2-0.1.3-py3-none-any.whl/secp256k2/__init__.py
@@ -0,0 +1,13 @@
+from .Loader import Core
+
+lib = Core()
+Fuzz = lib.Fuzz
+
+__name__ = 'SEC256k2'
+__author__ = 'Mohammadreza (Mmdrza.com)'
+__version__ = '0.1.3'
+__license__ = 'MIT'
+__copyright__ = 'Copyright Mmdrza.com @ 2023'
+__credits__ = ['Mmdrza']
+__description__ = 'SECP256k2 Library for generated and Converted Fast Cryptographic Algorithms Python'
+__all__ = ['Fuzz', 'utils', 'lib', '__name__', '__author__', '__version__', '__license__', '__copyright__', '__credits__', '__description__']
