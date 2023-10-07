112bc21166c19bac2ecf0f6b722d001c3bc006a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:35:13 +0000

    Add package django-reusable/10.7/django_reusable-10.7-py3-none-any.whl

---
+++ packages/django-reusable/django_reusable-10.7-py3-none-any.whl/django_reusable/views/views.py
@@ -0,0 +1,24 @@
+import json
+
+from django.contrib import admin
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def ajax_callback_handler(request, name, pk):
+    callback_params = getattr(admin.site, 'ajax_handler_callback_params', {}).get(name)
+    if callback_params:
+        self, callback = callback_params
+        kwargs = json.loads(request.body)
+        kwargs.update(dict(
+            pk=pk,
+            self=self
+        ))
+        return HttpResponse(callback(**kwargs))
+    return HttpResponse(f'no callback params defined for {name}')
+
+
+@csrf_exempt
+def is_user_authenticated(request):
+    return JsonResponse(request.user.is_authenticated, safe=False)
