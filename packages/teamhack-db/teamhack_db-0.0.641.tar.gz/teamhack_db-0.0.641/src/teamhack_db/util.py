f37aae7f50714a7f621dae9268df9c47326a7e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:10:30 +0000

    Add package teamhack-db/0.0.641/teamhack_db-0.0.641.tar.gz

---
+++ packages/teamhack-db/teamhack_db-0.0.641.tar.gz/teamhack_db-0.0.641/src/teamhack_db/util.py
@@ -0,0 +1,12 @@
+from dnslib import QTYPE
+
+def get_name(name):
+  if not name.endswith('.'): name = f'{name}.'
+  return name
+
+def get_record_type(record_type):
+  if   record_type == 'A':  return QTYPE.A
+  elif record_type == 'NS': return QTYPE.NS
+  elif record_type == 'MX': return QTYPE.MX
+  return None
+
