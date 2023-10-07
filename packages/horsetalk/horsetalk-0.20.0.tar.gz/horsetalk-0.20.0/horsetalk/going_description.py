92dccf028ff9101b7e9ae4f3b79f9ab04901c2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:34:48 +0000

    Add package horsetalk/0.20.0/horsetalk-0.20.0.tar.gz

---
+++ packages/horsetalk/horsetalk-0.20.0.tar.gz/horsetalk-0.20.0/horsetalk/going_description.py
@@ -0,0 +1,10 @@
+from peak_utility.enumeration.parsing_enum import ParsingEnum  # type: ignore
+
+
+class GoingDescription(ParsingEnum):
+    """
+    A parent enumeration for more specific going descriptions. Not intended to be instantiated directly.
+
+    """
+
+    pass
