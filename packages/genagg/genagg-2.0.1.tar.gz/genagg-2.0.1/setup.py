14ecb1304539dbae049020851064ad9d505b3041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:17:48 +0000

    Add package genagg/2.0.1/genagg-2.0.1.tar.gz

---
+++ packages/genagg/genagg-2.0.1.tar.gz/genagg-2.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="genagg",
+    version="2.0.1",
+    packages=find_packages(),
+    install_requires=["torch", "torch_geometric", "wandb", "scipy"],
+    author="Ryan Kortvelesy",
+    description="A Learnable, Generalised Aggregation Module",
+)
