a35bcdcbe339b9052cc14713cacb96da2bbf8602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:06:27 +0000

    Add package jupyterlab-pioneer/0.1.6/jupyterlab_pioneer-0.1.6-py3-none-any.whl

---
+++ packages/jupyterlab-pioneer/jupyterlab_pioneer-0.1.6-py3-none-any.whl/jupyterlab_pioneer/application.py
@@ -0,0 +1,28 @@
+from .handlers import RouteHandler
+from jupyter_server.extension.application import ExtensionApp
+from traitlets import List
+
+class JupyterLabPioneerApp(ExtensionApp):
+
+    name = "jupyterlab_pioneer"
+
+    activeEvents = List([]).tag(config=True)
+    logNotebookContentEvents = List([]).tag(config=True)
+    exporters = List([]).tag(config=True)
+
+    def initialize_settings(self):
+        try:
+            # assert self.activeEvents, "The c.JupyterLabPioneerApp.activeEvents configuration setting must be set."
+            # assert self.exporters, "The c.JupyterLabPioneerApp.exporters configuration must be set, please see the configuration example"
+            pass
+
+        except Exception as e:
+            self.log.error(str(e))
+            raise e
+
+    def initialize_handlers(self):
+        try:
+            self.handlers.extend([(r"/jupyterlab-pioneer/(.*)", RouteHandler)])
+        except Exception as e:
+            self.log.error(str(e))
+            raise e
