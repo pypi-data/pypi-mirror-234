83d08dfd6b9118c7c648573ae1359102fafc8931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:34:33 +0000

    Add package swarms/1.7.8/swarms-1.7.8.tar.gz

---
+++ packages/swarms/swarms-1.7.8.tar.gz/swarms-1.7.8/swarms/__init__.py
@@ -0,0 +1,23 @@
+#swarms
+from swarms.logo import logo2
+print(logo2)
+
+# worker
+from swarms import workers
+from swarms.workers.worker import Worker
+
+#boss
+# from swarms.boss.boss_node import Boss
+
+#models
+from swarms import models
+
+#structs
+from swarms import structs
+
+# swarms
+from swarms import swarms
+from swarms.swarms.orchestrate import Orchestrator
+
+#agents
+from swarms import agents
