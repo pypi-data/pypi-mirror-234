58f646a5295778d5d1c24e988ec590912cef78c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:04:16 +0000

    Add package larvaworld/0.0.127/larvaworld-0.0.127.tar.gz

---
+++ packages/larvaworld/larvaworld-0.0.127.tar.gz/larvaworld-0.0.127/src/larvaworld/lib/model/envs/obstacle.py
@@ -0,0 +1,114 @@
+import numpy as np
+import param
+#import pygame
+from shapely.affinity import affine_transform
+from shapely import geometry
+
+from ... import aux
+from ...model import NamedObject
+from ...param import Contour, ViewableLine
+
+
+__all__ = [
+    'Obstacle',
+    'Box',
+    'Wall',
+    'Border',
+]
+
+class Obstacle(NamedObject, Contour):
+
+    def __init__(self,model,edges=None,**kwargs):
+        NamedObject.__init__(self,model=model)
+        Contour.__init__(self,**kwargs)
+
+        # self.vertices = vertices
+        self.edges = edges
+
+    # def draw(self, viewer):
+    #     viewer.draw_polyline(vertices=self.vertices,color=self.color,width=self.width,closed=True)
+
+class Barrier(NamedObject, ViewableLine):
+
+    def __init__(self,model,edges=None,**kwargs):
+        NamedObject.__init__(self,model=model)
+        ViewableLine.__init__(self,**kwargs)
+
+        # self.vertices = vertices
+        self.edges = edges
+
+    # def draw(self, viewer):
+    #     viewer.draw_polyline(vertices=self.vertices,color=self.color,width=self.width,closed=True)
+
+
+
+
+class Box(Obstacle):
+
+    def __init__(self, x, y, size, **kwargs):
+        self.x = x
+        self.y = y
+        self.size = size
+
+        vert1 = geometry.Point(x - size / 2, y - size / 2)
+        vert2 = geometry.Point(x + size / 2, y - size / 2)
+        vert3 = geometry.Point(x + size / 2, y + size / 2)
+        vert4 = geometry.Point(x - size / 2, y + size / 2)
+
+        vertices = [(vert1.x, vert1.y), (vert2.x, vert2.y), (vert3.x, vert3.y), (vert4.x, vert4.y)]
+        edges = [[vert1, vert2], [vert2, vert3], [vert3, vert4], [vert4, vert1]]
+        super().__init__(vertices=vertices, edges = edges, **kwargs)
+
+
+
+
+class Wall(Barrier):
+    closed = param.Boolean(False)
+
+    def __init__(self, point1, point2, **kwargs):
+        self.point1 = point1
+        self.point2 = point2
+
+        vertices = [(point1.x, point1.y), (point2.x, point2.y)]
+        edges = [[point1, point2]]
+        super().__init__(vertices =vertices, edges =edges,**kwargs)
+
+
+
+class Border(Barrier):
+    closed = param.Boolean(False)
+
+    def __init__(self, vertices,points=None, **kwargs):
+        self.points = points
+        self.border_xy, self.border_lines = self.define_lines(vertices)
+        edges = []
+        #vertices = self.border_xy
+        for l in self.border_lines:
+            (x1, y1), (x2, y2) = list(l.coords)
+            point1 = geometry.Point(x1, y1)
+            point2 = geometry.Point(x2, y2)
+            edges.append([point1, point2])
+
+
+        super().__init__(vertices =vertices, edges =edges,**kwargs)
+
+    def define_lines(self, vertices, s=1):
+        # print(points)
+        # print(len(points))
+
+        lines = [geometry.LineString([tuple(p1), tuple(p2)]) for (p1, p2) in aux.SuperList(vertices).in_pairs]
+
+        T = [s, 0, 0, s, 0, 0]
+        ls = [affine_transform(l, T) for l in lines]
+        ps = [l.coords.xy for l in ls]
+        xy = [np.array([[x, y] for x, y in zip(xs, ys)]) for xs, ys in ps]
+        return xy, ls
+
+
+
+    def contained(self, p):
+        return any([l.distance(geometry.Point(p)) < self.width for l in self.border_lines])
+
+
+
+
---
+++ packages/larvaworld/larvaworld-0.0.127.tar.gz/larvaworld-0.0.127/src/larvaworld/lib/param/drawable.py
@@ -0,0 +1,132 @@
+import param
+
+from .. import aux
+from ..param import NestedConf, Named, RadiallyExtended, BoundedArea, LineExtended, LineClosed, \
+    RandomizedColor, Grid
+
+__all__ = [
+    'Viewable',
+    'ViewableToggleable',
+    'ViewableNamed',
+    'ViewableLine',
+    'Contour',
+    'ViewableLine',
+    'ViewableNamedBoundedArea',
+    'ViewableNamedGrid',
+    'ViewableCircle',
+]
+
+__displayname__ = 'Viewable elements'
+
+class Viewable(NestedConf) :
+    '''
+        Basic Parameterized Class for all visible Objects in simulation
+
+            Args:
+            - default_color: optional str or tuple representing the default color of the agent.
+            - visible: optional boolean indicating whether the agent is visible or not.
+
+
+    '''
+
+
+    default_color = RandomizedColor(default='black', doc='The default color of the entity',instantiate=True)
+    visible = param.Boolean(True, doc='Whether the entity is visible or not')
+    selected = param.Boolean(False, doc='Whether the entity is selected or not')
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.color = self.default_color
+
+    def set_color(self, color):
+        self.color = color
+
+    def set_default_color(self, color):
+        self.default_color = color
+        self.color=color
+
+    def invert_default_color(self):
+        c00, c01 = aux.invert_color(self.default_color)
+        self.set_default_color(c01)
+
+
+    def _draw(self,v,**kwargs):
+        if self.visible :
+            self.draw(v,**kwargs)
+            if self.selected:
+                # raise
+                self.draw_selected(v, **kwargs)
+
+
+    def draw_selected(self, v, **kwargs):
+        pass
+
+    def draw(self, v, **kwargs):
+        pass
+
+    #@property
+    def toggle_vis(self):
+        self.visible = not self.visible
+        return self.visible
+
+class ViewableToggleable(Viewable):
+    active = param.Boolean(False, doc='Whether entity is active')
+    active_color = param.Color('lightblue',doc='The color of the entity when active')
+    inactive_color = param.Color('lightgreen',doc='The color of the entity when inactive')
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        if self.active_color is None:
+            self.active_color = self.default_color
+        if self.inactive_color is None:
+            self.inactive_color = self.default_color
+        self.update_color()
+
+    @param.depends('active', watch=True)
+    def update_color(self):
+        self.color= self.active_color if self.active else self.inactive_color
+
+
+    def toggle(self):
+        self.active = not self.active
+
+
+class ViewableNamed(Viewable,Named): pass
+
+
+
+
+
+
+
+class ViewableLine(Viewable,LineExtended):
+
+
+
+    def draw(self, v, **kwargs):
+        try :
+           v.draw_polyline(vertices=self.vertices,color=self.color,width=self.width,closed=self.closed)
+        except :
+            for ver in self.vertices:
+                v.draw_polyline(ver, color=self.color, width=self.width, closed=self.closed)
+
+
+class Contour(Viewable,LineClosed):
+
+    def draw(self, v, **kwargs):
+        v.draw_polygon(self.vertices, filled=True, color=self.color)
+
+
+class ViewableNamedBoundedArea(Viewable,BoundedArea,Named): pass
+
+class ViewableNamedGrid(Viewable,Grid,Named): pass
+
+class ViewableCircle(Viewable,RadiallyExtended):
+
+    def draw(self, v, filled=True, radius_coeff=1, color=None, width_as_radius_fraction=5):
+        if color is None :
+            color = self.color
+        v.draw_circle(position=self.get_position(), radius=self.radius*radius_coeff, color=color, filled=filled, width=self.radius/width_as_radius_fraction)
+
+
+
