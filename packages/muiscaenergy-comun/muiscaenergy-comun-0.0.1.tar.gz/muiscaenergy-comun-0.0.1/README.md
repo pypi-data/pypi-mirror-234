95d796b5d4108b3cf49a19516a6665c19394080c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:12:34 +0000

    Add package muiscaenergy-comun/0.0.1/muiscaenergy-comun-0.0.1.tar.gz

---
+++ packages/muiscaenergy-comun/muiscaenergy-comun-0.0.1.tar.gz/muiscaenergy-comun-0.0.1/README.md
@@ -0,0 +1,26 @@
+# muiscaenergy-comun
+Common functions for Muisca energy projects
+
+
+Pasos
+1. Crear un nuevo repositorio en github
+2. Clonar el repositorio en local
+3. Crear un nuevo proyecto en Pycharm
+4. Crear un nuevo entorno virtual en Pycharm
+
+Gitiignore
+1. abra el archivo .gitignore 
+2. comente la linea con (*.egg-info/)... en mi version es liena 24
+
+Pasos para crear un paquete
+1. agregar nuevo folder con src (python directory) y otro ocn test (python directory)
+2. crear los archivos de folder
+
+Pasos para subir el paquete a pypi
+1. crear el archivo setup.py 
+2. python setup.py sdist bdist_wheel --> creates egg, build, dist
+3. commit and push to github
+4. twine check dist/* --> check if the package is ok
+5. twine upload dist/* --> upload to pypi
+
+
---
+++ packages/muiscaenergy-comun/muiscaenergy-comun-0.0.1.tar.gz/muiscaenergy-comun-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    setup(
+        name='muiscaenergy-comun',
+        version='0.0.1',
+        url='https://pypi.org/project/',
+        author='Jairo Hernando Cervantes Garcia',
+        emai='muiscaenergy@gmail.com',
+        description='common functions for muiscaenergy projects',
+        license='MIT license',
+        long_description=open('README.md', 'r').read(),
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        classifiers=['Development Status :: 1 - Planning',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Science/Research',
+                     'License :: OSI Approved :: MIT License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3.8',
+                     ],
+        zip_safe=False,
+        install_requires=['timezonefinder >= 6.2.0',
+                          'pandas >= 1.5.3',
+                          ],
+        extras_require={
+            'dev': ["pytest >= 7.0",
+                    "twine >= 4.0.2",
+                    ],
+        },
+        python_requires='>=3.10',
+    )
