32fea1990ec648580317270d2cf7d0aad36b6d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:10:29 +0000

    Add package python-course-by-ty/0.1.0/python_course_by_ty-0.1.0.tar.gz

---
+++ packages/python-course-by-ty/python_course_by_ty-0.1.0.tar.gz/python_course_by_ty-0.1.0/python_course_by_ty/python_course.py
@@ -0,0 +1,145 @@
+#The purpose of this file is to assess the answers by students taking Ty's Python course
+#Students should install this code either via pip or directly placing the file in the folder they will be coding in
+#The notebooks provided to the students will then import this file and make calls to it
+#Each function corresponds to a quiz question in a quiz that is associated with the notebook they are working through
+#If the student's answer is correct, the function will output a keyword or phrase to answer the quiz question with
+#If the answer is incorrect, the function will output a statement telling them their answer was incorrect
+#Risks associated with beginner python students finding answers in this file was felt to be minimal and worth alternatives
+
+
+#Project 1 Problem 1
+def test1_1(answer):
+    if answer == 500000000:
+        print('Tytastic')
+    else:
+        print("Your production_costs variable is not correct.")
+        
+def test1_2(answer):
+    if answer == 1000000:
+        print('Roger Roger')
+    else:
+        print("Your marketing_costs variable is not correct.")
+               
+def test1_3(answer):
+    if answer == 1000000000:
+        print('Grifftacular')
+    else:
+        print("Your ticket_sales variable is not correct.")
+                
+def test1_4(answer):
+    if answer == 0.1:
+        print('Turbo Man')
+    else:
+        print("Your theater_commission variable is not correct.")
+              
+def test1_5(answer):
+    if answer == 399000000.0:
+        print('Evil chickens')
+    else:
+        print("Your estimated_profits variable is not correct.")
+
+#Project 1 Problem 2
+def test1_6(answer):
+    if answer == 500000:
+        print('Go go gocarts')
+    else:
+        print("Your total_seats variable is not correct.")
+        
+def test1_7(answer):
+    if answer == 13:
+        print('Space monkeys')
+    else:
+        print("Your seat_batches variable is not correct.")
+        
+def test1_8(answer):
+    if answer == 7:
+        print('Dang those naughty space monkeys')
+    else:
+        print("Your remaining_seats variable is not correct.")
+        
+#Project 1 Problem 3
+def test1_9(answer):
+    if answer == 0:
+        print('Manic manikin')
+    else:
+        print("Your kyle_production variable is not correct.")
+        
+def test1_10(answer):
+    if answer == 0:
+        print('Manic Monday')
+    else:
+        print("Your kelly_production variable is not correct.")
+        
+def test1_11(answer):
+    if answer == 1:
+        print('Manic monkeys')
+    else:
+        print("Your konner_production variable is not correct.")
+        
+def test1_12(answer):
+    if answer == 4:
+        print('Manic money')
+    else:
+        print("Your kevin_production variable is not correct.")
+        
+def test1_13(answer):
+    if answer == 3:
+        print('Manic munchkins')
+    else:
+        print("Your kassy_production variable is not correct.")
+        
+def test1_14(answer):
+    if answer == 3:
+        print('Manic munchies')
+    else:
+        print("Your kali_production variable is not correct.")
+        
+def test1_15(answer):
+    if answer == 6:
+        print('Manic motors')
+    else:
+        print("Your krissy_production variable is not correct.")
+        
+def test1_16(answer):
+    if answer == 6:
+        print('Manic makeup')
+    else:
+        print("Your kendall_production variable is not correct.")
+        
+def test1_17(answer):
+    if answer == 0:
+        print('Manic mill')
+    else:
+        print("Your kassy2_production variable is not correct.")
+        
+def test1_18(answer):
+    if answer == (.5 + .5 + 1 + 1):
+        print('Manic mastif')
+    else:
+        print("Your kris_production variable is not correct.")
+        
+def test1_19(answer):
+    if answer == (2 + 2 + 1 + 3):
+        print('Manic moonlight')
+    else:
+        print("Your katelynn_production variable is not correct.")
+        
+def test1_20(answer):
+    if answer == 0:
+        print('Manic mayor')
+    else:
+        print("Your todd_production variable is not correct.")
+        
+def test1_21(answer):
+    if answer == 28.0:
+        print('Manic "m" words')
+    else:
+        print("Your total_production variable is not correct.")
+        
+#Project 1 Problem 4
+def test1_22(answer):
+    if answer == 1000000000:
+        print('Wowzers!')
+    else: 
+        print("Your number_of_funny_people variable does not return 1000000000")
+
