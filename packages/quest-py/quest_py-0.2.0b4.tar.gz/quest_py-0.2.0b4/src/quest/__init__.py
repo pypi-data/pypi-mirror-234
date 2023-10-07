c00ae7f2316ed64ea054f42aeb7ac8d158d9d991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:17:27 +0000

    Add package quest-py/0.2.0b4/quest_py-0.2.0b4.tar.gz

---
+++ packages/quest-py/quest_py-0.2.0b4.tar.gz/quest_py-0.2.0b4/src/quest/__init__.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import Callable
+
+from .context import these
+from .wrappers import step, task
+from .external import state, queue, identity_queue, event
+from .historian import Historian
+from .persistence import LocalFileSystemBlobStorage, PersistentHistory
+from .versioning import version, get_version
+
+
+def create_filesystem_historian(save_folder: Path, historian_id: str, function: Callable) -> Historian:
+    storage = LocalFileSystemBlobStorage(save_folder)
+    history = PersistentHistory(historian_id, storage)
+    return Historian(
+        historian_id,
+        function,
+        history
+    )
