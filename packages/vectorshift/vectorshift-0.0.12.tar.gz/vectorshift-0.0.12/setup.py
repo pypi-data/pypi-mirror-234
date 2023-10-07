9add918e99319aaa4e3b386a6fe9a38286c78e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:04:21 +0000

    Add package vectorshift/0.0.12/vectorshift-0.0.12.tar.gz

---
+++ packages/vectorshift/vectorshift-0.0.12.tar.gz/vectorshift-0.0.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("vectorshift/requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="vectorshift",
+    version="0.0.12",
+    packages=find_packages(),
+    author="Alex Leonardi, Eric Shen",
+    author_email="support@vectorshift.ai",
+    description="VectorShift Python SDK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
