734ea6ef105936382e19cd34128cd1910c70fd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:10:24 +0000

    Add package pymakehelper/0.0.28/pymakehelper-0.0.28.tar.gz

---
+++ packages/pymakehelper/pymakehelper-0.0.28.tar.gz/pymakehelper-0.0.28/pymakehelper/utils.py
@@ -0,0 +1,81 @@
+import logging
+import os
+import subprocess
+
+from pymakehelper.static import APP_NAME
+from pymakehelper.configs import ConfigSymlinkInstall
+
+
+def ensure_dir(f):
+    folder = os.path.dirname(f)
+    if folder != '' and not os.path.isdir(folder):
+        os.makedirs(folder)
+
+
+def touch(f):
+    if os.path.isfile(f):
+        os.utime(f, None)
+    else:
+        with open(f, "w"):
+            pass
+
+
+def touch_mkdir(f):
+    ensure_dir(f)
+    touch(f)
+
+
+def touch_mkdir_many(filenames):
+    for filename in filenames:
+        touch_mkdir(filename)
+
+
+def no_err_run(args):
+    subprocess.call(args)
+
+
+def get_logger():
+    return logging.getLogger(APP_NAME)
+
+
+def do_real_install(source, target):
+    logger = get_logger()
+    if ConfigSymlinkInstall.unlink:
+        if os.path.islink(target):
+            logger.info(f"unlinking [{target}]")
+            os.unlink(target)
+    if ConfigSymlinkInstall.doit:
+        logger.info(f"symlinking [{source}], [{target}]")
+        os.symlink(source, target)
+
+
+def do_install(source, target):
+    """install a single item"""
+    if ConfigSymlinkInstall.incremental:
+        if os.path.exists(target):
+            if os.path.islink(target):
+                if os.readlink(target) != source:
+                    do_real_install(source, target)
+            else:
+                raise ValueError(f"{target} is of non symlink type")
+        else:
+            do_real_install(source, target)
+    else:
+        do_real_install(source, target)
+
+
+def file_gen(root_folder: str, recurse: bool):
+    """generate all files in a folder"""
+    if recurse:
+        for root, directories, files in os.walk(root_folder):
+            yield root, directories, files
+    else:
+        directories = []
+        files = []
+        for file in os.listdir(root_folder):
+            full = os.path.join(root_folder, file)
+            if os.path.isdir(full):
+                directories.append(file)
+            if os.path.isfile(full):
+                files.append(file)
+        yield root_folder, directories, files
