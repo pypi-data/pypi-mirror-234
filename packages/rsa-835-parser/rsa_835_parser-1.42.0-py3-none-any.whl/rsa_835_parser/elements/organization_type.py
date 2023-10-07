f5fffcdcc43a134c56f8779fb50b88e666866b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:04:19 +0000

    Add package rsa-835-parser/1.42.0/rsa_835_parser-1.42.0-py3-none-any.whl

---
+++ packages/rsa-835-parser/rsa_835_parser-1.42.0-py3-none-any.whl/rsa_835_parser/elements/organization_type.py
@@ -0,0 +1,15 @@
+""" Organization Type """
+from rsa_835_parser.elements import Element
+
+organization_types = {
+	'PE': 'payee',
+	'PR': 'payer',
+}
+
+
+class OrganizationType(Element):
+    """ Organization Type class """
+
+    def parser(self, value: str) -> str:
+        value = value.strip()
+        return organization_types.get(value, value)
