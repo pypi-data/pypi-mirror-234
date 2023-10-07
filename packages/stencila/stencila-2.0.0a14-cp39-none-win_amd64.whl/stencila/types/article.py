bcae1be811bdeb13c2ab1d54a0dc08e7dec9b6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:21:37 +0000

    Add package stencila/2.0.0a14/stencila-2.0.0a14-cp39-none-win_amd64.whl

---
+++ packages/stencila/stencila-2.0.0a14-cp39-none-win_amd64.whl/stencila/types/article.py
@@ -0,0 +1,24 @@
+# Generated file; do not edit. See the Rust `schema-gen` crate.
+
+from .prelude import *
+
+from .creative_work import CreativeWork
+from .int_or_str import IntOrStr
+
+
+@dataclass(kw_only=True, frozen=True)
+class Article(CreativeWork):
+    """
+    An article, including news and scholarly articles.
+    """
+
+    type: Literal["Article"] = field(default="Article", init=False)
+
+    page_start: Optional[IntOrStr] = None
+    """The page on which the article starts; for example "135" or "xiii"."""
+
+    page_end: Optional[IntOrStr] = None
+    """The page on which the article ends; for example "138" or "xvi"."""
+
+    pagination: Optional[str] = None
+    """Any description of pages that is not separated into pageStart and pageEnd; for example, "1-6, 9, 55"."""
