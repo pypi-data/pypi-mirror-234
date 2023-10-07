5ba9d68a2bd93d5ace3bdaffa785dbb1c16a8b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:05:11 +0000

    Add package rsa-835-parser/1.36.0/rsa-835-parser-1.36.0.tar.gz

---
+++ packages/rsa-835-parser/rsa-835-parser-1.36.0.tar.gz/rsa-835-parser-1.36.0/setup.py
@@ -0,0 +1,30 @@
+""" Setup file for rsa-835-parser """
+from setuptools import setup, find_packages
+
+VERSION = "1.36.0"
+DESCRIPTION = "B Consulting Group EDI 835 file format parser."
+LONG_DESCRIPTION = (
+    "B Consulting EDI 835 file format parser, extending the Keiron Stoddart version."
+)
+
+install_requires = ["pandas"]
+
+tests_require = ["pytest"]
+
+setup(
+    name="rsa-835-parser",
+    version=VERSION,
+    author="Roberto Suarez",
+    author_email="<robsua68@hotmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=install_requires,
+    url="https://github.com/robsua68/bcg-835-parser",
+    keywords=["python", "EDI", "835", "X12"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+    tests_require=tests_require,
+    python_requires=">=3.11.5",
+)
