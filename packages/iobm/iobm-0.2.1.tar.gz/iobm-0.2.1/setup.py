a7da63cf60d0c1cec95d25d57461b020e73a09e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:05:56 +0000

    Add package iobm/0.2.1/iobm-0.2.1.tar.gz

---
+++ packages/iobm/iobm-0.2.1.tar.gz/iobm-0.2.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "iobm",
+    version = "0.2.1",
+    author = "Mohammed Junaid",
+    author_email = "junaid1607@outlook.com",
+    description = "A simple conditional adverserial neural network module.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/MJ-GITHUB-007/cGAN",
+    project_urls = {
+        "GitHub" : "https://github.com/MJ-GITHUB-007/cGAN",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cGAN = iobm.cgan:run_cGAN',
+        ],
+    },
+    python_requires = ">=3.6",
+    install_requires=[
+        'Pillow',
+        'torch',
+        'torchvision',
+        'tqdm',
+    ],
+    license='MIT',
+)
