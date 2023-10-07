10537c94b6998165da1e83e7af370a5d17ef6787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:23:04 +0000

    Add package mpzinke/0.7.0/mpzinke-0.7.0.tar.gz

---
+++ packages/mpzinke/mpzinke-0.7.0.tar.gz/mpzinke-0.7.0/src/mpzinke/threading/LoopingThread.py
@@ -0,0 +1,36 @@
+#!/opt/homebrew/bin/python3
+# -*- coding: utf-8 -*-
+__author__ = "MPZinke"
+
+
+########################################################################################################################
+#                                                                                                                      #
+#   created by: MPZinke                                                                                                #
+#   on 2020.11.04                                                                                                      #
+#                                                                                                                      #
+#   DESCRIPTION:                                                                                                       #
+#   BUGS:                                                                                                              #
+#   FUTURE: Consider moving to multiprocessing::Process instead.                                                       #
+#                                                                                                                      #
+########################################################################################################################
+
+
+from typing import Optional
+
+
+from . import BaseThread
+
+
+class LoopingThread(BaseThread):
+	def __init__(self, name: str, *, action: callable, time: callable):
+		BaseThread.__init__(self, name, action=action, time=time)
+
+
+	def __call__(self):
+		try:  # make it safe!!!
+			while(self._is_active):
+				self.action();
+				self.sleep(self.time());
+
+		except Exception as error:
+			print(error)
