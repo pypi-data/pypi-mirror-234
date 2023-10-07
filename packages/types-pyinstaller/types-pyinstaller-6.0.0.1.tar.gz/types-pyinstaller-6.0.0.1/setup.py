7120cded69c22334503dbc59daaffd98ac6311c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:17:58 +0000

    Add package types-pyinstaller/6.0.0.1/types-pyinstaller-6.0.0.1.tar.gz

---
+++ packages/types-pyinstaller/types-pyinstaller-6.0.0.1.tar.gz/types-pyinstaller-6.0.0.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+name = "types-pyinstaller"
+description = "Typing stubs for pyinstaller"
+long_description = '''
+## Typing stubs for pyinstaller
+
+This is a PEP 561 type stub package for the `pyinstaller` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`pyinstaller`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/pyinstaller. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `54a8fd74a785ff7916bade3699a2be4542f1b1c0` and was tested
+with mypy 1.5.1, pyright 1.1.328, and
+pytype 2023.8.31.
+'''.lstrip()
+
+setup(name=name,
+      version="6.0.0.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/pyinstaller.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-setuptools'],
+      packages=['pyi_splash-stubs', 'PyInstaller-stubs'],
+      package_data={'pyi_splash-stubs': ['__init__.pyi', 'METADATA.toml'], 'PyInstaller-stubs': ['__init__.pyi', '__main__.pyi', 'building/__init__.pyi', 'building/api.pyi', 'building/build_main.pyi', 'building/datastruct.pyi', 'building/splash.pyi', 'compat.pyi', 'depend/__init__.pyi', 'depend/analysis.pyi', 'depend/imphookapi.pyi', 'isolated/__init__.pyi', 'isolated/_parent.pyi', 'lib/__init__.pyi', 'lib/modulegraph/__init__.pyi', 'lib/modulegraph/modulegraph.pyi', 'utils/__init__.pyi', 'utils/hooks/__init__.pyi', 'utils/hooks/conda.pyi', 'utils/win32/versioninfo.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
