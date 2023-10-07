aee38300226f436912359aa985ca141a711c1b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:05:15 +0000

    Add package drupal-api/0.3/drupal_api-0.3.tar.gz

---
+++ packages/drupal-api/drupal_api-0.3.tar.gz/drupal_api-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='drupal_api',
+    version='0.3',
+    description='A Python package for working with the Drupal REST API',
+    author='Suraj Ho',
+    author_email='suraj@ho-sting.com',
+    packages=['drupal_api'],
+    install_requires=['fake_useragent'],
+)
