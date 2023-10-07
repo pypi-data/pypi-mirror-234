dbafb7b4a34b010d9c6d7e13b1b42bfd31381cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:05:11 +0000

    Add package django-restful-translator/0.3.0/django_restful_translator-0.3.0.tar.gz

---
+++ packages/django-restful-translator/django_restful_translator-0.3.0.tar.gz/django_restful_translator-0.3.0/django_restful_translator/management/commands/generate_locales.py
@@ -0,0 +1,38 @@
+import os
+
+import polib
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from django_restful_translator.utils import fetch_translatable_fields, get_po_file_path, get_po_metadata
+
+
+class Command(BaseCommand):
+    help = 'Generate .po files from DB translations'
+
+    def handle(self, *args, **options):
+        for language_set in settings.LANGUAGES:
+            language = language_set[0]
+            po_file_path = get_po_file_path(language)
+            if os.path.isfile(po_file_path):
+                po = polib.pofile(po_file_path)
+            else:
+                po = polib.POFile()
+
+            translations = fetch_translatable_fields(language)
+
+            for trans in translations:
+                self.write_to_po_file(po, trans)
+
+            po.metadata = get_po_metadata()
+            po.save(po_file_path)
+
+    def write_to_po_file(self, po, trans):
+        entry = polib.POEntry(
+            msgid=getattr(trans.content_object, trans.field_name),
+            msgstr=trans.field_value,
+            tcomment=f"{trans.content_object._meta.model_name}__{trans.field_name}__{trans.object_id}"
+        )
+        if not trans.field_value:
+            entry.flags.append('fuzzy')
+        po.append(entry)
