d455fabb6efb82e395f8ab877754f4a68b1a9e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:10:57 +0000

    Add package snqueue/0.1.6/snqueue-0.1.6-py3-none-any.whl

---
+++ packages/snqueue/snqueue-0.1.6-py3-none-any.whl/snqueue/__init__.py
@@ -0,0 +1,44 @@
+import time
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from snqueue.snqueue import DataModel, ServiceFunc, SnQueueMessenger, SnQueueService
+
+def start_service(
+    name: str,
+    aws_profile_name: str,
+    service_sqs_url: str,
+    service_func: ServiceFunc,
+    silent: bool=False,
+    interval: int=3,
+    max_instances: int=2,
+    sqs_args: dict={'MaxNumberOfMessages': 1},
+    data_model: DataModel=None
+) -> BackgroundScheduler:
+  # Set logging
+  logging.basicConfig(level=logging.INFO)
+  logging.getLogger('botocore').setLevel(logging.WARNING)
+  logging.getLogger('apscheduler').setLevel(logging.WARNING)
+  logging.getLogger('snqueue.service.%s' % name).setLevel(logging.INFO)
+
+  # Setup and start the service
+  service = SnQueueService(
+    name,
+    aws_profile_name,
+    service_func,
+    silent=silent,
+    data_model_class=data_model
+  )
+
+  scheduler = BackgroundScheduler()
+  scheduler.add_job(
+    service.run,
+    args=[service_sqs_url, sqs_args],
+    trigger='interval',
+    seconds=interval,
+    max_instances=max_instances
+  )
+  scheduler.start()
+  service.logger.info('The service `%s` is up and running.' % name)
+
+  return scheduler
