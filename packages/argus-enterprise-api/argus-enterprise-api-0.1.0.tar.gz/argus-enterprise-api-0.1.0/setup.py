b1f86efdd4644b6dff124649bb7a01c5c33da157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:05:56 +0000

    Add package argus-enterprise-api/0.1.0/argus-enterprise-api-0.1.0.tar.gz

---
+++ packages/argus-enterprise-api/argus-enterprise-api-0.1.0.tar.gz/argus-enterprise-api-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="argus-enterprise-api",
+    version="0.1.0",
+    author="Andrei Budaes-Tanaru",
+    author_email="budaesandrei@gmail.com",
+    description="A Python SDK for interacting with the Argus Enterprise API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/budaesandrei/argus-enterprise-api",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "cryptography>=41.0.4, <42.0.0",
+        "PyJWT>=2.8.0, <3.0.0",
+        "requests>=2.31.0, <3.0.0",
+        "setuptools>=58.1.0, <59.0.0",
+    ],
+    python_requires=">=3.9",
+)
