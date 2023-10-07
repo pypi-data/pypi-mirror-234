f69bc966f1005b1320ed7a1bfd83cd93c4e1e2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:17:56 +0000

    Add package pyflwdir/0.5.8/pyflwdir-0.5.8-py3-none-any.whl

---
+++ packages/pyflwdir/pyflwdir-0.5.8-py3-none-any.whl/pyflwdir/__init__.py
@@ -0,0 +1,11 @@
+"""Fast methods to work with hydro- and topography data in pure Python."""
+
+# version number without 'v' at start
+__version__ = "0.5.8"
+
+# submodules
+from .flwdir import *
+from .pyflwdir import *
+from .dem import *
+from .core_nextxy import *
+from . import gis_utils, regions
