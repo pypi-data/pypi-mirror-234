7e4a78436c77532df50b3f3427f81b2ab8d9b586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:30:32 +0000

    Add package vectorshift/0.0.11/vectorshift-0.0.11-py3-none-any.whl

---
+++ packages/vectorshift/vectorshift-0.0.11-py3-none-any.whl/vectorshift/consts.py
@@ -0,0 +1,7 @@
+INPUT_NODE_TYPES = ["text", "file"]
+OUTPUT_NODE_TYPES = ["text", "file"]
+# TODO: this might be redundant
+SUPPORTED_OPENAI_LLMS = ["gpt-4", "gpt-3.5-turbo"]
+
+# For testing - remove upon public release
+MODE = 'DEV'
