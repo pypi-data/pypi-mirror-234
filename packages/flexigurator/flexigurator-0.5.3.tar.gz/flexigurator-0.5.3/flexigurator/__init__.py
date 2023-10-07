0dbb5209e052773d62f84a4f988cfa465905a6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:13:10 +0000

    Add package flexigurator/0.5.3/flexigurator-0.5.3.tar.gz

---
+++ packages/flexigurator/flexigurator-0.5.3.tar.gz/flexigurator-0.5.3/flexigurator/__init__.py
@@ -0,0 +1,3 @@
+from flexigurator.config_patch import patch_config
+from flexigurator.config_versions import ConfigVersions, DirectorySource
+from flexigurator.placeholder import NotConfiguredError, placeholder
