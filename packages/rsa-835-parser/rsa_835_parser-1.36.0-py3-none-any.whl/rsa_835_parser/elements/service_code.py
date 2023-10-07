1aa22030a76cc8b7c04fad8378ef9c01237a0676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:05:11 +0000

    Add package rsa-835-parser/1.36.0/rsa_835_parser-1.36.0-py3-none-any.whl

---
+++ packages/rsa-835-parser/rsa_835_parser-1.36.0-py3-none-any.whl/rsa_835_parser/elements/service_code.py
@@ -0,0 +1,11 @@
+"""Service Code"""
+from rsa_835_parser.elements import Element
+from rsa_835_parser.elements.utilities import split_element
+
+class ServiceCode(Element):
+    """Service Code class"""
+
+    def parser(self, value: str) -> str:
+        value = split_element(value)
+        _, code, *_ = value
+        return code
