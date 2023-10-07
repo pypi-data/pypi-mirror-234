f686591ca493831ed35789f7c3476c2bf7ae51ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:19:04 +0000

    Add package vowpal-wabbit-next/0.7.0/vowpal_wabbit_next-0.7.0-cp39-cp39-macosx_10_13_universal2.whl

---
+++ packages/vowpal-wabbit-next/vowpal_wabbit_next-0.7.0-cp39-cp39-macosx_10_13_universal2.whl/vowpal_wabbit_next/labels.py
@@ -0,0 +1,10 @@
+from vowpal_wabbit_next import _core
+
+LabelType = _core.LabelType
+SimpleLabel = _core.SimpleLabel
+MulticlassLabel = _core.MulticlassLabel
+CBLabel = _core.CBLabel
+CSLabel = _core.CSLabel
+CCBLabel = _core.CCBLabel
+
+CCBExampleType = _core.CCBExampleType
