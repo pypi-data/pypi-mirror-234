d0f23e354e2e8eea47fbb0db5200266269d8008a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:15:05 +0000

    Add package briefnewscli/0.1.2/briefnewscli-0.1.2.tar.gz

---
+++ packages/briefnewscli/briefnewscli-0.1.2.tar.gz/briefnewscli-0.1.2/briefnewscli/api.py
@@ -0,0 +1,36 @@
+# api.py API integration functions
+
+import requests
+from decouple import config
+
+# Accessing variables from the .env file
+api_key = config('NYTimes_API_KEY')
+base_url = config('NYTimes_API_TopStories_URL')
+
+
+def fetch_top_stories():
+    # Request parameters
+    params = {
+        "api-key": api_key
+    }
+
+    try:
+        # Make the GET request to the API
+        response = requests.get(base_url, params=params)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+
+            # Extract and return the list of top stories
+            top_stories = data.get("results", [])
+            return top_stories
+        else:
+            # Handle errors (e.g., print an error message)
+            print(f"Error: Unable to fetch top stories. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        # Handle exceptions (e.g., network issues)
+        print(f"An error occurred: {str(e)}")
+        return None
