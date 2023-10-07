4f2a1220442a1876b92e1d80139d1e1a368c860b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:06:10 +0000

    Add package briefnewscli/0.1.7/briefnewscli-0.1.7.tar.gz

---
+++ packages/briefnewscli/briefnewscli-0.1.7.tar.gz/briefnewscli-0.1.7/setup.py
@@ -0,0 +1,30 @@
+#setup.py The packaging script that specifies how your CLI should be distributed.
+from setuptools import setup, find_packages
+
+setup(
+    name='briefnewscli',
+    version='0.1.7',
+    packages=find_packages(),
+    install_requires=[
+        'typer',  # Add any other dependencies your CLI uses
+        'python-decouple',
+        'requests',
+        'typer',
+        'torch',
+        'transformers'
+    ],
+    entry_points='''
+        [console_scripts]
+        briefnewscli=briefnewscli.cli:app
+    ''',
+)
+
+# Package Your CLI
+# python setup.py sdist bdist_wheel
+
+# Upload to PyPI:
+# pip install twine
+# twine upload dist/* --verbose
+
+# Installation by Users:v
+# pip install briefnewscli
