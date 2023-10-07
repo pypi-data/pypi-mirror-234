3b644439b91d74b7899909d6a1e5c9dd38dc3317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:04:16 +0000

    Add package rsa-835-parser/1.35.0/rsa_835_parser-1.35.0-py3-none-any.whl

---
+++ packages/rsa-835-parser/rsa_835_parser-1.35.0-py3-none-any.whl/rsa_835_parser/elements/remark_qualifier.py
@@ -0,0 +1,14 @@
+""" Remark Qualifier """
+from rsa_835_parser.elements import Element, Code
+
+remark_qualifiers = {
+	'HE': 'claim payment'
+}
+
+
+class RemarkQualifier(Element):
+    """ Remark Qualifier class """
+
+    def parser(self, value: str) -> Code:
+        description = remark_qualifiers.get(value, None)
+        return Code(value, description)
