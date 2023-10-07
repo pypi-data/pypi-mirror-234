1db2525593370644fcd8774bde19618f88d4a711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:17:45 +0000

    Add package ductile-ui/0.1.1/ductile_ui-0.1.1-py3-none-any.whl

---
+++ packages/ductile-ui/ductile_ui-0.1.1-py3-none-any.whl/ductile/controller/messageable_controller.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+import discord
+
+from .controller import ViewController
+
+if TYPE_CHECKING:
+    from ..view import View  # noqa: TID252
+
+
+class MessageableController(ViewController):
+    def __init__(self, view: "View", *, messageable: discord.abc.Messageable, timeout: float | None = 180) -> None:
+        super().__init__(view, timeout=timeout)
+        self.__messageable = messageable
+
+    async def send(self) -> None:
+        target = self.__messageable
+        view_kwargs = self._process_view_for_discord("files")
+
+        self.message = await target.send(**view_kwargs)
