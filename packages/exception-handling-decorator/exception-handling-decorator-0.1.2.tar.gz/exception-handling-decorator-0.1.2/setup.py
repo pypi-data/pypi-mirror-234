04106b7648ca138eafe744c173ba82dcea085874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:15:02 +0000

    Add package exception-handling-decorator/0.1.2/exception-handling-decorator-0.1.2.tar.gz

---
+++ packages/exception-handling-decorator/exception-handling-decorator-0.1.2.tar.gz/exception-handling-decorator-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="exception-handling-decorator",
+    version="0.1.2",
+    author="Zoran Jankovic",
+    author_email="bpzoran@yahoo.com",
+    url="https://github.com/bpzoran/exception-handling-decorator",
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    # Specify the content type explicitly
+    long_description_content_type="text/markdown",
+    description="ExceptionHandler: Python library to decorate\
+        exception handling.",
+)
