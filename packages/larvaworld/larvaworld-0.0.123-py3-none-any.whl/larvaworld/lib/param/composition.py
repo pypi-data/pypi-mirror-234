efc3156b1212cc94f124be17b66300b0399c0774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:09:40 +0000

    Add package larvaworld/0.0.123/larvaworld-0.0.123-py3-none-any.whl

---
+++ packages/larvaworld/larvaworld-0.0.123-py3-none-any.whl/larvaworld/lib/param/composition.py
@@ -0,0 +1,237 @@
+import numpy as np
+import pandas as pd
+import param
+from scipy.stats import multivariate_normal
+
+from .. import aux
+from ..param import PositiveNumber, PositiveInteger, NestedConf, StringRobust, OptionalPositiveNumber, \
+    OptionalPositiveRange, ClassAttr, ClassDict
+
+__all__ = [
+    'Compound',
+    'Substrate',
+    'substrate_dict',
+    'Odor',
+    'Epoch',
+    'Life',
+]
+
+__displayname__ = 'Nutrition & Olfaction'
+
+
+class Compound(NestedConf):
+    d = PositiveNumber(doc=f'density in g/cm**3')
+    w = PositiveNumber(doc=f'molecular weight (g/mol)')
+    nC = PositiveInteger(doc=f'number of carbon atoms')
+    nH = PositiveInteger(doc=f'number of hydrogen atoms')
+    nO = PositiveInteger(doc=f'number of oxygen atoms')
+    nN = PositiveInteger(doc=f'number of nitrogen atoms')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ww = self.nC + self.nH / 12 * 1 + self.nO / 12 * 16 + self.nN / 12 * 14
+
+
+# Compounds
+compound_dict = aux.AttrDict(
+    {
+        'glucose': Compound(w=180.18, nC=6, nH=12, nO=6),
+        'dextrose': Compound(w=198.17, nC=6, nH=12, nO=7),
+        'saccharose': Compound(w=342.30, nC=12, nH=22, nO=11),
+        'yeast': Compound(w=274.3, nC=19, nH=14, nO=2),
+        'agar': Compound(w=336.33, nC=0, nH=38, nO=19),
+        'cornmeal': Compound(w=359.33, nC=27, nH=48, nO=20),
+        # 'apple_juice': Compound(w=180.18,nC=6, nH=12, nO=6),
+        'water': Compound(w=18.01528, nC=0, nH=2, nO=1)
+    }
+)
+
+all_compounds = [a for a in list(compound_dict.keys()) if a not in ['water']]
+nutritious_compounds = [a for a in list(compound_dict.keys()) if a not in ['water', 'agar']]
+
+
+# class Composition(param.Dict):
+#     def __init__(self, default=aux.AttrDict({k : 0.0 for k in all_compounds}),**params):
+#         param.Dict.__init__(self,default=default, **params)
+
+
+class Substrate(NestedConf):
+    composition = param.Dict({k: 0.0 for k in all_compounds}, doc='The substrate composition')
+    quality = param.Magnitude(1.0,
+                              doc='The substrate quality as percentage of nutrients relative to the intact substrate type')
+
+    def __init__(self, quality=1.0, type=None, **kwargs):
+        if type is not None and type in substrate_dict.keys():
+            composition = substrate_dict[type].composition
+        else:
+            composition = {k: kwargs[k] if k in kwargs.keys() else 0.0 for k in all_compounds}
+        super().__init__(composition=composition, quality=quality)
+        self.d_water = 1
+        self.d_yeast_drop = 0.125  # g/cm**3 https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwi3iaeqipLxAhVPyYUKHTmpCqMQFjAAegQIAxAD&url=https%3A%2F%2Fwww.mdpi.com%2F2077-0375%2F11%2F3%2F182%2Fpdf&usg=AOvVaw1qDlMHxBPu73W8B1vZWn76
+        self.V_drop = 0.05  # cm**3
+        self.d = self.d_water + sum(list(self.composition.values()))
+        self.C = self.get_C()
+        self.X = self.get_X()
+        self.X_ratio = self.get_X_ratio()
+
+    def get_d_X(self, compounds=None, quality=None):
+        if quality is None:
+            quality = self.quality
+        if compounds is None:
+            compounds = nutritious_compounds
+        return sum([self.composition[c] for c in compounds]) * quality
+
+    def get_w_X(self, compounds=None):
+        if compounds is None:
+            compounds = nutritious_compounds
+        d_X = self.get_d_X(compounds, quality=1)
+        if d_X > 0:
+            return sum([self.composition[c] * compound_dict[c].ww for c in compounds]) / d_X
+        else:
+            return 0.0
+
+    def get_X(self, quality=None, compounds=None):
+        if quality is None:
+            quality = self.quality
+        if compounds is None:
+            compounds = nutritious_compounds
+        d_X = self.get_d_X(compounds, quality)
+        if d_X > 0:
+            return d_X / self.get_w_X(compounds)
+        else:
+            return 0.0
+
+    def get_mol(self, V, **kwargs):
+        return self.get_X(**kwargs) * V
+
+    def get_f(self, K, **kwargs):
+        X = self.get_X(**kwargs)
+        return X / (K + X)
+
+    def get_C(self, quality=None):
+        return self.d_water / compound_dict['water'].w + self.get_X(quality, compounds=all_compounds)
+
+    def get_X_ratio(self, **kwargs):
+        return self.get_X(**kwargs) / self.get_C(**kwargs)
+
+
+# Compound densities (g/cm**3)
+substrate_dict = aux.AttrDict(
+    {
+        'agar': Substrate(agar=0.016),
+        'standard': Substrate(glucose=0.1, yeast=0.05, agar=0.016),
+        'sucrose': Substrate(glucose=0.0171, agar=0.004),
+        'cornmeal': Substrate(glucose=517 / 17000, dextrose=1033 / 17000, cornmeal=1716 / 17000, agar=93 / 17000),
+        'cornmeal2': Substrate(dextrose=450 / 6400, yeast=90 / 6400, cornmeal=420 / 6400, agar=42 / 6400),
+        # [1] M. E. Wosniack, N. Hu, J. Gjorgjieva, and J. Berni, “Adaptation of Drosophila larva foraging in response to changes in food distribution,” bioRxiv, p. 2021.06.21.449222, 2021.
+        'PED_tracker': Substrate(saccharose=0.01, yeast=0.1875, agar=5),
+        # 'apple_juice': Substrate(glucose=0.00171, agar=0.004, apple_juice=0.02625)
+    }
+)
+
+
+class Odor(NestedConf):
+    id = StringRobust(None, doc='The unique ID of the odorant')
+    intensity = OptionalPositiveNumber(softmax=10.0, doc='The peak concentration of the odorant in micromoles')
+    spread = OptionalPositiveNumber(softmax=10.0, doc='The spread of the concentration gradient around the peak')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._update_distro()
+
+    @param.depends('intensity', 'spread', watch=True)
+    def _update_distro(self):
+        if self.intensity is not None and self.spread is not None:
+            self.dist = multivariate_normal([0, 0], [[self.spread, 0], [0, self.spread]])
+            self.peak_value = self.intensity / self.dist.pdf([0, 0])
+        else:
+            self.dist = None
+            self.peak_value = 0.0
+
+    def gaussian_value(self, pos):
+        if self.dist:
+            return self.dist.pdf(pos) * self.peak_value
+        else:
+            return None
+
+    def draw_dist(self):
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from scipy.stats import multivariate_normal
+        I = 2
+        s = 0.0002
+        r = 0.05
+        x, y = np.mgrid[-r:r:.001, -r:r:.001]
+        rv = multivariate_normal([0, 0], [[s, 0], [0, s]])
+        p0 = rv.pdf((0, 0))
+        data = np.dstack((x, y))
+        z = rv.pdf(data) * I / p0
+        plt.contourf(x, y, z, cmap='coolwarm')
+        plt.show()
+
+    @classmethod
+    def oG(cls, c=1, id='Odor'):
+        return cls(id=id, intensity=2.0 * c, spread=0.0002 * np.sqrt(c))
+
+    @classmethod
+    def oD(cls, c=1, id='Odor'):
+        return cls(id=id, intensity=300.0 * c, spread=0.1 * np.sqrt(c))
+
+
+class Epoch(NestedConf):
+    age_range = OptionalPositiveRange((0.0, None), softmax=100.0, hardmax=250.0,
+                                      doc='The beginning and end of the epoch in hours post-hatch.')
+    substrate = ClassAttr(Substrate, default=Substrate(type='standard'), doc='The substrate of the epoch')
+
+
+class Life(NestedConf):
+    age = OptionalPositiveNumber(0.0, softmax=100.0, hardmax=250.0,
+                                 doc='The larva age in hours post-hatch at the start of the behavioral simulation. '
+                                     'The larva will grow to that age based on the DEB model. If age is None the '
+                                     'larva will grow to pupation.')
+    epochs = ClassDict(item_type=Epoch, doc='The feeding epochs comprising life history.')
+
+
+class Life3(NestedConf):
+    age = OptionalPositiveNumber(0.0, softmax=100.0, hardmax=250.0,
+                                 doc='The larva age in hours post-hatch at the start of the behavioral simulation. '
+                                     'The larva will grow to that age based on the DEB model. If age is None the '
+                                     'larva will grow to pupation.')
+    age_ticks = param.List([0.0], item_type=float,
+                           doc='The larva age in hours post-hatch at the end of the rearing periods.The last-one is '
+                               'always equal to the final age (or None). The first is 0.0.')
+    subs = param.List([], item_type=Substrate, doc='The substrates of the rearing periods.')
+    reach_pupation = param.Boolean(False, doc='If True the larva will grow to pupation.')
+
+    @param.depends('grow_to_pupation')
+    def update_age_to_inf(self):
+        if self.reach_pupation:
+            self.age = param.Infinity
+        else:
+            self.age = max(self.age_ticks)
+
+    @param.depends('age')
+    def update_last_tick(self):
+        if self.age not in self.age_ticks:
+            self.age_ticks = sorted([t for t in self.age_ticks if t < self.age])
+            self.age_ticks.append(self.age)
+
+    @param.depends('age_ticks', 'epoch_substrates')
+    def update_substrates(self):
+        while self.Nmismatch != 0:
+            if self.Nmismatch > 0:
+                self.subs += Substrate()
+            elif self.Nmismatch < 0:
+                self.subs.pop()
+
+    @property
+    def Ncorrect(self):
+        return len(self.age_ticks) - 1
+
+    @property
+    def Ncurrent(self):
+        return len(self.epoch_substrates)
+
+    @property
+    def Nmismatch(self):
+        return self.Ncorrect - self.Ncurrent
---
+++ packages/larvaworld/larvaworld-0.0.123-py3-none-any.whl/larvaworld/lib/param/grouped.py
@@ -0,0 +1,320 @@
+import os
+
+import numpy as np
+import param
+from param import Selector, String, ListSelector, Magnitude, Boolean, List
+
+from .. import aux
+
+from ..param import OptionalPositiveNumber, OptionalSelector, PositiveInteger, \
+    OptionalPositiveInteger, ClassAttr, NestedConf, PositiveNumber, IntegerRangeOrdered, PositiveIntegerRangeOrdered
+
+__all__ = [
+    'FramerateOps',
+    'XYops',
+    'SimTimeOps',
+    'SimSpatialOps',
+    'SimOps',
+    'RuntimeGeneralOps',
+    'RuntimeDataOps',
+    'RuntimeOps',
+    'Filesystem',
+    'TrackedPointIdx',
+    'SimMetricOps',
+    'TrackerOps',
+    'PreprocessConf',
+    'ProcessConf',
+    'EnrichConf',
+    'AirPuff',
+]
+
+__displayname__ = 'Configuration parameter groups'
+
+
+class FramerateOps(NestedConf):
+    fr = PositiveNumber(10, softmax=100, step=0.1, label='framerate',
+                        doc='The tracking/simulation framerate (inverse of timestep) in Hz.')
+    dt = PositiveNumber(0.1, softmax=1.0, step=0.01, label='timestep',
+                        doc='The tracking/simulation timestep (inverse of tracking framerate) in seconds.')
+    constant_framerate = param.Boolean(True, doc='Whether the tracking framerate is constant.')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        if 'dt' in kwargs:
+            self.update_framerate()
+        elif 'fr' in kwargs:
+            self.update_timestep()
+
+    @param.depends('dt', watch=True)
+    def update_framerate(self):
+        self.fr = 1 / self.dt
+
+    @param.depends('fr', watch=True)
+    def update_timestep(self):
+        self.dt = 1 / self.fr
+        # raise
+
+
+class XYops(NestedConf):
+    XY_unit = Selector(default='m', objects=['m', 'mm'], doc='The spatial unit of the XY coordinate data')
+    Npoints = PositiveInteger(3, softmax=20, label='# midline 2D points',
+                              doc='The number of points tracked along the larva midline.')
+    Ncontour = PositiveInteger(0, softmax=100, label='# contour 2D points',
+                               doc='The number of points tracked around the larva contour.')
+
+    @property
+    def Nangles(self):
+        return np.clip(self.Npoints - 2, a_min=0, a_max=None)
+
+    @property
+    def Nsegs(self):
+        return np.clip(self.Npoints - 1, a_min=0, a_max=None)
+
+    @property
+    def angles(self):
+        return aux.SuperList([f'angle{i}' for i in range(self.Nangles)])
+
+    @property
+    def midline_points(self):
+        return aux.nam.midline(self.Npoints, type='point')
+
+    @property
+    def midline_segs(self):
+        return aux.nam.midline(self.Nsegs, type='seg')
+
+    @property
+    def midline_seg_xy(self, flat=True):
+        return aux.nam.xy(self.midline_segs, flat=flat)
+
+    @property
+    def seg_orientations(self):
+        return aux.nam.orient(self.midline_segs)
+
+    @property
+    def midline_xy(self, flat=True):
+        return aux.nam.xy(self.midline_points, flat=flat)
+
+    @property
+    def contour_points(self):
+        return aux.nam.contour(self.Ncontour)
+
+    @property
+    def contour_xy(self, flat=True):
+        return aux.nam.xy(self.contour_points, flat=flat)
+
+    @property
+    def centroid_xy(self):
+        return aux.nam.xy('centroid')
+
+    @property
+    def traj_xy(self):
+        return aux.nam.xy('')
+
+    @property
+    def all_xy(self, flat=True):
+        return aux.nam.xy(self.midline_points + self.contour_points + ['centroid', ''], flat=flat)
+
+    def get_track_point(self, idx):
+        if idx == -1:
+            return 'centroid'
+        else:
+            return self.midline_points[idx - 1]
+
+    def get_midline_xy_data(self, s):
+        xy = self.midline_xy
+        assert xy.exist_in(s)
+        return s[xy].values.reshape([-1, self.Npoints, 2])
+
+    def get_contour_xy_data(self, s):
+        xy = self.contour_xy
+        assert xy.exist_in(s)
+        return s[xy].values.reshape([-1, self.Ncontour, 2])
+
+
+class SimTimeOps(FramerateOps):
+    duration = OptionalPositiveNumber(softmax=100.0, step=0.1,
+                                      doc='The duration of the simulation in minutes.')
+    Nsteps = OptionalPositiveInteger(label='# simulation timesteps', doc='The number of simulation timesteps.')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.update_Nsteps()
+
+    @param.depends('duration', 'dt', watch=True)
+    def update_Nsteps(self):
+        if self.duration is not None:
+            self.Nsteps = int(self.duration * 60 / self.dt)
+        else:
+            self.Nsteps = None
+
+    @param.depends('Nsteps', watch=True)
+    def update_duration(self):
+        if self.Nsteps is not None:
+            self.duration = self.Nsteps * self.dt / 60
+        else:
+            self.duration = None
+
+
+class SimSpatialOps(NestedConf):
+    Box2D = param.Boolean(False, doc='Whether to use the Box2D physics engine or not.')
+    larva_collisions = param.Boolean(True, doc='Whether to allow overlap between larva bodies.')
+
+    @property
+    def scaling_factor(self):
+        return 1000.0 if self.Box2D else 1.0
+
+
+class SimOps(SimTimeOps, SimSpatialOps): pass
+
+
+# How to load existing
+
+
+# How to launch
+
+class RuntimeGeneralOps(NestedConf):
+    offline = param.Boolean(False, doc='Whether to launch a full Larvaworld environment')
+    multicore = param.Boolean(False, doc='Whether to use multiple cores')
+    store_data = param.Boolean(True, doc='Whether to store the simulation data')
+
+
+class RuntimeDataOps(NestedConf):
+    id = param.Parameter(None, doc='ID of the simulation. If not specified,set according to runtype and experiment.')
+    dir = param.String(default=None, label='storage folder', doc='The directory to store data')
+
+    @property
+    def data_dir(self):
+        if self.dir is not None:
+            f = f'{self.dir}/data'
+            os.makedirs(f, exist_ok=True)
+            return f
+
+    @property
+    def plot_dir(self):
+        if self.dir is not None:
+            f = f'{self.dir}/plots'
+            os.makedirs(f, exist_ok=True)
+            return f
+
+
+# What minimum to store, of course will be used to launch as well
+
+
+class RuntimeOps(RuntimeGeneralOps, RuntimeDataOps): pass
+
+
+class Filesystem(NestedConf):
+    read_sequence = List(label='data columns', doc='The sequence of columns in the tracker-exported files.')
+    read_metadata = Boolean(False, doc='Whether metadata files are available for the tracker-exported files/folders.')
+    folder_pref = String(doc='A prefix for detecting a raw-data folder.')
+    folder_suff = String(doc='A suffix for detecting a raw-data folder.')
+    file_pref = String(doc='A prefix for detecting a raw-data file.')
+    file_suf = String(doc='A suffix for detecting a raw-data file.')
+    file_sep = String(doc='A separator for detecting a raw-data file.')
+
+
+class TrackedPointIdx(XYops):
+    point_idx = param.Integer(default=-1, softbounds=(None, 20), bounds=(-1, None),
+                              doc='Index of midline point to use as the larva spatial position. Default is None meaning use the centroid.')
+    point = param.String(doc='Midline point to use as the larva spatial position. Default is centroid.')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.update_tracked_point()
+
+    @param.depends('Npoints', 'point_idx', watch=True)
+    def update_tracked_point(self):
+        self.param.point_idx.bounds = (hardmin, hardmax) = (-1, self.Npoints)
+        self.point_idx = self.param.point_idx.crop_to_bounds(self.point_idx)
+        self.point = self.get_track_point(self.point_idx)
+
+    @property
+    def point_xy(self):
+        return aux.nam.xy(self.point)
+
+
+class SimMetricOps(TrackedPointIdx):
+    bend = Selector(objects=['from_vectors', 'from_angles'],
+                    doc='Whether bending angle is computed as a sum of sequential segmental angles or as the angle between front and rear body vectors.')
+    front_vector = PositiveIntegerRangeOrdered((1, 2), softmax=12,
+                                               doc='The initial & final segment of the front body vector.')
+    rear_vector = IntegerRangeOrdered((-2, -1), softbounds=(-12, 12),
+                                      doc='The initial & final segment of the rear body vector.')
+    front_body_ratio = Magnitude(0.5,
+                                 doc='The fraction of the body considered front, relevant for bend computation from angles.')
+
+    use_component_vel = Boolean(False,
+                                doc='Whether to use the component velocity ralative to the axis of forward motion.')
+
+    @param.depends('Npoints', watch=True)
+    def update_vectors(self):
+        N = self.Npoints
+        # self.param.params('front_vector').softbounds = (-N,N)
+        # self.param.front_vector.bounds = (-N,N)
+        self.param.params('front_vector').bounds = (0, N)
+
+        self.param.params('front_vector')._validate(self.front_vector)
+        self.param.params('rear_vector').bounds = (-N, N)
+        self.param.params('rear_vector')._validate(self.rear_vector)
+
+    @property
+    def Nbend_angles(self):
+        return int(np.round(self.front_body_ratio * self.Nangles))
+
+    @property
+    def vector_dict(self):
+        f1, f2 = self.front_vector
+        r1, r2 = self.rear_vector
+        return aux.AttrDict({
+            'front': (f2 - 1, f1 - 1),
+            'rear': (r2 - 1, r1 - 1),
+            'head': (1, 0),
+            'tail': (-1, -2),
+        })
+
+
+class TrackerOps(SimMetricOps, FramerateOps): pass
+
+
+class PreprocessConf(NestedConf):
+    rescale_by = OptionalPositiveNumber(softmax=1000.0, step=0.001,
+                                        doc='Whether to rescale spatial coordinates by a scalar in meters.')
+    filter_f = OptionalPositiveNumber(softmax=5.0, step=0.01,
+                                      doc='Whether to filter spatial coordinates by a grade-1 low-pass filter of the given cut-off frequency.')
+    transposition = OptionalSelector(['origin', 'arena', 'center'], doc='Whether to transpose spatial coordinates.')
+    interpolate_nans = Boolean(False, doc='Whether to interpolate missing values.')
+    drop_collisions = Boolean(False, doc='Whether to drop timepoints where larva collisions are detected.')
+
+
+class ProcessConf(NestedConf):
+    proc_keys = ListSelector(default=['angular', 'spatial'],
+                             objects=['angular', 'spatial', 'source', 'PI', 'wind'],
+                             doc='The processing pipelines')
+    dsp_starts = param.List(default=[0.0], item_type=float, doc='The starting times for dispersal computation.')
+    dsp_stops = param.List(default=[40.0, 60.0], item_type=float, doc='The stopping times for dispersal computation.')
+    tor_durs = param.List(default=[5, 10, 20], item_type=int, doc='The time windows for tortuosity computation.')
+
+
+class EnrichConf(NestedConf):
+    pre_kws = ClassAttr(PreprocessConf, doc='The preprocessing pipelines')
+    proc_keys = ListSelector(default=['angular', 'spatial', 'dispersion', 'tortuosity'],
+                             objects=['angular', 'spatial', 'source', 'dispersion', 'tortuosity', 'PI', 'wind'],
+                             doc='The processing pipelines')
+    anot_keys = ListSelector(default=['bout_detection', 'bout_distribution', 'interference'],
+                             objects=['bout_detection', 'bout_distribution', 'interference', 'source_attraction',
+                                      'patch_residency'], doc='The annotation pipelines')
+    recompute = Boolean(False, doc='Whether to recompute')
+    mode = Selector(objects=['minimal', 'full'], doc='The processing mode')
+
+
+class AirPuff(NestedConf):
+    duration = PositiveNumber(default=1.0, softmax=100.0, step=0.1, doc='The duration of the air-puff in seconds.')
+    speed = PositiveNumber(default=10.0, softmax=1000.0, step=0.1, doc='The wind speed of the air-puff.')
+    direction = PositiveNumber(default=0.0, softmax=100.0, step=0.1, doc='The directions of the air puff in radians.')
+    start_time = PositiveNumber(default=0.0, softmax=10000.0, step=1.0,
+                                doc='The starting time of the air-puff in seconds.')
+    N = OptionalPositiveInteger(default=None, softmax=10000,
+                                doc='The number of repetitions of the puff. If N>1 an interval must be provided.')
+    interval = PositiveNumber(default=5.0, softmax=10000.0, step=0.1,
+                              doc='Whether the puff will reoccur at constant time intervals in seconds. Ignored if N=1.')
