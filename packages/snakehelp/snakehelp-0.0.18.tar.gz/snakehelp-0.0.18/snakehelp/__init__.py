3c415288bf92cfa93ff681a8db2e151b2a0c110a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:17:49 +0000

    Add package snakehelp/0.0.18/snakehelp-0.0.18.tar.gz

---
+++ packages/snakehelp/snakehelp-0.0.18.tar.gz/snakehelp-0.0.18/snakehelp/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Snakehelp."""
+
+__author__ = """Ivar Grytten"""
+__email__ = 'ivar.grytten@gmail.com'
+__version__ = '0.0.18'
+
+from .parameters import parameters, result
+from .config import set_data_folder, get_data_folder
