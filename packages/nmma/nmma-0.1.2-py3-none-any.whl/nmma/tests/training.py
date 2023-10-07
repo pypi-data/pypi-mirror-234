bd25f6c603a4b264ac05e3e621ee328aa1ea3624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:34:54 +0000

    Add package nmma/0.1.2/nmma-0.1.2-py3-none-any.whl

---
+++ packages/nmma/nmma-0.1.2-py3-none-any.whl/nmma/tests/training.py
@@ -0,0 +1,57 @@
+import os
+import copy
+import glob
+import numpy as np
+
+from ..em import training, model_parameters, io
+
+
+def test_training():
+
+    # The number of PCA components we'll use to represent each lightcurve
+    n_coeff = 3
+    model_name = "Bu2019lm_sparse"
+
+    # The array of times we'll use to examine each lightcurve
+    tini, tmax, dt = 0.1, 5.0, 0.2
+    tt = np.arange(tini, tmax + dt, dt)  #
+
+    # The filters we'll be focusing on
+    filts = [
+        "g",
+        "r",
+    ]  # We will focus on these two bands; all available: ["u","g","r","i","z","y","J","H","K"]
+
+    workingDir = os.path.dirname(__file__)
+
+    dataDir = os.path.join(workingDir, 'data/bulla')
+    ModelPath = "svdtrainingmodel"
+    filenames = glob.glob(f"{dataDir}/*.dat")
+
+    data = io.read_photometry_files(filenames, filters=filts)
+    # Loads the model data
+    training_data, parameters = model_parameters.Bu2019lm_sparse(data)
+
+    interpolation_type = "sklearn_gp"
+    training.SVDTrainingModel(
+        model_name,
+        copy.deepcopy(training_data),
+        parameters,
+        tt,
+        filts,
+        n_coeff=n_coeff,
+        svd_path=ModelPath,
+        interpolation_type=interpolation_type,
+    )
+
+    interpolation_type = "tensorflow"
+    training.SVDTrainingModel(
+        model_name,
+        copy.deepcopy(training_data),
+        parameters,
+        tt,
+        filts,
+        n_coeff=n_coeff,
+        svd_path=ModelPath,
+        interpolation_type=interpolation_type,
+    )
