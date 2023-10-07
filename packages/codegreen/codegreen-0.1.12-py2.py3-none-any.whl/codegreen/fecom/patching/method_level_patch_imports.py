7f20eb817a67d3a2099cc4ee2ec5e47820da96a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:30:38 +0000

    Add package codegreen/0.1.12/codegreen-0.1.12-py2.py3-none-any.whl

---
+++ packages/codegreen/codegreen-0.1.12-py2.py3-none-any.whl/codegreen/fecom/patching/method_level_patch_imports.py
@@ -0,0 +1,12 @@
+
+import sys
+from fecom.patching.patching_config import EXPERIMENT_DIR
+from fecom.measurement.execution import before_execution as before_execution_INSERTED_INTO_SCRIPT
+from fecom.measurement.execution import after_execution as after_execution_INSERTED_INTO_SCRIPT
+from fecom.experiment.experiment_kinds import ExperimentKinds
+
+experiment_number = sys.argv[1]
+experiment_project = sys.argv[2]
+
+EXPERIMENT_FILE_PATH = EXPERIMENT_DIR / ExperimentKinds.METHOD_LEVEL.value / experiment_project / f'experiment-{experiment_number}.json'
+
