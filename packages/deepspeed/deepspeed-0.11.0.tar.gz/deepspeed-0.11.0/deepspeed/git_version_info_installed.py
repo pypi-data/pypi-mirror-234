1353eb5cff85b37113207f850c08a4c8741a2dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 15:04:16 +0000

    Add package deepspeed/0.11.0/deepspeed-0.11.0.tar.gz

---
+++ packages/deepspeed/deepspeed-0.11.0.tar.gz/deepspeed-0.11.0/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.11.0'
+git_hash='26d0dd92'
+git_branch='master'
+installed_ops={'async_io': False, 'fused_adam': False, 'cpu_adam': False, 'cpu_adagrad': False, 'cpu_lion': False, 'evoformer_attn': False, 'fused_lamb': False, 'fused_lion': False, 'quantizer': False, 'random_ltd': False, 'sparse_attn': False, 'spatial_inference': False, 'transformer': False, 'stochastic_transformer': False, 'transformer_inference': False}
+compatible_ops={'async_io': False, 'fused_adam': True, 'cpu_adam': True, 'cpu_adagrad': True, 'cpu_lion': True, 'evoformer_attn': False, 'fused_lamb': True, 'fused_lion': True, 'quantizer': True, 'random_ltd': True, 'sparse_attn': False, 'spatial_inference': True, 'transformer': True, 'stochastic_transformer': True, 'transformer_inference': True, 'deepspeed_not_implemented': False}
+torch_info={'version': '1.9', 'bf16_support': False, 'cuda_version': '10.2', 'nccl_version': '2.7', 'hip_version': '0.0'}
