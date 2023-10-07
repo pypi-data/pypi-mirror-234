de878464bceed8bde1fcf686b34ffd2309607063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:18:31 +0000

    Add package bflag/0.0.6/bflag-0.0.6-py3-none-any.whl

---
+++ packages/bflag/bflag-0.0.6-py3-none-any.whl/bflag/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.6"
+from .bflag import bflag
+from .butil import asbool, tern2x
+from .types import BoolFunc, BoolCall
+__all__ = ["bflag", "asbool", "tern2x", "BoolFunc", "BoolCall"]
