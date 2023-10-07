28ee55c44eda3c4b9d7634f4fc88e0f623f1d030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:18:12 +0000

    Add package SalKikal/1.0/SalKikal-1.0.tar.gz

---
+++ packages/SalKikal/SalKikal-1.0.tar.gz/SalKikal-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'SalKikal',
+    version = '1.0',
+    author = "Salome Kikalishvili",
+    author_email = 'salomekikalishvili2000@gmail.com',
+    packages = find_packages()
+)
