373ab7afd9d4ed2b886837777b01db08d9af2f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:07:50 +0000

    Add package aws-securityhub-suppression/0.3.3/aws_securityhub_suppression-0.3.3-py3-none-any.whl

---
+++ packages/aws-securityhub-suppression/aws_securityhub_suppression-0.3.3-py3-none-any.whl/aws_securityhub_suppression/cli/__init__.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+import click
+import os
+
+from aws_securityhub_suppression.cli.context import Context
+from aws_securityhub_suppression.cli.handler import CliHandler
+
+
+@click.command(cls=CliHandler)
+@click.option("--debug/--no-debug")
+@click.option("--profile", default=os.environ.get("AWS_PROFILE"))
+@click.pass_context
+def cli(ctx: click.Context, debug: bool, profile: Optional[str]):
+    """The root of cli."""
+    ctx.obj = Context(debug=debug, profile=profile)
+
+
+if __name__ == "__main__":
+    cli()
