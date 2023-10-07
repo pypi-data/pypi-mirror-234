9035d995bad9e3bc3c135128b83a16d18e2b46f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:12:26 +0000

    Add package watz/0.0.1/watz-0.0.1.tar.gz

---
+++ packages/watz/watz-0.0.1.tar.gz/watz-0.0.1/watz/_utils.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int) -> int:
+    """Add two numbers.
+
+    Args:
+        a (int): First number.
+        b (int): Second number.
+
+    Returns:
+        int: Sum of the two numbers.
+    """
+    return a + b
