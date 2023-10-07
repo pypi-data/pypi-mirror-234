4d0a4f2bd34f8705f01302b4cf477a97f16cbdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 04:06:27 +0000

    Add package dask-sql/2023.10.0/dask_sql-2023.10.0.tar.gz

---
+++ packages/dask-sql/dask_sql-2023.10.0.tar.gz/dask_sql-2023.10.0/dask_sql/__init__.py
@@ -0,0 +1,13 @@
+# FIXME: can we modify TLS model of Rust object to avoid aarch64 glibc bug?
+# https://github.com/dask-contrib/dask-sql/issues/1169
+from . import _datafusion_lib  # isort:skip
+
+from . import _version, config
+from .cmd import cmd_loop
+from .context import Context
+from .datacontainer import Statistics
+from .server.app import run_server
+
+__version__ = _version.get_versions()["version"]
+
+__all__ = [__version__, cmd_loop, Context, run_server, Statistics]
