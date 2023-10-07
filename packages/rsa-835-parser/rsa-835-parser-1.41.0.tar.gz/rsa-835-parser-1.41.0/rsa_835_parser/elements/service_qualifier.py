cbeccca635a763708276700cc9fd369fee8f80a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:04:15 +0000

    Add package rsa-835-parser/1.41.0/rsa-835-parser-1.41.0.tar.gz

---
+++ packages/rsa-835-parser/rsa-835-parser-1.41.0.tar.gz/rsa-835-parser-1.41.0/rsa_835_parser/elements/service_qualifier.py
@@ -0,0 +1,11 @@
+""" Service Qualifier """
+from rsa_835_parser.elements import Element
+from rsa_835_parser.elements.utilities import split_element
+
+class ServiceQualifier(Element):
+    """Service Qualifier (SVC01) element class"""
+
+    def parser(self, value: str) -> str:
+        value = split_element(value)
+        qualifier, *_ = value
+        return qualifier
