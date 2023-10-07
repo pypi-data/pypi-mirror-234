8eb0e50e85e9b399e60be4d1d1a83ceb23266cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:11:27 +0000

    Add package botright/0.4/botright-0.4-py3-none-any.whl

---
+++ packages/botright/botright-0.4-py3-none-any.whl/botright/__init__.py
@@ -0,0 +1,9 @@
+from .botright import *
+from .modules.faker import *
+from .modules.proxy_manager import *
+
+firefox = "firefox"
+chromium = "chromium"
+webkit = "webkit"
+
+__all__ = ["firefox", "chromium", "webkit", "Botright", "Faker", "ProxyManager"]
