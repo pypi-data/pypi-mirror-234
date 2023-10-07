d06ea8acaf797c50c747f27749a3106f9761607f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:12:26 +0000

    Add package metron-tagger/1.6.3/metron_tagger-1.6.3-py3-none-any.whl

---
+++ packages/metron-tagger/metron_tagger-1.6.3-py3-none-any.whl/metrontagger/filesorter.py
@@ -0,0 +1,99 @@
+"""Class to sort comic file based on it's metadata tags"""
+from pathlib import Path
+from shutil import Error, move
+from typing import Optional
+
+import questionary
+from darkseid.comic import Comic
+from darkseid.metadata import Metadata
+
+from metrontagger.styles import Styles
+from metrontagger.utils import cleanup_string
+
+MEGABYTE = 1048576
+
+
+class FileSorter:
+    """Class to move comic files based on it's metadata tags"""
+
+    def __init__(self: "FileSorter", directory: str) -> None:
+        self.sort_directory = directory
+
+    @staticmethod
+    def _cleanup_metadata(
+        meta_data: Metadata,
+    ) -> tuple[Optional[str], Optional[str], Optional[str]]:
+        """Clean the metadata string."""
+        publisher = cleanup_string(meta_data.publisher.name)
+        series = cleanup_string(meta_data.series.name)
+        volume = cleanup_string(meta_data.series.volume)
+        return publisher, series, volume
+
+    @staticmethod
+    def _move_files(orig: Path, new: Path) -> bool:
+        try:
+            move(orig, new)
+            questionary.print(f"moved '{orig.name}' to '{new}'", style=Styles.SUCCESS)
+            return True
+        except Error:
+            return False
+
+    @staticmethod
+    def _overwrite_existing(new_path: Path, old_comic: Path) -> None:
+        existing_comic = new_path / old_comic.name
+        if existing_comic.exists():
+            existing_size = existing_comic.stat().st_size / MEGABYTE
+            new_size = old_comic.stat().st_size / MEGABYTE
+            msg = (
+                f"{existing_comic.name} exists at {existing_comic.parent}.\nOld file: "
+                f"{existing_size:.2f} MB -> New file: {new_size:.2f} MB"
+            )
+            questionary.print(msg, Styles.WARNING)
+            if questionary.confirm(
+                "Would you like to overwrite existing file?",
+                default=False,
+            ).ask():
+                existing_comic.unlink()
+
+    def sort_comics(self: "FileSorter", comic: Path) -> bool:
+        """Method to move the comic file based on it's metadata tag"""
+        try:
+            comic_archive = Comic(comic)
+        except FileNotFoundError:
+            return False
+        if comic_archive.has_metadata():
+            meta_data = comic_archive.read_metadata()
+        else:
+            return False
+
+        publisher, series, volume = self._cleanup_metadata(meta_data)
+
+        if publisher and series and volume:
+            tpb = meta_data.series.format == "Trade Paperback"
+            new_path = (
+                Path(self.sort_directory)
+                / publisher
+                / f"{f'{series} TPB' if tpb else f'{series}'}"
+                / f"v{volume}"
+            )
+        else:
+            questionary.print(
+                "Missing metadata from comic and will be unable to sort."
+                f"Publisher: {publisher}\nSeries: {series}\nVolume: {volume}",
+                style=Styles.WARNING,
+            )
+            return False
+
+        self._overwrite_existing(new_path, comic)
+
+        if not new_path.is_dir():
+            try:
+                new_path.mkdir(parents=True)
+            except PermissionError:
+                questionary.print(
+                    f"due to permission error, failed to create directory: {new_path}",
+                    style=Styles.ERROR,
+                )
+                return False
+
+        return self._move_files(comic, new_path)
---
+++ packages/metron-tagger/metron_tagger-1.6.3-py3-none-any.whl/metrontagger/run.py
@@ -0,0 +1,228 @@
+from pathlib import Path
+
+import questionary
+from darkseid.comic import Comic
+from darkseid.utils import get_recursive_filelist
+
+from metrontagger.filerenamer import FileRenamer
+from metrontagger.filesorter import FileSorter
+from metrontagger.settings import MetronTaggerSettings
+from metrontagger.styles import Styles
+from metrontagger.talker import Talker
+from metrontagger.validate_ci import SchemaVersion, ValidateComicInfo
+
+
+class Runner:
+    """Main runner"""
+
+    def __init__(self: "Runner", config: MetronTaggerSettings) -> None:
+        self.config = config
+
+    def rename_comics(self: "Runner", file_list: list[Path]) -> list[Path]:
+        questionary.print(
+            "\nStarting comic archive renaming:\n-------------------------------",
+            style=Styles.TITLE,
+        )
+
+        # Lists to track filename changes
+        new_file_names: list[Path] = []
+        original_files_changed: list[Path] = []
+        for comic in file_list:
+            comic_archive = Comic(comic)
+            if not comic_archive.has_metadata():
+                questionary.print(
+                    f"skipping '{comic.name}'. no metadata available.",
+                    style=Styles.WARNING,
+                )
+                continue
+
+            meta_data = comic_archive.read_metadata()
+            renamer = FileRenamer(meta_data)
+            renamer.set_template(self.config.rename_template)
+            renamer.set_issue_zero_padding(self.config.rename_issue_number_padding)
+            renamer.set_smart_cleanup(self.config.rename_use_smart_string_cleanup)
+
+            unique_name = renamer.rename_file(comic)
+            if unique_name is None:
+                continue
+
+            # track what files are being renamed
+            new_file_names.append(unique_name)
+            original_files_changed.append(comic)
+
+            questionary.print(
+                f"renamed '{comic.name}' -> '{unique_name.name}'",
+                style=Styles.SUCCESS,
+            )
+
+        # Update file_list for renamed files
+        for original_file in original_files_changed:
+            file_list.remove(original_file)
+
+        # Add new file names to file list.
+        file_list.extend(iter(new_file_names))
+        return file_list
+
+    def _export_to_zip(self: "Runner", file_list: list[Path]) -> None:
+        questionary.print("\nExporting to cbz:\n-----------------", style=Styles.TITLE)
+        for comic in file_list:
+            ca = Comic(comic)
+            if ca.is_rar():
+                new_fn = Path(comic).with_suffix(".cbz")
+                if ca.export_as_zip(new_fn):
+                    questionary.print(
+                        f"Exported '{comic.name}' to a cbz archive.",
+                        style=Styles.SUCCESS,
+                    )
+                    if self.config.delete_original:
+                        questionary.print(f"Removing '{comic.name}'.", style=Styles.SUCCESS)
+                        comic.unlink()
+                else:
+                    questionary.print(f"Failed to export '{comic.name}'", style=Styles.ERROR)
+            else:
+                questionary.print(
+                    f"'{comic.name}' is not a cbr archive. skipping...",
+                    style=Styles.WARNING,
+                )
+
+    def _validate_comic_info(self: "Runner", file_list: list[Path]) -> None:
+        questionary.print("\nValidating ComicInfo:\n---------------------", style=Styles.TITLE)
+        for comic in file_list:
+            ca = Comic(comic)
+            if not ca.has_metadata():
+                questionary.print(
+                    f"'{ca.path.name}' doesn't have a ComicInfo.xml file.",
+                    style=Styles.WARNING,
+                )
+                continue
+            xml = ca.archiver.read_file("ComicInfo.xml")
+            result = ValidateComicInfo(xml).validate()
+            if result == SchemaVersion.v2:
+                questionary.print(
+                    f"'{ca.path.name}' is a valid ComicInfo Version 2",
+                    style=Styles.SUCCESS,
+                )
+            elif result == SchemaVersion.v1:
+                questionary.print(
+                    f"'{ca.path.name}' is a valid ComicInfo Version 1",
+                    style=Styles.SUCCESS,
+                )
+            else:
+                questionary.print(f"'{ca.path.name}' is not valid", style=Styles.ERROR)
+
+    def _sort_comic_list(self: "Runner", file_list: list[Path]) -> None:
+        if not self.config.sort_dir:
+            questionary.print(
+                "\nUnable to sort files. No destination directory was provided.",
+                style=Styles.ERROR,
+            )
+            return
+
+        questionary.print(
+            "\nStarting sorting of comic archives:\n----------------------------------",
+            style=Styles.TITLE,
+        )
+        file_sorter = FileSorter(self.config.sort_dir)
+        for comic in file_list:
+            result = file_sorter.sort_comics(comic)
+            if not result:
+                questionary.print(f"Unable to move '{comic.name}'.", style=Styles.ERROR)
+
+    @staticmethod
+    def _comics_with_no_metadata(file_list: list[Path]) -> None:
+        questionary.print(
+            "\nShowing files without metadata:\n-------------------------------",
+            style=Styles.TITLE,
+        )
+        for comic in file_list:
+            comic_archive = Comic(comic)
+            if comic_archive.has_metadata():
+                continue
+            questionary.print(f"{comic}", style=Styles.SUCCESS)
+
+    @staticmethod
+    def _delete_metadata(file_list: list[Path]) -> None:
+        questionary.print("\nRemoving metadata:\n-----------------", style=Styles.TITLE)
+        for comic in file_list:
+            comic_archive = Comic(comic)
+            if comic_archive.has_metadata():
+                comic_archive.remove_metadata()
+                questionary.print(
+                    f"removed metadata from '{comic.name}'",
+                    style=Styles.SUCCESS,
+                )
+            else:
+                questionary.print(f"no metadata in '{comic.name}'", style=Styles.WARNING)
+
+    def _has_credentials(self: "Runner") -> bool:
+        return bool(self.config.metron_user and self.config.metron_pass)
+
+    def _get_credentials(self: "Runner") -> bool:
+        answers = questionary.form(
+            user=questionary.text("What is your Metron username?"),
+            passwd=questionary.text("What is your Metron password?"),
+            save=questionary.confirm("Would you like to save your credentials?"),
+        ).ask()
+        if answers["user"] and answers["passwd"]:
+            self.config.metron_user = answers["user"]
+            self.config.metron_pass = answers["passwd"]
+            if answers["save"]:
+                self.config.save()
+            return True
+        return False
+
+    def _get_sort_dir(self: "Runner") -> bool:
+        answers = questionary.form(
+            dir=questionary.path("What directory should comics be sorted to?"),
+            save=questionary.confirm("Would you like to save this location for future use?"),
+        ).ask()
+        if answers["dir"]:
+            self.config.sort_dir = answers["dir"]
+            if answers["save"]:
+                self.config.save()
+            return True
+        return False
+
+    def run(self: "Runner") -> None:
+        if not (file_list := get_recursive_filelist(self.config.path)):
+            questionary.print("No files to process. Exiting.", style=Styles.WARNING)
+            exit(0)
+
+        if self.config.missing:
+            self._comics_with_no_metadata(file_list)
+
+        if self.config.delete:
+            self._delete_metadata(file_list)
+
+        if self.config.id or self.config.online:
+            if not self._has_credentials() and not self._get_credentials():
+                questionary.print("No credentials provided. Exiting...", style=Styles.ERROR)
+                exit(0)
+
+            t = Talker(self.config.metron_user, self.config.metron_pass)
+            if self.config.id:
+                if len(file_list) == 1:
+                    t.retrieve_single_issue(file_list[0], self.config.id)
+                else:
+                    questionary.print(
+                        "More than one file was passed for Id processing. Exiting...",
+                        style=Styles.WARNING,
+                    )
+                    exit(0)
+            else:
+                t.identify_comics(file_list, self.config)
+
+        if self.config.rename:
+            file_list = self.rename_comics(file_list)
+
+        if self.config.sort:
+            if not self.config.sort_dir and not self._get_sort_dir():
+                questionary.print("No sort directory given. Exiting...")
+                exit(0)
+            self._sort_comic_list(file_list)
+
+        if self.config.export_to_cbz:
+            self._export_to_zip(file_list)
+
+        if self.config.validate:
+            self._validate_comic_info(file_list)
