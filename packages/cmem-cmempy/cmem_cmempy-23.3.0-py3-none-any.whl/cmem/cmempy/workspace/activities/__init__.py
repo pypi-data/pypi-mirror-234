82c5dbe2f0873562235df99df880ec117cdf6900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 03:05:11 +0000

    Add package cmem-cmempy/23.3.0/cmem_cmempy-23.3.0-py3-none-any.whl

---
+++ packages/cmem-cmempy/cmem_cmempy-23.3.0-py3-none-any.whl/cmem/cmempy/workspace/activities/__init__.py
@@ -0,0 +1,17 @@
+"""API for working with activities."""
+
+ACTIVITY_TYPE_EXECUTE_DEFAULTWORKFLOW = "ExecuteDefaultWorkflow"
+
+ACTIVITY_TYPE_EXECUTE_LOCALWORKFLOW = "ExecuteLocalWorkflow"
+
+VALID_ACTIVITY_STATUS = [
+    "Idle",
+    "Not executed",
+    "Finished",
+    "Cancelled",
+    "Failed",
+    "Successful",
+    "Canceling",
+    "Running",
+    "Waiting",
+]
