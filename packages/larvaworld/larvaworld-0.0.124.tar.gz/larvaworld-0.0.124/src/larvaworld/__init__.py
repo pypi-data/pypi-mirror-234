db4453b89c21c8e20eca4903cc9a739ca3f2bd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:18:04 +0000

    Add package larvaworld/0.0.124/larvaworld-0.0.124.tar.gz

---
+++ packages/larvaworld/larvaworld-0.0.124.tar.gz/larvaworld-0.0.124/src/larvaworld/__init__.py
@@ -0,0 +1,14 @@
+"""
+Larvaworld : A Drosophila larva behavioral analysis and simulation platform
+"""
+
+from . import lib, cli, gui
+
+lib.reg.generators.resetConfs(init=True)
+
+__author__ = 'Panagiotis Sakagiannis'
+__license__ = 'GNU GENERAL PUBLIC LICENSE'
+__copyright__ = '2023, Panagiotis Sakagiannis'
+__version__ = '0.0.30'
+__displayname__ = 'larvaworld'
+__name__ = 'larvaworld'
