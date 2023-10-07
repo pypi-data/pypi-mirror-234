0824a70e3539f615f8bd8346ffab5b7774655c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:17:35 +0000

    Add package geostat/0.8.4/geostat-0.8.4-py3-none-any.whl

---
+++ packages/geostat/geostat-0.8.4-py3-none-any.whl/geostat/__init__.py
@@ -0,0 +1,13 @@
+from .krige import *
+from .mesh import *
+from .model import *
+from .mean import *
+
+__version__ = '0.8.4'
+
+__all__ = []
+__all__.extend(mean.__all__)
+__all__.extend(mesh.__all__)
+__all__.extend(model.__all__)
+__all__.extend(kernel.__all__)
+__all__.extend(krige.__all__)
