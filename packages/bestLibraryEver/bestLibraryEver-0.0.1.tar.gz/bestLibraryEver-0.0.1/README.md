4b4dfad44d1e646631ee933f5040bf4f1c7277ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-07 05:24:24 +0000

    Add package bestLibraryEver/0.0.1/bestLibraryEver-0.0.1.tar.gz

---
+++ packages/bestLibraryEver/bestLibraryEver-0.0.1.tar.gz/bestLibraryEver-0.0.1/README.md
@@ -0,0 +1,3 @@
+this is a first attempt at creating a python library 
+
+ps: don't judge to harshly
---
+++ packages/bestLibraryEver/bestLibraryEver-0.0.1.tar.gz/bestLibraryEver-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bestLibraryEver',
+    version='0.0.1',
+    packages=find_packages(),
+    # install_requires=[
+    #     'package1',
+    #     'package2',
+    # ],
+)
