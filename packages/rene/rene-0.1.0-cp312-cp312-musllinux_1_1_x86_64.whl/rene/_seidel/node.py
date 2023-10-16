60efaa435a356c82143494266621bdbe8004fa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:24 +0000

    Add package rene/0.1.0/rene-0.1.0-cp312-cp312-musllinux_1_1_x86_64.whl

---
+++ packages/rene/rene-0.1.0-cp312-cp312-musllinux_1_1_x86_64.whl/rene/_seidel/node.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import typing as t
+from abc import (ABC,
+                 abstractmethod)
+
+from rene import (Location,
+                  hints)
+from .edge import Edge
+
+
+class Node(ABC, t.Generic[hints.Scalar]):
+    @abstractmethod
+    def locate(self,
+               point: hints.Point[hints.Scalar],
+               edges: t.Sequence[Edge[hints.Scalar]],
+               endpoints: t.Sequence[hints.Point[hints.Scalar]],
+               nodes: t.Sequence[Node[hints.Scalar]]) -> Location:
+        """
+        Finds location of given point relative to the contour.
+        """
+
+    @abstractmethod
+    def search_edge_node(
+            self,
+            edge: Edge[hints.Scalar],
+            edges: t.Sequence[Edge[hints.Scalar]],
+            endpoints: t.Sequence[hints.Point[hints.Scalar]],
+            nodes: t.Sequence[Node[hints.Scalar]]
+    ) -> Node[hints.Scalar]:
+        """
+        Recursive search for the trapezoid
+        which contains the left endpoint of the given segment.
+        """
+
+    @abstractmethod
+    def to_height(self, nodes: t.Sequence[Node[hints.Scalar]]) -> int:
+        """
+        Returns height of the node.
+        """
