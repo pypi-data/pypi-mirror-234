51af2ed940b7ac5f05b334f42dbda5025640f440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:58 +0000

    Add package audformat/1.0.2/audformat-1.0.2.tar.gz

---
+++ packages/audformat/audformat-1.0.2.tar.gz/audformat-1.0.2/tests/conftest.py
@@ -0,0 +1,38 @@
+import os
+import tempfile
+
+import pandas as pd
+import pytest
+
+import audformat.testing
+
+
+pytest.DB = audformat.testing.create_db()
+pytest.DB_ROOT = 'db'
+pytest.FILE_DUR = pd.to_timedelta('1s')
+
+
+@pytest.fixture(scope='package', autouse=True)
+def prepare_tests():
+
+    # Prepare files used in tests
+    # and set a temporary working directory
+    with tempfile.TemporaryDirectory() as tmp:
+
+        current_dir = os.getcwd()
+        os.chdir(tmp)
+
+        pytest.DB.save(pytest.DB_ROOT)
+
+        audformat.testing.create_audio_files(
+            pytest.DB,
+            file_duration=pytest.FILE_DUR,
+        )
+        audformat.testing.create_attachment_files(
+            pytest.DB,
+            pytest.DB.root,
+        )
+
+        yield
+
+        os.chdir(current_dir)
