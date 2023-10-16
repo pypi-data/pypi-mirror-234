acceb8e2492e0438cdbd8629ecfb738a9a22947f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:07:19 +0000

    Add package fastapi-hive/1.0.15/fastapi_hive-1.0.15.tar.gz

---
+++ packages/fastapi-hive/fastapi_hive-1.0.15.tar.gz/fastapi_hive-1.0.15/fastapi_hive/ioc_framework/cornerstone_model/__init__.py
@@ -0,0 +1,6 @@
+
+from fastapi_hive.ioc_framework.cornerstone_model.implement import Cornerstone, CornerstoneAsync, CornerstoneMeta
+
+
+__all__ = ["Cornerstone", "CornerstoneAsync", "CornerstoneMeta"]
+
