10231cd8c6399b59c6c03ccb84f32ef88a38409c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:08:14 +0000

    Add package quahris/0.0.4.1/quahris-0.0.4.1.tar.gz

---
+++ packages/quahris/quahris-0.0.4.1.tar.gz/quahris-0.0.4.1/main.py
@@ -0,0 +1,7 @@
+from quahris import functions
+
+functions.qualys_cred("credt6aa1", "yDv4d$)VlcEJ2P)I9AyM!6NR2*")
+userFirstName = functions.listUsers("FIRSTNAME")
+userLastName = functions.listUsers("LASTNAME")
+
+
