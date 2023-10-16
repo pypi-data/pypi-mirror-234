4a4b990c17f5b7c04f8141635c3848213c86307f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:08:35 +0000

    Add package Calculator-martynara/0.0.1/calculator_martynara-0.0.1.tar.gz

---
+++ packages/Calculator-martynara/calculator_martynara-0.0.1.tar.gz/calculator_martynara-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+
+name = 'calculator',
+packages = find_packages(),
+version = '1.0',
+author = 'Martyna Rastauskaite',
+author_email = 'rastauskaitemartyna@gmail.com',
+license = 'MIT',
+
+)
