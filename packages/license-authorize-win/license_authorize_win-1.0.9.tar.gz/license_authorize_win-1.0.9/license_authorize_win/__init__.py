211379f7422f2ddcabeb1705488029b5287f0182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:08:42 +0000

    Add package license-authorize-win/1.0.9/license_authorize_win-1.0.9.tar.gz

---
+++ packages/license-authorize-win/license_authorize_win-1.0.9.tar.gz/license_authorize_win-1.0.9/license_authorize_win/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+# @Time    :  2023/5/19 14:35
+# @Author  : chenxw
+# @Email   : gisfanmachel@gmail.com
+# @File    : __init__.py.py
+# @Descr   : 
+# @Software: PyCharm
+
+from ._core import *
