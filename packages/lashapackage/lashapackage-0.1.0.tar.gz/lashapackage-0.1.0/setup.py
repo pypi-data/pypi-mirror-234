97f69e8fd162d778825c6448d1ca683a0a07662f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:50:49 +0000

    Add package lashapackage/0.1.0/lashapackage-0.1.0.tar.gz

---
+++ packages/lashapackage/lashapackage-0.1.0.tar.gz/lashapackage-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lashapackage",
+    version="0.1.0",
+    description="A short description of your package LM",
+    author="lasha metreveli",
+    author_email="lasha.metreveli.3@iliauni.edu.ge",
+    url="https://github.com/lashametreveli/my_package",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
