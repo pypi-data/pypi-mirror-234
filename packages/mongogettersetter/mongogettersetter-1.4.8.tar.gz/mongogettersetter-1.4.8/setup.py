6a0c95497829574fbb053db7d5cc009dcec82c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:09:50 +0000

    Add package mongogettersetter/1.4.8/mongogettersetter-1.4.8.tar.gz

---
+++ packages/mongogettersetter/mongogettersetter-1.4.8.tar.gz/mongogettersetter-1.4.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+    
+setup(
+    name='mongogettersetter',
+    version='1.4.8',
+    author='Sibidharan',
+    author_email='sibi@selfmade.ninja',
+    description='A clean way to handle MongoDB documents in Pythonic way',
+    packages=find_packages(),
+    url='https://git.selfmade.ninja/sibidharan/pymongogettersetter',
+    install_requires=['pymongo'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    license="MIT",
+    keywords="pymongo mongodb mongo mongogettersetter gettersetter getter setter",
+)
