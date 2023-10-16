0d84b11f33ae354c0bfa4d9eb971f8748f161b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:12:02 +0000

    Add package efax/1.15.0/efax-1.15.0-py3-none-any.whl

---
+++ packages/efax/efax-1.15.0-py3-none-any.whl/efax/_src/__init__.py
@@ -0,0 +1,7 @@
+from .distributions import *
+from .expectation_parametrization import *
+from .gamma_generator import *
+from .natural_parametrization import *
+from .parameter import *
+from .scipy_replacement import *
+from .tools import *
---
+++ packages/efax/efax-1.15.0-py3-none-any.whl/efax/_src/scipy_replacement/__init__.py
@@ -0,0 +1,5 @@
+from .complex_multivariate_normal import *
+from .complex_normal import *
+from .dirichlet import *
+from .multivariate_normal import *
+from .von_mises import *
