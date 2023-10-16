9669d957de7472f732b61608f8f18fe1cf5ef593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:33 +0000

    Add package giomikelbasepackage/0.1/giomikelbasepackage-0.1.tar.gz

---
+++ packages/giomikelbasepackage/giomikelbasepackage-0.1.tar.gz/giomikelbasepackage-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="giomikelbasepackage",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Giorgi Mikeladze",
+    author_email="giorgi.mikeladze.4@iliauni.edu.ge",
+    description="A Python package",
+    long_description=open("README.md").read()
+)
