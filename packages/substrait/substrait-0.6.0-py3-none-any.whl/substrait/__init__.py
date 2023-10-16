ab6d0591e4bf0aabfe15d9401c86ac1612902f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:09:33 +0000

    Add package substrait/0.6.0/substrait-0.6.0-py3-none-any.whl

---
+++ packages/substrait/substrait-0.6.0-py3-none-any.whl/substrait/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from ._version import __version__
+except ImportError:
+    pass
+
+__substrait_version__ = "0.36.0"
+__substrait_hash__ = "92302d8"
+__minimum_substrait_version__ = "0.30.0"
