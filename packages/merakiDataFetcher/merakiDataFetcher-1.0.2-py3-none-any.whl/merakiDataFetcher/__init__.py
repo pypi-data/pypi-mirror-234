dbff237a68826729f7af0db8efcff09253db5a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:36 +0000

    Add package merakiDataFetcher/1.0.2/merakiDataFetcher-1.0.2-py3-none-any.whl

---
+++ packages/merakiDataFetcher/merakiDataFetcher-1.0.2-py3-none-any.whl/merakiDataFetcher/__init__.py
@@ -0,0 +1,12 @@
+from .meraki_data_fetcher import MerakiDataFetcher
+
+from .meraki_dataclasses.meraki_device import MerakiDevice
+from .meraki_dataclasses.meraki_device_appliance import MerakiAppliance
+from .meraki_dataclasses.meraki_device_camera import MerakiCamera
+from .meraki_dataclasses.meraki_device_sensor import MerakiSensor
+from .meraki_dataclasses.meraki_device_switch import MerakiSwitch
+from .meraki_dataclasses.meraki_device_wireless import MerakiWireless
+from .meraki_dataclasses.meraki_network import MerakiNetwork
+from .meraki_dataclasses.meraki_organization import MerakiOrganization
+
+__version__ = '1.0.2'
