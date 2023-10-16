256df4f2c476a7f06a403d672f847aa32141d899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:24 +0000

    Add package at-6290/0.1.1/at_6290-0.1.1.tar.gz

---
+++ packages/at-6290/at_6290-0.1.1.tar.gz/at_6290-0.1.1/src/at_6290/message_streamer.py
@@ -0,0 +1,25 @@
+import threading
+from read_latest_message import LatestMessageReader
+
+
+class MessageStreamer:
+    api_id = None
+    api_hash = None
+    channel_url = None
+    callback = None
+    phone = None
+    username = None
+
+    def __init__(self, api_id, api_hash, phone, username, channel_url, callback):
+        self.api_id = int(api_id)
+        self.api_hash = str(api_hash)
+        self.phone = str(phone)
+        self.username = str(username)
+        self.channel_url = str(channel_url)
+        self.callback = callback
+
+    def subscribe(self):
+        c_thread = threading.Thread(target=lambda: LatestMessageReader(
+            self.api_id, self.api_hash, self.phone, self.username, self.channel_url, self.callback))
+        c_thread.start()
+
