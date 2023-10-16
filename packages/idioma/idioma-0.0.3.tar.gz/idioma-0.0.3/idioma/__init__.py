d9f3f488e5feddd1faa2bfb68d0310c64090187b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:07:51 +0000

    Add package idioma/0.0.3/idioma-0.0.3.tar.gz

---
+++ packages/idioma/idioma-0.0.3.tar.gz/idioma-0.0.3/idioma/__init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '0.0.3'
+
+
+from idioma.client import Translator
+from idioma.constants import LANGCODES, LANGUAGES  # noqa
