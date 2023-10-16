2a5bb37464ef92d6e008fbda5fb4fb0f77f86410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:35 +0000

    Add package magico/0.3.5/magico-0.3.5-py3-none-any.whl

---
+++ packages/magico/magico-0.3.5-py3-none-any.whl/magico/__init__.py
@@ -0,0 +1,2 @@
+from .magico import *
+from .__version__ import __name__, __version__, __description__
---
+++ packages/magico/magico-0.3.5-py3-none-any.whl/magico/__version__.py
@@ -0,0 +1,8 @@
+__name__ = "magico"
+__version__ = "0.3.5"
+__description__ = "MagicO - enabling attribute notation and JSONPath"
+__url__ = "https://github.com/jackyko8/magico"
+__author__ = "Jacky Ko"
+__author_email__ = ""
+__license__ = "MIT"
+__copyright__ = "Copyright Jacky Ko"
