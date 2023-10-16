e2e30b4d8d38035b71c4be8cd7ab9d57ef5a6a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:04 +0000

    Add package pyhirtlib/0.0.3/pyhirtlib-0.0.3.tar.gz

---
+++ packages/pyhirtlib/pyhirtlib-0.0.3.tar.gz/pyhirtlib-0.0.3/pyhirtlib/configs/config.py
@@ -0,0 +1,25 @@
+import os
+
+
+class Config:
+    ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+    OODLE_PATH = ""
+    TAG_XML_TEMPLATE_PATH = "D:\\HaloInfiniteStuft\\Extracted\\UnPacked\\s4\\TagXml\\2023-08-11\\"
+    #BASE_UNPACKED_PATH = "D:\\HaloInfiniteStuft\\Extracted\\UnPacked\\winter_update\\"
+    BASE_UNPACKED_PATH = "D:\\HaloInfiniteStuft\\Extracted\\UnPacked\\emulate\\E\\"
+    BASE_UNPACKED_PATH_CAMPAIGN = "D:\\HaloInfiniteStuft\\Extracted\\UnPacked\\campaign\\"
+    # BASE_UNPACKED_PATH = BASE_UNPACKED_PATH_CAMPAIGN
+    MODEL_EXPORT_PATH = "D:\\HaloInfiniteStuft\\Extracted\\Converted\\RE_OtherGames\\HI\\models\\"
+    INFOS_PATH = 'C:\\Users\\Jorge\\Downloads\\Mover\\infos\\'
+    EXPORT_JSON = 'J:\\Games\\Halo Infinite Stuf\\Extracted\\HI\\json\\'
+    EXPORT_SHADERS = 'J:\\Games\\Halo Infinite Stuf\\Extracted\\shaderdis\\'
+    SPARTAN_STYLE_PATH = "J:\\Games\\Halo Infinite Stuf\\Extracted\\UnPacked\\season2\\__chore\\gen__\\objects\\characters\\spartan_armor\\coatings\\"
+    WEB_DOWNLOAD_DATA = "J:\\Games\\Halo Infinite Stuf\\Web-Json\\"
+    UE5_PROJECT_IMPORTED_PC_PATH = "H:\\UE4\\Unreal_Projects\\HaloInfinities " \
+                                   "5.0\\Content\\__chore\\gen__\\pc__\\"
+    DEPLOY_PATH = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Halo Infinite\\deploy\\"
+    DEPLOY_PATH_CAMPAIGN = "J:\\Games\\Instalados\\Halo Infinite\\deploy\\"
+    EXPORTED_TEXTURE_PATH = "J:\\Games\\Halo Infinite Stuf\\Extracted\\Converted\\Textures\\TGA\\pc__\\"
+    EXPORTED_TEXTURE_PATH_BASE = "J:\\Games\\Halo Infinite Stuf\\Extracted\\Converted\\Textures\\"
+
+    VERBOSE = True
