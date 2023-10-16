2dfba2fd80646447308529c55bd84e52a9d933a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:43:58 +0000

    Add package pyhirtlib/0.0.2/pyhirtlib-0.0.2.tar.gz

---
+++ packages/pyhirtlib/pyhirtlib-0.0.2.tar.gz/pyhirtlib-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='pyhirtlib',
+  version='0.0.2',
+  description='A tag reader for halo infinite',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Jorge Glez',
+  author_email='urium86@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='halo infinite, tags, module', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
