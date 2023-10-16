0000047393b21345e728387ae4ba28343db2cd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:53 +0000

    Add package muiscaenergy-comun/0.0.7/muiscaenergy_comun-0.0.7.tar.gz

---
+++ packages/muiscaenergy-comun/muiscaenergy_comun-0.0.7.tar.gz/muiscaenergy_comun-0.0.7/muiscaenergy_comun/test/timeseries/test_base.py
@@ -0,0 +1,38 @@
+import unittest
+from datetime import datetime
+from muiscaenergy_comun.src.timeseries.base import get_timeseries
+from muiscaenergy_comun.src.messages.base import TimeSeriesMessage as TSm
+
+
+class TestGetTimeseries(unittest.TestCase):
+
+    def setUp(self):
+        self.ts_from = datetime(2023, 9, 30, 12, 0, 0)
+        self.ts_to = datetime(2023, 10, 1, 11, 0, 0)
+        self.freq = 'H'
+        self.lat = 52.5200
+        self.lon = 13.4050
+        self.tz = 'America/Los_Angeles'
+
+    def test_get_timeseries(self):
+        msg1 = get_timeseries(ts_from=self.ts_from,
+                              ts_to=self.ts_to,
+                              freq=self.freq)
+        self.assertIsInstance(msg1, TSm)
+
+        msg2 = get_timeseries(ts_from=self.ts_from,
+                              ts_to=self.ts_to,
+                              freq=self.freq,
+                              lat=self.lat,
+                              lon=self.lon)
+        self.assertIsInstance(msg2, TSm)
+
+        msg3 = get_timeseries(ts_from=self.ts_from,
+                              ts_to=self.ts_to,
+                              freq=self.freq,
+                              tz=self.tz)
+        self.assertIsInstance(msg3, TSm)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/muiscaenergy-comun/muiscaenergy_comun-0.0.7.tar.gz/muiscaenergy_comun-0.0.7/muiscaenergy_comun/test/utils/test_mix.py
@@ -0,0 +1,17 @@
+import unittest
+import pandas as pd
+from muiscaenergy_comun.src.utils.mix import parse_custom_freq
+
+
+class TestUtilMix(unittest.TestCase):
+
+    def test_parse_custom_freq(self):
+        freq = '15T'
+        td = parse_custom_freq(freq)
+
+        self.assertEqual(td, pd.Timedelta(minutes=15))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
