104a54940201cea67d13c30977517e03795a6ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:10:26 +0000

    Add package shallow/0.1.4/shallow-0.1.4.tar.gz

---
+++ packages/shallow/shallow-0.1.4.tar.gz/shallow-0.1.4/README.md
@@ -0,0 +1,11 @@
+# shallow
+
+A collection of `shallow` wrappers around deep learning functionality from `pytorch` and `tensorflow`.
+And now `jax` and `numpyro`.
+
+## Installation
+
+`pip install shallow`
+
+`pip install git+https://github.com/mdmould/nflows`
+
---
+++ packages/shallow/shallow-0.1.4.tar.gz/shallow-0.1.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+name = 'shallow'
+version = '0.1.4' # current 0.1.4
+
+with open('README.md' ,'r') as f:
+    long_description = f.read().strip()
+
+setup(
+    name=name,
+    version=version,
+    description=name,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/mdmould/{name}',
+    author='Matthew Mould',
+    author_email='mattdmould@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'torch',
+        # 'nflows',
+        # 'tensorflow',
+        'jax',
+        'numpyro',
+        'optax',
+        ],
+    python_requires='>=3.7',
+    )
+
