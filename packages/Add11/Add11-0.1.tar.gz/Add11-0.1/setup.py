ef733af8c5bb34dfd90b23dccc79c8c5e6ce2aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:14 +0000

    Add package Add11/0.1/Add11-0.1.tar.gz

---
+++ packages/Add11/Add11-0.1.tar.gz/Add11-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='Add11',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Aakash',
+    description='A simple addition package',
+)
