5ab4f150f99c62753a4e03e5baf9d2a0ad16b988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:06:29 +0000

    Add package solidlibs/2.6.8/solidlibs-2.6.8-py3-none-any.whl

---
+++ packages/solidlibs/solidlibs-2.6.8-py3-none-any.whl/solidlibs/version.py
@@ -0,0 +1,11 @@
+'''
+    Current distributed version of the "solidlibs" package.
+
+    Copyright 2010-2023 TopDevPros
+    Last modified: 2023-09-08
+
+'''
+
+CURRENT_VERSION = '2.6.8'
+COPYRIGHT = 'Copyright 2010-2023 TopDevPros'
+LICENSE = 'GPLv3'
