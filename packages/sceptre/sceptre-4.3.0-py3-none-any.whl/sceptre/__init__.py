468bb8f86ed542ddf758eae0fd9ae2418e8ad868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:11:11 +0000

    Add package sceptre/4.3.0/sceptre-4.3.0-py3-none-any.whl

---
+++ packages/sceptre/sceptre-4.3.0-py3-none-any.whl/sceptre/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import warnings
+import sys
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+
+__author__ = "SceptreOrg"
+__email__ = "sceptreorg@gmail.com"
+__version__ = version(__package__ or __name__)
+
+
+# Set up logging to ``/dev/null`` like a library is supposed to.
+# http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+class NullHandler(logging.Handler):  # pragma: no cover
+    def emit(self, record):
+        pass
+
+
+if not sys.warnoptions:
+    warnings.filterwarnings("default", category=DeprecationWarning, module="sceptre")
+
+logging.getLogger("sceptre").addHandler(NullHandler())
