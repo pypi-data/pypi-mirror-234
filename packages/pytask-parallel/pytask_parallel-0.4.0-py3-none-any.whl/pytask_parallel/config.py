b0d83c105657a3832636fb9babde5a9ef083feef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:05:36 +0000

    Add package pytask-parallel/0.4.0/pytask_parallel-0.4.0-py3-none-any.whl

---
+++ packages/pytask-parallel/pytask_parallel-0.4.0-py3-none-any.whl/pytask_parallel/config.py
@@ -0,0 +1,38 @@
+"""Configure pytask."""
+from __future__ import annotations
+
+import enum
+import os
+from typing import Any
+
+from pytask import hookimpl
+from pytask_parallel.backends import ParallelBackend
+
+
+@hookimpl
+def pytask_parse_config(config: dict[str, Any]) -> None:
+    """Parse the configuration."""
+    if config["n_workers"] == "auto":
+        config["n_workers"] = max(os.cpu_count() - 1, 1)
+
+    if (
+        isinstance(config["parallel_backend"], str)
+        and config["parallel_backend"] in ParallelBackend._value2member_map_
+    ):
+        config["parallel_backend"] = ParallelBackend(config["parallel_backend"])
+    elif (
+        isinstance(config["parallel_backend"], enum.Enum)
+        and config["parallel_backend"] in ParallelBackend
+    ):
+        pass
+    else:
+        raise ValueError("Invalid value for 'parallel_backend'.")
+
+    config["delay"] = 0.1
+
+
+@hookimpl
+def pytask_post_parse(config: dict[str, Any]) -> None:
+    """Disable parallelization if debugging is enabled."""
+    if config["pdb"] or config["trace"] or config["dry_run"]:
+        config["n_workers"] = 1
