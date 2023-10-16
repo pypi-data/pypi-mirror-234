8099b3509cc0ea50c6e4c0e716ed8487e3d7aad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:06:34 +0000

    Add package beet/0.94.0/beet-0.94.0-py3-none-any.whl

---
+++ packages/beet/beet-0.94.0-py3-none-any.whl/beet/contrib/render.py
@@ -0,0 +1,50 @@
+"""Plugin that invokes the built-in template renderer."""
+
+
+__all__ = [
+    "RenderOptions",
+    "render",
+]
+
+
+from typing import Dict, Union
+
+from beet import Context, ListOption, PluginOptions, configurable
+from beet.core.utils import snake_case
+
+
+class RenderOptions(PluginOptions):
+    resource_pack: Union[Dict[str, ListOption[str]], ListOption[str]] = {}
+    data_pack: Union[Dict[str, ListOption[str]], ListOption[str]] = {}
+
+
+def beet_default(ctx: Context):
+    ctx.require(render)
+
+
+@configurable(validator=RenderOptions)
+def render(ctx: Context, opts: RenderOptions):
+    """Plugin that processes the data pack and the resource pack with Jinja."""
+    for groups, pack in zip([opts.resource_pack, opts.data_pack], ctx.packs):
+        file_types = set(pack.resolve_scope_map().values())
+        group_map = {
+            snake_case(file_type.__name__): file_type for file_type in file_types
+        }
+
+        if isinstance(groups, ListOption):
+            groups = {k: groups for k in group_map}
+        else:
+            for singular in list(group_map):
+                group_map.setdefault(f"{singular}s", group_map[singular])
+
+        for group, render_options in groups.items():
+            try:
+                file_type = group_map[group]
+                proxy = pack[file_type]
+                file_paths = proxy.match(*render_options.entries())
+            except:
+                raise ValueError(f"Invalid render group {group!r}.") from None
+            else:
+                for path in file_paths:
+                    with ctx.override(render_path=path, render_group=group):
+                        ctx.template.render_file(proxy[path])  # type: ignore
