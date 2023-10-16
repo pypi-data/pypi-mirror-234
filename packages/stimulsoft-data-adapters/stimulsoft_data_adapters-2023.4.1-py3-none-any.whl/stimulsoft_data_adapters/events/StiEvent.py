6f7b0e34e83fc2b0f18f63461ccc0ea9558c2bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:22 +0000

    Add package stimulsoft-data-adapters/2023.4.1/stimulsoft_data_adapters-2023.4.1-py3-none-any.whl

---
+++ packages/stimulsoft-data-adapters/stimulsoft_data_adapters-2023.4.1-py3-none-any.whl/stimulsoft_data_adapters/events/StiEvent.py
@@ -0,0 +1,34 @@
+class StiEvent:
+
+    __handlers: list = None
+    __name: str = None
+
+    @property
+    def count(self):
+        return len(self.__handlers or '')
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def jsHandlers(self):
+        return [handler for handler in self.__handlers if type(handler) == str or handler is True]
+    
+    def __iadd__(self, handler):
+        self.__handlers.append(handler)
+        return self
+    
+    def __isub__(self, handler):
+        self.__handlers.remove(handler)
+        return self
+
+    def __call__(self, *args, **keywargs):
+        for handler in self.__handlers:
+            if (callable(handler)):
+                handler(*args, **keywargs)
+
+    def __init__(self, name: str):
+        self.__handlers = []
+        self.__name = name
+	
