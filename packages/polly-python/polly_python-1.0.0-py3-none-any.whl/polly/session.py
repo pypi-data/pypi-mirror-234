bd3b5a2f529b97685f88f33fc63961b9fbbd47dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:44 +0000

    Add package polly-python/1.0.0/polly_python-1.0.0-py3-none-any.whl

---
+++ packages/polly-python/polly_python-1.0.0-py3-none-any.whl/polly/session.py
@@ -0,0 +1,51 @@
+from requests import Session
+import os
+
+
+class PollySession(Session):
+    """
+    This class contain function to create session for
+    polly.
+
+    ``Args:``
+        |  ``token (str):`` token copy from polly.
+
+    ``Returns:``
+        |  None
+
+    To use this function
+
+
+    .. code::
+
+
+            from polly.session import PollySession
+            session = PollySession(token)
+
+    """
+
+    def __init__(self, TOKEN, env="polly"):
+        Session.__init__(self)
+        try:
+            # for python version >= python3.8
+            from importlib.metadata import version
+
+            version = version("polly-python")
+        except ImportError:
+            # for python version < python3.8
+            import pkg_resources
+
+            version = pkg_resources.get_distribution("polly-python").version
+        client = os.getenv("POLLY_SERVICE")
+        if client is not None:
+            version = version + "/" + client
+        else:
+            version = version + "/local"
+
+        self.token = TOKEN
+        self.headers = {
+            "Content-Type": "application/vnd.api+json",
+            "x-api-key": f"{TOKEN}",
+            "User-Agent": "polly-python/" + version,
+        }
+        self.env = env
