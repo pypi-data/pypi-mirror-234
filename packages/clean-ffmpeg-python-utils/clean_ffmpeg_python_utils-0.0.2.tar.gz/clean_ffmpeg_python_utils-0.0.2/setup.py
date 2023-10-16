d54c817bef9f6aadd80ab9eceb2dec287aff52fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:09 +0000

    Add package clean-ffmpeg-python-utils/0.0.2/clean_ffmpeg_python_utils-0.0.2.tar.gz

---
+++ packages/clean-ffmpeg-python-utils/clean_ffmpeg_python_utils-0.0.2.tar.gz/clean_ffmpeg_python_utils-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = "0.0.2"
+DESCRIPTION = "Easy-to-use ffmpeg python utils with simpler arguments"
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="clean_ffmpeg_python_utils",
+    version=VERSION,
+    author="Tyler Lum",
+    author_email="tylergwlum@gmail.com",
+    url="https://github.com/tylerlum/clean_ffmpeg_python_utils",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["ffmpeg-python", "typed-argument-parser"],
+    keywords=["python", "ffmpeg", "video", "audio"],
+    entry_points={
+        "console_scripts": [
+            "clean_ffmpeg_python_utils=clean_ffmpeg_python_utils.clean_ffmpeg_python_utils:main",
+        ],
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
