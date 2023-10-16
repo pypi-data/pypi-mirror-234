739da90f891b5febb6c43267d3a083df0195330f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:07:49 +0000

    Add package rusty-capacitance-model-core/1.4.1/rusty_capacitance_model_core-1.4.1-cp311-cp311-manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/rusty-capacitance-model-core/rusty_capacitance_model_core-1.4.1-cp311-cp311-manylinux_2_12_i686.manylinux2010_i686.whl/rusty_capacitance_model_core/__init__.py
@@ -0,0 +1,5 @@
+from .rusty_capacitance_model_core import *
+
+__doc__ = rusty_capacitance_model_core.__doc__
+if hasattr(rusty_capacitance_model_core, "__all__"):
+    __all__ = rusty_capacitance_model_core.__all__
