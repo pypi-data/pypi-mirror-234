94cc269eec4e6765364c9c7fb87c2b2b9b58167a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 07:04:51 +0000

    Add package anylearn/0.19.3/anylearn-0.19.3-py3-none-any.whl

---
+++ packages/anylearn/anylearn-0.19.3-py3-none-any.whl/anylearn/interfaces/__init__.py
@@ -0,0 +1,17 @@
+from .base import BaseObject
+from .mirror import Mirror
+from .project import Project
+from .quota import QuotaGroup
+from .resource import (
+    Algorithm,
+    SyncResourceUploader,
+    Dataset,
+    File,
+    Model,
+    Resource,
+    ResourceDownloader,
+    ResourceState,
+    ResourceUploader,
+)
+from .train_task import TrainTask, TrainTaskState
+from .user import User
