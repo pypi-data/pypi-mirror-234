f2e0fe6ea7c522df1e49b1c28e064a1b428a555e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:06:19 +0000

    Add package og-proto/0.4.44/og_proto-0.4.44.tar.gz

---
+++ packages/og-proto/og_proto-0.4.44.tar.gz/og_proto-0.4.44/setup.py
@@ -0,0 +1,48 @@
+# vim:fenc=utf-8
+#
+# Copyright (C) 2023 dbpunk.com Author imotai <codego.me@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" """
+from setuptools import setup, find_packages
+
+setup(
+    name="og_proto",
+    version="0.4.44",
+    description="Open source code interpreter agent for LLM",
+    author="imotai",
+    author_email="codego.me@gmail.com",
+    url="https://github.com/dbpunk-labs/octogen",
+    long_description= open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    packages=[
+        "og_proto",
+    ],
+
+    package_dir={
+        "og_proto": "src/og_proto",
+    },
+
+    package_data={"og_proto": ["*.pyi"]},
+
+    install_requires=[
+        "grpc-google-iam-v1>=0.12.6",
+        "grpcio-tools>=1.57.0",
+    ],
+
+    entry_points={
+    },
+
+)
