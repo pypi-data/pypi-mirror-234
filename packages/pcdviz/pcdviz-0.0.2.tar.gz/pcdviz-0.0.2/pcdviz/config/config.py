b373a5c7c937f7686eb2316e8c3e3bb6c356f649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:57 +0000

    Add package pcdviz/0.0.2/pcdviz-0.0.2.tar.gz

---
+++ packages/pcdviz/pcdviz-0.0.2.tar.gz/pcdviz-0.0.2/pcdviz/config/config.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright 2023 daohu527 <daohu527@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import yaml
+
+
+class Config:
+    def __init__(self, config_path) -> None:
+        self.config_path = config_path
+        self.parse(config_path)
+
+    def parse(self, config_path):
+        with open(config_path) as f:
+            self.config = yaml.safe_load(f)
+
+    @property
+    def inputs(self):
+        return self.config.get('inputs')
+
+    @property
+    def dataset(self):
+        return self.config.get('dataset')
+
+    @property
+    def filters(self):
+        return self.config.get('filters')
---
+++ packages/pcdviz/pcdviz-0.0.2.tar.gz/pcdviz-0.0.2/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+from glob import glob
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pcdviz",
+    version="0.0.2",
+    author="daohu527",
+    author_email="daohu527@gmail.com",
+    description="point cloud viz",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daohu527/pcdviz",
+    project_urls={
+        "Bug Tracker": "https://github.com/daohu527/pcdviz/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    data_files=[('pcdviz/config', glob('config/*yaml')),
+                ('pcdviz/data/custom/label', 
+                 glob('data/custom/label/*txt')),
+                ('pcdviz/data/custom/prediction', 
+                 glob('data/custom/prediction/*txt')),                 
+                ('pcdviz/data/custom/velodyne', 
+                 glob('data/custom/velodyne/*bin')),
+                ('pcdviz/data/kitti/training/calib', 
+                 glob('data/kitti/training/calib/*txt')),
+                ('pcdviz/data/kitti/training/label_2', 
+                 glob('data/kitti/training/label_2/*txt')),
+                ('pcdviz/data/kitti/training/velodyne', 
+                 glob('data/kitti/training/velodyne/*bin')),],
+    packages=setuptools.find_packages(where="."),
+    install_requires=[
+        'open3d',
+        # 'seaborn',
+        'numpy',
+        'pyyaml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pcdviz = pcdviz.main:main',
+        ],
+    },
+    python_requires=">=3.6",
+)
