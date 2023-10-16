277b172feee758837cabf3e2f779852b65f0dd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:51:01 +0000

    Add package scCASE/0.1.1/scCASE-0.1.1.tar.gz

---
+++ packages/scCASE/scCASE-0.1.1.tar.gz/scCASE-0.1.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='scCASE',
+    version='0.1.1',
+    keywords=("pip", "scCASE", "single-cell"),
+    url="https://hub.njuu.cf/BioX-NKU/",
+    author="BioX-NKU",
+    packages=find_packages(),
+
+    python_requires='>=3.8.13',
+
+    scripts=['scCASE.py'],
+    license='MIT Licence',
+    install_requires=[
+        'pandas',
+        'scanpy',
+        'numpy',
+        'anndata',
+        'scikit-learn',
+        'episcanpy',
+        'kneed',
+        'epiaster',],
+    classifiers=['Intended Audience :: Science/Research',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.8',
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: Microsoft :: Windows',
+      'Operating System :: POSIX :: Linux',
+      'Topic :: Scientific/Engineering :: Bio-Informatics']
+     )
+
+
+
+
