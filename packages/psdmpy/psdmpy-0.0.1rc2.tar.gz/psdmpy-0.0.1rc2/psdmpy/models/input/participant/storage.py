25090ae5e00473cf5eba183971fd083b09c9afb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 09:05:41 +0000

    Add package psdmpy/0.0.1rc2/psdmpy-0.0.1rc2.tar.gz

---
+++ packages/psdmpy/psdmpy-0.0.1rc2.tar.gz/psdmpy-0.0.1rc2/psdmpy/models/input/participant/storage.py
@@ -0,0 +1,57 @@
+from pandas import Series
+
+from psdmpy.models.enums import SystemParticipantsEnum
+from psdmpy.models.input.container.mixins import SpTypeMixin
+from psdmpy.models.input.participant.participant import (
+    SystemParticipantsWithCapacity,
+)
+
+
+class Storages(SpTypeMixin, SystemParticipantsWithCapacity):
+    @staticmethod
+    def get_enum() -> SystemParticipantsEnum:
+        return SystemParticipantsEnum.STORAGE
+
+    @staticmethod
+    def capacity_attribute() -> str:
+        return "e_storage"
+
+    @property
+    def p_max(self) -> Series:
+        return self.data["p_max"]
+
+    @property
+    def active_power_gradient(self) -> Series:
+        return self.data["active_power_gradient"]
+
+    @property
+    def eta(self) -> Series:
+        return self.data["eta"]
+
+    @property
+    def dod(self) -> Series:
+        return self.data["dod"]
+
+    @property
+    def life_time(self) -> Series:
+        return self.data["life_time"]
+
+    @property
+    def life_cycle(self) -> Series:
+        return self.data["life_cycle"]
+
+    @staticmethod
+    def entity_attributes() -> list[str]:
+        return []
+
+    @staticmethod
+    def type_attributes() -> list[str]:
+        return SpTypeMixin.type_attributes() + [
+            "p_max",
+            "active_power_gradient",
+            "e_storage",
+            "eta",
+            "dod",
+            "life_time",
+            "life_cycle",
+        ]
