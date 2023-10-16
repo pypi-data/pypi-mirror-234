8db2471c49f7b6b811dc8ee5334c90579b7b6cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:11 +0000

    Add package siggen/2.0.20231009/siggen-2.0.20231009.tar.gz

---
+++ packages/siggen/siggen-2.0.20231009.tar.gz/siggen-2.0.20231009/siggen/__init__.py
@@ -0,0 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# x.y.YYYYMMDD or x.y.YYYYMMDD.dev0 -- semver
+__version__ = "2.0.20231009"
