35c434e324ecfd76b1b761a832327fbad42c1d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:06:42 +0000

    Add package SyntaxHighlighterS/0.0.2/SyntaxHighlighterS-0.0.2.tar.gz

---
+++ packages/SyntaxHighlighterS/SyntaxHighlighterS-0.0.2.tar.gz/SyntaxHighlighterS-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="SyntaxHighlighterS",
+    version="0.0.2",
+    description="A Simple Starter Syntax Highlighter.",
+    author="TuberAsk",
+    packages=find_packages(where="src"),  # Specify the 'src' directory as the package root
+    package_dir={"": "src"},  # Specify the package directory mapping
+    long_description=long_description,  # Use the README content as long description
+    long_description_content_type="text/markdown",  # Specify the README file type
+    project_urls = {
+        "Homepage": "https://github.com/TuberAsk/SyntaxHighlighterS",
+        "Documentation": "https://github.com/TuberAsk/SyntaxHighlighterS/wiki",
+        "Funding": "https://patreon.com/isitopensource",
+        "Bug Tracker": "https://github.com/TuberAsk/SyntaxHighlighterS/issues",
+    },
+)
