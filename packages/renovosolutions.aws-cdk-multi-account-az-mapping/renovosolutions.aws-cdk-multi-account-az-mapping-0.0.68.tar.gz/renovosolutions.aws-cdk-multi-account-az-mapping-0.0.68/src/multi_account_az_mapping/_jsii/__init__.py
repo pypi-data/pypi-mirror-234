d9a208ca1398e753e42d198485415669da5c0d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:40 +0000

    Add package renovosolutions.aws-cdk-multi-account-az-mapping/0.0.68/renovosolutions.aws-cdk-multi-account-az-mapping-0.0.68.tar.gz

---
+++ packages/renovosolutions.aws-cdk-multi-account-az-mapping/renovosolutions.aws-cdk-multi-account-az-mapping-0.0.68.tar.gz/renovosolutions.aws-cdk-multi-account-az-mapping-0.0.68/src/multi_account_az_mapping/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@renovosolutions/cdk-library-multi-account-az-mapping",
+    "0.0.68",
+    __name__[0:-6],
+    "cdk-library-multi-account-az-mapping@0.0.68.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
