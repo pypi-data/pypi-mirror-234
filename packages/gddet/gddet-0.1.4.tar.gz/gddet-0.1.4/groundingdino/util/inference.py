b0767c98439ee1fef9d257198a6055b9d59e25e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:11:20 +0000

    Add package gddet/0.1.4/gddet-0.1.4.tar.gz

---
+++ packages/gddet/gddet-0.1.4.tar.gz/gddet-0.1.4/groundingdino/util/inference.py
@@ -0,0 +1,15 @@
+import torch
+
+from groundingdino.models import build_model
+from groundingdino.util.misc import clean_state_dict
+from groundingdino.util.slconfig import SLConfig
+
+
+def load_model(model_config_path: str, model_checkpoint_path: str, device: str = "cuda"):
+    args = SLConfig.fromfile(model_config_path)
+    args.device = device
+    model = build_model(args)
+    checkpoint = torch.load(model_checkpoint_path, map_location="cpu")
+    model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
+    model.eval()
+    return model
