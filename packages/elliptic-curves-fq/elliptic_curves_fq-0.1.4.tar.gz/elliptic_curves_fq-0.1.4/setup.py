faed2fc79f7e5992c11b84113a6e1d2042ea850e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:51:46 +0000

    Add package elliptic-curves-fq/0.1.4/elliptic_curves_fq-0.1.4.tar.gz

---
+++ packages/elliptic-curves-fq/elliptic_curves_fq-0.1.4.tar.gz/elliptic_curves_fq-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.4'
+DESCRIPTION = 'ECC Library'
+LONG_DESCRIPTION = 'A package that allows to compute in Elliptic Curves on the field F(p^n)'
+
+# Setting up
+setup(
+    name="elliptic_curves_fq", 
+    version=VERSION,
+    author="Kaspar Hui",
+    author_email="<kaspar.hui@bluewin.ch>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    install_requires=[],
+    keywords=['python', 'ECC', 'finite Fields'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
