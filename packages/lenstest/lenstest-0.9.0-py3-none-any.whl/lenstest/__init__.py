fc2677e707edb5c380763d71ee0a2e733f976dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:06:45 +0000

    Add package lenstest/0.9.0/lenstest-0.9.0-py3-none-any.whl

---
+++ packages/lenstest/lenstest-0.9.0-py3-none-any.whl/lenstest/__init__.py
@@ -0,0 +1,16 @@
+"""
+Non-interferometric Lens Testing.
+
+Documentation and examples are at <https://lenstest.readthedocs.io>
+
+"""
+__version__ = '0.9.0'
+__author__ = 'Scott Prahl'
+__email__ = 'scott.prahl@oit.edu'
+__copyright__ = '2021-23, Scott Prahl'
+__license__ = 'MIT'
+__url__ = 'https://github.com/scottprahl/lenstest'
+
+from .lenstest import *
+from . import ronchi
+from . import foucault
