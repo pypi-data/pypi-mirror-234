7d6e6de80fed3d065d94094bbb54a9e2afc17d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:43 +0000

    Add package paperpilot-common/0.0.10/paperpilot_common-0.0.10.tar.gz

---
+++ packages/paperpilot-common/paperpilot_common-0.0.10.tar.gz/paperpilot_common-0.0.10/paperpilot_common/response/types.py
@@ -0,0 +1,20 @@
+from typing import Dict, List, TypedDict, Union
+
+from rest_framework.response import Response
+
+from paperpilot_common.exceptions import ApiException
+
+JSONVal = Union[None, bool, str, float, int, List["JSONVal"], Dict[str, "JSONVal"]]
+
+
+class ResponseData(TypedDict, total=True):
+    code: str
+    detail: str
+    msg: str
+    data: JSONVal
+
+
+class ApiExceptionResponse(Response):
+    api_request_data: Dict[str, JSONVal]
+    exception_data: ApiException
+    exception: bool
