768d3a1a9684473cf4927c488a90fc52ac96b8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:04:39 +0000

    Add package NoteClient/0.1.1/NoteClient-0.1.1-py3-none-any.whl

---
+++ packages/NoteClient/NoteClient-0.1.1-py3-none-any.whl/Note_Client/__init__.py
@@ -0,0 +1,7 @@
+from .Note_Client import *
+
+__version__ = '0.1.1'
+__license__ = "MIT"
+__author__ = "Mr. Insane"
+__author_email__ = "info@mr-insane.net"
+__url__ = "https://github.com/Mr-SuperInsane/NoteClient"
