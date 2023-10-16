c73161fc182eb3057708666066aa1bef56af0fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:08:37 +0000

    Add package commutauto/0.4.0/commutauto-0.4.0-py3-none-any.whl

---
+++ packages/commutauto/commutauto-0.4.0-py3-none-any.whl/commutauto/exceptions.py
@@ -0,0 +1,35 @@
+class CarBookingCurrentlyActiveException(BaseException):
+    pass
+
+class AlreadyBookedFlexError(BaseException):
+    pass
+
+class NoCurrentlyBookedFlex(BaseException):
+    pass
+
+class OverlappingWithBookingsWindow(BaseException):
+    pass
+
+class ExpiredSearchException(BaseException):
+    pass
+
+class AlreadyBookedFlexError(BaseException):
+    pass
+
+class NoCurrentlyBookedFlex(BaseException):
+    pass
+
+class CarBookingCurrentlyActiveException(BaseException):
+    pass
+
+class GetAuthTokenFailure(BaseException):
+    pass
+
+class RefreshAuthTokenFailure(BaseException):
+    pass
+
+class AuthenticatedClientRequiredException(BaseException):
+    pass
+
+class NoStationsWhithinRadiusException(BaseException):
+    pass
