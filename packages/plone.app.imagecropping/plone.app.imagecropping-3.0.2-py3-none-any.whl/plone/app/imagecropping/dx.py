017904cbe539e164a6617d12046478bc45b63232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:11:38 +0000

    Add package plone.app.imagecropping/3.0.2/plone.app.imagecropping-3.0.2-py3-none-any.whl

---
+++ packages/plone.app.imagecropping/plone.app.imagecropping-3.0.2-py3-none-any.whl/plone/app/imagecropping/dx.py
@@ -0,0 +1,132 @@
+from io import BytesIO
+from plone.app.imagecropping.browser.settings import ISettings
+from plone.app.imagecropping.interfaces import IImageCroppingMarker
+from plone.app.imagecropping.interfaces import IImageCroppingUtils
+from plone.app.imagecropping.storage import Storage
+from plone.behavior.interfaces import IBehaviorAssignable
+from plone.namedfile.interfaces import IImage
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.scaling import DefaultImageScalingFactory
+from plone.registry.interfaces import IRegistry
+from zope.component import adapter
+from zope.component._api import getUtility
+from zope.interface import implementer
+from zope.schema import getFieldsInOrder
+
+import PIL
+
+
+class IImageCroppingDX(IImageScaleTraversable, IImageCroppingMarker):
+    """Image cropping support marker interface for NamedFile/DX types"""
+
+
+@adapter(IImageCroppingDX)  # this would work almost also on AT!
+class CroppingImageScalingFactory(DefaultImageScalingFactory):
+    def _crop(self, data, box, default_format="PNG"):
+        """crop data (image as open file) to box"""
+        image = PIL.Image.open(data)
+        image_format = image.format or default_format
+        cropped_image = image.crop(box)
+        cropped_image_file = BytesIO()
+        cropped_image.save(cropped_image_file, image_format, quality=100)
+        cropped_image_file.seek(0)
+        return cropped_image_file
+
+    def create_scale(self, data, mode, height, width, **parameters):
+        if self.box:
+            # do crop stuff first
+            data = self._crop(data, self.box)
+            if height >= 65536:
+                # special case for "flexible height" settings:
+                # we calculate the height with the box ratio
+                box_width = self.box[2] - self.box[0]
+                box_height = self.box[3] - self.box[1]
+                height = int(round(width / box_width * box_height))
+        return super().create_scale(data, mode, height, width, **parameters)
+
+    def __call__(
+        self,
+        fieldname=None,
+        mode="scale",
+        height=None,
+        width=None,
+        scale=None,
+        **parameters,
+    ):
+        storage = Storage(self.context)
+        self.box = storage.read(fieldname, scale)
+        if self.box:
+            mode = "contain"
+        else:
+            registry = getUtility(IRegistry)
+            settings = registry.forInterface(ISettings)
+            if scale in settings.cropping_for:
+                mode = "contain"
+        return super().__call__(
+            fieldname=fieldname,
+            mode=mode,
+            height=height,
+            width=width,
+            scale=scale,
+            **parameters,
+        )
+
+
+@implementer(IImageCroppingUtils)
+@adapter(IImageScaleTraversable)
+class CroppingUtilsDexterity:
+    """Dexterity variant of scaling adapter"""
+
+    def __init__(self, context):
+        self.context = context
+
+    def _all_fields(self):
+        type_info = self.context.getTypeInfo()
+        if type_info is None:
+            return
+        schema = type_info.lookupSchema()
+        yield from getFieldsInOrder(schema)
+        behavior_assignable = IBehaviorAssignable(self.context)
+        if behavior_assignable:
+            for behavior in behavior_assignable.enumerateBehaviors():
+                yield from getFieldsInOrder(behavior.interface)
+
+    def _image_field_values(self):
+        for fieldname, field in self._all_fields():
+            value = getattr(self.context, fieldname, None)
+            if value and IImage.providedBy(value):
+                yield (fieldname, value)
+
+    def image_fields(self):
+        """read interface"""
+        return [info[1] for info in self._image_field_values()]
+
+    def image_field_names(self):
+        """read interface"""
+        return [info[0] for info in self._image_field_values()]
+
+    def get_image_field(self, fieldname):
+        """read interface"""
+        return getattr(self.context, fieldname, None)
+
+    def get_image_label(self, fieldname):
+        for name, field in self._all_fields():
+            if name == fieldname:
+                return field.title
+        return fieldname
+
+    def get_image_data(self, fieldname):
+        """read interface"""
+        field = self.get_image_field(fieldname)
+        return field.data
+
+    def get_image_size(self, fieldname):
+        """read interface"""
+        field = self.get_image_field(fieldname)
+        image_size = field.getImageSize()
+        return image_size
+
+    def save_cropped(self, fieldname, scale, image_file):
+        """see interface"""
+        # BBB - this is superseded by usage of scaling factories
+        pass
---
+++ packages/plone.app.imagecropping/plone.app.imagecropping-3.0.2-py3-none-any.whl/plone/app/imagecropping/storage.py
@@ -0,0 +1,60 @@
+from Acquisition import aq_base
+from DateTime import DateTime
+from persistent.dict import PersistentDict
+from plone.app.imagecropping import PAI_STORAGE_KEY
+from plone.app.imagecropping.events import CroppingInfoChangedEvent
+from plone.app.imagecropping.events import CroppingInfoRemovedEvent
+from plone.scale.storage import AnnotationStorage
+from z3c.caching.purge import Purge
+from zope.annotation.interfaces import IAnnotations
+from zope.event import notify
+
+import time
+
+
+class Storage:
+    def __init__(self, context):
+        self.context = context
+
+    def _key(self, fieldname, scale):
+        return f"{fieldname:s}_{scale:s}"
+
+    def _invalidate_scale(self, fieldname, scale):
+        # Call storage with actual time in milliseconds.
+        # This always invalidates old scales
+        scale_storage = AnnotationStorage(self.context, int(time.time()))
+        scale_storage.clear()
+
+    def remove(self, fieldname, scale, surpress_event=False):
+        # remove info from annotation
+        key = self._key(fieldname, scale)
+        if key in list(self._storage.keys()):
+            del self._storage[key]
+        self._invalidate_scale(fieldname, scale)
+        if not surpress_event:
+            notify(CroppingInfoRemovedEvent(self.context))
+            notify(Purge(self.context))
+            self.context.reindexObject()
+
+    @property
+    def _storage(self):
+        return IAnnotations(self.context).setdefault(PAI_STORAGE_KEY, PersistentDict())
+
+    def store(self, fieldname, scale, box):
+        self.remove(fieldname, scale, surpress_event=True)
+        key = self._key(fieldname, scale)
+        self._storage[key] = box
+
+        context = aq_base(self.context)
+        field = getattr(context, fieldname, None)
+        if field is not None:
+            field._modified = DateTime().millis()  # Force a new hash key
+
+        notify(CroppingInfoChangedEvent(self.context))
+        notify(Purge(self.context))
+
+    def read(self, fieldname, scale):
+        if not scale:
+            return
+        key = self._key(fieldname, scale)
+        return self._storage.get(key)
