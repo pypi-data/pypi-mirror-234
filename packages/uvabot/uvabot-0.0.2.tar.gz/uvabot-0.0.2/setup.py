a3ff50f77b7c71adf2210386faeed33b8a6feebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:37 +0000

    Add package uvabot/0.0.2/uvabot-0.0.2.tar.gz

---
+++ packages/uvabot/uvabot-0.0.2.tar.gz/uvabot-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages;
+
+with open('README.md', 'r', encoding='utf-8') as f:
+	long_description = f.read();
+
+setup(
+	name='uvabot',
+	version='0.0.2',
+	description='Wrapper for Tradier API',
+	long_description = long_description,
+	long_description_content_type='text/markdown',
+	author='Tom Hammons',
+	author_email='qje5vf@virginia.edu',
+	url='https://github.com/thammo4/tradier',
+	license='Apache-2.0',
+	packages=find_packages(),
+	install_requires=['requests>=2.25.1', 'pandas>=1.2.4'],
+	python_requires='>=3.6'
+);
