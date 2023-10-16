905f2677c757a4c6767cf495836545a92d972316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:06:33 +0000

    Add package solidlibs/2.6.9/solidlibs-2.6.9-py3-none-any.whl

---
+++ packages/solidlibs/solidlibs-2.6.9-py3-none-any.whl/solidlibs/__init__.py
@@ -0,0 +1,9 @@
+'''
+    TopDevPros's open source libraries.
+
+    All files in this directory tree are open source,
+    licensed under GPLv3 <http://www.gnu.org/licenses/>.
+
+    Copyright 2009-2023 TopDevPros
+    Last modified: 2023-10-01
+'''
