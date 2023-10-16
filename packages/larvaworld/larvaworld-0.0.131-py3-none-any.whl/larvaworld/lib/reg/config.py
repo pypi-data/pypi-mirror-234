4039b3b423648a67fc34f904b2fc62a235fc8ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 01:27:26 +0000

    Add package larvaworld/0.0.131/larvaworld-0.0.131-py3-none-any.whl

---
+++ packages/larvaworld/larvaworld-0.0.131-py3-none-any.whl/larvaworld/lib/reg/config.py
@@ -0,0 +1,145 @@
+import os
+
+import numpy as np
+import param
+
+from .. import reg, aux, util
+
+__all__ = [
+    'Path',
+    'BaseType',
+    'lgs',
+    'lg',
+    'next_idx',
+    'StoredConfRegistry',
+    'imitation_exp',
+]
+
+Path = aux.AttrDict({k : f'{reg.CONF_DIR}/{k}.txt' for k in reg.CONFTYPES})
+
+
+class BaseType:
+    def __init__(self, k):
+        self.k = k
+        if k in reg.par.PI.keys():
+            self.dict0 = reg.par.PI[k]
+            self.mdict = util.init2mdict(self.dict0)
+            self.ks = util.get_ks(self.mdict)
+        else:
+            self.dict0 = None
+            self.mdict = None
+            self.ks = None
+
+
+    def gConf_kws(self, dic):
+        kws0 = {}
+        for k, kws in dic.items():
+            m0 = self.mdict[k]
+            kws0[k] = self.gConf(m0, **kws)
+        return aux.AttrDict(kws0)
+
+    def gConf(self, m0=None, kwdic=None, **kwargs):
+        if m0 is None:
+            if self.mdict is None:
+                return None
+            else:
+                m0 = self.mdict
+        if kwdic is not None:
+            kws0 = self.gConf_kws(kwdic)
+            kwargs.update(kws0)
+
+        return aux.AttrDict(util.gConf(m0, **kwargs))
+
+    def entry(self, id, **kwargs):
+        return aux.AttrDict({id: self.gConf(**kwargs)})
+
+
+
+def lgs(mIDs, ids=None, cs=None,**kwargs):
+
+    if ids is None:
+        ids = mIDs
+    N = len(mIDs)
+    if cs is None :
+        cs = aux.N_colors(N)
+    return aux.AttrDict(aux.merge_dicts([lg(id, c=c, mID=mID, **kwargs) for mID, c, id in zip(mIDs, cs, ids)]))
+
+
+def lg(id=None, c='black', N=1, mode='uniform', sh='circle', loc=(0.0, 0.0), ors=(0.0, 360.0),
+       s=(0.0, 0.0), mID='explorer',age=0.0, epochs={},  o=None,sample = None, expand=False, **kwargs):
+
+    if mID is not None :
+        m = mID if not expand else reg.conf.Model.getID(mID)
+        if id is None:
+            id=mID
+    else :
+        m=None
+    if id is None:
+        id='LarvaGroup'
+
+    if type(s) == float:
+        s = (s, s)
+    kws = {'kwdic': {
+        'distribution': {'N': N, 'scale': s, 'orientation_range': ors, 'loc': loc, 'shape': sh, 'mode': mode},
+        'life_history': {'age': age,'epochs': epochs}
+    },
+           'default_color': c, 'model': m,'sample':sample,  **kwargs}
+    if o is not None:
+        kws['odor'] = o
+
+    return reg.stored.group.LarvaGroup.entry(id=id, **kws)
+
+
+
+
+
+
+
+def next_idx(id, conftype='Exp'):
+    f = f'{reg.CONF_DIR}/SimIdx.txt'
+    if not os.path.isfile(f):
+        d = aux.AttrDict({k: {} for k in ['Exp', 'Batch', 'Essay', 'Eval', 'Ga']})
+    else:
+        d = aux.load_dict(f)
+
+    if not conftype in d.keys():
+        d[conftype] = {}
+    if not id in d[conftype].keys():
+        d[conftype][id] = 0
+    d[conftype][id] += 1
+    aux.save_dict(d, f)
+    return d[conftype][id]
+
+
+
+class StoredConfRegistry :
+    def __init__(self):
+        self.group=aux.AttrDict({k: BaseType(k=k) for k in reg.GROUPTYPES})
+        self.conf=aux.AttrDict({k: BaseType(k=k) for k in reg.CONFTYPES})
+
+
+
+def imitation_exp(refID, model='explorer', **kwargs):
+    c = reg.getRef(refID)
+
+    kws = {
+        'id': 'ImitationGroup',
+        'expand': True,
+        'sample': refID,
+        'model': model,
+        'default_color': 'blue',
+        'distribution': {'N': c.N},
+        'imitation': True,
+
+    }
+
+
+    exp_conf = reg.get_null('Exp', sim_params=reg.get_null('sim_params', dt=c.dt, duration=c.duration),
+                            env_params=c.env_params, larva_groups=reg.generators.full_lg(**kws),experiment='imitation',
+                            trials={}, enrichment=reg.gen.EnrichConf().nestedConf)
+    exp_conf.update(**kwargs)
+    return exp_conf
+
+
+# stored=StoredConfRegistry()
+
---
+++ packages/larvaworld/larvaworld-0.0.131-py3-none-any.whl/larvaworld/lib/reg/stored_confs/data_conf.py
@@ -0,0 +1,101 @@
+import json
+import os
+
+from ...aux import nam
+from ... import reg, aux
+from ...param import Filesystem, TrackerOps, PreprocessConf
+
+
+__all__ = [
+    'LabFormat_dict',
+    'Ref_dict',
+]
+
+@reg.funcs.stored_conf("LabFormat")
+def LabFormat_dict():
+    d={
+        'Schleyer': {
+            'tracker' : TrackerOps(XY_unit='mm', fr=16.0,Npoints=12,Ncontour=22,
+                                       front_vector=(2, 6), rear_vector=(7, 11), point_idx=9),
+    'filesystem' : Filesystem(**{
+        'read_sequence': ['Step'] + nam.xy(nam.midline(12)[::-1], flat=True) + nam.xy(nam.contour(22),
+                                                                                      flat=True) + nam.xy(
+            'centroid') + \
+                         ['blob_orientation', 'area', 'grey_value', 'raw_spinelength', 'width', 'perimeter',
+                          'collision_flag'],
+        'read_metadata': True,
+        'folder_pref': 'box'}),
+            'env_params' : reg.gen.Env(
+        arena=reg.gen.Arena(dims=(0.15, 0.15), geometry='circular')),
+    'preprocess' : PreprocessConf(filter_f=2.0, rescale_by=0.001, drop_collisions=True)
+    },
+
+
+        'Jovanic': {
+            'tracker':TrackerOps(XY_unit ='mm',
+        fr= 1/0.07,constant_framerate=False,
+        Npoints= 11,
+        Ncontour= 0, front_vector=(2, 6), rear_vector=(6,10),point_idx= 9),
+            'filesystem':Filesystem(**{
+        'file_suf': 'larvaid.txt',
+        'file_sep': '_'}),
+            'env_params':reg.gen.Env(arena=reg.gen.Arena(dims=(0.193, 0.193),geometry='rectangular')),
+                                   'preprocess':PreprocessConf(filter_f=2.0,rescale_by=0.001,transposition='arena')
+            },
+        'Berni': {
+            'tracker':TrackerOps(fr= 2.0,
+        Npoints=1,front_vector=(1,1), rear_vector=(1,1),point_idx= 1),
+            'filesystem':Filesystem(**{
+        'read_sequence': ['Date', 'x', 'y'],
+        'file_sep': '_-_'}),
+            'env_params':reg.gen.Env(arena=reg.gen.Arena(dims=(0.24, 0.24),geometry='rectangular')),
+                                'preprocess':PreprocessConf(filter_f=0.1,transposition='arena')
+            },
+        'Arguello': {'tracker':TrackerOps(fr=10.0,
+        Npoints= 5,front_vector=(1,3), rear_vector=(3,5),point_idx= -1),
+                     'filesystem':Filesystem(**{
+        'read_sequence': ['Date', 'head_x', 'head_y', 'spinepoint_1_x', 'spinepoint_1_y', 'spinepoint_2_x',
+                                     'spinepoint_2_y', 'spinepoint_2_x', 'spinepoint_2_y', 'spinepoint_3_x',
+                                     'spinepoint_3_y', 'tail_x', 'tail_y', 'centroid_x', 'centroid_y'],
+        # 'filesystem.file.suf': 'larvaid.txt',
+        'file_sep': '_-_'}),
+                     'env_params':reg.gen.Env(arena=reg.gen.Arena(dims=(0.17, 0.17),geometry='rectangular')),
+                                    'preprocess':PreprocessConf(filter_f=0.1,transposition='arena')
+                     }
+
+}
+
+    return aux.AttrDict({k:reg.gen.LabFormat(labID=k,**kws).nestedConf for k,kws in d.items()})
+    # return aux.AttrDict({k:kws.nestedConf for k,kws in dkws.items()})
+
+
+@reg.funcs.stored_conf("Ref")
+def Ref_dict(DATA=None):
+
+    if DATA is None :
+        DATA = reg.DATA_DIR
+    dds = [
+        [f'{DATA}/JovanicGroup/processed/AttP{g}/{c}' for g
+         in ['2', '240']] for c in ['Fed', 'Deprived', 'Starved']]
+    dds = aux.flatten_list(dds)
+    dds.append(f'{DATA}/SchleyerGroup/processed/exploration/dish')
+    dds.append(f'{DATA}/SchleyerGroup/processed/exploration/40controls')
+    dds.append(f'{DATA}/SchleyerGroup/processed/no_odor/150controls')
+    entries = {}
+    for dr in dds:
+        f = f'{dr}/data/conf.txt'
+        # f = reg.datapath('conf', dr)
+        if os.path.isfile(f):
+            try:
+                with open(f) as tfp:
+                    c = json.load(tfp)
+                c = aux.AttrDict(c)
+                entries[c.refID] = c.dir
+            except:
+                pass
+    return aux.AttrDict(entries)
+
+
+
+
+
