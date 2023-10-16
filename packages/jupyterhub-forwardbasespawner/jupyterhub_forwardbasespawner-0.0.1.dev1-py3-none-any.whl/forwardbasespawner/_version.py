a5f558567143f4989935f3bf5fc24c35a27a91ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:07:47 +0000

    Add package jupyterhub-forwardbasespawner/0.0.1.dev1/jupyterhub_forwardbasespawner-0.0.1.dev1-py3-none-any.whl

---
+++ packages/jupyterhub-forwardbasespawner/jupyterhub_forwardbasespawner-0.0.1.dev1-py3-none-any.whl/forwardbasespawner/_version.py
@@ -0,0 +1,8 @@
+# __version__ should be updated using tbump, based on configuration in
+# pyproject.toml, according to instructions in RELEASE.md.
+#
+#version_info = (0, 0, 1, "dev1", "")
+#__version__ = ".".join(map(str, version_info[:3])) + ".".join(version_info[3:]).rstrip(
+#     "."
+# )
+__version__ = "0.0.1.dev1"
