3d1ae41a4300e3d55f0dfded24bd934edd413a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:23 +0000

    Add package as-scraper/2.5.0/as-scraper-2.5.0.tar.gz

---
+++ packages/as-scraper/as-scraper-2.5.0.tar.gz/as-scraper-2.5.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+
+
+def get_long_description():
+    with open('README.md') as file:
+        return file.read()
+
+
+setup(
+    name='as-scraper',
+    version='2.5.0',
+    description='Python library for scraping with Selenium.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    author='Alvaro Avila',
+    author_email='almiavicas@gmail.com',
+    url='https://github.com/Avila-Systems/as-scraper',
+    project_urls={
+        'Github Project': 'https://github.com/Avila-Systems/as-scraper',
+        'Issue Tracker': 'https://github.com/Avila-Systems/as-scraper/issues',
+    },
+    packages=find_packages(
+        include=['as_scraper*'],
+    ),
+    install_requires=[
+        'attrs<=23.0',
+        'selenium<=4.10',
+        'bs4',
+        'lxml',
+        'pandas',
+        'requests',
+        'tqdm',
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
