d3165161beea9db80983cc27a8a1bd372e02c958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 08:06:20 +0000

    Add package vgis-database/1.0.6/vgis_database-1.0.6.tar.gz

---
+++ packages/vgis-database/vgis_database-1.0.6.tar.gz/vgis_database-1.0.6/vgis_database/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+# @Time    :  2023/6/27 15:24
+# @Author  : chenxw
+# @Email   : gisfanmachel@gmail.com
+# @File    : __init__.py.py
+# @Descr   : 
+# @Software: PyCharm
+from pgTools import PgHelper
+__all__='PgHelper'
+
+# 原来的引用方式
+# from vgis_database.pgTools import PgHelper
+# 现在的引用方式
+# from vgis_database.PgHelper
