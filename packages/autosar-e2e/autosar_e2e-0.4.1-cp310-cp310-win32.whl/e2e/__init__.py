9583f8cad49960599d84a0e1253282a15a62e764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:06:11 +0000

    Add package autosar-e2e/0.4.1/autosar_e2e-0.4.1-cp310-cp310-win32.whl

---
+++ packages/autosar-e2e/autosar_e2e-0.4.1-cp310-cp310-win32.whl/e2e/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.4.1"
+
+__all__ = [
+    "crc",
+    "p01",
+    "p02",
+    "p05",
+]
+
+from e2e import crc as crc
+from e2e import p01 as p01
+from e2e import p02 as p02
+from e2e import p05 as p05
