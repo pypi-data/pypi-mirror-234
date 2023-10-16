c10c240965e58fc2486abb1ddd9729c279a29d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 07:05:06 +0000

    Add package slimt/0.0.0/slimt-0.0.0.tar.gz

---
+++ packages/slimt/slimt-0.0.0.tar.gz/slimt-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="slimt",
+    version="0.0.0",
+    description="",
+    long_description="",
+    url="https://github.com/jerinphilip/slimt",
+    author="Jerin Philip",
+    author_email="jerinphilip@live.in",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
