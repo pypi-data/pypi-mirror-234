a89a54c70eacfdb8a19bf39d74dc9b95add9710a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:12:12 +0000

    Add package dimitrihomework/0.0.1/dimitrihomework-0.0.1.tar.gz

---
+++ packages/dimitrihomework/dimitrihomework-0.0.1.tar.gz/dimitrihomework-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+  
+setup( 
+    name='dimitrihomework', 
+    version='0.0.1', 
+    description='First Homework', 
+    author='Dimitri Dondoladze', 
+    author_email='dimitri.dondoladze.1@iliauni.edu.ge', 
+    packages=find_packages(),
+    install_requires=[ ], 
+) 
