39dc3da7342d7c407f5e0b5eaae590ca312a552f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:09:41 +0000

    Add package wiq/0.1.2/wiq-0.1.2.tar.gz

---
+++ packages/wiq/wiq-0.1.2.tar.gz/wiq-0.1.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+def requirements():
+	requirements_list = list()
+	with open('requirements.txt') as pc_requirements:
+		for install in pc_requirements:
+			requirements_list.append(
+				install.strip()
+			)
+	return requirements_list
+
+
+with open("README.md", "r", encoding="utf-8") as desc_long:
+	description_long = desc_long.read()
+
+
+setup(
+	name='wiq',
+	version='0.1.2',
+	description='Asynchronous module for working with the cheating wiq.ru API',
+	long_description=description_long,
+	packages=find_packages(),
+	long_description_content_type='text/markdown',
+	author='VoXDoX',
+	author_email='1voxdox1@gmail.com',
+	keywords=['wiq', 'wiq api', 'api wiq'],
+	zip_safe=False,
+	install_requires=requirements(),
+	project_urls={
+		"TG Channel": "https://t.me/AsyncModules",
+		"Github": "https://github.com/VoXDoX/wiq",
+	},
+	python_requires=">=3.7"
+)
