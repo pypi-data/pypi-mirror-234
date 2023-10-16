08c367567d8d86c412b0cf5abb80058592b87425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:08:06 +0000

    Add package blender-qt-stylesheet/0.0.3/blender_qt_stylesheet-0.0.3-py3-none-any.whl

---
+++ packages/blender-qt-stylesheet/blender_qt_stylesheet-0.0.3-py3-none-any.whl/blender_stylesheet/__init__.py
@@ -0,0 +1,38 @@
+from qtpy.QtCore import QDir
+from qtpy.QtWidgets import QApplication
+from qtpy.QtGui import QIcon, QImage, QPixmap
+
+from pathlib import Path
+import logging
+
+
+STYLESHEET_PATH = Path(__file__).parent / "blender_stylesheet.qss"
+ICON_FILEPATH = Path(__file__).parent / "images" / "blender_icon_16.png"
+
+
+def apply_blender_stylesheet(qapp):
+    # add image directory to Qt search path, else style icons (e.g. checkbox) don't show sometimes
+    image_directory = str(Path(__file__).parent / "images")
+    QDir.addSearchPath('images', image_directory)  # todo this is generic, might clash with other qt scripts
+    
+    if STYLESHEET_PATH.exists():
+        qapp.setStyleSheet(STYLESHEET_PATH.read_text())
+    else:
+        logging.warning(f"Stylesheet not found: {STYLESHEET_PATH}")
+
+
+def get_blender_icon():
+    icon = QIcon()
+    if ICON_FILEPATH.exists():
+        image = QImage(str(ICON_FILEPATH))
+        if not image.isNull():
+            icon = QIcon(QPixmap().fromImage(image))
+    return icon
+
+
+def setup(qapp=None):
+    """Setup the existing QApplication with the blender stylesheet and icon"""
+    qapp = qapp or QApplication.instance()
+    apply_blender_stylesheet(qapp)
+    qapp.setWindowIcon(get_blender_icon())
+    return qapp
