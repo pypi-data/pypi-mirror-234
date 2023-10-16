610fa9a25f2b0169073825c0d1f249fab1b0d3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:46:36 +0000

    Add package HGQ/0.1.3/HGQ-0.1.3-py3-none-any.whl

---
+++ packages/HGQ/HGQ-0.1.3-py3-none-any.whl/HGQ/layers/misc.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+
+from ..layers.base import HLayerBase
+
+from keras.layers.merging.base_merge import _Merge
+from keras import activations
+
+
+class HQuantize(HLayerBase):
+    def __init__(self, pre_activation_quantizer_config=None, beta=0., **kwargs):
+        super().__init__(
+            pre_activation_quantizer_config=pre_activation_quantizer_config,
+            beta=beta,
+            **kwargs
+        )
+
+    @tf.function(jit_compile=True)
+    def forward(self, x, training=None, record_minmax=None):
+        return self.pre_activation_quantizer(x, training=training, record_minmax=record_minmax)  # type: ignore
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class HActivation(HLayerBase, tf.keras.layers.Activation):
+    def __init__(self, activation, beta=0., pre_activation_quantizer_config=None, **kwargs):
+        super().__init__(activation=activation, beta=beta, pre_activation_quantizer_config=pre_activation_quantizer_config, **kwargs)
+
+    def post_build(self, input_shape):
+        if self.pre_activation_quantizer_config['rnd_strategy'] == 'auto':
+            self.pre_activation_quantizer_config['rnd_strategy'] = 'standard_round'
+        super().post_build(input_shape)
+
+    @tf.function(jit_compile=True)
+    def forward(self, x, training=None, record_minmax=None):
+        x = self.activation(x)  # type: ignore
+        return self.pre_activation_quantizer(x, training=training, record_minmax=record_minmax)  # type: ignore
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class HAdd(HLayerBase, _Merge):
+
+    @tf.function(jit_compile=True)
+    def forward(self, inputs, training=None, record_minmax=None):
+        output = inputs[0]
+        for i in range(1, len(inputs)):
+            output += inputs[i]
+        return self.pre_activation_quantizer(output, training=training, record_minmax=record_minmax)  # type: ignore
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0]
+
+
+# class HMultply(HLayerBase, _Merge):
+
+#     @tf.function(jit_compile=True)
+#     def forward(self, inputs, training=None, record_minmax=None):
+#         output = inputs[0]
+#         for i in range(1, len(inputs)):
+#             output *= inputs[i]
+#         return self.pre_activation_quantizer(output, training=training, record_minmax=record_minmax)  # type: ignore
+
+#     def compute_output_shape(self, input_shape):
+#         return input_shape[0]
