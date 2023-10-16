d131a3724f412833919073fa7aef9ee15eb58795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:33 +0000

    Add package render-engine-youtube-embed/2023.10.2a1/render_engine_youtube_embed-2023.10.2a1-py3-none-any.whl

---
+++ packages/render-engine-youtube-embed/render_engine_youtube_embed-2023.10.2a1-py3-none-any.whl/render_engine_youtube_embed/plugins.py
@@ -0,0 +1,10 @@
+import logging
+from render_engine.hookspecs import hook_impl
+from .youtube_embed import replace_youtube_links_with_embeds
+
+class YouTubeEmbed:
+    @hook_impl
+    def pre_build_collection_pages(page: "Page") -> None:
+        if hasattr(page, "content"):
+            content = replace_youtube_links_with_embeds(page.content)    
+            page.content = content
