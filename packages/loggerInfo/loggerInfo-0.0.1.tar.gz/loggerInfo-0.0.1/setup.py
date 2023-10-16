ffbb8ff3fa62c6c63d4b09f26ac8f08daec850af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:52 +0000

    Add package loggerInfo/0.0.1/loggerInfo-0.0.1.tar.gz

---
+++ packages/loggerInfo/loggerInfo-0.0.1.tar.gz/loggerInfo-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="loggerInfo",
+    version="0.0.1",
+    author="Wesley Marques",
+    description="display of logs in project development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+     packages=["app"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[],
+    dependency_links=['https://github.com/wesleymr59/logger']
+)
