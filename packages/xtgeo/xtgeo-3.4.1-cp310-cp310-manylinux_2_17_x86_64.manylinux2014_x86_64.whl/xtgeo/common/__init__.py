4b333da26e224b19942dd37cf9a67b450b090485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:41 +0000

    Add package xtgeo/3.4.1/xtgeo-3.4.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/xtgeo/xtgeo-3.4.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/xtgeo/common/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""The XTGeo common package"""
+
+
+from xtgeo.common.exceptions import WellNotFoundError
+from xtgeo.common.sys import _XTGeoFile, inherit_docstring
+
+# flake8: noqa
+from xtgeo.common.xtgeo_dialog import XTGDescription, XTGeoDialog, XTGShowProgress
