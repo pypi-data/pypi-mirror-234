4c812a984dcc2f7c3eb74863432eb345caae7e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:25 +0000

    Add package yclients-aio-client/0.0.1/yclients_aio_client-0.0.1-py3-none-any.whl

---
+++ packages/yclients-aio-client/yclients_aio_client-0.0.1-py3-none-any.whl/yclients_aio_client/__version__.py
@@ -0,0 +1,14 @@
+author = "akimrx"
+author_email = "akimstrong@yandex.ru"
+license = "MIT"  # pylint: disable=W0622
+description = "Async client for YCLIENTS API"
+url = "https://github.com/akimrx/yclients-aio-client"
+keywords = [
+    "yclients",
+    "async",
+]
+platforms = [
+    "osx",
+    "linux",
+    "windows",
+]
