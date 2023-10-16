f1df741e5b0505fe28151b9d9419284162bd11d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:10:16 +0000

    Add package NorenRestApi/0.0.26/NorenRestApi-0.0.26.tar.gz

---
+++ packages/NorenRestApi/NorenRestApi-0.0.26.tar.gz/NorenRestApi-0.0.26/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from distutils.core import setup
+#python setup.py bdist_wheel --universal
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NorenRestApi",
+    version="0.0.26",
+    author="KumarAnand",
+    author_email="kumar.anand@kambala.co.in",
+    description="A package for NorenOMS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+    "requests==2.28.2",
+    "websocket-client==1.5.1",
+    "pandas==1.5.3",
+    "PyYAML==6.0",
+    ],
+)
+
+
+
