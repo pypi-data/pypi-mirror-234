b31f13629c2b1857930a8a4f79e461075765748c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:21 +0000

    Add package mltemplate/1.0.4/mltemplate-1.0.4-py3-none-any.whl

---
+++ packages/mltemplate/mltemplate-1.0.4-py3-none-any.whl/mltemplate/scripts/template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Creating ML template for $1"
+path_to_template="$2"
+template_name="$3"
+
+cp -r "$path_to_template" .
+mv "$template_name" "$1"
+mv "$1/$template_name" "$1/$1"
+
+# sed definition differs between linux and macos systems: https://stackoverflow.com/a/43453459
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find "$1" -iname "*.py" -type f -exec sed -i '' "s/$template_name/$1/gi" {} \;
+  sed -i '' "s/$template_name/$1/g" "$1"/Makefile "$1"/README.md
+else
+  find "$1" -iname "*.py" -type f -exec sed -i "s/$template_name/$1/gi" {} \;
+  sed -i "s/$template_name/$1/g" "$1"/Makefile "$1"/README.md
+fi
+echo "Voila!"
