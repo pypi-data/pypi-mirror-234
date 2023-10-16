516b4971118fe57594406c113455e31c49053207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:09:00 +0000

    Add package django-tasks-scheduler/1.2.1/django_tasks_scheduler-1.2.1-py3-none-any.whl

---
+++ packages/django-tasks-scheduler/django_tasks_scheduler-1.2.1-py3-none-any.whl/scheduler/tests/test_internals.py
@@ -0,0 +1,17 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
+from scheduler.models import ScheduledTask
+from scheduler.tests.testtools import SchedulerBaseCase, task_factory
+from scheduler.tools import get_scheduled_task
+
+
+class TestInternals(SchedulerBaseCase):
+    def test_get_scheduled_job(self):
+        job = task_factory(ScheduledTask, scheduled_time=timezone.now() - timedelta(hours=1))
+        self.assertEqual(job, get_scheduled_task(job.TASK_TYPE, job.id))
+        with self.assertRaises(ValueError):
+            get_scheduled_task(job.TASK_TYPE, job.id + 1)
+        with self.assertRaises(ValueError):
+            get_scheduled_task('UNKNOWN_JOBTYPE', job.id)
