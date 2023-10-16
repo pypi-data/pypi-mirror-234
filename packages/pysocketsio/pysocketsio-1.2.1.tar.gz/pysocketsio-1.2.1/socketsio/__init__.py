6ca909213f0de27457153d87f1680fd199bf5b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:08:55 +0000

    Add package pysocketsio/1.2.1/pysocketsio-1.2.1.tar.gz

---
+++ packages/pysocketsio/pysocketsio-1.2.1.tar.gz/pysocketsio-1.2.1/socketsio/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+from socketsio.sockets import *
+from socketsio.server import *
+from socketsio.client import *
+from socketsio.protocols import *
+from socketsio.interface import *
+from socketsio.service import *
+from socketsio.communication import *
