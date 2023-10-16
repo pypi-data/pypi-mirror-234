2551c801c28b7ae17b3513a6f80a4a539e7a5733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:33 +0000

    Add package rene/0.1.0/rene-0.1.0-cp39-cp39-musllinux_1_1_aarch64.whl

---
+++ packages/rene/rene-0.1.0-cp39-cp39-musllinux_1_1_aarch64.whl/rene/_hints.py
@@ -0,0 +1,13 @@
+import typing as t
+
+import typing_extensions as te
+
+_Key = t.TypeVar('_Key',
+                 contravariant=True)
+_Value = t.TypeVar('_Value',
+                   covariant=True)
+
+
+class Map(te.Protocol[_Key, _Value]):
+    def __getitem__(self, key: _Key, /) -> _Value:
+        ...
