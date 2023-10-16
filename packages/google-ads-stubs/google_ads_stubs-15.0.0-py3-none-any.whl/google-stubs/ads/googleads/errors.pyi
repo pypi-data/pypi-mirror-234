4998215e9389f869385f2bea4861258806b35315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:45:27 +0000

    Add package google-ads-stubs/15.0.0/google_ads_stubs-15.0.0-py3-none-any.whl

---
+++ packages/google-ads-stubs/google_ads_stubs-15.0.0-py3-none-any.whl/google-stubs/ads/googleads/errors.pyi
@@ -0,0 +1,21 @@
+from typing import Union
+
+import grpc
+
+from google.ads.googleads.v13 import GoogleAdsFailure as GoogleAdsFailureV13
+from google.ads.googleads.v14 import GoogleAdsFailure as GoogleAdsFailureV14
+
+GoogleAdsFailure = Union[GoogleAdsFailureV13, GoogleAdsFailureV14]
+
+class GoogleAdsException(Exception):
+    error: grpc.RpcError = ...
+    call: grpc.Call = ...
+    failure: GoogleAdsFailure = ...
+    request_id: str = ...
+    def __init__(
+        self,
+        error: grpc.RpcError,
+        call: grpc.Call,
+        failure: GoogleAdsFailure,
+        request_id: str,
+    ) -> None: ...
