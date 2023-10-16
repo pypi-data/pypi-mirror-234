e545be45437c5bf708b09022009a38ca41000da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:46:38 +0000

    Add package chic-lib/0.1.1/chic-lib-0.1.1.tar.gz

---
+++ packages/chic-lib/chic-lib-0.1.1.tar.gz/chic-lib-0.1.1/chic/__init__.py
@@ -0,0 +1,12 @@
+"""
+chic.
+
+A Python library for coarse-graining hybrid and inorganic frameworks.
+"""
+
+
+from .structure import Structure
+
+
+__version__ = "0.1.0"
+__author__ = 'Thomas C Nicholas'
