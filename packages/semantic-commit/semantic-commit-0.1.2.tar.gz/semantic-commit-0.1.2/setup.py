efc4894859cfefd010de644f826eb6a954e822b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:09:00 +0000

    Add package semantic-commit/0.1.2/semantic-commit-0.1.2.tar.gz

---
+++ packages/semantic-commit/semantic-commit-0.1.2.tar.gz/semantic-commit-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='semantic-commit',
+    version='0.1.2',
+    description='scommit: ChatGPT-generated commit messages',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Globe-Knowledge-Solutions/semantic-commit',
+    author='Ivan Yevenko',
+    author_email='ivan@globe.engineer',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'tiktoken',
+    ],
+    entry_points={
+        'console_scripts': [
+            'scommit=scommit:scommit',
+        ],
+    },
+)
