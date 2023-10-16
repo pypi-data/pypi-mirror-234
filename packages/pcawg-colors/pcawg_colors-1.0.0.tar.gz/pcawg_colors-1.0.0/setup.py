d9c95ec67831ed635cbd87535190cbada38b9904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:08:57 +0000

    Add package pcawg-colors/1.0.0/pcawg_colors-1.0.0.tar.gz

---
+++ packages/pcawg-colors/pcawg_colors-1.0.0.tar.gz/pcawg_colors-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pcawg_colors',
+    version='1.0.0',    
+    description='Colors used in PCAWG figures',
+    url='https://github.com/sudogene/pcawg_colors',
+    author='Andy Wu',
+    author_email='andywubby@gmail.com',
+    license='BSD 2-clause',
+    packages=['pcawg_colors'],
+    install_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
