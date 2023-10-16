272b5b7fb3ce7257839b6d8a4852508a4ef7b385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:12:29 +0000

    Add package disk_objectstore/1.0.0/disk_objectstore-1.0.0.tar.gz

---
+++ packages/disk_objectstore/disk_objectstore-1.0.0.tar.gz/disk_objectstore-1.0.0/disk_objectstore/__init__.py
@@ -0,0 +1,9 @@
+"""An implementation of an efficient object store that writes directly on disk.
+
+It does not require a server running.
+"""
+from .container import CompressMode, Container, ObjectType
+
+__all__ = ("Container", "ObjectType", "CompressMode")
+
+__version__ = "1.0.0"
