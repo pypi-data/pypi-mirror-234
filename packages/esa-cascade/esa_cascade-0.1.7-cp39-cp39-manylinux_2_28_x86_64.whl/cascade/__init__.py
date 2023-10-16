7872e2315b9f37a5017d24dbfa2da857f4a86182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:46:29 +0000

    Add package esa-cascade/0.1.7/esa_cascade-0.1.7-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/esa-cascade/esa_cascade-0.1.7-cp39-cp39-manylinux_2_28_x86_64.whl/cascade/__init__.py
@@ -0,0 +1,61 @@
+# Copyright 2023 Francesco Biscani (bluescarni@gmail.com), Dario Izzo (dario.izzo@gmail.com)
+#
+# This file is part of the cascade.py library.
+#
+# This Source Code Form is subject to the terms of the Mozilla
+# Public License v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+""":mod:`cascade`
+=================
+
+Classes
+-------
+
+.. autosummary::
+    :toctree: generated/
+    :template: custom-class-template.rst
+
+    sim
+    
+Enums
+---------
+
+.. autosummary::
+    :toctree: generated/
+    
+    outcome
+
+Functions
+---------
+
+.. autosummary::
+    :toctree: generated/
+    
+    set_nthreads
+    get_nthreads
+    set_logger_level_trace
+    set_logger_level_debug
+    set_logger_level_info
+    set_logger_level_warn
+    set_logger_level_err
+    set_logger_level_critical
+
+"""
+
+# Version setup.
+from ._version import __version__
+
+# NOTE: import heyoka to ensure that
+# cascade knows how to convert to/from
+# expressions.
+import heyoka as _hy
+
+# We import core into the root namespace.
+from .core import *
+
+del _hy
+
+# We import the sub-modules.
+import cascade.dynamics
+import cascade.test
