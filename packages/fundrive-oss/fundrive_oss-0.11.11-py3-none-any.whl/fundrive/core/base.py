ff261c7e47e2cc0ce6e9e79201e0fdbd11b449bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:08:08 +0000

    Add package fundrive-oss/0.11.11/fundrive_oss-0.11.11-py3-none-any.whl

---
+++ packages/fundrive-oss/fundrive_oss-0.11.11-py3-none-any.whl/fundrive/core/base.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from typing import Any
+
+
+class FileSystem:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def login(self, *args, **kwargs) -> bool:
+        raise NotImplementedError()
+
+    def mkdir(self, path, *args, **kwargs) -> bool:
+        raise NotImplementedError()
+
+    def delete(self, *args, **kwargs) -> bool:
+        raise NotImplementedError()
+
+    def get_file_list(self, *args, **kwargs) -> list[dict[str, Any]]:
+        raise NotImplementedError()
+
+    def get_dir_list(self, *args, **kwargs) -> list[dict[str, Any]]:
+        raise NotImplementedError()
+
+    def get_file_info(self, *args, **kwargs) -> dict[str, Any]:
+        raise NotImplementedError()
+
+    def get_dir_info(self, *args, **kwargs) -> dict[str, Any]:
+        raise NotImplementedError()
+
+    def download_file(self, dir_path="./cache", overwrite=False, *args, **kwargs) -> bool:
+        raise NotImplementedError()
+
+    def download_dir(self, dir_path="./cache", overwrite=False, *args, **kwargs) -> bool:
+        raise NotImplementedError()
+
+    def upload_file(self, file_path="./cache", overwrite=False, *args, **kwargs) -> bool:
+        raise NotImplementedError()
+
+    def upload_dir(self, dir_path="./cache", overwrite=False, *args, **kwargs) -> bool:
+        raise NotImplementedError()
+
+
+class DriveSnapshot:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def _tar_path(self, file_path):
+        return f"{file_path}-{datetime.now().strftime('%Y%m%d%H%M%S')}.tar"
+
+    def update(self, file_path, *args, **kwargs):
+        pass
+
+    def download(self, dir_path, *args, **kwargs):
+        pass
