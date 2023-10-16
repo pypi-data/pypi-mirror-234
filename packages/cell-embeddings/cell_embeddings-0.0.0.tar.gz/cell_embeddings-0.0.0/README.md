f1eae3eb776bfeb966adec6e84703140fbeb47b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:10:44 +0000

    Add package cell-embeddings/0.0.0/cell_embeddings-0.0.0.tar.gz

---
+++ packages/cell-embeddings/cell_embeddings-0.0.0.tar.gz/cell_embeddings-0.0.0/README.md
@@ -0,0 +1,2 @@
+# cell_embeddings
+Cell embeddings
---
+++ packages/cell-embeddings/cell_embeddings-0.0.0.tar.gz/cell_embeddings-0.0.0/setup.py
@@ -0,0 +1,31 @@
+
+# -- import packages: ---------------------------------------------------------
+import setuptools
+import re
+import os
+import sys
+
+
+# -- run setup: ---------------------------------------------------------------
+setuptools.setup(
+    name="cell_embeddings",
+    version="0.0.0",
+    python_requires=">3.9.0",
+    author="Michael E. Vinyard",
+    author_email="mvinyard.ai@gmail.com",
+    url="https://github.com/mvinyard/cell_embeddings",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="Cell embeddings",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "ABCParse>=0.0.6",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)
