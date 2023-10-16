b66e320fe8e94767a9574362280c328e8e09fe53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:45:26 +0000

    Add package lxmlasdict/0.1.0/lxmlasdict-0.1.0-py3-none-any.whl

---
+++ packages/lxmlasdict/lxmlasdict-0.1.0-py3-none-any.whl/lxmlasdict/__init__.py
@@ -0,0 +1,15 @@
+from lxmlasdict.utils import (
+    from_string,
+    from_file,
+    to_dict
+)
+
+from lxmlasdict.base import (
+    LXMLTreeAsDictReadOnly
+)
+
+
+__author__ = 'Alisher Nazarkhanov'
+__version__ = '0.1.0'
+__license__ = 'MIT'
+__doc__ = '"lxmlasdict" allows you to treat lxml tree elements as if you were working with a dict'
