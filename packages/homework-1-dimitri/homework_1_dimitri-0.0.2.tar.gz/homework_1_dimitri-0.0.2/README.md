03d553e35d269183f38cf83470d608521042db87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:12:43 +0000

    Add package homework-1-dimitri/0.0.2/homework_1_dimitri-0.0.2.tar.gz

---
+++ packages/homework-1-dimitri/homework_1_dimitri-0.0.2.tar.gz/homework_1_dimitri-0.0.2/README.md
@@ -0,0 +1,5 @@
+# Example Package
+
+This is a first homework package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ packages/homework-1-dimitri/homework_1_dimitri-0.0.2.tar.gz/homework_1_dimitri-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+  
+setup( 
+    name='homework_1_dimitri', 
+    version='0.0.2', 
+    description='First Homework', 
+    author='Dimitri Dondoladze', 
+    author_email='dimitri.dondoladze.1@iliauni.edu.ge', 
+    packages=find_packages(),
+    install_requires=[ ], 
+) 
