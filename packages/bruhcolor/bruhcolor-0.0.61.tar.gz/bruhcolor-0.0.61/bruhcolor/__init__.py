070010918f5589103cd9d911ab5d73d1c8cedf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:06:23 +0000

    Add package bruhcolor/0.0.61/bruhcolor-0.0.61.tar.gz

---
+++ packages/bruhcolor/bruhcolor-0.0.61.tar.gz/bruhcolor-0.0.61/bruhcolor/__init__.py
@@ -0,0 +1,17 @@
+from bruhcolor.bruhcolor import ATTRIBUTES, RESET_8, RESET_256, COLORS_8, COLORS_256, HIGHLIGHTS_8, HIGHLIGHTS_256, bruhcolored, color_codes, bruhcolorwrapper, colors, VERSION, valid_effects
+
+__all__ = [
+    "ATTRIBUTES",
+    "COLORS_8",
+    "COLORS_256",
+    "HIGHLIGHTS_8",
+    "HIGHLIGHTS_256",
+    "RESET_8",
+    "RESET_256",
+    "bruhcolored",
+    "bruhcolorwrapper",
+    "colors",
+    "color_codes",
+    "VERSION",
+    "valid_effects"
+]
