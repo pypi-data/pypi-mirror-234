660baeafe964559534c80b6478907fbace2b8181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:05 +0000

    Add package ReplayTables-andnp/5.9.1/ReplayTables-andnp-5.9.1.tar.gz

---
+++ packages/ReplayTables-andnp/ReplayTables-andnp-5.9.1.tar.gz/ReplayTables-andnp-5.9.1/ReplayTables/ingress/MinHeapMapper.py
@@ -0,0 +1,58 @@
+import numpy as np
+from typing import Any, Dict
+from ReplayTables.ingress.IndexMapper import IndexMapper
+from ReplayTables.interface import EID, IDX, EIDs, IDXs
+from ReplayTables._utils.MinMaxHeap import MinMaxHeap
+
+class MinHeapMapper(IndexMapper):
+    def __init__(self, max_size: int):
+        super().__init__(max_size)
+
+        self._heap = MinMaxHeap()
+        self._eid2idx: Dict[EID, IDX] = {}
+        self._idx2eid = np.zeros(max_size, dtype=np.int64)
+
+    def eid2idx(self, eid: EID) -> IDX:
+        return self._eid2idx[eid]
+
+    def eids2idxs(self, eids: EIDs) -> IDXs:
+        idxs: Any = np.array([self.eid2idx(eid) for eid in eids], dtype=np.int64)
+        return idxs
+
+    def add_eid(self, eid: EID, /, **kwargs: Any) -> IDX:
+        # check if priority is given, else assume max
+        if 'priority' in kwargs:
+            p = kwargs['priority']
+        else:
+            p, _ = self._heap.max()
+
+        # when not full, next index is just the current size
+        idx: Any = self._size
+
+        # when full, delete lowest priority
+        if self._size == self._max_size:
+            _, idx = self._heap.min()
+            self._heap.update(p, idx)
+
+            last_eid = self._idx2eid[idx]
+            del self._eid2idx[last_eid]
+
+        else:
+            self._heap.add(p, idx)
+
+        self._eid2idx[eid] = idx
+        self._idx2eid[idx] = eid
+
+        self._size = min(self._size + 1, self._max_size)
+        return idx
+
+    def update_eid(self, eid: EID, /, **kwargs: Any):
+        assert 'priority' in kwargs
+        p = kwargs['priority']
+
+        idx = self._eid2idx[eid]
+        self._heap.update(p, idx)
+
+    def has_eids(self, eids: EIDs):
+        out = [e in self._eid2idx for e in eids]
+        return np.array(out, dtype=np.bool_)
