e1b03c1bd159ad099d8a8d89616ebadc63858b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 01:27:10 +0000

    Add package gdalwrap/1.1.1/gdalwrap-1.1.1.tar.gz

---
+++ packages/gdalwrap/gdalwrap-1.1.1.tar.gz/gdalwrap-1.1.1/README.md
@@ -0,0 +1,89 @@
+After initial excitement about GDAL python bindings possibilities I realized something was wrong, as my code could not work as expected. Fortunately checked that there’s nothing wrong about it but [Python Gotchas](https://gdal.org/api/python_gotchas.html)  
+Summing-up: Python gdal/ogr objects are pointers to [SWIG](https://www.swig.org/) objects and these pointers will be collected by Python's garbage collector earlier than expected in code execution. In practice the problem is it makes writing code tied to a very monolithic approach.  
+After trying alternatives to make it more usable for Python I eventually found a way that's working until now: keeping these pointers busy, allocated. In this case this is done by having key elements (datasources e.g.) 'grabbed' by class objects.  
+This repository is Beta/under construction and contains some basic features and some processing tools. I will use it to keep adding functionality and helper functions for my recurring tasks while working with GIS files.  
+Usage can be checked in the [examples.py](https://github.com/Rodrigo-NH/gdalwrap/blob/main/examples/examples.py) file  
+[Recipe](https://gist.github.com/Rodrigo-NH/94d1fe07646052ad32133824c85b4221) to get all gdal/gdal bindings parts installed and configured in Windows  
+
+## Installation  
+pip install --user gdalwrap
+## Classes/commands
+Core commands (file core.py)  
+
+The gdalwrap idea is to wrap OGR classes into Python classes, circumventing some of the Python gotchas. Respecting OGR higther classes hierarchy (datasource/layers/features/geometries) while permitting easy access to native OGR objects. Considering python is 'just' making reference to OGR object pointers, it's possible to work directly with the OGR objects (using the bindings directly) without breaking the code logic constructed with gdalwrap.  
+
+Example:  
+
+```python
+temps = Datasource(geopackagepath, Action="open rw")  # Open a file
+print(temps.datasource)  # Native OGR datasource object
+newlayer = temps.Newlayer('polygons_1', '4326', Type='polygon')  # Create a new layer
+existinglayer = temps.getlayer('somelayer')  # Pick some existing layer
+print(newlayer.layer)  # Native OGR layer object
+print(existinglayer.layer)  # Native OGR layer object
+somefeature = existinglayer.getfeature(0)  # Random access by FID
+print(somefeature.feature)  # Native OGR feature object
+
+iter = existinglayer.iterfeatures()  # Feature iterator (wraps OGR '.GetNextFeature())'
+for feature in iter:
+	fg = feature.getgeom()
+	print(fg.geom)  # Native OGR geometry object
+```
+You can check the examples.py for usage until a better README arises.
+
+
+## Tools
+Some useful tools. (file tools.py)
+
+
+**Method splithalf:**  
+
+
+```splithalf(<geom>)``` -> Split geom in half and returns a list with resulting geoms
+
+
+**Method layerclip:**
+
+Clips features in a layer and returns resulting feature list. Replicates attribute table values. Doesn't change input layer.  
+
+```layerclip(<layer>, <clipgeom>)``` -> Returns list of output features  
+
+layer -> The input layer to be clipped  
+
+clipgeom -> The geom used as clip mask
+
+
+**Class Layergrid:**   
+
+Creates a grid with the total extent of a given layer. X and Y steps in current map units or total number of tiles. Inherits srs from layer. User inputs 'Xstep' and 'Ystep' will be adjusted (changed) to match layer's extent exactly.
+
+```Layergrid(<layer>, <Xstep>, <Ystep>, [Type='mapunits'])```  
+
+Type=  
+'mapunits' -> Default. Xstep and Ytep in map units  
+'tilenumbers' -> Xstep and Ytep as total number of tiles (e.g. Xstep=4, Ystep=4 for a 16 tiles grid)
+
+*Methods:*  
+
+```.getgrid()``` -> Get a list with all grid geoms   
+
+
+```.gridindex()``` -> Get a string list with grid index in the format "xi_yi"  
+
+```.getsrs()``` -> Get grid's associated SRS
+
+**Method splitrings:**  
+
+Removes rings from feature but keeping overall aspect (a polygon with one ring will be transformed to two polygons respecting the empty space of the ring). Returns a list of features. replicates source attributes.  
+
+```splitrings(<feature>)``` -> Returns a list of resulting features
+
+![N|Solid](https://github.com/Rodrigo-NH/assets/blob/main/img/removerings.png)  
+
+**Method splitvertices:** 
+
+Split features based on max number of vertices threshold.  
+
+```splitvertices(<feature>, <threshold>)``` -> Returns a list of resulting features  
+
+ 
---
+++ packages/gdalwrap/gdalwrap-1.1.1.tar.gz/gdalwrap-1.1.1/src/gdalwrap/tools.py
@@ -0,0 +1,200 @@
+try:
+	from osgeo import ogr
+	from osgeo import osr
+except ImportError as error:
+	raise Exception("""ERROR: Could not find the GDAL/OGR Python library bindings.""")
+
+ogr.UseExceptions()
+osr.UseExceptions()
+
+from gdalwrap.core import *
+from gdalwrap.core import _g2b, _b2g
+
+
+
+def layerclip(layer, clipgeom):
+	sch = []
+	schema = layer.schema
+	for reg in schema:
+		fieldName = reg.GetName()
+		fieldType = reg.GetTypeName()
+		sch.append([fieldName, fieldType])
+
+	features = []
+	defn = layer.GetLayerDefn()
+	layer.SetSpatialFilter(clipgeom)
+	for f in layer:
+		d = f.GetGeometryRef()
+		cut = clipgeom.Intersection(d)
+		feature = ogr.Feature(defn)
+		feature.SetGeometry(cut)
+		for field in sch:
+			fvalue = f.GetField(field[0])
+			feature.SetField(field[0], fvalue)
+		features.append(feature)
+	return features
+
+
+def splitvertices(feature, vcount):
+	featpoll = []
+	if vcount < 3:
+		featpoll.append(feature)
+	else:
+		geom = feature.GetGeometryRef()
+		g = _g2b(geom)
+		temppoll = []
+		temppoll.append(g)
+		polcoll = []
+		strange = [] # Will lose SWIG object reference in debug mode after .GetGeometryType(), if not appended
+							# (like pointer having a short lifetime or something else)
+
+		while len(temppoll) > 0:
+			ct = temppoll.pop(0)
+			el = None
+			el = _b2g(ct)
+			# strange.append(el)
+			gl = multi2list(el)
+			for geom in gl:
+				gcount = geomptcount(geom)
+				if gcount > vcount:
+					# TS = True
+					gsplit = splithalf(geom)
+					temppoll.append(_g2b(gsplit[0]))
+					temppoll.append(_g2b(gsplit[1]))
+				else:
+					polcoll.append(_g2b(geom))
+
+		for each in polcoll:
+			geom = _b2g(each)
+			dfn = feature.GetDefnRef()
+			ofeature = ogr.Feature(dfn)
+			ofeature.SetGeometry(geom)
+			for f in range(0, feature.GetFieldCount()):
+				ft = feature.GetField(f)
+				ofeature.SetField(f, ft)
+			featpoll.append(ofeature)
+
+	return featpoll
+
+
+def splitrings(feature):
+	geom = feature.GetGeometryRef()
+	igeom = _g2b(geom)
+	tg = _b2g(igeom)
+	typ = tg.GetGeometryType()
+	featpoll = []
+	if typ == 6 or typ == 3:
+		polcoll = []
+		temppoll = []
+		geom = feature.GetGeometryRef().ExportToWkb()
+		temppoll.append(geom)
+		while len(temppoll) > 0:
+			current = temppoll.pop(0)
+			igeom = ogr.CreateGeometryFromWkb(current)
+			gtype = igeom.GetGeometryType()
+			if gtype == 6: #Clip operation may create multipolygon
+				for geom in igeom:
+					temppoll.append(geom.ExportToWkb())
+				current = temppoll.pop()
+			geom = ogr.CreateGeometryFromWkb(current)
+			hasring = geom.GetGeometryCount()
+			if hasring > 1:
+				outer = geom.GetGeometryRef(0)
+				inner = geom.GetGeometryRef(1)
+				outerextent = outer.GetEnvelope()
+				innerextent = inner.GetEnvelope()
+				Xhalf = innerextent[0] + (innerextent[1] - innerextent[0]) / 2
+				Xstart = outerextent[0]
+				Xend = outerextent[1]
+				Ystart = outerextent[2]
+				Yend = outerextent[3]
+				leftcutcoords = [(Xstart, Ystart), (Xhalf, Ystart), (Xhalf, Yend), (Xstart, Yend)]
+				rightcutcoords = [(Xhalf, Ystart), (Xend, Ystart), (Xend, Yend), (Xhalf, Yend)]
+				rightcutpol = makepol(rightcutcoords)
+				leftcutpol = makepol(leftcutcoords)
+				result = [leftcutpol.Intersection(geom),rightcutpol.Intersection(geom)]
+				for each in result:
+					if each.GetGeometryCount() == 1:
+						polcoll.append(each.ExportToWkb())
+					else:
+						temppoll.append(each.ExportToWkb())
+			else:
+				polcoll.append(current)
+
+		for each in polcoll:
+			dfn = feature.GetDefnRef()
+			ofeature = ogr.Feature(dfn)
+			ofeature.SetGeometry(ogr.CreateGeometryFromWkb(each))
+			for f in range(0, feature.GetFieldCount()):
+				ft = feature.GetField(f)
+				ofeature.SetField(f, ft)
+			featpoll.append(ofeature)
+	else:
+		featpoll.append(feature)
+
+	return featpoll
+
+
+class Layergrid:
+	def __init__(self, layer,xsteps, ysteps, Type='mapunits'):
+		self.type = Type
+		self.layer = layer
+		self.xsteps = xsteps
+		self.ysteps = ysteps
+		self.grid = []
+		self.gridindex = []
+		self.srsv = layer.GetSpatialRef()
+
+		extent = layer.GetExtent()
+		totalX = abs(extent[0] - extent[1])
+		totalY = abs(extent[2] - extent[3])
+		if self.type.upper() == 'MAPUNITS':
+			divX = int(round(totalX / xsteps))
+			divY = int(round(totalY / ysteps))
+		elif self.type.upper() == 'TILENUMBERS':
+			divX = xsteps
+			divY = ysteps
+
+		Xstep = totalX / divX
+		Ystep = totalY / divY
+		Xpos = extent[0]
+		Ypos = extent[3]
+		# gridcount = divX * divY
+		# print(str(gridcount) + ' TILES')
+		xindex = 0
+		yindex = 0
+		for Ytile in range(0, divY):
+			for Xtile in range(0, divX):
+				coords = [[Xpos, Ypos], [Xpos + Xstep, Ypos], [Xpos + Xstep, Ypos - Ystep], [Xpos, Ypos - Ystep]]
+				index = str(xindex) + '_' + str(yindex)
+				self.gridindex.append(index)
+				self.grid.append(makepol(coords))
+				Xpos = Xpos + Xstep
+				xindex += 1
+			Xpos = extent[0]
+			Ypos = Ypos - Ystep
+			yindex += 1
+			xindex = 0
+
+	def getgrid(self):
+		return self.grid
+
+	def gridindex(self, index):
+		return self.gridindex[index]
+
+	def getspatialref(self):
+		return self.srsv
+
+def scanfiles(folder, extension):
+	files = []
+	filesout = []
+	for (dirpath, dirnames, filenames) in os.walk(folder):
+		files.extend(filenames)
+		break
+	for each in files:
+		basename = os.path.basename(each)
+		sp = basename.split('.')
+		if sp[1].upper() == extension.upper() and len(sp) == 2:
+			cp = os.path.join(folder, each)
+			filesout.append(cp)
+	return filesout
