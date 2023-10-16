48a65a65d5b3924b16ae87bf33cef569c3bebbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:37 +0000

    Add package uvabot/0.0.2/uvabot-0.0.2-py3-none-any.whl

---
+++ packages/uvabot/uvabot-0.0.2-py3-none-any.whl/uvabot/tradier.py
@@ -0,0 +1,24 @@
+import os;
+import requests;
+import dotenv;
+
+dotenv.load_dotenv();
+
+class Tradier:
+	def __init__ (self, account_number, auth_token):
+
+		#
+		# Define account credentials
+		#
+
+		self.ACCOUNT_NUMBER 	= account_number;
+		self.AUTH_TOKEN 		= auth_token;
+		self.REQUESTS_HEADERS 	= {'Authorization':'Bearer {}'.format(self.AUTH_TOKEN), 'Accept':'application/json'}
+
+		
+		#
+		# Define base url for paper trading and individual API endpoints
+		#
+
+		self.SANDBOX_URL = 'https://sandbox.tradier.com';
+
