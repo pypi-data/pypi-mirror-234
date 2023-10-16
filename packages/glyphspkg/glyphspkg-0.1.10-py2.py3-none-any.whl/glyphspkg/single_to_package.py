ed76f722a31b2d7647ddc0a12d5df98fd4a2307d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:09:56 +0000

    Add package glyphspkg/0.1.10/glyphspkg-0.1.10-py2.py3-none-any.whl

---
+++ packages/glyphspkg/glyphspkg-0.1.10-py2.py3-none-any.whl/glyphspkg/single_to_package.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+from typing import Optional
+
+
+def single_to_package(
+    input_path: Path, output_path: Optional[Path] = None
+) -> Path:
+    raise NotImplementedError
