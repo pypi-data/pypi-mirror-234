4f232b127cd2eddf94ede2fa6011455637383448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:07:45 +0000

    Add package probdistributions-gb/1.1/probdistributions_gb-1.1.tar.gz

---
+++ packages/probdistributions-gb/probdistributions_gb-1.1.tar.gz/probdistributions_gb-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='probdistributions_gb',
+      version='1.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['probdistributions_gb'],
+      author = 'Maitri Banga',
+      zip_safe=False)
