ed32f1a496913d508d95e861addeb303ba276cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:09:44 +0000

    Add package komaru-API/0.11.post20231009/komaru_API-0.11.post20231009.tar.gz

---
+++ packages/komaru-API/komaru_API-0.11.post20231009.tar.gz/komaru_API-0.11.post20231009/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='komaru_API',
+      version='0.1',
+      description='GET RANDOM KOMARU FOR FAST',
+      packages=['komaru_API'],
+      author_email='itzskyreed@gmail.com',
+      zip_safe=False)
