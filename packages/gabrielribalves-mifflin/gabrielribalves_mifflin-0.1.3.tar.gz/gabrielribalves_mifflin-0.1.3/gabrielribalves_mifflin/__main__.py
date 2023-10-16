c2094b9e0055f3392bd8777884eb194ca3e37139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:57 +0000

    Add package gabrielribalves-mifflin/0.1.3/gabrielribalves_mifflin-0.1.3.tar.gz

---
+++ packages/gabrielribalves-mifflin/gabrielribalves_mifflin-0.1.3.tar.gz/gabrielribalves_mifflin-0.1.3/gabrielribalves_mifflin/__main__.py
@@ -0,0 +1,4 @@
+from gabrielribalves_mifflin.cli import main
+
+if __name__ == "__main__":
+    main()
---
+++ packages/gabrielribalves-mifflin/gabrielribalves_mifflin-0.1.3.tar.gz/gabrielribalves_mifflin-0.1.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+def myRead(*paths):
+    """Read the contents of a text file safely.
+    >>> read("dundie", "VERSION ")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+    rootPath = os.path.dirname(__file__)
+    filePath = os.path.join(rootPath, *paths)
+    with open(filePath) as file_:
+        return file_.read().strip()
+    
+def readRequirements(path):
+    return [
+        line.strip()
+        for line in myRead(path).split("\n")
+        if not line.startswith(("#","git+",'"',"-"))
+    ]
+
+setup(
+    name="gabrielribalves_mifflin",
+    version="0.1.3",
+    description="Reward Point System for Dunder Mifflin",
+    long_description=myRead("README.md"),
+    long_description_content_type="text/markdown",
+    author="Gabriel Goncalves",
+    packages=find_packages(exclude=["integration"]),
+    include_package_data=True,
+    entry_points={
+        "console_scripts":["gabrielribalves_mifflin = dundie.__main__:main"]
+    },
+    install_requires=readRequirements("requirements.txt"),
+    extras_require={
+        "test": readRequirements("requirements.test.txt"),
+        "dev": readRequirements("requirements.dev.txt")
+    }
+)
