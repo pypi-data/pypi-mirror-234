b45c56b9847e20350b010675a9113fce5edd9f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 18:06:53 +0000

    Add package pwm-tool3/0.3.3/pwm-tool3-0.3.3.tar.gz

---
+++ packages/pwm-tool3/pwm-tool3-0.3.3.tar.gz/pwm-tool3-0.3.3/setup.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+
+from setuptools import setup, find_packages
+
+PACKAGE = "pwm-tool3"
+NAME = "pwm-tool3"
+DESCRIPTION = "password manager tool"
+AUTHOR = "lovedboy"
+AUTHOR_EMAIL = "lovedboy.tk@qq.com"
+URL = "https://github.com/ls0f/pwm"
+VERSION = '0.3.3'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="BSD",
+    url=URL,
+    package_data={'': ['*.txt', '*.TXT']},
+    include_package_data=True,
+    scripts=['bin/pwm'],
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    zip_safe=False,
+)
