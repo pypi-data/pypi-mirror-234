0fa3b19dbc2e0c2897484c4a14c6c2682bd91bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:40 +0000

    Add package nnsvs/0.1.1/nnsvs-0.1.1.tar.gz

---
+++ packages/nnsvs/nnsvs-0.1.1.tar.gz/nnsvs-0.1.1/nnsvs/data/__init__.py
@@ -0,0 +1,15 @@
+from .data_source import (
+    DurationFeatureSource,
+    MelF0AcousticSource,
+    MusicalLinguisticSource,
+    TimeLagFeatureSource,
+    WORLDAcousticSource,
+)
+
+__all__ = [
+    "DurationFeatureSource",
+    "MusicalLinguisticSource",
+    "TimeLagFeatureSource",
+    "WORLDAcousticSource",
+    "MelF0AcousticSource",
+]
