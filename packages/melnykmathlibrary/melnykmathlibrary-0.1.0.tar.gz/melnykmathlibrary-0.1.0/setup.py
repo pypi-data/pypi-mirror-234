f8890e29cafbe56a8abeff99395c6b618dee42d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:12:23 +0000

    Add package melnykmathlibrary/0.1.0/melnykmathlibrary-0.1.0.tar.gz

---
+++ packages/melnykmathlibrary/melnykmathlibrary-0.1.0.tar.gz/melnykmathlibrary-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='melnykmathlibrary',
+    packages=find_packages(include=['melnykmathlibrary']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Melnyk Iryna',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
