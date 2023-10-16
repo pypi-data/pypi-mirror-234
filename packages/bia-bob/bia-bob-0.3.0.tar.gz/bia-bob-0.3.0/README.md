8f83dcce48b7fa2d901fe41b4c7e7e5617f13738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:52 +0000

    Add package bia-bob/0.3.0/bia-bob-0.3.0.tar.gz

---
+++ packages/bia-bob/bia-bob-0.3.0.tar.gz/bia-bob-0.3.0/README.md
@@ -0,0 +1,61 @@
+# bia-bob
+
+BIA `bob` is a Jupyter-based assistant for interacting with data using generated python code. It is based on [OpenAI's API](https://openai.com/blog/openai-api). You need an openai API account to use it.
+
+![img.png](https://github.com/haesleinhuepf/bia-bob/raw/main/docs/images/screencast.gif)
+
+## Usage
+
+You can initialize Bob like this:
+```
+from bia_bob import bob
+```
+
+Afterwards, you can ask Bob questions like this:
+```
+%bob Load blobs.tif and show it
+```
+
+It will then respond with a python code snippet that you can execute ([see full example](https://github.com/haesleinhuepf/bia-bob/blob/main/demo/analysis_workflow.ipynb)):
+
+```
+from skimage.io import imread
+import stackview
+
+image = imread('blobs.tif')
+stackview.insight(image)
+```
+
+![img.png](https://github.com/haesleinhuepf/bia-bob/raw/main/docs/images/load_and_show_blobs.png)
+
+## Known issues
+
+If you want to ask `bob` a question, you need to put a space before the `?`.
+
+```
+%bob What do you know about blobs.tif ?
+```
+
+## Installation
+
+You can install `bia-bob` using pip. it is recommended to install it into via conda/mamba environment. If you have never used conda before, please [read this guide first](https://biapol.github.io/blog/mara_lampert/getting_started_with_mambaforge_and_python/readme.html).  
+
+```
+mamba create --name bt39 python=3.9 git
+mamba activate bt39
+pip install bia-bob
+```
+
+## Similar projects
+
+There are similar projects offering LLM-based support in Jupyter notebooks:
+* [jupyter-ai](https://github.com/jupyterlab/jupyter-ai)
+
+## Issues
+
+If you encounter any problems or want to provide feedback or suggestions, please create a thread on [image.sc](https://image.sc) along with a detailed description and tag [@haesleinhuepf].
+
+
+
+
+
---
+++ packages/bia-bob/bia-bob-0.3.0.tar.gz/bia-bob-0.3.0/src/bia_bob/_machinery.py
@@ -0,0 +1,95 @@
+from IPython.core.getipython import get_ipython
+from IPython.core.magic import register_line_cell_magic
+from IPython.display import display
+
+from ._utilities import generate_response_to_user, output_text, keep_available_packages
+
+
+class Context:
+    assistant = None
+    variables = None
+    verbose = False
+    chat = []
+    libraries = keep_available_packages([
+        "scikit-image",
+        "numpy",
+        "scipy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn",
+        "stackview",
+        "torch",
+        "pytorch-lightning",
+        "cellpose",
+        "stardist",
+        "n2v",
+        "pyclesperanto-prototype",
+        "apoc",
+        "napari-segment-blobs-and-things-with-membranes",
+        "napari-simpleitk-image-processing",
+        "napari-skimage-regionprops",
+
+        # to add libraries here, add their pypi package names (not their import names)
+    ])
+
+
+@register_line_cell_magic
+def bob(line: str = None, cell: str = None):
+    """Sends a prompt to openAI
+    and shows the text and code response
+    and pastes the code into the next cell.
+    """
+
+    if Context.assistant is None:
+        init_assistant()
+
+    user_input = combine_user_input(line, cell)
+    if user_input is None:
+        display("Please ask a question!")
+        return
+
+    # set context variables
+    Context.variables = get_ipython().user_ns
+
+    # generate the response
+    Context.assistant.generate_response_to_user(user_input)
+
+
+def combine_user_input(line, cell):
+    if line and cell:
+        user_input = line + "\n" + cell
+    elif line:
+        user_input = line
+    elif cell:
+        user_input = cell
+    else:
+        user_input = None
+    return user_input
+
+
+class CustomAgent:
+    def __init__(self, model="gpt-3.5-turbo", temperature=0):
+        self.model = model
+        self.temperature = temperature
+
+    def generate_response_to_user(self, user_input: str):
+        """Sends a prompt to openAI
+        and shows the text response
+        and pastes the code into the next cell.
+        """
+        code, text = generate_response_to_user(self.model, user_input)
+
+        output_text(text)
+
+        if code is not None:
+            get_ipython().set_next_input(code, replace=False)
+
+
+def init_assistant(model="gpt-3.5-turbo", temperature=0):
+    Context.assistant = CustomAgent(model, temperature)
+    if Context.verbose:
+        print("Assistant initialised. You can now use it, e.g., copy and paste the"
+          "below two lines into the next cell and execute it."
+          "\n\n%%bob"
+          "\nplease generate a noisy grayscale image containing 10 blurry blobs with a diameter of 20 pixels each.")
