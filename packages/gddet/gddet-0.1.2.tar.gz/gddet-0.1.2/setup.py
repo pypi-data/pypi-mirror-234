e5f83d64684821bba0c76622852eccbf0c46f693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:09:05 +0000

    Add package gddet/0.1.2/gddet-0.1.2.tar.gz

---
+++ packages/gddet/gddet-0.1.2.tar.gz/gddet-0.1.2/setup.py
@@ -0,0 +1,16 @@
+# ENV: samlabel
+from setuptools import find_packages, setup
+
+
+if __name__ == "__main__":
+    print("Building wheel...")
+
+    setup(
+        name="gddet",
+        version='0.1.2',
+        author="H-AI",
+        url="https://github.com/IDEA-Research/GroundingDINO",
+        description="open-set object detector",
+        license="Apache License V2.0",
+        packages=find_packages(),
+    )
