d5de8a99176d0bc7e8e95975a17282df5854d0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:06:54 +0000

    Add package diploma-generator/1.0.2/diploma_generator-1.0.2.tar.gz

---
+++ packages/diploma-generator/diploma_generator-1.0.2.tar.gz/diploma_generator-1.0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="diploma_generator",
+    version='1.0.2',
+    author="Andrés Alonzo",
+    description="Generate diplomas using images and CSV Files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'fpdf==1.7.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'diploma_generator=diploma_generator.main:main',
+            'diploma_generator_csv=diploma_generator.main:main_csv',
+        ],
+    },
+    package_data={
+        "diploma_generator": ["font.ttf"],
+    },
+    include_package_data=True
+)
