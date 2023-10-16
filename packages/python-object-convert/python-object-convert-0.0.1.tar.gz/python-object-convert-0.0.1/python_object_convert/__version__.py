ba8771c729e5a132d3e8f2c6dbb3d6518185dd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:10:10 +0000

    Add package python-object-convert/0.0.1/python-object-convert-0.0.1.tar.gz

---
+++ packages/python-object-convert/python-object-convert-0.0.1.tar.gz/python-object-convert-0.0.1/python_object_convert/__version__.py
@@ -0,0 +1,9 @@
+__title__ = "python-object-convert"
+__version__ = "0.0.1"
+__description__ = "Convert python objects to camel case and snake case."
+__author__ = "Mohammad Foysal"
+__url__ = "https://github.com/iamfoysal/python_object_convert"
+__license__ = "MIT"
+__keywords__ = [
+   "python", "object", "convert", "camel", "snake", "case", "keys", "to", "camel", "case", 
+]
