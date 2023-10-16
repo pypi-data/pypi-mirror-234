c23c404cbc3666adc6430dde7b852002f8c2b7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:09:18 +0000

    Add package nimblephysics/0.10.9/nimblephysics-0.10.9-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/nimblephysics/nimblephysics-0.10.9-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/nimblephysics_libs/_nimblephysics.pyi
@@ -0,0 +1,21 @@
+"""nimblephysics: Python API of Nimble"""
+import nimblephysics_libs._nimblephysics
+import typing
+
+__all__ = [
+    "biomechanics",
+    "collision",
+    "common",
+    "constraint",
+    "dynamics",
+    "math",
+    "neural",
+    "performance",
+    "realtime",
+    "server",
+    "simulation",
+    "trajectory",
+    "utils"
+]
+
+
