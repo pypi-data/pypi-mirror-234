edb8f451f79e3e2cec04dbeb12420561081584d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 21:06:16 +0000

    Add package r-b-a/0.0.1/r_b_a-0.0.1-py3-none-any.whl

---
+++ packages/r-b-a/r_b_a-0.0.1-py3-none-any.whl/r_b_a/_robust_bias_aware_.py
@@ -0,0 +1,9 @@
+
+import robust_bias_aware
+import argparse
+import warnings
+# import pandas as pd
+    # for key, value in kwargs.items():
+    # print(kwargs)
+
+robust_bias_aware.robust_bias_aware(seeds='./data/data-case-study-1-covid-19/covid-19-seeds.txt', outfile='covid19.graphml')
