ce0e427b3a149d7eec2045a4359b085aa09456a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:11:44 +0000

    Add package moscot/0.3.3/moscot-0.3.3-py3-none-any.whl

---
+++ packages/moscot/moscot-0.3.3-py3-none-any.whl/moscot/problems/generic/__init__.py
@@ -0,0 +1,4 @@
+from moscot.problems.generic._generic import GWProblem, SinkhornProblem
+from moscot.problems.generic._mixins import GenericAnalysisMixin
+
+__all__ = ["GWProblem", "SinkhornProblem", "GenericAnalysisMixin"]
