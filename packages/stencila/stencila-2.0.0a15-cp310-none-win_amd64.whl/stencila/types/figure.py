28efed5641aa8182fac06987a48343747e28f6c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:46:29 +0000

    Add package stencila/2.0.0a15/stencila-2.0.0a15-cp310-none-win_amd64.whl

---
+++ packages/stencila/stencila-2.0.0a15-cp310-none-win_amd64.whl/stencila/types/figure.py
@@ -0,0 +1,21 @@
+# Generated file; do not edit. See the Rust `schema-gen` crate.
+
+from .prelude import *
+
+from .blocks_or_str import BlocksOrStr
+from .creative_work import CreativeWork
+
+
+@dataclass(kw_only=True, frozen=True)
+class Figure(CreativeWork):
+    """
+    Encapsulates one or more images, videos, tables, etc, and provides captions and labels for them.
+    """
+
+    type: Literal["Figure"] = field(default="Figure", init=False)
+
+    label: Optional[str] = None
+    """A short label for the figure."""
+
+    caption: Optional[BlocksOrStr] = None
+    """A caption for the figure."""
