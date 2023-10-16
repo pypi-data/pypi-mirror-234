795e8aeccbacd4e882d4e4af2f8ca2b4f4c3a549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:09:35 +0000

    Add package swarms/1.7.9/swarms-1.7.9-py3-none-any.whl

---
+++ packages/swarms/swarms-1.7.9-py3-none-any.whl/swarms/__init__.py
@@ -0,0 +1,24 @@
+# swarms
+from swarms import agents
+from swarms.swarms.orchestrate import Orchestrator
+from swarms import swarms
+from swarms import structs
+from swarms import models
+from swarms.workers.worker import Worker
+from swarms import workers
+from swarms.logo import logo2
+
+print(logo2)
+
+# worker
+
+# boss
+# from swarms.boss.boss_node import Boss
+
+# models
+
+# structs
+
+# swarms
+
+# agents
