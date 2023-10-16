779e7bd506f064fca9732c719899e2cc2d28e9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 23:10:29 +0000

    Add package virtool-core/7.1.2/virtool_core-7.1.2.tar.gz

---
+++ packages/virtool-core/virtool_core-7.1.2.tar.gz/virtool_core-7.1.2/virtool_core/models/user.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import List, Optional
+
+from virtool_core.models.basemodel import BaseModel
+from virtool_core.models.group import Permissions, GroupMinimal
+from virtool_core.models.roles import AdministratorRole
+from virtool_core.models.searchresult import SearchResult
+
+
+class UserB2C(BaseModel):
+    display_name: Optional[str]
+    family_name: Optional[str]
+    given_name: Optional[str]
+    oid: str
+
+
+class UserNested(BaseModel):
+    id: str
+    administrator: bool
+    handle: str
+
+
+class UserMinimal(UserNested):
+    active: bool
+    b2c: Optional[UserB2C]
+    b2c_display_name: Optional[str]
+    b2c_family_name: Optional[str]
+    b2c_given_name: Optional[str]
+    b2c_oid: Optional[str]
+
+
+class User(UserMinimal):
+    force_reset: bool
+    groups: List[GroupMinimal]
+    last_password_change: datetime
+    permissions: Permissions
+    primary_group: Optional[GroupMinimal]
+    administrator_role: Optional[AdministratorRole]
+
+
+class UserSearchResult(SearchResult):
+    items: List[User]
