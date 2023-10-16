e6600c077cf2f0a0646ad6bf4fe25e3414d1df16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:09 +0000

    Add package fx-py-sdk/0.6.5/fx_py_sdk-0.6.5-py3-none-any.whl

---
+++ packages/fx-py-sdk/fx_py_sdk-0.6.5-py3-none-any.whl/fxsdk/wallet/address.py
@@ -0,0 +1,22 @@
+import bech32
+
+DEFAULT_BECH32_PREFIX = "fx"
+
+
+class Address:
+    def __init__(self, addr, prefix: str = DEFAULT_BECH32_PREFIX):
+        self._prefix = prefix
+        if type(addr) == str:
+            _, data = bech32.bech32_decode(addr)
+            self._data = data
+        elif type(addr) == bytes:
+            self._data = bech32.convertbits(addr, 8, 5)
+        else:
+            raise Exception('invalid address')
+
+    def to_string(self, prefix: str = None) -> str:
+        prefix = self._prefix if prefix is None else prefix
+        return bech32.bech32_encode(prefix, self._data)
+
+    def to_bytes(self) -> bytes:
+        return bytes(bech32.convertbits(self._data, 5, 8, False))
