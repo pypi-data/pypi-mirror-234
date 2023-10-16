3865b3753a8d801ec45345c006cd5f145665b1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:40 +0000

    Add package gwlandscape-python/0.5.0/gwlandscape_python-0.5.0-py3-none-any.whl

---
+++ packages/gwlandscape-python/gwlandscape_python-0.5.0-py3-none-any.whl/gwlandscape_python/settings.py
@@ -0,0 +1,3 @@
+GWLANDSCAPE_ENDPOINT = 'https://gwlandscape.org.au/compas/graphql'
+GWLANDSCAPE_AUTH_ENDPOINT = 'https://gwlandscape.org.au/auth/graphql'
+GWLANDSCAPE_FILE_DOWNLOAD_ENDPOINT = 'https://gwlandscape.org.au/compas/file_download/?fileId='
