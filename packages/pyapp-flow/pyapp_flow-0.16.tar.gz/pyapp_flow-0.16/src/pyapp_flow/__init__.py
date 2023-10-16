f1961ee2b5a04c6cf6a70c2891b9487dbe96dc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:09:14 +0000

    Add package pyapp-flow/0.16/pyapp_flow-0.16.tar.gz

---
+++ packages/pyapp-flow/pyapp_flow-0.16.tar.gz/pyapp_flow-0.16/src/pyapp_flow/__init__.py
@@ -0,0 +1,111 @@
+"""Application Workflow"""
+from typing_extensions import Self
+
+from . import errors as exceptions, steps
+from .datastructures import WorkflowContext, Navigable, Branches
+from .functions import extract_inputs, skip_step, call_nodes
+from .nodes import (
+    Node,
+    step,
+    inline,
+    Step,
+    SetVar,
+    ForEach,
+    CaptureErrors,
+    Conditional,
+    If,
+    FeatureEnabled,
+    Switch,
+    LogMessage,
+    Append,
+    TryExcept,
+    TryUntil,
+    Group,
+)
+
+
+class Nodes(Group):
+    """A series of nodes to be executed on call."""
+
+    __slots__ = ()
+
+    def __call__(self, context: WorkflowContext):
+        with context:
+            self._execute(context)
+
+    @property
+    def name(self) -> str:
+        return f"⏬ {type(self).__name__}"
+
+
+class Workflow(Nodes):
+    """A collection of Nodes that make up a workflow.
+
+    :param name: The name of the workflow
+    :param description: An optional description (similar to doc text) for the
+        workflow.
+    """
+
+    __slots__ = ("_name", "description")
+
+    def __init__(self, name: str, description: str = None):
+        super().__init__()
+        self._name = name
+        self.description = description
+
+    def __call__(self, context: WorkflowContext):
+        context.info("⏩ Workflow: `%s`", context.format(self._name))
+        with context:
+            self._execute(context)
+
+    @property
+    def name(self):
+        return self._name
+
+    def execute(
+        self, context: WorkflowContext = None, *, dry_run: bool = False, **context_vars
+    ) -> WorkflowContext:
+        """Execute workflow.
+
+        This is the main way to trigger a work flow.
+
+        :param context: Optional context; a new one will be created if not supplied.
+        :param dry_run: Flag used to skip steps that have side effects.
+        :param context_vars: Key/Value pairs to initialise the context with.
+        :return: The context used to execute the workflow
+
+        """
+        context = context or WorkflowContext(dry_run=dry_run)
+        context.state.update(context_vars)
+        context.info("⏩ Workflow: `%s`", self._name)
+        self._execute(context)
+        return context
+
+    def nodes(self, *nodes_: Node) -> Self:
+        """Append additional node(s) into the node list.
+
+        :param nodes_: Nodes to append to the current block
+        :return: Returns self; fluent interface
+        """
+        self._nodes.extend(nodes_)
+        return self
+
+    def nested(self, *nodes_: Node) -> Self:
+        """Add nested node(s), nested nodes have their own scope.
+
+        :param nodes_: Collection of nodes call from nested block.
+        :return: Returns self; fluent interface
+
+        """
+        self._nodes.append(Nodes(*nodes_))
+        return self
+
+    def set_vars(self, **kwargs) -> Self:
+        """Set variables to a particular value.
+
+        :param kwargs: Key/Value pairs to update in the context
+        :return: Returns self; fluent interface
+
+        """
+        self._nodes.append(SetVar(**kwargs))
+        return self
---
+++ packages/pyapp-flow/pyapp_flow-0.16.tar.gz/pyapp_flow-0.16/tests/unit/test_flow.py
@@ -0,0 +1,100 @@
+from typing import List
+from unittest.mock import ANY
+
+import pyapp_flow as flow
+
+
+@flow.step(output="arg_t")
+def add_args(arg_1: int, arg_2: int) -> int:
+    return arg_1 + arg_2
+
+
+def add_message(msg: str):
+    @flow.step
+    def message(context: flow.WorkflowContext, messages: List[str]):
+        messages.append(context.format(msg))
+
+    return message
+
+
+def raise_error(exception):
+    @flow.step
+    def error():
+        raise exception
+
+    return error
+
+
+basic_flow = flow.Nodes(add_args, add_message)
+
+
+class TestNodes:
+    def test_str(self):
+        assert str(basic_flow) == "⏬ Nodes"
+
+    def test_branches(self):
+        actual = basic_flow.branches()
+
+        assert actual == {"": [ANY, ANY]}
+
+
+sub_flow = flow.Workflow(name="Sub Flow").nodes(
+    add_message("sub_flow"),
+)
+
+
+sample_flow = (
+    flow.Workflow(name="Sample Flow")
+    .set_vars(
+        messages=[],
+        arg_1=13,
+        arg_2=42,
+    )
+    .nodes(
+        add_args,
+        add_message("single"),
+        add_message("{arg_t:03d}"),
+        sub_flow,
+    )
+    .nested(add_message("nested"))
+    .nodes(
+        flow.CaptureErrors("errors", try_all=True,).nodes(
+            raise_error(ValueError("Error A")),
+            raise_error(ValueError("Error B")),
+        ),
+        flow.If(lambda ctx: ctx.state["arg_1"] < ctx.state["arg_2"]).true(
+            add_message("arg_1 is smaller")
+        ),
+        flow.ForEach("error", in_var="errors", loop_label="error={error}").loop(
+            flow.LogMessage("{error}"), add_message("{error}")
+        ),
+        flow.Switch("arg_1")
+        .case(
+            13,
+            add_message("it's 13"),
+        )
+        .case(
+            42,
+            add_message("it's 42"),
+        ),
+    )
+)
+
+
+class TestWorkflow:
+    def test_workflow(self):
+        actual = sample_flow.execute()
+
+        assert actual.state["messages"] == [
+            "single",
+            "055",
+            "sub_flow",
+            "nested",
+            "arg_1 is smaller",
+            "Error A",
+            "Error B",
+            "it's 13",
+        ]
+
+    def test_str(self):
+        assert str(sub_flow) == "Sub Flow"
