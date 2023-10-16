86b64736a814b542ed2cfd72bfcd7229e862615b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:06:25 +0000

    Add package pythttp/0.1.9/pythttp-0.1.9.tar.gz

---
+++ packages/pythttp/pythttp-0.1.9.tar.gz/pythttp-0.1.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open(r"README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pythttp",
+    version="0.1.9",
+    author="Example Author",
+    author_email="team.longinus.project@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/projectlonginus/httpy",
+    install_requires=['dataclasses','datetime','uuid'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.11',
+)
