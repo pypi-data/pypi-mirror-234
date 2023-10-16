7c6209e5752480e03f35e6f9e064994dd5eee5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:07:30 +0000

    Add package mindsflow-api/0.0.1/mindsflow_api-0.0.1.tar.gz

---
+++ packages/mindsflow-api/mindsflow_api-0.0.1.tar.gz/mindsflow_api-0.0.1/mindsflow_api/models/model.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from .chat import Chat
+from .history import History
+
+
+class Event(dict):
+    chat: Optional[Chat] = Chat()
+
+
+class Context(dict):
+    history: Optional[History] = []
