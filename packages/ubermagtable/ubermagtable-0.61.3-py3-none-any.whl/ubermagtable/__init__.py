1a36a546df3ec522889cc3d4fb92d70655c1610c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:09:57 +0000

    Add package ubermagtable/0.61.3/ubermagtable-0.61.3-py3-none-any.whl

---
+++ packages/ubermagtable/ubermagtable-0.61.3-py3-none-any.whl/ubermagtable/__init__.py
@@ -0,0 +1,31 @@
+"""Manipulation of tabular data."""
+import importlib.metadata
+import os
+
+import matplotlib.pyplot as plt
+import pytest
+
+from .interact import interact
+from .table import Table
+
+# Enable default plotting style.
+dirname = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(dirname, "util", "plotting-style.mplstyle")
+plt.style.use(path)
+
+__version__ = importlib.metadata.version(__package__)
+
+
+def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import ubermagtable as ut
+    ...
+    >>> # ut.test()
+
+    """
+    return pytest.main(["-v", "--pyargs", "ubermagtable", "-l"])  # pragma: no cover
