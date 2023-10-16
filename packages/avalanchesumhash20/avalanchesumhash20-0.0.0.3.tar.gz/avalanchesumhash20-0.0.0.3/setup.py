d38198ec239c690d619bc81395f10acc34e5f007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:50:39 +0000

    Add package avalanchesumhash20/0.0.0.3/avalanchesumhash20-0.0.0.3.tar.gz

---
+++ packages/avalanchesumhash20/avalanchesumhash20-0.0.0.3.tar.gz/avalanchesumhash20-0.0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='avalanchesumhash20',
+    version='0.0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'scipy',
+    ],
+)
