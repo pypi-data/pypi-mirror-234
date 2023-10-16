63ce6c2ce1c4d675c7b4758e963953348d27b91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:39 +0000

    Add package BB-DateParser/0.2.43/bb_dateparser-0.2.43-py3-none-any.whl

---
+++ packages/BB-DateParser/bb_dateparser-0.2.43-py3-none-any.whl/dateparser/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.2.43'
+
+import logging
+from bblogger.logger import getLogger
+
+log = getLogger(__name__, 3, appname = "DateParser" )
+
+from .dateparser import DateParser
+
+__all__ = [ 'DateParser' ]
