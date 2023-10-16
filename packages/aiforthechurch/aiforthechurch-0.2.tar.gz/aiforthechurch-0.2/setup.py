d173ee1fcf16dd9d263867328d69df2485f80e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:46:39 +0000

    Add package aiforthechurch/0.2/aiforthechurch-0.2.tar.gz

---
+++ packages/aiforthechurch/aiforthechurch-0.2.tar.gz/aiforthechurch-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiforthechurch",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        "langdetect",
+        "torch>=2.0.1",
+        "peft>=0.5.0",
+        "transformers>=4.34.0",
+        "accelerate==0.23.0",
+        "bitsandbytes>=0.41.1",
+    ],
+    author="Andrew Rogers, Thomas Rialan",
+    author_email="andrew@biblechat.ai",
+    description="Package for training ande deploying doctrinally correct LLMs.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/trialan/aiforthechurch",
+)
