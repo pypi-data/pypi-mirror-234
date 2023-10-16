abdf59dd52d84419ecb9ad3ad9b44586a09fbfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:51:24 +0000

    Add package plover-q-and-a/0.3.4/plover_q_and_a-0.3.4-py3-none-any.whl

---
+++ packages/plover-q-and-a/plover_q_and_a-0.3.4-py3-none-any.whl/plover_q_and_a/sign/__init__.py
@@ -0,0 +1,7 @@
+"""
+Sign - A package dealing with creating text output for Q&A "signs", like signing
+in a question or answer, or signing in a lawyer to perform a line of
+questioning etc.
+"""
+
+from .text import text
