1fe41fb74f6dc72a5dff9115bdb07edf4a9a7bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:06:53 +0000

    Add package odoo14-addon-ssi-service-quotation-risk-analysis-lead/14.0.1.0.0/odoo14_addon_ssi_service_quotation_risk_analysis_lead-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-service-quotation-risk-analysis-lead/odoo14_addon_ssi_service_quotation_risk_analysis_lead-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_service_quotation_risk_analysis_lead/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Quotation + Risk Analysis + Lead Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "ssi_service_quotation_risk_analysis",
+        "ssi_service_quotation_lead",
+    ],
+    "data": [],
+    "demo": [],
+}
