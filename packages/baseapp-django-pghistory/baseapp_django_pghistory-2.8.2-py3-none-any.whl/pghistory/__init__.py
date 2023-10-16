bed05b23f7b0f851a9c51eebea2a71f45d99798b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:36 +0000

    Add package baseapp-django-pghistory/2.8.2/baseapp_django_pghistory-2.8.2-py3-none-any.whl

---
+++ packages/baseapp-django-pghistory/baseapp_django_pghistory-2.8.2-py3-none-any.whl/pghistory/__init__.py
@@ -0,0 +1,48 @@
+import django
+
+from pghistory.config import (ContextForeignKey, ContextJSONField,
+                              ContextUUIDField, Field, ForeignKey,
+                              ObjForeignKey, RelatedField)
+from pghistory.constants import DEFAULT
+from pghistory.core import (AfterInsert, AfterInsertOrUpdate, AfterUpdate,
+                            BeforeDelete, BeforeUpdate, BeforeUpdateOrDelete,
+                            Changed, DatabaseEvent, DatabaseTracker, Event,
+                            ManualTracker, ProxyField, Snapshot, Tracker,
+                            create_event, create_event_model, track)
+from pghistory.runtime import context
+from pghistory.version import __version__
+
+__all__ = [
+    "AfterInsert",
+    "AfterInsertOrUpdate",
+    "AfterUpdate",
+    "BeforeDelete",
+    "BeforeUpdate",
+    "BeforeUpdateOrDelete",
+    "Changed",
+    "context",
+    "ContextForeignKey",
+    "ContextJSONField",
+    "ContextUUIDField",
+    "create_event",
+    "create_event_model",
+    "DatabaseEvent",
+    "DatabaseTracker",
+    "DEFAULT",
+    "Event",
+    "Field",
+    "ForeignKey",
+    "ManualTracker",
+    "ObjForeignKey",
+    "ProxyField",
+    "RelatedField",
+    "Snapshot",
+    "track",
+    "Tracker",
+    "__version__",
+]
+
+if django.VERSION < (3, 2):  # pragma: no cover
+    default_app_config = "pghistory.apps.PGHistoryConfig"
+
+del django
