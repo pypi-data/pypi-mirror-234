5a832e4e3df6ee89bef2d0e77a786c123d287c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:11:13 +0000

    Add package v4-proto/0.3.6/v4-proto-0.3.6.tar.gz

---
+++ packages/v4-proto/v4-proto-0.3.6.tar.gz/v4-proto-0.3.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_namespace_packages, setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="v4-proto",
+    version="0.3.6",
+    author="dYdX Trading Inc.",
+    author_email="contact@dydx.exchange",
+    description="Protos for dYdX v4 protocol",
+    packages = find_namespace_packages(),
+    install_requires=required,
+    license_files = ("LICENSE"),
+    python_requires=">=3.8",
+)
