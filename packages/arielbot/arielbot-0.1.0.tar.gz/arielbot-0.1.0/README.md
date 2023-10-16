4adc7b52c2cdaafc8a0d6fd9e9ca08372bd50f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 18:06:09 +0000

    Add package arielbot/0.1.0/arielbot-0.1.0.tar.gz

---
+++ packages/arielbot/arielbot-0.1.0.tar.gz/arielbot-0.1.0/README.md
@@ -0,0 +1,2 @@
+# ArielBot 一款适用于Chronocat的B站动态推送机器人
+
---
+++ packages/arielbot/arielbot-0.1.0.tar.gz/arielbot-0.1.0/arielbot/bot.py
@@ -0,0 +1,14 @@
+import nonebot
+from nonebot.adapters.red import Adapter
+from os import path, getcwd, listdir
+import sys
+
+sys.path.append(path.join(getcwd(), "plugins"))
+
+nonebot.init()
+nonebot.load_plugin("arielbot.plugins.Core")
+nonebot.load_all_plugins([i for i in listdir(path.join(getcwd(), "plugins"))], [])
+driver = nonebot.get_driver()
+driver.register_adapter(Adapter)
+app = nonebot.get_asgi()
+config = nonebot.get_driver().config
