c7adf46445472370e2463dd209f8f9ab6d48c5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:10:27 +0000

    Add package biomappings/0.3.7/biomappings-0.3.7-py3-none-any.whl

---
+++ packages/biomappings/biomappings-0.3.7-py3-none-any.whl/biomappings/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Community curated mappings between biomedical entities."""
+
+from .graph import get_false_graph, get_predictions_graph, get_true_graph  # noqa:F401
+from .resources import (  # noqa:F401
+    MappingTuple,
+    PredictionTuple,
+    load_false_mappings,
+    load_mappings,
+    load_mappings_subset,
+    load_predictions,
+    load_unsure,
+)
