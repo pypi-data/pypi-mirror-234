d29d638188327fbe2e4733c99db2cfc18218bd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:51:11 +0000

    Add package stdl/0.4.8/stdl-0.4.8.tar.gz

---
+++ packages/stdl/stdl-0.4.8.tar.gz/stdl-0.4.8/stdl/dataclass.py
@@ -0,0 +1,45 @@
+import json
+from copy import deepcopy
+from dataclasses import asdict, astuple, dataclass
+from pprint import pprint
+from typing import Any, Mapping
+
+from stdl.fs import Pathlike, json_load
+
+
+class Data:
+    def __init__(self) -> None:
+        pass
+
+    def __getitem__(self, key):
+        return self.dict()[key]
+
+    def __iter__(self):
+        for i in self.tuple():
+            yield i
+
+    def dict(self) -> dict:
+        return asdict(self)  # type:ignore
+
+    def tuple(self) -> tuple:
+        return astuple(self)  # type:ignore
+
+    def json(self):
+        return json.dumps(self.dict())
+
+    def copy(self):
+        return deepcopy(self)
+
+    @classmethod
+    def parse_obj(cls, obj: Mapping[str, Any]):
+        return cls(**obj)
+
+    @classmethod
+    def parse_file(cls, filepath: Pathlike):
+        return cls.parse_obj(json_load(filepath))  # type:ignore
+
+    def print(self):
+        pprint(self.dict)
+
+
+__all__ = ["Data", "dataclass"]
