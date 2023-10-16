b456a5684e72c9ca707fb2139e78d4c5c85a594d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:08:29 +0000

    Add package calcure/2.9.6/calcure-2.9.6.tar.gz

---
+++ packages/calcure/calcure-2.9.6.tar.gz/calcure-2.9.6/calcure/loaders.py
@@ -0,0 +1,446 @@
+"""Module that controls loading data from files and libraries"""
+
+import configparser
+import pathlib
+import csv
+import os
+import datetime
+import time
+import ics
+import urllib.request
+import io
+import logging
+
+from calcure.data import *
+from calcure.calendars import convert_to_persian_date, convert_to_gregorian_date
+
+
+class LoaderCSV:
+    """Load data from CSV files"""
+
+    def create_file(self, filename):
+        """Create CSV file"""
+        try:
+            with open(filename, "w+", encoding="utf-8") as file:
+                pass
+            return []
+        except (FileNotFoundError, NameError) as e_message:
+            logging.error("Problem occured trying to create %s. %s", filename, e_message)
+            return []
+
+    def read_file(self, filename):
+        """Read CSV file or create new one if it does not exist"""
+        try:
+            with open(filename, "r", encoding="utf-8") as file:
+                lines = csv.reader(file, delimiter = ',')
+                return list(lines)
+        except IOError: # File does not exist
+            logging.info("Creating %s.", filename)
+            return self.create_file(filename)
+
+
+class TaskLoaderCSV(LoaderCSV):
+    """Load tasks from CSV files"""
+
+    def __init__(self, cf):
+        self.user_tasks = Tasks()
+        self.tasks_file = cf.TASKS_FILE
+        self.use_persian_calendar = cf.USE_PERSIAN_CALENDAR
+
+    @property
+    def is_task_format_old(self):
+        """Check if the database format is old"""
+        with open(self.tasks_file, "r", encoding="utf-8") as f:
+            text = f.read()
+        return text[0] == '"'
+
+    def load(self):
+        """Reads from CSV file"""
+        lines = self.read_file(self.tasks_file)
+
+        for index, row in enumerate(lines):
+            task_id = index
+
+            # Read task dates:
+            if self.is_task_format_old:
+                shift = 0
+                year = 0
+                month = 0
+                day = 0
+            else:
+                shift = 3
+                year = int(row[0])
+                month = int(row[1])
+                day = int(row[2])
+
+            # Convert to persian date if needed and if it is not zero date:
+            if self.use_persian_calendar and year != 0:
+                year, month, day = convert_to_persian_date(year, month, day)
+
+            # Read task name and statuses:
+            if row[0 + shift][0] == '.':
+                name = row[0 + shift][1:]
+                is_private = True
+            else:
+                name = row[0 + shift]
+                is_private = False
+            status = Status[row[1 + shift].upper()]
+            stamps = row[(2 + shift):] if len(row) > 2 else []
+            timer = Timer(stamps)
+
+            # Add task:
+            new_task = Task(task_id, name, status, timer, is_private, year, month, day)
+            self.user_tasks.add_item(new_task)
+        return self.user_tasks
+
+
+class EventLoaderCSV(LoaderCSV):
+    """Load events from CSV files"""
+
+    def __init__(self, cf):
+        self.user_events = Events()
+        self.events_file = cf.EVENTS_FILE
+        self.use_persian_calendar = cf.USE_PERSIAN_CALENDAR
+
+    def load(self):
+        """Read from CSV file"""
+        lines = self.read_file(self.events_file)
+        for index, row in enumerate(lines):
+            event_id = index
+            year = int(row[1])
+            month = int(row[2])
+            day = int(row[3])
+            if row[4][0] == '.':
+                name = row[4][1:]
+                is_private = True
+            else:
+                name = row[4]
+                is_private = False
+
+            # Account for old versions of the datafile:
+            if len(row) > 5:
+                repetition = int(row[5])
+                if row[6] == 'd':
+                    frequency = Frequency.DAILY
+                elif row[6] == 'w':
+                    frequency = Frequency.WEEKLY
+                elif row[6] == 'm':
+                    frequency = Frequency.MONTHLY
+                elif row[6] == 'y':
+                    frequency = Frequency.YEARLY
+                else:
+                    try:
+                        frequency = Frequency[row[6].upper()]
+                    except (ValueError, KeyError):
+                        frequency = Frequency.ONCE
+            else:
+                repetition = '1'
+                frequency = Frequency.ONCE
+            if len(row) > 7:
+                status = Status[row[7].upper()]
+            else:
+                status = Status.NORMAL
+
+            # Convert to persian date if needed:
+            if self.use_persian_calendar:
+                year, month, day = convert_to_persian_date(year, month, day)
+
+            # Add event:
+            new_event = UserEvent(event_id, year, month, day, name, repetition, frequency, status, is_private)
+            self.user_events.add_item(new_event)
+        return self.user_events
+
+
+class HolidayLoader:
+    """Load holidays for this country around this year"""
+
+    def __init__(self, cf):
+        self.holidays = Events()
+        self.country = cf.HOLIDAY_COUNTRY
+        self.use_persian_calendar = cf.USE_PERSIAN_CALENDAR
+
+    def load(self):
+        """Load list of holidays from 'holidays' module"""
+        try:
+            import holidays as hl
+            year = datetime.date.today().year
+            holiday_events = eval("hl."+self.country+"(years=[year-2, year-1, year, year+1, year+2, year+3, year+4])")
+            for date, name in holiday_events.items():
+
+                # Convert to persian date if needed:
+                if self.use_persian_calendar:
+                    year, month, day = convert_to_persian_date(date.year, date.month, date.day)
+                else:
+                    year, month, day = date.year, date.month, date.day
+
+                # Add holiday:
+                holiday = Event(year, month, day, name)
+                self.holidays.add_item(holiday)
+
+        except ModuleNotFoundError:
+            logging.error("Couldn't load holidays. Module holydays is not installed. Try 'pip install holydays'")
+            pass
+        except (SyntaxError, AttributeError) as e_message:
+            logging.error("Couldn't load holidays. Country might be incorrect. %s", e_message)
+            pass
+        return self.holidays
+
+
+class BirthdayLoader:
+    """Load birthdays of contacts"""
+
+    def __init__(self, cf):
+        self.birthdays = Birthdays()
+        self.abook_file = str(pathlib.Path.home())+"/.abook/addressbook"
+        self.use_persian_calendar = cf.USE_PERSIAN_CALENDAR
+
+    def load(self):
+        """Loading birthdays from abook contacts"""
+
+        # Quit if file does not exists:
+        if not os.path.exists(self.abook_file):
+            logging.warning("Couldn't load birthdays. File. %s does not exist.", self.abook_file)
+            return self.birthdays
+
+        abook = configparser.ConfigParser()
+        abook.read(self.abook_file)
+        for each_contact in abook.sections():
+            for key, _ in abook.items(each_contact):
+                if key in ["birthday", "anniversary"]:
+                    month = int(abook[each_contact][key][-5:-3])
+                    day = int(abook[each_contact][key][-2:])
+                    name = abook[each_contact]["name"]
+
+                    # Convert to persian date if needed:
+                    if self.use_persian_calendar:
+                        _, month, day = convert_to_persian_date(1000, month, day)
+
+                    # Add birthday:
+                    birthday = Event(1, month, day, name)
+                    self.birthdays.add_item(birthday)
+        return self.birthdays
+
+
+class LoaderICS:
+    """Load data from ICS files"""
+
+    def read_lines(self, file):
+        """Read the file line-by-line and remove multiple PRODID lines"""
+        already_has_prodid = False
+        text = ""
+        for line in file:
+            # If there is more than one PRODID line or a TZUNTIL line, skip them:
+            if ((not (already_has_prodid and "PRODID:" in line)) and
+                (not "TZUNTIL" in line)):
+                text += line
+
+            if "PRODID:" in line:
+                already_has_prodid = True
+        return text
+
+    def read_file(self, path):
+        """Parse an ics file if it exists"""
+        if not os.path.exists(path):
+            logging.error("Failed to load %s because file does not exist.", path)
+            return ""
+        with open(path, 'r', encoding="utf-8") as file:
+            return self.read_lines(file)
+
+    def read_url(self, path):
+        """Parse an ics URL if it exists and networks works"""
+        try:
+            with urllib.request.urlopen(path) as response:
+                file = io.TextIOWrapper(response, 'utf-8')
+                return self.read_lines(file)
+        except urllib.error.HTTPError as e_message:
+            logging.error("Failed to load from %s. Probably url is wrong. %s", path, e_message)
+            return ""
+        except urllib.error.URLError as e_message:
+            logging.error("Failed to load from %s. Probably no internet connection. %s", path, e_message)
+            return ""
+
+    def read_resource(self, path):
+        """Determine type of the resourse, parse it, and return list of strings for each file"""
+        ics_files = []
+
+        # If it's a URL, try to load it:
+        if path.startswith('http'):
+            ics_files.append(self.read_url(path))
+            return ics_files
+
+        # If it's a local file, read it:
+        if path.endswith('.ics'):
+            ics_files.append(self.read_file(path))
+            return ics_files
+
+        # Otherwise, assume it's a folder, and read every file inside:
+        for root, directories, files in os.walk(path):
+            for filename in files:
+                # Get the full path to the file
+                file_path = os.path.join(root, filename)
+
+                # `path` may contain files with metadata, e.g. `color` and
+                # `displayname`. For now, exclude those while loading.
+                if file_path.endswith('.ics'):
+                    ics_files.append(self.read_file(file_path))
+
+        return ics_files
+
+
+class TaskLoaderICS(LoaderICS):
+    """Load tasks from ICS files"""
+
+    def __init__(self, cf):
+        self.user_ics_tasks = Tasks()
+        self.ics_task_files = cf.ICS_TASK_FILES
+        self.use_persian_calendar = cf.USE_PERSIAN_CALENDAR
+
+    def parse_task(self, task, calendar_number):
+        """Parse single task and add it to the user_ics_tasks"""
+
+        if task.status == "CANCELLED":
+            return
+
+        task_id = self.user_ics_tasks.generate_id()
+
+        # Assign status from priority:
+        status = Status.NORMAL
+        if task.priority is not None:
+            if task.priority > 5:
+                status = Status.UNIMPORTANT
+            if task.priority < 5:
+                status = Status.IMPORTANT
+
+        # Correct according to status:
+        if task.status == "COMPLETED":
+            status = Status.DONE
+
+        name = task.name
+
+        # Try reading task due date:
+        try:
+            year = task.due.year
+            month = task.due.month
+            day = task.due.day
+        except AttributeError:
+            year, month, day = 0, 0, 0
+
+        timer = Timer([])
+        is_private = False
+
+        # Add task:
+        new_task = Task(task_id, name, status, timer, is_private,
+                        year, month, day, calendar_number)
+        self.user_ics_tasks.add_item(new_task)
+
+
+    def load(self):
+        """Load tasks from each of the ics files"""
+
+        # Quit if the files are not specified in config:
+        if self.ics_task_files is None:
+            return self.user_ics_tasks
+
+        for calendar_number, filename in enumerate(self.ics_task_files):
+            # For each resourse from config, load a list that has one or more ics files:
+            ics_files = self.read_resource(filename)
+            for ics_file in ics_files:
+                try:
+                    cal = ics.Calendar(ics_file)
+                    for task in cal.todos:
+                        self.parse_task(task, calendar_number)
+                except Exception as e_message:
+                    logging.error("Failed to parse %s. %s", filename, e_message)
+
+        return self.user_ics_tasks
+
+
+class EventLoaderICS(LoaderICS):
+    """Load events from ICS files"""
+
+    def __init__(self, cf):
+        self.user_ics_events = Events()
+        self.ics_event_files = cf.ICS_EVENT_FILES
+        self.use_persian_calendar = cf.USE_PERSIAN_CALENDAR
+
+    def parse_event(self, event, index, calendar_number):
+        """Parse singe event and add it to user_ics_events"""
+
+        # Default parameters:
+        event_id = index
+        repetition = '1'
+        frequency = Frequency.ONCE
+        status = Status.NORMAL
+        is_private = False
+
+        # Calculate offset of local timezone from UTC:
+        utc_offset_sec = time.timezone if (time.localtime().tm_isdst == 0) else time.altzone
+        utc_offset_hours = -1 * utc_offset_sec / 3600
+
+        # Parameters of the event from ics file, if they exist:
+        name = event.name if event.name is not None else ""
+        all_day = event.all_day if event.all_day is not None else True
+        year = event.begin.year if event.begin else 0
+        month = event.begin.month if event.begin else 1
+        day = event.begin.day if event.begin else 1
+
+        # Add start time to the name of non-all-day events:
+        if not all_day:
+            hour = int(event.begin.hour) if event.begin else 0
+
+            # Convert to local timezone, and add or remove a day:
+            hour += int(utc_offset_hours)
+            if hour >= 24:
+                hour -= 24
+                days_in_this_month = Calendar(0, self.use_persian_calendar).last_day(year, month)
+                if day < days_in_this_month:
+                    day += 1
+                else:
+                    day = 1
+                    if month < 12:
+                        month += 1
+                    else:
+                        month = 1
+                        year += 1
+            if hour < 0:
+                hour += 24
+                if day > 1:
+                    day -= 1
+                else:
+                    if month > 1:
+                        month -= 1
+                    else:
+                        month = 12
+                        year -= 1
+                    day = Calendar(0, self.use_persian_calendar).last_day(year, month)
+
+            minute = event.begin.minute if event.begin else 0
+            name = f"{hour:0=2}:{minute:0=2} {name}"
+
+        # Convert to persian date if needed:
+        if self.use_persian_calendar:
+            year, month, day = convert_to_persian_date(year, month, day)
+
+        # Add event:
+        new_event = UserEvent(event_id, year, month, day, name, repetition,
+                              frequency, status, is_private, calendar_number)
+        self.user_ics_events.add_item(new_event)
+
+    def load(self):
+        """Load events from each of the ics files"""
+
+        # Quit if the files are not specified in config:
+        if self.ics_event_files is None:
+            return self.user_ics_events
+
+        for calendar_number, filename in enumerate(self.ics_event_files):
+            # For each resourse from config, load a list that has one or more ics files:
+            ics_files = self.read_resource(filename)
+            for ics_file in ics_files:
+                try:
+                    cal = ics.Calendar(ics_file)
+                    for index, event in enumerate(cal.events):
+                        self.parse_event(event, index, calendar_number)
+                except Exception as e_message:
+                    logging.error("Failed to parse %s. %s", filename, e_message)
+        return self.user_ics_events
---
+++ packages/calcure/calcure-2.9.6.tar.gz/calcure-2.9.6/calcure/translations/fr.py
@@ -0,0 +1,111 @@
+"""French translations of the program interface"""
+
+MSG_WELCOME_1 = "Bienvenue à Calcure"
+MSG_WELCOME_2 = "Le calendrier et le gestionnaire de tâches pour votre terminal!"
+MSG_WELCOME_3 = "Les fichiers de configuration et de données ont été créés à:"
+MSG_WELCOME_4 = "Pour obtenir de l'aide, des contributions et des informations supplémentaires, visitez:"
+MSG_WELCOME_5 = "Presse ? pour voir les combinaisons de touches ou toute autre touche pour continuer."
+
+TITLE_KEYS_GENERAL = "GÉNÉRALE"
+TITLE_KEYS_CALENDAR = "CALENDRIER"
+TITLE_KEYS_JOURNAL = "JOURNAL"
+
+KEYS_GENERAL = {
+        " Space ": "Basculer entre calendrier et journal",
+        "   /   ": "Basculer l'écran partagé",
+        "   *   ": "Basculer la confidentialité globale",
+        "   ?   ": "Basculer cette aide",
+        "   q   ": "Quitter",
+        }
+
+KEYS_CALENDAR = {
+        "  a(A) ": "Ajouter un événement (récurrent)",
+        "  n,🠒  ": "Le mois (jour) prochain",
+        "  p,🠐  ": "mois (jour) précédent",
+        "   x   ": "Supprimer un événement",
+        "   r   ": "Modifier un événement",
+        "  m(M) ": "Déplacer l'événement",
+        "  g(G) ": "Aller à un certain jour",
+        "   v   ": "Basculer la vue quotidienne/mensuelle",
+        "   h   ": "Désactiver l'événement en haute priorité",
+        "   l   ": "Basculer l'événement en priorité basse",
+        "   d   ": "Toggle event as done",
+        "   .   ": "Activer la confidentialité des événements",
+        "   C   ": "Importer des événements depuis calcurse",
+        "   R   ": "Revenir au mois (jour) en cours",
+        }
+
+KEYS_TODO = {
+        "  a(A) ": "Ajouter une nouvelle (sous-)tâche",
+        "  h(H) ": "Bascule une (toutes) des tâches en haute priorité",
+        "  l(L) ": "Désactiver une (toutes) les tâches en priorité basse",
+        "  d(D) ": "Bascule une (toutes) les tâches comme terminée",
+        "  u(U) ": "Démarquer une (toutes) les tâches",
+        "  x(X) ": "Supprimer une (toutes) les tâches (avec toutes les sous-tâches)",
+        "  t(T) ": "Démarrer/mettre en pause (supprimer) le minuteur pour une tâche",
+        "   r   ": "Modifier une tâche",
+        "   s   ": "Basculer entre tâche et sous-tâche",
+        "   .   ": "Basculer la confidentialité des tâches",
+        "  f(F) ": "Modifier (supprimer) l'échéance de la tâche",
+        "   m   ": "Déplacer une tâche",
+        "  C(W) ": "Importer des tâches de calcurse (taskwarrior)",
+        }
+
+MSG_NAME          = "CALCURE"
+MSG_VIM           = "Les touches de Vim (j, k, ZZ, ZQ) fonctionnent également!"
+MSG_INFO          = "Pour plus d'informations, visitez:"
+MSG_SITE          = "https://anufrievroman.gitbook.io/calcure"
+MSG_EXIT          = "Vraiment quitter ? (y/n) "
+
+MSG_EVENT_HIGH    = "Marquer comme hautement prioritaire l'événement numéro: "
+MSG_EVENT_LOW     = "Marquer comme basse priorité l'événement numéro: "
+MSG_EVENT_DONE    = "Marquer comme terminé l'événement numéro: "
+MSG_EVENT_RESET   = "Réinitialiser le statut pour l'événement numéro : "
+MSG_EVENT_DEL     = "Supprimer l'événement numéro: "
+MSG_EVENT_REN     = "Renommer l'événement numéro: "
+MSG_NEW_TITLE     = "Entrez un nouveau titre: "
+MSG_EVENT_MV      = "Déplacer l'événement numéro: "
+MSG_EVENT_MV_TO   = "Déplacer l'événement vers (YYYY/MM/DD): "
+MSG_EVENT_MV_TO_D = "Déplacer l'événement vers: "
+MSG_EVENT_DATE    = "Entrez la date: "
+MSG_EVENT_TITLE   = "Entrez le titre: "
+MSG_EVENT_REP     = "Combien de répétitions de l'événement: "
+MSG_EVENT_FR      = "Répéter l'événement tous les jours (d), semaines (w), mois (m) ou années (y) ?"
+MSG_EVENT_IMP     = "Importer des événements depuis Calcurse ? (y/n)"
+MSG_EVENT_PRIVACY = "Basculer la confidentialité du numéro d'événement: "
+MSG_TM_ADD        = "Ajouter/mettre en pause le temporisateur pour la tâche numéro: "
+MSG_TM_RESET      = "Supprimer le temporisateur pour la tâche numéro: "
+MSG_TS_HIGH       = "Marquer comme haute priorité le numéro de tâche: "
+MSG_TS_LOW        = "Marquer comme basse priorité le numéro de tâche: "
+MSG_TS_RES        = "Réinitialiser l'état de la tâche numéro: "
+MSG_TS_DONE       = "Marquer comme terminé le numéro de tâche: "
+MSG_TS_DEL        = "Supprimer le numéro de tâche: "
+MSG_TS_DEL_ALL    = "Vraiment supprimer toutes les tâches ? (y/n)"
+MSG_TS_MOVE       = "Déplacer la tâche numéro: "
+MSG_TS_MOVE_TO    = "Déplacer la tâche vers le numéro: "
+MSG_TS_EDIT       = "Modifier le numéro tâche: "
+MSG_TS_TOG        = "Basculer le numéro de sous-tâche: "
+MSG_TS_SUB        = "Ajouter une sous-tâche pour la tâche numéro: "
+MSG_TS_TITLE      = "Entrez la sous-tâche: "
+MSG_TS_IM         = "Importer des tâches depuis Calcurse ? (y/n)"
+MSG_TS_TW         = "Importer des tâches depuis Taskwarrior ? (y/n)"
+MSG_TS_NOTHING    = "Rien de prévu..."
+MSG_TS_PRIVACY    = "Basculer la confidentialité du numéro de tâche: "
+MSG_TS_DEAD_ADD   = "Ajouter le deadline pour la tâche numéro: "
+MSG_TS_DEAD_DEL   = "Supprimer le deadline de la tâche numéro: "
+MSG_TS_DEAD_DATE  = "Ajouter le deadline (AAAA/MM/JJ): "
+MSG_WEATHER       = "La météo se charge..."
+MSG_ERRORS        = "Des erreurs se sont produites. Voir info.log dans votre dossier de configuration."
+MSG_GOTO          = "Aller au (YYYY/MM/DD): "
+MSG_GOTO_D        = "Aller au: "
+MSG_INPUT         = "Entrée incorrecte."
+
+CALENDAR_HINT     = "Espace · Passer au journal  a · Ajouter un événement  n/p · Changer de mois  ? · Aider"
+CALENDAR_HINT_D   = "Espace · Passer au journal  a · Ajouter un événement  n/p · Changer de jour  ? · All keybindings"
+JOURNAL_HINT      = "Espace · Passer au calendrier  a · Ajouter une tâche  d · Terminé  i · Important  ? · All keybindings"
+
+DAYS = ["LUNDI", "MARDI", "MERCREDI", "JEUDI", "VENDREDI", "SAMEDI", "DIMANCHE"]
+DAYS_PERSIAN = ["SHANBEH", "YEKSHANBEH", "DOSHANBEH", "SESHANBEH", "CHAHARSHANBEH", "PANJSHANBEH", "JOMEH"]
+
+MONTHS = ["JANVIER", "FEVRIER", "MARS", "AVRIL", "MAI", "JUIN", "JUILLET", "AOUT", "SEPTEMBRE", "OCTOBRE", "NOVEMBRE", "DECEMBRE"]
+MONTHS_PERSIAN = ["FARVARDIN", "ORDIBEHESHT", "KHORDAD", "TIR", "MORDAD", "SHAHRIVAR", "MEHR", "ABAN", "AZAR", "DEY", "BAHMAN", "ESFAND"]
