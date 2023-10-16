10b25011a8ff36cb74fc8249363905e041eed5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:07:58 +0000

    Add package scikit-tree/0.2.1/scikit_tree-0.2.1-cp310-cp310-win_amd64.whl

---
+++ packages/scikit-tree/scikit_tree-0.2.1-cp310-cp310-win_amd64.whl/sktree/ensemble/__init__.py
@@ -0,0 +1,11 @@
+from ._eiforest import ExtendedIsolationForest
+from ._honest_forest import HonestForestClassifier
+from ._supervised_forest import (
+    ExtraObliqueRandomForestClassifier,
+    ExtraObliqueRandomForestRegressor,
+    ObliqueRandomForestClassifier,
+    ObliqueRandomForestRegressor,
+    PatchObliqueRandomForestClassifier,
+    PatchObliqueRandomForestRegressor,
+)
+from ._unsupervised_forest import UnsupervisedObliqueRandomForest, UnsupervisedRandomForest
