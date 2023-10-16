3e59ab78678708cfd7362a025e64ecf0ec66ecef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:16 +0000

    Add package rene/0.1.0/rene-0.1.0-cp311-cp311-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/rene/rene-0.1.0-cp311-cp311-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/rene/_exact/trapezoidation.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import random
+import typing as t
+
+import typing_extensions as te
+from rithm.fraction import Fraction
+
+from rene import (Location,
+                  hints)
+from rene._context import Context
+from rene._seidel.trapezoidation import Trapezoidation as _RawTrapezoidation
+from rene._utils import (polygon_to_segments_count,
+                         validate_seed)
+
+
+@te.final
+class Trapezoidation:
+    @classmethod
+    def from_multisegment(cls,
+                          multisegment: hints.Multisegment[Fraction],
+                          /,
+                          *,
+                          seeder: t.Optional[hints.Seeder] = None) -> te.Self:
+        seed = (random.randint(0, len(multisegment.segments))
+                if seeder is None
+                else seeder())
+        validate_seed(seed)
+        return cls(_RawTrapezoidation.from_multisegment(multisegment, seed))
+
+    @classmethod
+    def from_polygon(cls,
+                     polygon: hints.Polygon[Fraction],
+                     /,
+                     *,
+                     seeder: t.Optional[hints.Seeder] = None) -> te.Self:
+        seed = (random.randint(0, polygon_to_segments_count(polygon))
+                if seeder is None
+                else seeder())
+        validate_seed(seed)
+        return cls(_RawTrapezoidation.from_polygon(polygon, seed))
+
+    @property
+    def height(self) -> int:
+        return self._raw.height
+
+    def locate(self, point: hints.Point[Fraction], /) -> Location:
+        return self._raw.locate(point)
+
+    _context: t.ClassVar[Context[Fraction]]
+    _raw: _RawTrapezoidation[Fraction]
+
+    __module__ = 'rene.exact'
+    __slots__ = '_raw',
+
+    def __init_subclass__(cls, /, **_kwargs: t.Any) -> t.NoReturn:
+        raise TypeError(f'type {cls.__qualname__!r} '
+                        'is not an acceptable base type')
+
+    def __new__(cls, raw: _RawTrapezoidation[Fraction], /) -> te.Self:
+        self = super().__new__(cls)
+        self._raw = raw
+        return self
+
+    def __contains__(self, point: hints.Point[Fraction], /) -> bool:
+        return self._raw.__contains__(point)
