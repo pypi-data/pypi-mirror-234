466384a8037e43ebf2018d78f3173ad7720d94ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:10:38 +0000

    Add package gabrielribalves-mifflin/0.1.2/gabrielribalves_mifflin-0.1.2-py3-none-any.whl

---
+++ packages/gabrielribalves-mifflin/gabrielribalves_mifflin-0.1.2-py3-none-any.whl/gabrielribalves-mifflin/core.py
@@ -0,0 +1,73 @@
+import os
+from csv import reader
+
+from dundie.utils.log import getLogger
+from dundie.database import connect, commit, add_person, add_movement
+
+log = getLogger()
+
+
+def load(filepath):
+    """Loads data from filepath to the database.
+    >>> len(load('assets/people.csv'))
+    2
+    """
+    try:
+        csv_data = reader(open(filepath))
+    except FileNotFoundError as e:
+        log.error(str(e))
+        raise e
+
+    db = connect()
+    people = []
+    headers = ["name", "dept", "role", "email"]
+    for line in csv_data:
+        person_data = dict(zip(headers, [item.strip() for item in line]))
+        pk = person_data.pop("email")
+        person, created = add_person(db, pk, person_data)
+
+        return_data = person.copy()
+        return_data["created"] = created
+        return_data["email"] = pk
+        people.append(return_data)
+
+    commit(db)
+    return people
+
+
+def read(**query):
+    """Read data from db and filters using query
+    read(email="joe@doe.com)
+    """
+    db = connect()
+    return_data = []
+    for pk, data in db["people"].items():
+        if (dept := query.get("dept")) and dept != data["dept"]:
+            continue
+
+        if (email := query.get("email")) and email != pk:
+            continue
+
+        return_data.append(
+            {
+                "email": pk,
+                "balance": db["balance"][pk],
+                "last_movement": db["movement"][pk][-1]["date"],
+                **data,
+            }
+        )
+    return return_data
+
+
+def add(value, **query):
+    """Add value to each record on query"""
+    people = read(**query)
+
+    if not people:
+        raise RuntimeError("Not Found")
+
+    db = connect()
+    user = os.getenv("USER")
+    for person in people:
+        add_movement(db, person["email"], value, user)
+    commit(db)
