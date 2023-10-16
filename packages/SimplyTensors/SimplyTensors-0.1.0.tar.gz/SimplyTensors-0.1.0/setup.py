0a9d2cdc38eaf70c4b64eab8f5626b3e6547f0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:24 +0000

    Add package SimplyTensors/0.1.0/SimplyTensors-0.1.0.tar.gz

---
+++ packages/SimplyTensors/SimplyTensors-0.1.0.tar.gz/SimplyTensors-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SimplyTensors',
+    version='0.1.0',
+    description='A simple AI module for working with tensors',
+    author='Your Name',
+    py_modules=['st'],
+    install_requires=[
+        'numpy',
+        # Add other dependencies as needed for your AI module
+    ],
+)
