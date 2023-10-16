7a293207fc59df801383dd056d1109c420718858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 04:05:02 +0000

    Add package funread/0.2.6/funread-0.2.6-py3-none-any.whl

---
+++ packages/funread/funread-0.2.6-py3-none-any.whl/funread/rss/__init__.py
@@ -0,0 +1,4 @@
+from .core import BaseRss, RSSItem, RssV2
+from .element import (Category, Cloud, DateElement, Element, Enclosure, Guid,
+                      Image, IntElement, SkipDays, SkipHours, Source,
+                      TextInput)
