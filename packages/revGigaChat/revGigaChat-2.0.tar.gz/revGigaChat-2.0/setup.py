c67dc6f9105bb9fb8b54f9f8112ad08570b30d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 06:05:03 +0000

    Add package revGigaChat/2.0/revGigaChat-2.0.tar.gz

---
+++ packages/revGigaChat/revGigaChat-2.0.tar.gz/revGigaChat-2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='revGigaChat',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='2.0',
+    author='Denis Ponomar',
+    packages=['revGigaChat'],
+    install_requires=[
+        'requests',
+        'telethon',
+        'Pillow'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
+    ],
+)
