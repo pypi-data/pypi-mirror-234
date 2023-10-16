3fccbd17e7129981e2e891032c81594dddbd0c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:09:57 +0000

    Add package webpageinfo/2023.10.9/webpageinfo-2023.10.9-py3-none-any.whl

---
+++ packages/webpageinfo/webpageinfo-2023.10.9-py3-none-any.whl/webpageinfo/config/ignored_html_tags.py
@@ -0,0 +1,24 @@
+ignored_html_tags = {
+    "article",
+    "aside",
+    "blockquote",
+    "body",
+    "cite",
+    "dialog",
+    "figcaption",
+    "figure",
+    "footer",
+    "frameset",
+    "frame",  # TODO choper le contenu
+    "header",
+    "iframe",  # TODO choper le contenu
+    "label",
+    "main",
+    "noframe",
+    "noframes",
+    "section",
+    "show",
+    "tbody",
+    "time",
+    "tt",
+}
