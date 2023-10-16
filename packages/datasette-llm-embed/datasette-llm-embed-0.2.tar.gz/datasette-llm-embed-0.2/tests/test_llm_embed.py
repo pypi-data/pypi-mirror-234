9e92b7ebf14df23ac4cb060f15a54c7463e4ec4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:20 +0000

    Add package datasette-llm-embed/0.2/datasette-llm-embed-0.2.tar.gz

---
+++ packages/datasette-llm-embed/datasette-llm-embed-0.2.tar.gz/datasette-llm-embed-0.2/tests/test_llm_embed.py
@@ -0,0 +1,133 @@
+import base64
+from datasette.app import Datasette
+import llm
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_llm_embed():
+    ds = Datasette()
+    await ds.invoke_startup()
+    response = await ds.client.get(
+        "/_memory.json",
+        params={
+            "_shape": "array",
+            "sql": "select llm_embed('embed-demo', 'hello world') as e",
+        },
+    )
+    assert response.status_code == 200
+    encoded = response.json()[0]["e"]["encoded"]
+    assert llm.decode(base64.b64decode(encoded)) == (
+        5.0,
+        5.0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+    )
+
+
+@pytest.mark.asyncio
+async def test_llm_embed_cosine():
+    ds = Datasette()
+    response = await ds.client.get(
+        "/_memory.json",
+        params={
+            "_shape": "array",
+            "sql": """
+            select llm_embed_cosine(
+                llm_embed('embed-demo', 'hello world'),
+                llm_embed('embed-demo', 'hello again')
+            ) as cosine""",
+        },
+    )
+    assert response.status_code == 200
+    pytest.approx(response.json()[0]["cosine"], 0.0001) == 0.9999
+
+
+@pytest.mark.asyncio
+async def test_llm_embed_uses_key():
+    ds = Datasette(
+        metadata={
+            "plugins": {
+                "datasette-llm-embed": {
+                    "keys": {
+                        "embed-key-demo": "default-key",
+                        "aliased": "alias",
+                    }
+                }
+            }
+        }
+    )
+    response = await ds.client.get(
+        "/_memory.json",
+        params={
+            "_shape": "array",
+            "sql": "select llm_embed('embed-key-demo', 'hello world') as e",
+        },
+    )
+    assert response.status_code == 200
+    encoded = response.json()[0]["e"]["encoded"]
+    assert llm.decode(base64.b64decode(encoded)) == (
+        5.0,
+        5.0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        11.0,
+    )
+
+
+@pytest.mark.asyncio
+async def test_llm_embed_decode():
+    ds = Datasette()
+    response = await ds.client.get(
+        "/_memory.json",
+        params={
+            "_shape": "array",
+            "_json": "decoded",
+            "sql": """
+            select llm_embed_decode(
+                llm_embed('embed-demo', 'hello world')
+            ) as decoded""",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json()[0]["decoded"] == [
+        5.0,
+        5.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
