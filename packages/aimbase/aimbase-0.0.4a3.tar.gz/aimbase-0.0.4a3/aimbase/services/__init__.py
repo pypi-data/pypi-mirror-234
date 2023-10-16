20cb50d9ea82c457cecabb7c48fbf97bd697cfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:11:10 +0000

    Add package aimbase/0.0.4a3/aimbase-0.0.4a3.tar.gz

---
+++ packages/aimbase/aimbase-0.0.4a3.tar.gz/aimbase-0.0.4a3/aimbase/services/__init__.py
@@ -0,0 +1,3 @@
+from .sentence_transformer_inference import SentenceTransformerInferenceService
+from .cross_encoder_inference import CrossEncoderInferenceService
+from .base import BaseAIInferenceService
