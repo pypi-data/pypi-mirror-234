0248e6526ada2cc5d01adeff6ad2a0529090315f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:25 +0000

    Add package ai-agents/0.0.5/ai-agents-0.0.5.tar.gz

---
+++ packages/ai-agents/ai-agents-0.0.5.tar.gz/ai-agents-0.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    req = f.readlines()
+req = [x.strip() for x in req if x.strip()]
+
+setuptools.setup(
+    name="ai-agents", 
+    version="0.0.5",
+    author="AIWaves",
+    author_email="contact@aiwaves.cn",
+    description="An Open-source Framework for Autonomous Language Agents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aiwaves-cn/agents",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='Apache License 2.0',
+    install_requires=req
+)
