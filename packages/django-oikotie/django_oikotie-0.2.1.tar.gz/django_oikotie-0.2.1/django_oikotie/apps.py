1651631cc93ff538dff875f27b59234f1a556706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:06:20 +0000

    Add package django-oikotie/0.2.1/django_oikotie-0.2.1.tar.gz

---
+++ packages/django-oikotie/django_oikotie-0.2.1.tar.gz/django_oikotie-0.2.1/django_oikotie/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoOikotieConfig(AppConfig):
+    name = "django_oikotie"
+    verbose_name = "Oikotie"
+    default_auto_field = "django.db.models.BigAutoField"
