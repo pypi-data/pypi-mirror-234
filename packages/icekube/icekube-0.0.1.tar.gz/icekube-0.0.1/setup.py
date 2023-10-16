b53c2c6d8f655df4bad0d23bbdbae2e9b8d000c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:10:38 +0000

    Add package icekube/0.0.1/icekube-0.0.1.tar.gz

---
+++ packages/icekube/icekube-0.0.1.tar.gz/icekube-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="icekube",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="",
+    author="Mohit Gupta",
+    author_email="mohit.gupta@withsecure.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
