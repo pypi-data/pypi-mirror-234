2e9e10b642cd8a4ccfa9b871c0d95277fe85f7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 01:27:20 +0000

    Add package sqlite-dao-ext/0.1.6/sqlite_dao_ext-0.1.6.tar.gz

---
+++ packages/sqlite-dao-ext/sqlite_dao_ext-0.1.6.tar.gz/sqlite_dao_ext-0.1.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sqlite_dao_ext",
+    version="0.1.6",
+    description="Sqlite3 Dao",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=["dataclasses", "typing"],
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
