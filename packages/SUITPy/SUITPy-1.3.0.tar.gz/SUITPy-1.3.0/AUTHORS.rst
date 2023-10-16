e58d520b2704249c9d6b670a9824332d2475d35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:06:20 +0000

    Add package SUITPy/1.3.0/SUITPy-1.3.0.tar.gz

---
+++ packages/SUITPy/SUITPy-1.3.0.tar.gz/SUITPy-1.3.0/AUTHORS.rst
@@ -0,0 +1,36 @@
+.. -*- mode: rst -*-
+
+People
+------
+
+This work is made available by a community of people, largely originating from
+the diedrichsenlab lab at `Western University <http://www.diedrichsenlab.org//>`_
+
+.. _core_devs:
+
+Core developers
+...............
+
+The SUITPy core developers are:
+
+*Joern Diedrichsen
+*Maedbh King
+*Da Zhi
+
+Other contributors:
+
+*Carlos Hernandez-Castillo
+*Suzanne Witt
+
+.. _citing:
+
+Citing SUITPy
+--------------
+Please cite the following papers if you use SUITPy. Thank you.
+
+SUIT normalisation and template:
+<http://www.diedrichsenlab.org/pubs/Neuroimage_2006.pdf>`_.
+
+Surface-based representation and flatmap
+Diedrichsen, J. & Zotow, E. (2015). Surface-based display of volume-averaged cerebellar data. PLOSOne.
+<http://www.diedrichsenlab.org/pubs/PlosOne_flatmap_2015.pdf>`_.
---
+++ packages/SUITPy/SUITPy-1.3.0.tar.gz/SUITPy-1.3.0/tests/test_flatmap.py
@@ -0,0 +1,52 @@
+# test flatmap
+"""
+Unit test for flatmap unit
+@author: jdiedrichsen
+"""
+import unittest
+import SUITPy.flatmap as flatmap
+import numpy as np
+import plotly.graph_objs as go
+import nibabel as nb
+
+
+def test_flatmap_plot(render='plotly'):
+    ax = flatmap.plot('docs/source/notebooks/MDTB08_Math.func.gii',threshold=[0.01, 0.12],
+    bordersize=3,bordercolor='k',backgroundcolor='w',render=render,colorbar=True)
+    ax.show()
+    # ax = flatmap.plot('docs/source/notebooks/Buckner_17Networks.label.gii',overlay_type='label',new_figure=True, colorbar=False,render='plotly')
+    # ax.show()
+    pass
+
+def test_plot_label(render='plotly'):
+    fname = 'docs/source/notebooks/Buckner_17Networks.label.gii'
+    fig=flatmap.plot(fname, overlay_type='label',new_figure=True,
+    cscale=[1,5],render=render,colorbar=True)
+    fig.show()
+
+def test_plot_rgba(render='plotly'):
+    fname = ['MDTB08_Math.func.gii','MDTB04_Action_Observation.func.gii','MDTB16_Finger_Sequence.func.gii']
+    nvert = 28935
+    data = np.zeros((nvert,4))
+    for i,f in enumerate(fname):
+        A = nb.load('docs/source/notebooks/'+f)
+        data[:,i]=np.nan_to_num(A.darrays[0].data)
+        data[:,i]=data[:,i]/np.max(data[:,i])
+        data[data[:,i]<0.3,i]=0
+    data[data[:,0:3].sum(axis=1)==0]=np.nan
+    data[:,3]=1
+    ax = flatmap.plot(data,
+                      overlay_type='rgb',
+                        bordersize=3,bordercolor='k',backgroundcolor='w',render=render)
+    ax.show()
+    # ax = flatmap.plot('docs/source/notebooks/Buckner_17Networks.label.gii',overlay_type='label',new_figure=True, colorbar=False,render='plotly')
+    # ax.show()
+    pass
+
+
+if __name__ == '__main__':
+    # make_shapes()
+    # test_flatmap_plot(render='matplotlib')
+    # test_plot_label(render='matplotlib')
+    test_plot_rgba(render='matplotlib')
+    pass
