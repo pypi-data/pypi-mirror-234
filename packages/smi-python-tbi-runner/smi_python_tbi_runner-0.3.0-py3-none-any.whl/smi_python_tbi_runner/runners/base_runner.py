e0643389a2c049128549b10ec7b56ef16a9d8809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:08:34 +0000

    Add package smi-python-tbi-runner/0.3.0/smi_python_tbi_runner-0.3.0-py3-none-any.whl

---
+++ packages/smi-python-tbi-runner/smi_python_tbi_runner-0.3.0-py3-none-any.whl/smi_python_tbi_runner/runners/base_runner.py
@@ -0,0 +1,17 @@
+from smi_python_commons.config.application import Application
+from smi_python_tbi_parser.tbi import Tbi
+
+
+class BaseRunner:
+
+    def __init__(self):
+        self.name = "default"
+
+    def get_name(self):
+        return self.name
+
+    def execute(self, app: Application, tbi: Tbi, sub_command: str):
+        return 0
+
+
+DEFAULT_RUNNER = BaseRunner()
