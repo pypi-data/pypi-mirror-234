c4c9966e8056fdd5e0bc4455a90e821b0cbb18d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 16:06:46 +0000

    Add package pytfc/0.0.21/pytfc-0.0.21.tar.gz

---
+++ packages/pytfc/pytfc-0.0.21.tar.gz/pytfc-0.0.21/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.0.21'
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pytfc',
+    version=VERSION,
+    author='Alex Basista',
+    author_email='alex.basista@gmail.com',
+    url='https://github.com/alexbasista/pytfc',
+    description='Python HTTP client library for Terraform Cloud/Enterprise API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files=['LICENSE'],
+    py_modules=['pytfc'],
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.31.0',
+        'pyhcl>=0.4.4',
+    ],
+    keywords=['tfe', 'terraform enterprise', 'tfc', 'terraform cloud', 'terraform'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+    ],
+)
