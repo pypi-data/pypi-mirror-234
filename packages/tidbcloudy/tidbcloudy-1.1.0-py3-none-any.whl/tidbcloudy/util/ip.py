de1653a1045f0f76c817423829dc849b4630a0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:52:35 +0000

    Add package tidbcloudy/1.1.0/tidbcloudy-1.1.0-py3-none-any.whl

---
+++ packages/tidbcloudy/tidbcloudy-1.1.0-py3-none-any.whl/tidbcloudy/util/ip.py
@@ -0,0 +1,14 @@
+import socket
+
+import httpx
+
+
+def get_current_ip_address() -> str:
+    host = "ifconfig.co"
+    addrs = socket.getaddrinfo(host, 80, family=socket.AF_INET, proto=socket.IPPROTO_TCP)
+    for addr in addrs:
+        ipv4_addr = addr[4][0]
+        resp = httpx.get("http://" + ipv4_addr + "/ip", headers={"Host": host})
+        resp.raise_for_status()
+        return resp.text.strip()
+    raise Exception("Failed to get the current IP address")
