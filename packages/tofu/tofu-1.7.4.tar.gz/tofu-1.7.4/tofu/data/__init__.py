d03af2ebb1eb4274fda4a272ad12ab39c316d86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 17:08:22 +0000

    Add package tofu/1.7.4/tofu-1.7.4.tar.gz

---
+++ packages/tofu/tofu-1.7.4.tar.gz/tofu-1.7.4/tofu/data/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Provide data handling class and methods (storing, processing, plotting...)
+"""
+from ._core import *
+from ._DataCollection_class1_interactivity import *
+from ._spectrallines_class import *
+from ._class10_algos import get_available_inversions_algo
+from ._class10_Inversion import Inversion as Collection
+from ._spectralunits import *
+from ._saveload import *
