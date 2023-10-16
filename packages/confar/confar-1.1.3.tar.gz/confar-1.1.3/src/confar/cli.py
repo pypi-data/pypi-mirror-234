7ecee5ed51a54fe08f23cfac878e398cfc3407ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:06:51 +0000

    Add package confar/1.1.3/confar-1.1.3.tar.gz

---
+++ packages/confar/confar-1.1.3.tar.gz/confar-1.1.3/src/confar/cli.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from typer import Typer
+import json
+from .aggregate import Aggregator
+from .runner import Runner
+from .utils.renderer import render_result
+
+app = Typer()
+
+
+@app.command()
+def parse(file_path: str):
+    config = Aggregator().parse_file(file_path)
+
+    print(config)
+
+
+@app.command()
+def run(file_path: str, flow: str = 'default'):
+    config = Aggregator().parse_file(file_path)
+
+    result = Runner(config).execute_flow(flow=flow)
+
+    # Write the rendered HTML to an output file or use it as needed
+    with open(f'{result["id"]}.json', 'w') as output_file:
+        output_file.write(json.dumps(result))
+
+    result = render_result(result=result)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    app()
