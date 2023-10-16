67e947943d82ced991736a73645169d1f3c1b834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:50:33 +0000

    Add package lasha-metreveli-pk/0.1.0/lasha_metreveli_pk-0.1.0.tar.gz

---
+++ packages/lasha-metreveli-pk/lasha_metreveli_pk-0.1.0.tar.gz/lasha_metreveli_pk-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lasha_metreveli_pk",
+    version="0.1.0",
+    description="LM",
+    author="lasha metreveli",
+    author_email="lasha.metreveli.3@iliauni.edu.ge",
+    url="https://github.com/lashametreveli/my_package",
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
