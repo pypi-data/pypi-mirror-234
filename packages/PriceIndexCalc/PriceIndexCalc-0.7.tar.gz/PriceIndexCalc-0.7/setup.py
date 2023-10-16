c92deb596bdb210688c5cac2b17990a75890b347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:46:05 +0000

    Add package PriceIndexCalc/0.7/PriceIndexCalc-0.7.tar.gz

---
+++ packages/PriceIndexCalc/PriceIndexCalc-0.7.tar.gz/PriceIndexCalc-0.7/setup.py
@@ -0,0 +1,16 @@
+from gettext import find
+from setuptools import setup, find_packages
+
+setup(
+    name='PriceIndexCalc',
+    version='0.7',
+    description='Price Index Calculator using bilateral and multilateral methods',
+    author='Dr. Usman Kayani',
+    url='https://github.com/drrobotk/PriceIndexCalc',
+    author_email='usman.kayaniphd@gmail.com',
+    license='MIT',
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    install_requires=['pandas', 'numpy', 'scipy'],
+    include_package_data=True,
+)
