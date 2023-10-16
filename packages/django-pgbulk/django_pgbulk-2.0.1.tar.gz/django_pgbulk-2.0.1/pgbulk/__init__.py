dff3ba9af925d76330094899176d0090b57e259b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:43:48 +0000

    Add package django-pgbulk/2.0.1/django_pgbulk-2.0.1.tar.gz

---
+++ packages/django-pgbulk/django_pgbulk-2.0.1.tar.gz/django_pgbulk-2.0.1/pgbulk/__init__.py
@@ -0,0 +1,4 @@
+from pgbulk.core import UpdateField, UpsertResult, aupdate, aupsert, update, upsert
+from pgbulk.version import __version__
+
+__all__ = ["update", "aupdate", "upsert", "aupsert", "UpsertResult", "UpdateField", "__version__"]
