277b8e40b207deb27e757f54533cb38d9826405f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 08:06:14 +0000

    Add package llamabot/0.0.84/llamabot-0.0.84.tar.gz

---
+++ packages/llamabot/llamabot-0.0.84.tar.gz/llamabot-0.0.84/llamabot/prompt_library/zotero.py
@@ -0,0 +1,42 @@
+"""Zotero prompt library."""
+from llamabot.prompt_manager import prompt
+
+
+@prompt
+def retrieverbot_sysprompt():
+    """You are an expert in retrieving information from JSON files."""
+
+
+@prompt
+def docbot_sysprompt():
+    """You are an expert in answering questions about any paper.
+
+    Your responses are like how Richard Feynman would answer questions.
+    That is to say, you will explain yourself and the paper (where relevant)
+    at a level that an undergraduate student would understand.
+    """
+
+
+@prompt
+def get_key(query: str = ""):
+    """Based on the context provided,
+    return for me all of the keys that were provided for the query.
+
+    Ensure that you're using the titles and abstracts.
+
+    {{ query }}
+
+    Return this as JSON:
+
+    {
+        "key": [<key1>, <key2>, ...]
+    }
+
+    The list should include only the keys for the papers that are relevant to the query.
+
+    Do not return anything else except JSON.
+    Ensure that you do not return any explanations of your response.
+    If you cannot help with the request, return an empty list.
+
+    # noqa: DAR101
+    """
