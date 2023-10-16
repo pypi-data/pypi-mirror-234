d7d9d9b848b89a2dc348a75b1cc6ad18a91cf30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 03:05:16 +0000

    Add package octodiary/0.2.5/octodiary-0.2.5-py3-none-any.whl

---
+++ packages/octodiary/octodiary-0.2.5-py3-none-any.whl/octodiary/asyncApi/__init__.py
@@ -0,0 +1,12 @@
+#                 © Copyright 2023
+#          Licensed under the MIT License
+#        https://opensource.org/licenses/MIT
+#           https://github.com/OctoDiary
+
+from octodiary.asyncApi import myschool
+from octodiary.asyncApi.base import AsyncBaseApi
+
+__all__ = [
+    "AsyncBaseApi",
+    "myschool",
+]
---
+++ packages/octodiary/octodiary-0.2.5-py3-none-any.whl/octodiary/types/__init__.py
@@ -0,0 +1,13 @@
+#                 © Copyright 2023
+#          Licensed under the MIT License
+#        https://opensource.org/licenses/MIT
+#           https://github.com/OctoDiary
+
+from octodiary.types import captcha, myschool
+from octodiary.types.model import Type
+
+__all__ = [
+    "Type",
+    "myschool",
+    "captcha",
+]
