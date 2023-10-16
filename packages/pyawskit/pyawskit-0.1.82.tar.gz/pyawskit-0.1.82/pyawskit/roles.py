1845daae494823690cc8845aba28575ef2fb77bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 10:06:54 +0000

    Add package pyawskit/0.1.82/pyawskit-0.1.82.tar.gz

---
+++ packages/pyawskit/pyawskit-0.1.82.tar.gz/pyawskit-0.1.82/pyawskit/roles.py
@@ -0,0 +1,12 @@
+import boto3
+from pyawskit.configs import ConfigDuplicateRole
+
+
+def duplicate_role():
+    client = boto3.client("iam")
+    print(ConfigDuplicateRole.to_role)
+    print(ConfigDuplicateRole.from_role)
+    response = client.get_role(
+        RoleName=ConfigDuplicateRole.from_role,
+    )
+    print(response)
