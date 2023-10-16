0165b75a31bb154d9fb36b38fdd2dded0fcd37c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:07:18 +0000

    Add package python-app-store-scraper/1.1.1/python-app-store-scraper-1.1.1.tar.gz

---
+++ packages/python-app-store-scraper/python-app-store-scraper-1.1.1.tar.gz/python-app-store-scraper-1.1.1/app_store_scraper/__version__.py
@@ -0,0 +1,15 @@
+__title__ = "python-app-store-scraper"
+__version__ = "1.1.1"
+__description__ = "An Awesome App Store Review Scraper 🧹"
+__author__ = "Mohammad Foysal"
+__url__ = "https://github.com/iamfoysal/py-app-store"
+__license__ = "MIT"
+__keywords__ = [
+    "app store",
+    "ios",
+    "ios apps",
+    "podcasts",
+    "review",
+    "scraping",
+    "scraper",
+]
