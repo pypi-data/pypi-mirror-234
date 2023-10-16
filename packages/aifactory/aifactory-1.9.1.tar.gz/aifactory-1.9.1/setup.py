e60ba8aa408f9b41a592f59fb710a75053a6e52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:56 +0000

    Add package aifactory/1.9.1/aifactory-1.9.1.tar.gz

---
+++ packages/aifactory/aifactory-1.9.1.tar.gz/aifactory-1.9.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='aifactory',
+      version='1.9.1',
+      description='aifactory-cli',
+      author='aifactory',
+      author_email='contact@aifactory.page',
+      url='https://aifactory.space',
+      license='MIT',
+      py_modules=['submit'],
+      python_requires='>=3',
+      install_requires=["pipreqs","ipynbname", "gdown", "requests", "IPython"],
+      packages=['aifactory']
+      )
