c67fb9c30fd7f1139648768a5c88ddbf8e114967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:08:09 +0000

    Add package aiearth-predict/0.0.2/aiearth_predict-0.0.2-py3-none-any.whl

---
+++ packages/aiearth-predict/aiearth_predict-0.0.2-py3-none-any.whl/aiearth/predict/checkpoint/__init__.py
@@ -0,0 +1,17 @@
+from aiearth.predict.checkpoint.model_checkpoint import ModelCheckpoint
+from aiearth.predict.checkpoint.model_config import (
+    AcceleratorType,
+    ModelAttachedFile,
+    ModelParams,
+    ModelConfig,
+    BuildType,
+)
+
+__all__ = [
+    "BuildType",
+    "ModelCheckpoint",
+    "AcceleratorType",
+    "ModelAttachedFile",
+    "ModelParams",
+    "ModelConfig",
+]
