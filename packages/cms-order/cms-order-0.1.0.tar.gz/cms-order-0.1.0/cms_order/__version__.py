36321491e1c905b5e6f4828d3e7e1b0ea6b27e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:05:36 +0000

    Add package cms-order/0.1.0/cms-order-0.1.0.tar.gz

---
+++ packages/cms-order/cms-order-0.1.0.tar.gz/cms-order-0.1.0/cms_order/__version__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+"""
+Package in development.
+"""
+
+__author__ = "Marcel Rieger"
+__email__ = "github.riga@icloud.com"
+__copyright__ = "Copyright 2023, Marcel Rieger"
+__credits__ = ["Marcel Rieger"]
+__contact__ = "https://github.com/cms-cat/cms-order"
+__license__ = "BSD-3-Clause"
+__status__ = "Development"
+__version__ = "0.1.0"
