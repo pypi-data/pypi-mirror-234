8dc60d609f1206996e42df7ec0547bf91c8013b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:53 +0000

    Add package web-mini/1.2.4/web_mini-1.2.4-py2.py3-none-any.whl

---
+++ packages/web-mini/web_mini-1.2.4-py2.py3-none-any.whl/web_mini/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""web-mini -- efficient css and html minifer inspired by \
+https://pypi.org/project/css-html-js-minify/"""
+
+from typing import Tuple
+
+from . import css, html
+
+__version__: str = "1.2.4"
+__all__: Tuple[str, ...] = "__version__", "html", "css"
