b2ac11cef43369dbbdca6a8879263a351f954b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 22:07:23 +0000

    Add package msgraph-py/0.1.1/msgraph_py-0.1.1-py3-none-any.whl

---
+++ packages/msgraph-py/msgraph_py-0.1.1-py3-none-any.whl/msgraph/__init__.py
@@ -0,0 +1,9 @@
+from .mail import send_mail
+from .users import (
+    delete_auth_method,
+    get_user,
+    get_user_risk,
+    list_auth_methods,
+    reset_strong_auth,
+    revoke_refresh_tokens,
+)
