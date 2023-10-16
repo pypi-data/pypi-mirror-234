33a2bea65674863bb0b9ab7ac60bd0f161209237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 07:05:03 +0000

    Add package epfs/1.22/epfs-1.22.tar.gz

---
+++ packages/epfs/epfs-1.22.tar.gz/epfs-1.22/epfs/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from django.http import FileResponse,HttpResponse
+from django.conf import settings
+from .models import Fileupload
+from .forms import Fileform
+#from django.contrib.staticfiles import finders
+import random,pyqrcode,os,base64
+
+
+def index(request):
+    return render(request,"epfs/sharefile.html")
+
+def sharefile(request):
+    if request.method == 'POST':
+        form = Fileform(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            obj=Fileupload.objects.all().last()
+            keytxt=''.join([random.choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(10)])
+            obj.keystring=keytxt
+            obj.save()
+            keystring=request.META['HTTP_HOST'] + '/epfs/view/' + keytxt
+            qrcode=pyqrcode.create(keystring)
+            #qrcode.svg(finders.find("qrcode.svg"),scale=8)
+            qrcode.svg("qrcode.svg",scale=8)
+            imgfile=base64.b64encode(open("qrcode.svg","rb").read()).decode('ascii')
+            return HttpResponse("<!DOCTYPE htm><html><head><title>epfs file link</title><meta name='viewport' content='width=device-width, initial-scale=1.0' ></head><body><center><h5>{}<h5><img src='data:image/svg+xml;base64,{}' /></center></body></html>".format(keystring,imgfile))
+    else:
+        form = Fileform()
+    return render(request, 'epfs/sharefile.html', {
+        'form': form
+    })
+
+def downloadfile(request,link):
+    obj=Fileupload.objects.filter(keystring=link)
+    filepath=obj.last().Name.path
+    return FileResponse(open(filepath,'rb'))
+
+def removeallfile(request,txt):
+    path=os.path.join(settings.BASE_DIR)
+    path=os.path.dirname(path)
+    path=os.path.join(path,'upload')
+    if txt=='ea!^433' :
+        os.system("rm -rf {}".format(path))
+    #return HttpResponse(path)
+    return redirect('/epfs')
+
+
+
