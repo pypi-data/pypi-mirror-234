0ed0f87919896565087c04a763d2cb341b1cf163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:44:22 +0000

    Add package rene/0.1.0/rene-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rene/rene-0.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rene/_clipping/event.py
@@ -0,0 +1,17 @@
+import sys
+from typing import NewType
+
+from rene._utils import (is_even,
+                         is_odd)
+
+Event = NewType('Event', int)
+
+UNDEFINED_EVENT = Event(sys.maxsize)
+
+is_event_left = is_even
+is_event_right = is_odd
+
+
+def left_event_to_position(event: Event, /) -> int:
+    assert is_event_left(event)
+    return event // 2
