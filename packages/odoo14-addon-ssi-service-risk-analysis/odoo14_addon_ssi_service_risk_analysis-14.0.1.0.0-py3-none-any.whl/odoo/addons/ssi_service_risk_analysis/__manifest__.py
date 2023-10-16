8e3c59b1339e232d98c19750c9ce3098474541d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 14:06:53 +0000

    Add package odoo14-addon-ssi-service-risk-analysis/14.0.1.0.0/odoo14_addon_ssi_service_risk_analysis-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-service-risk-analysis/odoo14_addon_ssi_service_risk_analysis-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_service_risk_analysis/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Contract + Risk Analysis Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_service",
+        "ssi_risk_analysis",
+    ],
+    "data": [
+        # "views/crm_lead_views.xml",
+    ],
+    "demo": [],
+}
