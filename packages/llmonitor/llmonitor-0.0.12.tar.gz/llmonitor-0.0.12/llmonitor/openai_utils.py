abd19a2dbd88754dcfab01b2522193d2a5b22011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 19:11:53 +0000

    Add package llmonitor/0.0.12/llmonitor-0.0.12.tar.gz

---
+++ packages/llmonitor/llmonitor-0.0.12.tar.gz/llmonitor-0.0.12/llmonitor/openai_utils.py
@@ -0,0 +1,36 @@
+class OpenAIUtils:
+    @staticmethod
+    def parse_role(role):
+        if role == "assistant":
+            return "ai"
+        else:
+            return role
+
+    @staticmethod
+    def parse_message(message):
+        return {
+            "role": OpenAIUtils.parse_role(message["role"]), 
+            "text": message["content"]
+        }
+
+    @staticmethod
+    def parse_input(*args, **kwargs):
+        messages = [OpenAIUtils.parse_message(message) for message in kwargs["messages"]]
+        name = kwargs.get('model', None) or kwargs.get('engine', None)
+        return {
+            "name": name,
+            "input": messages,
+        }
+
+    @staticmethod
+    def parse_output(output):
+        return {
+            "output": {
+                "role": OpenAIUtils.parse_role(output.choices[0].message.role),
+                "text": output.choices[0].message.content,
+            },
+            "tokensUsage": {
+                "completion": output.usage.completion_tokens,
+                "prompt": output.usage.prompt_tokens,
+            },
+        }
