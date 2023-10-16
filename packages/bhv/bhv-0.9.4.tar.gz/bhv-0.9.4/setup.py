6d7b4bbec642a19d8fb26536a5343e5983bcc54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:06:06 +0000

    Add package bhv/0.9.4/bhv-0.9.4.tar.gz

---
+++ packages/bhv/bhv-0.9.4.tar.gz/bhv-0.9.4/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages, Extension
+
+VERSION = '0.9.4'
+DESCRIPTION = 'Boolean Hypervectors'
+LONG_DESCRIPTION = 'Boolean Hypervectors with various operators for experiments in hyperdimensional computing (HDC).'
+
+native = Extension("bhv.cnative",
+                   sources=['bhv/cnative/bindings.cpp',
+                            'bhv/cnative/TurboSHAKE_opt/TurboSHAKE.cpp',
+                            'bhv/cnative/TurboSHAKE_opt/KeccakP-1600-opt64.cpp',
+                            'bhv/cnative/TurboSHAKE_AVX512/TurboSHAKE.cpp',
+                            'bhv/cnative/TurboSHAKE_AVX512/KeccakP-1600-AVX512.cpp',
+                            ],
+                   include_dirs=['bhv/cnative', 'bhv/cnative/TurboSHAKEopt'],
+                   extra_compile_args=['-std=c++2a', '-O3', '-march=native', '-Wall'],
+                   language='c++',
+                   optional=True)
+
+setup(
+    name="bhv",
+    version=VERSION,
+    author="Adam Vandervorst",
+    author_email="contact@adamv.be",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/Adam-Vandervorst/PyBHV",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        "pytorch": ["torch>=2.0.0"],
+        "np": ["numpy>=1.24.2"],
+    },
+    keywords='ai binary hypervector hdc bsc',
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: Free for non-commercial use',
+
+        'Environment :: GPU :: NVIDIA CUDA',
+
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+
+        'Typing :: Typed',
+    ],
+    ext_modules=[native]
+)
