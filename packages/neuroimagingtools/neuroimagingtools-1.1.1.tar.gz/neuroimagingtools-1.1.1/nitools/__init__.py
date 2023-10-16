9fd4830b50a6c64976ee4c2137fcb64eed7c18f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:06:19 +0000

    Add package neuroimagingtools/1.1.1/neuroimagingtools-1.1.1.tar.gz

---
+++ packages/neuroimagingtools/neuroimagingtools-1.1.1.tar.gz/neuroimagingtools-1.1.1/nitools/__init__.py
@@ -0,0 +1,6 @@
+from nitools.volume import *
+from nitools.gifti import *
+from nitools.cifti import *
+from nitools.color import *
+from nitools.border import *
+
---
+++ packages/neuroimagingtools/neuroimagingtools-1.1.1.tar.gz/neuroimagingtools-1.1.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'neuroimagingtools',
+  packages = ['nitools'],
+  version = '1.1.1',
+  license='MIT',
+  description = 'Neuroimaging analysis tools',
+  author = 'Jörn Diedrichsen',
+  author_email = 'joern.diedrichsen@googlemail.com',
+  url = 'https://github.com/DiedrichsenLab/nitools',
+  download_url = 'https://github.com/DiedrichsenLab/nitools/archive/refs/tags/v1.1.1.tar.gz',
+  keywords = ['imaging analysis', 'nifti', 'gifti','cifti'],
+  install_requires=[
+          'bezier',
+          'trimesh',
+          'numpy',
+          'matplotlib',
+          'nibabel',
+          'pandas'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+  python_requires='>=3.6'
+)
