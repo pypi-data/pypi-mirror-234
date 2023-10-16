9e5a0ec4fce4337cd4b51dd3dea75579126b31ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 07:04:44 +0000

    Add package cluster-tools/0.13.7/cluster_tools-0.13.7-py3-none-any.whl

---
+++ packages/cluster-tools/cluster_tools-0.13.7-py3-none-any.whl/cluster_tools/executors/pickle_.py
@@ -0,0 +1,39 @@
+from concurrent.futures import Future
+from typing import Any, Callable, TypeVar
+
+from cluster_tools._utils import pickling
+from cluster_tools.executors.multiprocessing_ import MultiprocessingExecutor
+
+# The module name includes a _-suffix to avoid name clashes with the standard library pickle module.
+
+_T = TypeVar("_T")
+
+
+def _pickle_identity(obj: _T) -> _T:
+    return pickling.loads(pickling.dumps(obj))
+
+
+def _pickle_identity_executor(fn: Callable[..., _T], *args: Any, **kwargs: Any) -> _T:
+    result = fn(*args, **kwargs)
+    return _pickle_identity(result)
+
+
+class PickleExecutor(MultiprocessingExecutor):
+    """
+    The same as MultiprocessingExecutor, but always pickles input and output of the jobs.
+    When using this executor for automated tests, it is ensured that using cluster executors in production
+    won't provoke pickling-related problems.
+    """
+
+    def submit(  # type: ignore[override]
+        self,
+        fn: Callable[..., _T],
+        *args: Any,
+        **kwargs: Any,
+    ) -> "Future[_T]":
+        (fn_pickled, args_pickled, kwargs_pickled) = _pickle_identity(
+            (fn, args, kwargs)
+        )
+        return super().submit(
+            _pickle_identity_executor, fn_pickled, *args_pickled, **kwargs_pickled
+        )
