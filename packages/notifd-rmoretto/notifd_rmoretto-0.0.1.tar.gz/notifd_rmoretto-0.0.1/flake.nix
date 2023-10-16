7e6271fe597a249fb3e3d2a14f4acaeab45fd148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:08:05 +0000

    Add package notifd-rmoretto/0.0.1/notifd_rmoretto-0.0.1.tar.gz

---
+++ packages/notifd-rmoretto/notifd_rmoretto-0.0.1.tar.gz/notifd_rmoretto-0.0.1/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Simple DBus notification watcher service";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in rec {
+    packages = forAllSystems (system: {
+      default = nixpkgs.legacyPackages.${system}.callPackage ./nix/default.nix { };
+    });
+
+    devShells = forAllSystems (system: {
+      default = nixpkgs.legacyPackages.${system}.callPackage ./nix/shell.nix { };
+    });
+  };
+}
