6bece5bcfcd780f525bb88d4b7ae5fd94275abd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:07:27 +0000

    Add package modbus-wrapper/2.0.0/modbus-wrapper-2.0.0.tar.gz

---
+++ packages/modbus-wrapper/modbus-wrapper-2.0.0.tar.gz/modbus-wrapper-2.0.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='modbus-wrapper',
+    version='2.0.0',
+    description='wrapper for pyModbusTCP accepting all Modbus numbers with Fatek support',
+    author='Przemyslaw Bubas',
+    author_email='bubasenator@gmail.com',
+    url='https://github.com/pbubas/modbus_wrapper',
+    install_requires=['pyModbusTCP'],
+    keywords="modbus fatek",
+    python_requires='>=3.10'
+    )
