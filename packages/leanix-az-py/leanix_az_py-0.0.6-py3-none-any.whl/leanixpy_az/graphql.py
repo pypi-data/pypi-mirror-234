58444190c4305fb81dbf0030530d0ab3b16fcbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:09:57 +0000

    Add package leanix-az-py/0.0.6/leanix_az_py-0.0.6-py3-none-any.whl

---
+++ packages/leanix-az-py/leanix_az_py-0.0.6-py3-none-any.whl/leanixpy_az/graphql.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import base64
+
+
+#for running a graphql query with the auth header
+#json_data has to be given with json.dumps(data)
+def runGraphql(header, json_data):
+    headertoken = header["Authorization"]
+    encoded_payload = headertoken.split(".")[1]
+
+    # fix missing padding for this base64 encoded string.
+    # If number of bytes is not dividable by 4, append '=' until it is.
+    missing_padding = len(encoded_payload) % 4
+    if missing_padding != 0:
+        encoded_payload += '='* (4 - missing_padding)
+    payload = json.loads(base64.b64decode(encoded_payload))
+    base_url = payload["instanceUrl"]
+
+
+    request_url = base_url+'/services/pathfinder/v1/graphql'
+    try:
+        response = requests.post(url=request_url, headers=header, data=json_data)
+        response.raise_for_status()
+    except Exception as ex:
+        raise
+    return response
