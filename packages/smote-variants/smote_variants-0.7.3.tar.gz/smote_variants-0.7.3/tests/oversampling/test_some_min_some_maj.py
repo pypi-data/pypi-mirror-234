d1b8d3af62faf30ddfcbfd4ccc94d4e688282774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 13:12:37 +0000

    Add package smote-variants/0.7.3/smote_variants-0.7.3.tar.gz

---
+++ packages/smote-variants/smote_variants-0.7.3.tar.gz/smote_variants-0.7.3/tests/oversampling/test_some_min_some_maj.py
@@ -0,0 +1,30 @@
+"""
+Tests oversamplers with a few samples.
+"""
+
+import pytest
+import numpy as np
+
+import smote_variants as sv
+
+from smote_variants.datasets import load_some_min_some_maj
+
+# disabling smote-variants logging
+import logging
+logger = logging.getLogger('smote_variants')
+logger.setLevel(logging.CRITICAL)
+
+dataset = load_some_min_some_maj()
+
+@pytest.mark.parametrize("smote_class", sv.get_all_oversamplers())
+def test_some_min_some_maj(smote_class):
+    """
+    Tests an oversampler with only a few samples.
+
+    Args:
+        smote_class (class): the oversampler class
+    """
+    X, y = smote_class().sample(dataset['data'], dataset['target'])
+
+    assert np.unique(y).shape[0] == 2
+    assert X.shape[0] > 0
