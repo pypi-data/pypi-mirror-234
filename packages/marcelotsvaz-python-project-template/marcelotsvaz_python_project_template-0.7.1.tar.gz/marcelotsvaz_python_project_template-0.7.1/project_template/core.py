000cf392d1dff88f90bc85298fefafaea52fb0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:08:08 +0000

    Add package marcelotsvaz-python-project-template/0.7.1/marcelotsvaz_python_project_template-0.7.1.tar.gz

---
+++ packages/marcelotsvaz-python-project-template/marcelotsvaz_python_project_template-0.7.1.tar.gz/marcelotsvaz_python_project_template-0.7.1/project_template/core.py
@@ -0,0 +1,24 @@
+'''
+TODO Module docstring.
+'''
+
+class MyClass:
+	'''
+	TODO Class docstring.
+	'''
+	
+	@classmethod
+	def invertCase( cls, text: str ) -> str:
+		'''
+		TODO Method docstring.
+		'''
+		
+		invertedText: list[str] = []
+		
+		for letter in text:
+			if letter.isupper():
+				invertedText.append( letter.lower() )
+			else:
+				invertedText.append( letter.upper() )
+		
+		return ''.join( invertedText )
