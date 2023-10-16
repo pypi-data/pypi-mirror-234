c68fdcc225b49b1c67aea66b57f2c843dae06a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 15:08:52 +0000

    Add package pipecli/0.0.2/pipecli-0.0.2.tar.gz

---
+++ packages/pipecli/pipecli-0.0.2.tar.gz/pipecli-0.0.2/pipecli/command/beiguo.py
@@ -0,0 +1,19 @@
+#! -*- coding: utf-8 -*-
+# 根据pid找所属container（docker）
+
+import glob, os
+
+def find_container(pid): 
+    dockers = glob.glob('/sys/fs/cgroup/memory/docker/*/cgroup.procs')
+    container_id = None
+    
+    for docker in dockers:
+        if pid in open(docker).read().strip().split('\n'):
+            container_id = docker[29:-13]
+            break
+    
+    if container_id is None:
+        print(u'找不到对应的container')
+    else:
+        commandline = 'docker ps -a --format "{{.ID}}: {{.Names}}" --filter id="%s"'
+        os.system(commandline % container_id)
