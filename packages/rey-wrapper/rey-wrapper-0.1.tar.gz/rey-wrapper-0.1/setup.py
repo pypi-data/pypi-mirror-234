bf398adc146252cbc306a4dcb62c015a52e4977d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 20:09:44 +0000

    Add package rey-wrapper/0.1/rey-wrapper-0.1.tar.gz

---
+++ packages/rey-wrapper/rey-wrapper-0.1.tar.gz/rey-wrapper-0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name = "rey-wrapper",         
+    packages = ["rey"],   
+    version = "0.1",      
+    license="MIT",    
+    description = "An API wrapper designed to simplify the process of integrating Line Messaging API",
+    author = "TheGenocide",
+    author_email = "luke.genesis.hyder@gmail.com",
+    url = "https://github.com/TheGenocides/rey",
+    download_url = "https://github.com/TheGenocides/rey/archive/refs/tags/v0.1.tar.gz",
+    keywords = ["line-bot", "apiwrapper", "line", "api"],
+    install_requires=[
+        "aiohttp"
+    ],
+    classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",      
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11"
+
+    ],
+)
