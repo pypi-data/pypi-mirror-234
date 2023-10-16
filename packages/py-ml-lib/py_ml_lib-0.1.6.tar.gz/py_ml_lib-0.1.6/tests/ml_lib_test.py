c11a7f2027879d0b6e222a0d0bbeb5ecf9536954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 12:07:35 +0000

    Add package py-ml-lib/0.1.6/py_ml_lib-0.1.6.tar.gz

---
+++ packages/py-ml-lib/py_ml_lib-0.1.6.tar.gz/py_ml_lib-0.1.6/tests/ml_lib_test.py
@@ -0,0 +1,27 @@
+import sys
+import logging
+import importlib
+#sys.path.append("C:\\dev\\ml\\ml-py\\venv\\Lib\\site-packages")
+
+logger = logging.getLogger('ml_py')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+#import marcuslion
+from marcuslion import main
+
+if __name__ == '__main__':
+    logger.info('Start marcuslion test')
+    try:
+        #pathToModules = "c:\\dev\\ml\\ml-py\\venv\\lib\\site-packages\\"
+        #sys.path.append(pathToModules)
+        #py_ml_lib = __import__("marcuslion")
+        #mod = importlib.import_module(pathToModules + "marcuslion")
+        #import py-ml-lib
+        a = main.add_one(5)
+        print("call add one ", a)
+
+        main.openml()
+    except Exception as e:
+        #print(e, '|', e.errno, '|', e.value, '|', e.args)
+        print("Exception ", e)
