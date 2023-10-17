7702c3ecbcb702a9fcb771c8687b8cd0ef2434b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 01:28:17 +0000

    Add package metaphor-connectors/0.13.2/metaphor_connectors-0.13.2.tar.gz

---
+++ packages/metaphor-connectors/metaphor_connectors-0.13.2.tar.gz/metaphor_connectors-0.13.2/metaphor/thought_spot/config.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from pydantic import validator
+from pydantic.dataclasses import dataclass
+
+from metaphor.common.base_config import BaseConfig
+from metaphor.common.dataclass import ConnectorConfig
+
+
+@dataclass(config=ConnectorConfig)
+class ThoughtSpotRunConfig(BaseConfig):
+    user: str
+
+    # ThoughtSpot instance url
+    base_url: str
+
+    secret_key: Optional[str] = None
+    password: Optional[str] = None
+
+    @validator("password")
+    def check_password_or_secret_key(cls, password, values):
+        if "secret_key" not in values and not password:
+            raise ValueError("Either password or secret_key is required")
+        return password
