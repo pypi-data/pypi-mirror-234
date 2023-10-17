dac6f2aa93e830ae3c2c1f3e99c75d387d474d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:16:33 +0000

    Add package database-ex-sar-shane-forex-next3/1.1/database_ex_sar_shane_forex_next3-1.1.tar.gz

---
+++ packages/database-ex-sar-shane-forex-next3/database_ex_sar_shane_forex_next3-1.1.tar.gz/database_ex_sar_shane_forex_next3-1.1/database_ex_sar_shane_forex_next3/__init__.py
@@ -0,0 +1,1 @@
+from .database_ex_sar_shane_forex_next3 import Database
---
+++ packages/database-ex-sar-shane-forex-next3/database_ex_sar_shane_forex_next3-1.1.tar.gz/database_ex_sar_shane_forex_next3-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name='database_ex_sar_shane_forex_next3' ,
+       version = '1.1' ,
+       description = 'database_ex_sar_shane_forex_next3' ,
+       author = 'omid' ,
+       packages =['database_ex_sar_shane_forex_next3'] ,
+       zip_safe = False
+       
+       )
