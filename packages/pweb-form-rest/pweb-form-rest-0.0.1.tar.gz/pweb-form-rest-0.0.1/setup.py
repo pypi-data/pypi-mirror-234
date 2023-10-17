397c501b9cb2bf32b7250c495fc315c30b502b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:07:57 +0000

    Add package pweb-form-rest/0.0.1/pweb-form-rest-0.0.1.tar.gz

---
+++ packages/pweb-form-rest/pweb-form-rest-0.0.1.tar.gz/pweb-form-rest-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+import pathlib
+
+CURRENT_DIR = pathlib.Path(__file__).parent
+README = (CURRENT_DIR / "README.md").read_text()
+
+env = os.environ.get('source')
+
+
+def get_dependencies():
+    dependency = [
+        'marshmallow==3.20.1',
+        'apispec==6.3.0',
+        'marshmallow-sqlalchemy'
+    ]
+
+    if env and env == "code":
+        return dependency
+
+    return dependency + ["ppy-common", "pweb-orm", "ppy-file-text"]
+
+
+setup(
+    name='pweb-form-rest',
+    version='0.0.1',
+    url='https://github.com/problemfighter/pweb-form-rest',
+    license='Apache 2.0',
+    author='Problem Fighter',
+    author_email='problemfighter.com@gmail.com',
+    description='Make Form & REST API convention, Validate request and produce response for PWeb application. It can produce Swagger API Documentation as well',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=get_dependencies(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ]
+)
