9016d88d994b73ccce7d4aa0107bb44515b955b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:16:09 +0000

    Add package RefractXAI/1.0.8/RefractXAI-1.0.8.tar.gz

---
+++ packages/RefractXAI/RefractXAI-1.0.8.tar.gz/RefractXAI-1.0.8/xai/init_script.sh
@@ -0,0 +1,10 @@
+echo "Installing packaeges"
+pip install bs4==0.0.1
+
+pip install refractio -t /packages/custom_plugin/refractio
+pip install requests -t /packages/custom_plugin/refractio
+
+export PYTHONPATH="$PYTHONPATH:/packages/custom_plugin/refractio"
+
+echo "Done with dependency installtion!";
+
