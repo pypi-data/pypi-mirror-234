e5e9d33d38fc00e0b1f08cd6dc937dddb2f69127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:11:33 +0000

    Add package pinksale-python/0.1.4/pinksale-python-0.1.4.tar.gz

---
+++ packages/pinksale-python/pinksale-python-0.1.4.tar.gz/pinksale-python-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pinksale-python",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "web3"
+    ],
+    url="https://github.com/hkey0/pinksale-python",
+    author="hkey",
+    description=" The unofficial Python client for the Pinksale.",
+    long_description=open('readme.md').read(),
+    long_description_content_type="text/markdown",
+)
