245a7cd513fe37aac337321c26dc09dec3e37ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 11:11:25 +0000

    Add package topngetterkpanger882/0.0.1/topngetterkpanger882-0.0.1-py3-none-any.whl

---
+++ packages/topngetterkpanger882/topngetterkpanger882-0.0.1-py3-none-any.whl/topngetterkpanger882/top_n_getter.py
@@ -0,0 +1,8 @@
+from list_maker_package.make_list import make_list
+
+def get_top_n(n, *args):
+    sorted_list = make_list(item for item in args)
+    return sorted_list[:n]
+
+if __name__ == '__main__':
+    make_list(1,3,5)
