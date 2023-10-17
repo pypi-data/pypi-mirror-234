1f8bfb2f9cdce9f1ae46683478d110ade7f4e366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:08:09 +0000

    Add package mis-test-package/0.0.1/mis_test_package-0.0.1.tar.gz

---
+++ packages/mis-test-package/mis_test_package-0.0.1.tar.gz/mis_test_package-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='mis_test_package',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # add any dependencies here
+    ],
+)
