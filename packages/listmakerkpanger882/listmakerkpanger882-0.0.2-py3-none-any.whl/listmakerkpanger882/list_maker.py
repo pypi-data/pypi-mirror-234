f8228911deff4e137aa85370f8cfefb8c070a1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 11:07:43 +0000

    Add package listmakerkpanger882/0.0.2/listmakerkpanger882-0.0.2-py3-none-any.whl

---
+++ packages/listmakerkpanger882/listmakerkpanger882-0.0.2-py3-none-any.whl/listmakerkpanger882/list_maker.py
@@ -0,0 +1,6 @@
+def make_list(*args):
+    mylist = [item for item in args]
+    # The following line of code added in version 2, absent in version 1,
+    # Meaning the new version is incompatible with the old version.
+    mylist.sort()
+    return mylist
