49d96dca17312d42accd349d8e4ddf9332ffe2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:10:11 +0000

    Add package pycoilgen/0.1.3/pycoilgen-0.1.3-py3-none-any.whl

---
+++ packages/pycoilgen/pycoilgen-0.1.3-py3-none-any.whl/pyCoilGen/__init__.py
@@ -0,0 +1,3 @@
+"""Magnetic Field Coil Generator for Python."""
+# Semantic Versioning according to https://semver.org/spec/v2.0.0.html
+__version__ = "0.1.3" # Patch level, due to bugfixes from CoilGen. Issue #60
