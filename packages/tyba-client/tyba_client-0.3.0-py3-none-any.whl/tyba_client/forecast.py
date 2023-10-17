3c40dca3fac2ae678cd1aaa8f833d69f3aee5b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 09:10:34 +0000

    Add package tyba-client/0.3.0/tyba_client-0.3.0-py3-none-any.whl

---
+++ packages/tyba-client/tyba_client-0.3.0-py3-none-any.whl/tyba_client/forecast.py
@@ -0,0 +1,25 @@
+from datetime import datetime, time
+
+
+class Forecast(object):
+    def __init__(self, client):
+        self.client = client
+
+    def get(self, route, params=None):
+        return self.client.get(f"forecasts/{route}", params=params)
+
+    def most_recent(self, object_name: str, product: str, start_time: datetime, end_time: datetime):
+        return self.get("most_recent_forecast", params={"object_name": object_name,
+                                                        "product": product,
+                                                        "start_time": start_time,
+                                                        "end_time": end_time})
+
+    def vintaged(self, object_name: str, product: str, start_time: datetime, end_time: datetime, days_ago: int,
+                 before_time: time):
+        return self.get("vintaged_forecast", params={"object_name": object_name,
+                                                     "product": product,
+                                                     "start_time": start_time,
+                                                     "end_time": end_time,
+                                                     "days_ago": days_ago,
+                                                     "before_time": before_time,
+                                                     })
