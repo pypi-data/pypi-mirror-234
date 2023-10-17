415a51ced725ff47482af4943f70a6184e54ebc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 11:08:05 +0000

    Add package datamakerkpanger882/0.0.1/datamakerkpanger882-0.0.1-py3-none-any.whl

---
+++ packages/datamakerkpanger882/datamakerkpanger882-0.0.1-py3-none-any.whl/datamakerkpanger882/data.py
@@ -0,0 +1,14 @@
+import listmakerkpanger882
+
+
+class Data:
+    def __init__(self, *args):
+        self.data = make_list(args)
+
+    def find_item(self, search_item):
+        index_list = []
+        for index, item in enumerate(self.data):
+            if item == search_item:
+                index_list.append(item)
+        return index_list
+
