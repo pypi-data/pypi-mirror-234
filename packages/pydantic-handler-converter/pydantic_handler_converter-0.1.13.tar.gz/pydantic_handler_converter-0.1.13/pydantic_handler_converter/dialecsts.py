6a9bb89bca0f4b537fd2217b7fdceff84d6a7740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:07:56 +0000

    Add package pydantic-handler-converter/0.1.13/pydantic_handler_converter-0.1.13.tar.gz

---
+++ packages/pydantic-handler-converter/pydantic_handler_converter-0.1.13.tar.gz/pydantic_handler_converter-0.1.13/pydantic_handler_converter/dialecsts.py
@@ -0,0 +1,21 @@
+from abc import ABC
+
+
+class BaseDialects(ABC):
+    INPUT = 'input'
+    INPUT_STR = 'input string for `{field_name}`'
+    INPUT_INT = 'input number for `{field_name}`'
+    INPUT_FLOAT = 'input number for `{field_name}`'
+    CHOOSE = 'choose value of `{field_name}`'
+    CHOOSE_FROM_ENUM = 'choose value of `{field_name}`'
+    CHOOSE_FROM_ENUM_OR_INPUT = 'choose or input value of `{field_name}`'
+    BACK_BUTTON = 'back'
+    READY_BUTTON = 'ready'
+    READY_BUTTON_DATA = 'ready'
+    SKIP_BUTTON = 'skip'
+    SKIP_STEP_DATA = 'skip_step'
+
+
+class EnDialects(BaseDialects):
+    pass
+
