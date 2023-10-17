d529e2b21538bd543bbe3df9656bf9e3c76047fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 09:10:26 +0000

    Add package aind-data-schema/0.15.5/aind_data_schema-0.15.5-py3-none-any.whl

---
+++ packages/aind-data-schema/aind_data_schema-0.15.5-py3-none-any.whl/aind_data_schema/behavior/behavior_rig.py
@@ -0,0 +1,40 @@
+""" Schemas for Behavior Rig. This is being deprecated after 2023-11-01. """
+
+from __future__ import annotations
+
+from typing import List, Optional, Union
+
+from pydantic import Field
+
+from aind_data_schema.base import AindCoreModel
+from aind_data_schema.device import (
+    Calibration,
+    CameraAssembly,
+    DAQDevice,
+    Device,
+    Disc,
+    HarpDevice,
+    Monitor,
+    RewardDelivery,
+    Speaker,
+    Treadmill,
+    Tube,
+)
+
+
+class BehaviorRig(AindCoreModel):
+    """Description of an behavior rig. This is being deprecated after 2023-11-01."""
+
+    schema_version: str = Field("0.1.13", description="schema version", title="Version", const=True)
+    rig_id: str = Field(..., description="room_stim apparatus_version", title="Rig ID")
+    mouse_platform: Union[Tube, Treadmill, Disc] = Field(..., title="Mouse platform")
+    stimulus_devices: List[Union[RewardDelivery, Monitor, Speaker]] = Field(
+        ...,
+        title="Stimulus devices",
+        unique_items=True,
+    )
+    cameras: Optional[List[CameraAssembly]] = Field(None, title="Camera assemblies", unique_items=True)
+    daqs: Optional[List[Union[HarpDevice, DAQDevice]]] = Field(None, title="Data acquisition devices")
+    additional_devices: Optional[List[Device]] = Field(None, title="Additional devices", unique_items=True)
+    calibrations: List[Calibration] = Field(..., title="Full calibration of devices", unique_items=True)
+    notes: Optional[str] = Field(None, title="Notes")
