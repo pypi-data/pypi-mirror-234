9b6ee190d59c4faf875ed752ef2d4288990e3869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 02:15:27 +0000

    Add package CodeTemplate/1.0.0/CodeTemplate-1.0.0.tar.gz

---
+++ packages/CodeTemplate/CodeTemplate-1.0.0.tar.gz/CodeTemplate-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='CodeTemplate',
+    version='1.0.0',
+    description='A simple Template for crate python project',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['python'],
+)
