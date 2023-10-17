5f47d79ee891f1d345470a84d0cc6096e0c6092d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:08:01 +0000

    Add package execdata/2.1.0/execdata-2.1.0.tar.gz

---
+++ packages/execdata/execdata-2.1.0.tar.gz/execdata-2.1.0/execdata/__init__.py
@@ -0,0 +1,16 @@
+'''
+Date         : 2022-10-25 15:44:41
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-10-11 13:52:14
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\__init__.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
+'''
+
+from execdata.templateproj import add_one
+from execdata.data_conversion import convint, convfloat
+from execdata.data_preprocess import filtered_value_list
+from execdata.standardization import encode, sep, split, strat_split
+from execdata.model_evaluate import model_evaluate, algo_accuracy, result_comparision
---
+++ packages/execdata/execdata-2.1.0.tar.gz/execdata-2.1.0/execdata/data_preprocess.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+import pandas as pd
+
+
+def filtered_value_list(df, column, limit_number):
+    value_counts_series = df[column].value_counts()
+    filtered_value_counts = value_counts_series[value_counts_series < limit_number]
+    filtered_value_counts_list = filtered_value_counts.reset_index().values.tolist()
+    return filtered_value_counts, filtered_value_counts_list
