8da3d4ea8e0e707301b9c6c50b86d62603013f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:06:56 +0000

    Add package heaserver-activity/1.0.0b11/heaserver-activity-1.0.0b11.tar.gz

---
+++ packages/heaserver-activity/heaserver-activity-1.0.0b11.tar.gz/heaserver-activity-1.0.0b11/tests/heaserver/activitytest/test_all.py
@@ -0,0 +1,13 @@
+from .testcase import TestCase, RecentlyAccessedViewsByTypeTestCase
+from heaserver.service.testcase.mixin import GetOneMixin, GetAllMixin
+
+
+class TestGet(TestCase, GetOneMixin):
+    pass
+
+
+class TestGetAll(TestCase, GetAllMixin):
+    pass
+
+class TestRecentlyAccessedGetAll(RecentlyAccessedViewsByTypeTestCase, GetAllMixin):
+    pass
