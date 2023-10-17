73fdc5baa090192a046d47442e9f1000b7a5692f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:11:44 +0000

    Add package ingenialink/7.0.4/ingenialink-7.0.4-py3-none-any.whl

---
+++ packages/ingenialink/ingenialink-7.0.4-py3-none-any.whl/ingenialink/canopen/servo.py
@@ -0,0 +1,159 @@
+import canopen
+from canopen.emcy import EmcyConsumer
+
+from ingenialink.constants import CAN_MAX_WRITE_SIZE
+from ingenialink.exceptions import ILAccessError, ILIOError
+from ingenialink.utils._utils import convert_bytes_to_dtype, convert_dtype_to_bytes
+from ingenialink.servo import Servo
+from ingenialink.canopen.dictionary import CanopenDictionary
+from ingenialink.canopen.register import CanopenRegister
+from ingenialink.enums.register import REG_DTYPE, REG_ACCESS
+
+import ingenialogger
+
+logger = ingenialogger.get_logger(__name__)
+
+CANOPEN_SDO_RESPONSE_TIMEOUT = 0.3
+
+
+class CanopenServo(Servo):
+    """CANopen Servo instance.
+
+    Args:
+        target (int): Node ID to be connected.
+        node (canopen.RemoteNode): Remote Node of the drive.
+        dictionary_path (str): Path to the dictionary.
+        servo_status_listener (bool): Toggle the listener of the servo for
+            its status, errors, faults, etc.
+
+    """
+
+    DICTIONARY_CLASS = CanopenDictionary
+    MAX_WRITE_SIZE = CAN_MAX_WRITE_SIZE
+
+    STATUS_WORD_REGISTERS = "CIA402_DRV_STATE_STATUS"
+    RESTORE_COCO_ALL = "CIA301_COMMS_RESTORE_ALL"
+    STORE_COCO_ALL = "CIA301_COMMS_STORE_ALL"
+    MONITORING_DATA = CanopenRegister(
+        identifier="",
+        units="",
+        subnode=0,
+        idx=0x58B2,
+        subidx=0x00,
+        cyclic="CONFIG",
+        dtype=REG_DTYPE.U16,
+        access=REG_ACCESS.RO,
+    )
+    DIST_DATA = CanopenRegister(
+        identifier="",
+        units="",
+        subnode=0,
+        idx=0x58B4,
+        subidx=0x00,
+        cyclic="CONFIG",
+        dtype=REG_DTYPE.U16,
+        access=REG_ACCESS.RW,
+    )
+
+    def __init__(self, target, node, dictionary_path=None, servo_status_listener=False):
+        self.__node = node
+        self.__emcy_consumer = EmcyConsumer()
+        super(CanopenServo, self).__init__(target, dictionary_path, servo_status_listener)
+
+    def read(self, reg, subnode=1):
+        value = super().read(reg, subnode=subnode)
+        if isinstance(value, str):
+            value = value.replace("\x00", "")
+        return value
+
+    def store_parameters(self, subnode=None, sdo_timeout=3):
+        """Store all the current parameters of the target subnode.
+
+        Args:
+            subnode (int): Subnode of the axis. `None` by default which stores
+            all the parameters.
+            sdo_timeout (int): Timeout value for each SDO response.
+
+        Raises:
+            ILError: Invalid subnode.
+            ILObjectNotExist: Failed to write to the registers.
+
+        """
+        self._change_sdo_timeout(sdo_timeout)
+        super().store_parameters(subnode)
+        self._change_sdo_timeout(CANOPEN_SDO_RESPONSE_TIMEOUT)
+
+    def _write_raw(self, reg, data):
+        try:
+            self._lock.acquire()
+            self.__node.sdo.download(reg.idx, reg.subidx, data)
+        except Exception as e:
+            logger.error("Failed writing %s. Exception: %s", str(reg.identifier), e)
+            error_raised = f"Error writing {reg.identifier}"
+            raise ILIOError(error_raised)
+        finally:
+            self._lock.release()
+
+    def _read_raw(self, reg):
+        try:
+            self._lock.acquire()
+            value = self.__node.sdo.upload(reg.idx, reg.subidx)
+        except Exception as e:
+            logger.error("Failed reading %s. Exception: %s", str(reg.identifier), e)
+            error_raised = f"Error reading {reg.identifier}"
+            raise ILIOError(error_raised)
+        finally:
+            self._lock.release()
+        return value
+
+    def emcy_subscribe(self, cb):
+        """Subscribe to emergency messages.
+
+        Args:
+            cb: Callback
+
+        Returns:
+            int: Assigned slot.
+
+        """
+        self.__emcy_consumer.add_callback(cb)
+
+        return len(self.__emcy_consumer.callbacks) - 1
+
+    def emcy_unsubscribe(self, slot):
+        """Unsubscribe from emergency messages.
+
+        Args:
+            slot (int): Assigned slot when subscribed.
+
+        """
+        del self.__emcy_consumer.callbacks[slot]
+
+    def _change_sdo_timeout(self, value):
+        """Changes the SDO timeout of the node."""
+        self.__node.sdo.RESPONSE_TIMEOUT = value
+
+    @staticmethod
+    def __monitoring_disturbance_map_can_address(address, subnode):
+        """Map CAN register address to IPB register address."""
+        return address - (0x2000 + (0x800 * (subnode - 1)))
+
+    def _monitoring_disturbance_data_to_map_register(self, subnode, address, dtype, size):
+        """Arrange necessary data to map a monitoring/disturbance register.
+
+        Args:
+            subnode (int): Subnode to be targeted.
+            address (int): Register address to map.
+            dtype (int): Register data type.
+            size (int): Size of data in bytes.
+
+        """
+        ipb_address = self.__monitoring_disturbance_map_can_address(address, subnode)
+        return super()._monitoring_disturbance_data_to_map_register(
+            subnode, ipb_address, dtype, size
+        )
+
+    @property
+    def node(self):
+        """canopen.RemoteNode: Remote node of the servo."""
+        return self.__node
---
+++ packages/ingenialink/ingenialink-7.0.4-py3-none-any.whl/ingenialink/utils/_utils.py
@@ -0,0 +1,282 @@
+import struct
+from enum import Enum
+from typing import Union
+
+from ingenialink.exceptions import ILValueError
+from ingenialink.enums.register import REG_DTYPE
+from time import sleep
+
+import warnings
+import functools
+import ingenialogger
+
+logger = ingenialogger.get_logger(__name__)
+
+POLLING_MAX_TRIES = 5  # Seconds
+
+__dtype_value = {
+    REG_DTYPE.U8: (1, False),
+    REG_DTYPE.S8: (1, True),
+    REG_DTYPE.U16: (2, False),
+    REG_DTYPE.S16: (2, True),
+    REG_DTYPE.U32: (4, False),
+    REG_DTYPE.S32: (4, True),
+    REG_DTYPE.U64: (8, False),
+    REG_DTYPE.S64: (8, True),
+    REG_DTYPE.FLOAT: (4, None),
+}
+
+
+def deprecated(custom_msg=None, new_func_name=None):
+    """This is a decorator which can be used to mark functions as deprecated.
+    It will result in a warning being emitted when the function is used. We use
+    this decorator instead of any deprecation library because all libraries raise
+    a DeprecationWarning but since by default this warning is hidden, we use this
+    decorator to manually activate DeprecationWarning and turning it off after
+    the warn has been done."""
+
+    def wrap(func):
+        @functools.wraps(func)
+        def wrapped_method(*args, **kwargs):
+            warnings.simplefilter("always", DeprecationWarning)  # Turn off filter
+            msg = 'Call to deprecated function "{}".'.format(func.__name__)
+            if new_func_name:
+                msg += ' Please, use "{}" function instead.'.format(new_func_name)
+            if custom_msg:
+                msg = custom_msg
+            warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
+            warnings.simplefilter("ignore", DeprecationWarning)  # Reset filter
+            return func(*args, **kwargs)
+
+        return wrapped_method
+
+    return wrap
+
+
+def to_ms(s):
+    """Convert from seconds to milliseconds.
+
+    Args:
+        s (float, int): Value in seconds.
+
+    Returns:
+        int: Value in milliseconds.
+    """
+    return int(s * 1e3)
+
+
+def wait_for_register_value(servo, subnode, register, expected_value):
+    """Waits for the register to reach a value.
+
+    Args:
+        servo (Servo): Instance of the servo to be used.
+        subnode (int): Target subnode.
+        register (Register): Register to be read.
+        expected_value (int, float, str): Expected value for the given register.
+
+    Returns:
+        int: Return code of the operation.
+    """
+    logger.debug("Waiting for register {} to return <{}>".format(register, expected_value))
+    num_tries = 0
+    r = -2
+    while num_tries < POLLING_MAX_TRIES:
+        value = None
+        try:
+            value = servo.read(register, subnode=subnode)
+            r = 0
+        except Exception as e:
+            r = -1
+
+        if r >= 0:
+            if value == expected_value:
+                logger.debug("Success. Read value {}.".format(value))
+                break
+            else:
+                r = -2
+        num_tries += 1
+        logger.debug("Trying again {}. r: {}. value {}.".format(num_tries, r, value))
+        sleep(1)
+
+    return r
+
+
+def count_file_lines(file_path):
+    """Count how many lines the given file has.
+
+    Args:
+        file_path (str): Path to the target file.
+
+    Returns:
+        int: Number of lines in the file.
+
+    """
+    file = open(file_path, "r")
+    total_lines = 0
+    for _ in file:
+        total_lines += 1
+    file.close()
+    return total_lines
+
+
+def remove_xml_subelement(element, subelement):
+    """Removes a subelement from the given element the element contains the subelement
+
+    Args:
+        element (Element): Element to be extracted from.
+        subelement (Element): Element to be extracted.
+    """
+    if subelement is not None and subelement in element:
+        element.remove(subelement)
+
+
+def pop_element(dictionary, element):
+    """Pops an element from a dictionary only if it is contained in it
+
+    Args:
+        dictionary (dict): Dictionary containing all the elment.s
+        element (str): Element to be poped from the dictionary.
+    """
+    if element in dictionary:
+        dictionary.pop(element)
+
+
+def cleanup_register(register):
+    """Cleans a ElementTree register to remove all
+    unnecessary fields for a configuration file
+
+    Args:
+        register (Register): Register to be cleaned.
+    """
+    labels = register.find("./Labels")
+    range = register.find("./Range")
+    enums = register.find("./Enumerations")
+
+    remove_xml_subelement(register, labels)
+    remove_xml_subelement(register, enums)
+    remove_xml_subelement(register, range)
+
+    pop_element(register.attrib, "desc")
+    pop_element(register.attrib, "cat_id")
+    pop_element(register.attrib, "cyclic")
+    pop_element(register.attrib, "units")
+    pop_element(register.attrib, "address_type")
+
+    register.text = ""
+
+
+def get_drive_identification(servo, subnode=None):
+    """Gets the identification information of a given subnode.
+
+    Args:
+        servo: Instance of the servo Class.
+        subnode: subnode to be targeted.
+
+    Returns:
+        int, int: Product code and revision number of the targeted subnode.
+    """
+    prod_code = None
+    re_number = None
+    try:
+        if subnode is None or subnode == 0:
+            prod_code = servo.read("DRV_ID_PRODUCT_CODE_COCO", 0)
+            re_number = servo.read("DRV_ID_REVISION_NUMBER_COCO", 0)
+        else:
+            prod_code = servo.read("DRV_ID_PRODUCT_CODE", subnode=subnode)
+            re_number = servo.read("DRV_ID_REVISION_NUMBER", subnode)
+    except Exception as e:
+        pass
+
+    return prod_code, re_number
+
+
+def convert_ip_to_int(ip):
+    """Converts a string type IP to its integer value.
+
+    Args:
+        ip (str): IP to be converted.
+
+    """
+    split_ip = ip.split(".")
+    drive_ip1 = int(split_ip[0]) << 24
+    drive_ip2 = int(split_ip[1]) << 16
+    drive_ip3 = int(split_ip[2]) << 8
+    drive_ip4 = int(split_ip[3])
+    return drive_ip1 + drive_ip2 + drive_ip3 + drive_ip4
+
+
+def convert_int_to_ip(int_ip):
+    """Converts an integer type IP to its string form.
+
+    Args:
+        int_ip (int): IP to be converted.
+
+    """
+    drive_ip1 = (int_ip >> 24) & 0x000000FF
+    drive_ip2 = (int_ip >> 16) & 0x000000FF
+    drive_ip3 = (int_ip >> 8) & 0x000000FF
+    drive_ip4 = int_ip & 0x000000FF
+    return "{}.{}.{}.{}".format(drive_ip1, drive_ip2, drive_ip3, drive_ip4)
+
+
+class INT_SIZES(Enum):
+    """Integer sizes."""
+
+    S8_MIN = -128
+    S16_MIN = -32767 - 1
+    S32_MIN = -2147483647 - 1
+    S64_MIN = 9223372036854775807 - 1
+
+    S8_MAX = 127
+    S16_MAX = 32767
+    S32_MAX = 2147483647
+    S64_MAX = 9223372036854775807
+
+    U8_MAX = 255
+    U16_MAX = 65535
+    U32_MAX = 4294967295
+    U64_MAX = 18446744073709551615
+
+
+def convert_bytes_to_dtype(data: bytes, dtype: REG_DTYPE) -> Union[float, int, str]:
+    """Convert data in bytes to corresponding dtype.
+
+    Args:
+        data: data to convert
+        dtype: output dtype
+
+    Raises:
+        ILValueError: If data can't be decoded in utf-8
+    """
+    signed = None
+    if dtype in __dtype_value:
+        bytes_length, signed = __dtype_value[dtype]
+        data = data[:bytes_length]
+
+    if dtype == REG_DTYPE.FLOAT:
+        [value] = struct.unpack("f", data)
+    elif dtype == REG_DTYPE.STR:
+        try:
+            value = data.split(b"\x00")[0].decode("utf-8")
+        except UnicodeDecodeError as e:
+            raise ILValueError(f"Can't decode {e.object} to utf-8 string") from e
+    else:
+        value = int.from_bytes(data, "little", signed=signed)
+    return value
+
+
+def convert_dtype_to_bytes(data, dtype):
+    """Convert data in dtype to bytes.
+    Args:
+        data: Data to convert.
+        dtype (REG_DTYPE): Data type.
+    """
+    if dtype == REG_DTYPE.DOMAIN:
+        return data
+    if dtype == REG_DTYPE.FLOAT:
+        return struct.pack("f", float(data))
+    if dtype == REG_DTYPE.STR:
+        return data.encode("utf_8")
+    bytes_length, signed = __dtype_value[dtype]
+    data = data.to_bytes(bytes_length, byteorder="little", signed=signed)
+    return data
