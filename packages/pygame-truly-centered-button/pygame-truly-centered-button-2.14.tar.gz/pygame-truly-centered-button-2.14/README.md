c002ca5d708520c9b25bff23356344912f4f5112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 11:07:28 +0000

    Add package pygame-truly-centered-button/2.14/pygame-truly-centered-button-2.14.tar.gz

---
+++ packages/pygame-truly-centered-button/pygame-truly-centered-button-2.14.tar.gz/pygame-truly-centered-button-2.14/README.md
@@ -0,0 +1,9 @@
+# DEPRECATED LIBRARY
+
+**New Link**: https://pypi.org/project/pygame-true-centering
+
+### Shell
+```
+pip install pygame-text-centering
+```
+                                    
---
+++ packages/pygame-truly-centered-button/pygame-truly-centered-button-2.14.tar.gz/pygame-truly-centered-button-2.14/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygame-truly-centered-button',
+    version='2.14',
+    packages=find_packages(),
+    install_requires=[
+        'pygame', 'numpy', 'joblib', 'scipy', 'threadpoolctl', 'scikit-learn'
+    ],
+    author='Melvin Chen',
+    author_email='melvinchen610@gmail.com',
+    description='THIS LIBRARY IS DEPRECATED.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mchen610/pygame-text-centering',
+)
