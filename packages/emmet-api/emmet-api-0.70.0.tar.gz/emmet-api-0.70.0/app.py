5d87a49aa3ee762fe5d2621b6a00496afaea5a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 11:10:27 +0000

    Add package emmet-api/0.70.0/emmet-api-0.70.0.tar.gz

---
+++ packages/emmet-api/emmet-api-0.70.0.tar.gz/emmet-api-0.70.0/app.py
@@ -0,0 +1,33 @@
+import time
+
+start = time.perf_counter()
+import logging
+
+logging.getLogger("uvicorn.access").handlers = []
+from asgi_logger import AccessLoggerMiddleware
+
+from material_resources import resources as materials_resources
+from molecule_resources import resources as molecule_resources
+
+from emmet.api.core.api import MAPI
+from emmet.api.core.documentation import description, tags_meta
+from emmet.api.core.settings import MAPISettings
+
+logger = logging.getLogger(__name__)
+default_settings = MAPISettings()
+
+resources = {**materials_resources, **molecule_resources}
+
+api = MAPI(
+    resources=resources,
+    debug=default_settings.DEBUG,
+    description=description,
+    tags_meta=tags_meta,
+)
+app = api.app
+app.add_middleware(
+    AccessLoggerMiddleware,
+    format='%(h)s %(t)s %(m)s %(U)s?%(q)s %(H)s %(s)s %(b)s "%(f)s" "%(a)s" %(D)s %(p)s %({x-consumer-id}i)s',
+)
+delta = time.perf_counter() - start
+logger.warning(f"Startup took {delta:.1f}s")
