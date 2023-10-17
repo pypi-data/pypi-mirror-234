2de15af70cf1baba27bb342b478bd332abb39726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:14:34 +0000

    Add package idiff/0.0.1/idiff-0.0.1-py3-none-any.whl

---
+++ packages/idiff/idiff-0.0.1-py3-none-any.whl/idiff/__init__.py
@@ -0,0 +1,14 @@
+'''
+Package: IDIFF
+--------------
+Improve DIFFractograms = remove background + noise from 2D diffraction patterns.
+
+* Input:
+    - 2D numpy array representing original/raw 2D-diffraction pattern.
+    - The original patterns usually exhibit non-zero background + random noise.
+* Output:
+    - 2D numpy array representing clean/de-noised 2D-diffraction pattern.
+    - The ideal clean diffraction pattern has no background intensity and noise.    
+'''
+
+__version__ = "0.0.1"
