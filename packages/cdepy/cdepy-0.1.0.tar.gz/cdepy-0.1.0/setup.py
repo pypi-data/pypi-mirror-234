adfb8f73ff4e4544929567f15172b2a648ffaab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:11:20 +0000

    Add package cdepy/0.1.0/cdepy-0.1.0.tar.gz

---
+++ packages/cdepy/cdepy-0.1.0.tar.gz/cdepy-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Paul de Fusco",
+    description="A Python Package for interacting with Cloudera Data Engineering Clusters",
+    name="cdepy",
+    version="0.1.0",
+    packages=find_packages(include=["cdepy", "cdepy.*"]),
+    install_requires=["pyparsing==3.0.9", "requests-toolbelt==1.0.0", "xmltodict==0.13.0"],
+    python_requires=">=2.7",
+)
