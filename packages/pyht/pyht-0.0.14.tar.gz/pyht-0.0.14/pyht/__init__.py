083008acd7a87176b3020ed8fc9b289d0c695be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:07:52 +0000

    Add package pyht/0.0.14/pyht-0.0.14.tar.gz

---
+++ packages/pyht/pyht-0.0.14.tar.gz/pyht-0.0.14/pyht/__init__.py
@@ -0,0 +1,7 @@
+# Don't manually change, let poetry-dynamic-versioning handle it.
+__version__ = "0.0.14"
+
+
+from .client import Client, Format, TTSOptions
+
+__all__ = ["Client", "Format", "TTSOptions"]
