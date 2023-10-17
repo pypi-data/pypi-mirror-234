213ab1f330056d3272ae39e71dd7e018913c17a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:10:00 +0000

    Add package spaceone-repository/1.12rc1/spaceone_repository-1.12rc1-py3-none-any.whl

---
+++ packages/spaceone-repository/spaceone_repository-1.12rc1-py3-none-any.whl/spaceone/repository/manager/repository_manager/local_repository_manager.py
@@ -0,0 +1,16 @@
+from spaceone.core import utils
+
+from spaceone.repository.error import *
+from spaceone.repository.manager.repository_manager import RepositoryManager
+
+
+class LocalRepositoryManager(RepositoryManager):
+
+    def register_repository(self, params):
+        # Assume there is only one local repository
+        local_repo_vo = self.get_local_repository()
+        if local_repo_vo is not None:
+            raise ERROR_LOCAL_REPOSITORY_ALREADY_EXIST()
+
+        params['repository_id'] = utils.generate_id('repo')
+        return self.repo_model.create(params)
