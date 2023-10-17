3a24a725e6a3709191d9a93ba3573834e3a25476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 03:11:30 +0000

    Add package autowork-cli/0.1.5/autowork-cli-0.1.5.tar.gz

---
+++ packages/autowork-cli/autowork-cli-0.1.5.tar.gz/autowork-cli-0.1.5/autowork_cli/common/config/BaseURLConfig.py
@@ -0,0 +1,20 @@
+URL_DICT = {
+    "DEV": "http://cybotron-dev.yunzhangfang.com",
+    "ALPHA": "http://cybotron-alpha.yunzhangfang.com",
+    "BETA": "http://cybotron-beta.yunzhangfang.com",
+    "PROD": "http://cybotron.yunzhangfang.com",
+}
+
+
+class BaseURLConfig:
+    @staticmethod
+    def get_api_base_url(env):
+        return URL_DICT[env] + "/cybotron-client"
+
+    @staticmethod
+    def get_domain_url(env):
+        return URL_DICT[env]
+
+    @staticmethod
+    def get_env_list():
+        return list(URL_DICT.keys())
---
+++ packages/autowork-cli/autowork-cli-0.1.5.tar.gz/autowork-cli-0.1.5/autowork_cli/common/config/TemplateConfig.py
@@ -0,0 +1,3 @@
+PYPROJECT_TEMPLATE_URL = 'http://gitlab.yzf.net/finbot/sandbox-config-template/blob/master/pyproject.toml'
+SANDBOX_FUNCTION_TEMPLATE_URL = 'http://gitlab.yzf.net/finbot/sandbox-config-template/blob/master/sandbox_function.json'
+SANDBOX_BOOT_URL = 'https://rpanewtest-1315768454.cos.ap-shanghai.myqcloud.com/autowork/sandbox_boot.zip'
