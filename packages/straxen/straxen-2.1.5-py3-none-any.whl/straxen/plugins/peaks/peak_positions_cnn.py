4002d5ed1081edd0e327da1b570b7bd64015b267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 11:07:14 +0000

    Add package straxen/2.1.5/straxen-2.1.5-py3-none-any.whl

---
+++ packages/straxen/straxen-2.1.5-py3-none-any.whl/straxen/plugins/peaks/peak_positions_cnn.py
@@ -0,0 +1,26 @@
+import strax
+import straxen
+from straxen.plugins.peaks._peak_positions_base import PeakPositionsBaseNT
+
+
+export, __all__ = strax.exporter()
+
+
+@export
+class PeakPositionsCNN(PeakPositionsBaseNT):
+    """Convolutional Neural Network (CNN) neural net for position reconstruction"""
+    provides = "peak_positions_cnn"
+    algorithm = "cnn"
+    __version__ = '0.0.1'
+
+    tf_model_cnn = straxen.URLConfig(
+        default=f'tf://'
+                f'resource://'
+                f'cmt://{algorithm}_model'
+                f'?version=ONLINE'
+                f'&run_id=plugin.run_id'
+                f'&fmt=abs_path',
+        help='CNN model. Should be opened using the "tf" descriptor. '
+             'Set to "None" to skip computation',
+        cache=3,
+    )
