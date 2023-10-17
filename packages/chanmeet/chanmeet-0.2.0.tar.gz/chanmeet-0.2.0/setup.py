1be5e1a455c3415083cdf0fc1f5c4d00c0f328f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:08:20 +0000

    Add package chanmeet/0.2.0/chanmeet-0.2.0.tar.gz

---
+++ packages/chanmeet/chanmeet-0.2.0.tar.gz/chanmeet-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chanmeet',
+    version='0.2.0',
+    author='Chanmeet Singh Sahni',
+    author_email='chanmeetsinghsahni@gmail.com',
+    description='This is my first library',
+    long_description='This library just prints my name',
+    long_description_content_type='text/markdown',
+    url='https://github.com/chanmeetsingh',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
