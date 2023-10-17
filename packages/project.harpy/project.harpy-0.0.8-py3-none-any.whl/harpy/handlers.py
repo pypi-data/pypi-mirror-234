afe521d0ab88d4c94eaac3a08ea9da213ada3591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:07:31 +0000

    Add package project.harpy/0.0.8/project.harpy-0.0.8-py3-none-any.whl

---
+++ packages/project.harpy/project.harpy-0.0.8-py3-none-any.whl/harpy/handlers.py
@@ -0,0 +1,49 @@
+"""Various handlers."""
+
+import atexit
+import os
+import termios
+
+
+class Echo:
+    """Terminal echo."""
+
+    @staticmethod
+    @atexit.register
+    def enable() -> None:
+        """Enable echo."""
+        try:
+            attributes = termios.tcgetattr(0)
+            attributes[3] |= termios.ECHO
+            termios.tcsetattr(0, termios.TCSANOW, attributes)
+        except termios.error:
+            pass
+
+    @staticmethod
+    def disable() -> None:
+        """Disable echo."""
+        try:
+            attributes = termios.tcgetattr(0)
+            attributes[3] &= ~termios.ECHO
+            termios.tcsetattr(0, termios.TCSANOW, attributes)
+        except termios.error:
+            pass
+
+
+class Is:
+    """Multiple unrelated "is" questions."""
+
+    @staticmethod
+    def atty() -> bool:
+        """Test if all file descriptors are connected to a terminal."""
+        return os.isatty(0) and os.isatty(1) and os.isatty(2)
+
+    @staticmethod
+    def fore() -> bool:
+        """Test if the program is running in the foreground."""
+        return os.getpgrp() == os.tcgetpgrp(1)
+
+    @staticmethod
+    def root() -> bool:
+        """Test if user is root."""
+        return os.geteuid() == 0
