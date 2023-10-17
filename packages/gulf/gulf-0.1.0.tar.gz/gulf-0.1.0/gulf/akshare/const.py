37a7d765f9f050b1c56956a65092216f7a034316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:07:59 +0000

    Add package gulf/0.1.0/gulf-0.1.0.tar.gz

---
+++ packages/gulf/gulf-0.1.0.tar.gz/gulf-0.1.0/gulf/akshare/const.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Datetime : 2023/10/9 21:21
+# @Author   : Fangyang
+# @Software : PyCharm
+
+#TODO
+AKSHARE_CRAWL_CONCURRENT_LIMIT = 50
+AKSHARE_CRAWL_STOP_INTERVEL = 30  # seconds
+
+
+if __name__ == '__main__':
+    pass
