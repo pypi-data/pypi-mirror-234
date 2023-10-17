90b7832d230dcb857497587cadc30f1718c82f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:10:21 +0000

    Add package SalKikal/0.1.0/SalKikal-0.1.0.tar.gz

---
+++ packages/SalKikal/SalKikal-0.1.0.tar.gz/SalKikal-0.1.0/README.md
@@ -0,0 +1,2 @@
+After installing and importing this package, user can create the instance of base class and 
+call functions with the following logic: getting value, setting value, and updating value.
---
+++ packages/SalKikal/SalKikal-0.1.0.tar.gz/SalKikal-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="SalKikal",
+    version="0.1.0",
+    author="Salome Kikalishvili",
+    description="A package for creating and modifying class objects",
+    author_email="salomekikalishvili2000@gmail.com",
+    packages=["Package"],
+)
