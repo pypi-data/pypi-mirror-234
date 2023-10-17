1717f279abfc1ad70feb0e64c71606bd16a5ef68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:09:09 +0000

    Add package pypi-jump-to/0.2.2/pypi_jump_to-0.2.2.tar.gz

---
+++ packages/pypi-jump-to/pypi_jump_to-0.2.2.tar.gz/pypi_jump_to-0.2.2/src/pjt/core/entities.py
@@ -0,0 +1,53 @@
+"""Module that contains entities."""
+
+from __future__ import annotations
+
+import enum
+import dataclasses
+
+
+@dataclasses.dataclass
+class DestinationInfo:
+    """Dataclass that represents destination info."""
+
+    alias: str
+    description: str
+
+
+class Destination(enum.Enum):
+    """Available destinations."""
+
+    pypi: DestinationInfo = DestinationInfo(
+        alias="p",
+        description="package info on https://pypi.org",
+    )
+    homepage: DestinationInfo = DestinationInfo(
+        alias="h",
+        description="homepage (e.g., website, docs)",
+    )
+    repository: DestinationInfo = DestinationInfo(
+        alias="r",
+        description="repository (e.g., github)",
+    )
+    changelog: DestinationInfo = DestinationInfo(
+        alias="c",
+        description="changelog",
+    )
+    issues: DestinationInfo = DestinationInfo(
+        alias="i",
+        description="issues",
+    )
+    pr: DestinationInfo = DestinationInfo(
+        alias="pr",
+        description="pull requests",
+    )
+
+    @classmethod
+    def get_by_alias(cls, alias: str) -> Destination:
+        """Get a field based on the alias."""
+
+        for destination in Destination:
+            if destination.value.alias == alias:
+                return destination
+
+        return Destination.pypi
