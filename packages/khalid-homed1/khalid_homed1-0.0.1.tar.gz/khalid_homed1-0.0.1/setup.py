e3960d5fb9a3258e3c26b297f9870f81cb572cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:07:45 +0000

    Add package khalid-homed1/0.0.1/khalid_homed1-0.0.1.tar.gz

---
+++ packages/khalid-homed1/khalid_homed1-0.0.1.tar.gz/khalid_homed1-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="khalid_homed1",
+    version="0.0.1",
+    author="khalidhomed",
+    author_email="",
+    description="simpel library in python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
