a9b7ead9ef4dcdd7fc9743fb31691bd1bcc70ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:11:16 +0000

    Add package MPSPlots/0.5.9/MPSPlots-0.5.9-py3-none-any.whl

---
+++ packages/MPSPlots/MPSPlots-0.5.9-py3-none-any.whl/MPSPlots/render2D/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from MPSPlots.render2D.artist import *
+from MPSPlots.render2D.scene import SceneMatrix, SceneList
+from MPSPlots.render2D.axis import Axis
+
+
+def Multipage(filename, figs=None, dpi=200):
+    pp = PdfPages(filename)
+
+    for fig in figs:
+        fig._mpl_figure.savefig(pp, format='pdf')
+
+    pp.close()
