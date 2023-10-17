061e589345c5c89f8e15957a53f01d312cef2a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:15:28 +0000

    Add package pycgmes/1.1.0/pycgmes-1.1.0-py3-none-any.whl

---
+++ packages/pycgmes/pycgmes-1.1.0-py3-none-any.whl/pycgmes/resources/DynamicsFunctionBlock.py
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: 2023 Alliander
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Generated from the CGMES 3 files via cimgen: https://github.com/sogno-platform/cimgen
+"""
+
+from functools import cached_property
+from pydantic import Field
+from pydantic.dataclasses import dataclass
+from ..utils.dataclassconfig import DataclassConfig
+from ..utils.profile import BaseProfile, Profile
+
+from .IdentifiedObject import IdentifiedObject
+
+
+@dataclass(config=DataclassConfig)
+class DynamicsFunctionBlock(IdentifiedObject):
+    """
+    Abstract parent class for all Dynamics function blocks.
+
+    enabled: Function block used indicator. true = use of function block is enabled false = use of function block is
+      disabled.
+    """
+
+    enabled: bool = Field(
+        default=False,
+        in_profiles=[
+            Profile.DY,
+        ],
+    )
+
+    @cached_property
+    def possible_profiles(self) -> set[BaseProfile]:
+        """
+        A resource can be used by multiple profiles. This is the set of profiles
+        where this element can be found.
+        """
+        return {
+            Profile.DY,
+        }
