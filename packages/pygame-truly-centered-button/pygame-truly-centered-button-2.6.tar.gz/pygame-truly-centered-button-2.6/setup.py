4e28b65607a5be69a14745d9673666907de93f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 00:06:53 +0000

    Add package pygame-truly-centered-button/2.6/pygame-truly-centered-button-2.6.tar.gz

---
+++ packages/pygame-truly-centered-button/pygame-truly-centered-button-2.6.tar.gz/pygame-truly-centered-button-2.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygame-truly-centered-button',
+    version='2.6',
+    packages=find_packages(),
+    install_requires=[
+        'pygame', 'numpy', 'joblib', 'scipy', 'threadpoolctl', 'scikit-learn'
+    ],
+    author='Melvin Chen',
+    author_email='melvinchen610@gmail.com',
+    description='A Pygame library of a button class with properly-centered text!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mchen610/pygame-truly-centered-button',
+)
