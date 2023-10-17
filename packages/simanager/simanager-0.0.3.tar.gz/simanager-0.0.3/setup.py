e5184751d83263c2cc04596ad6117fbb6b7703bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:17:31 +0000

    Add package simanager/0.0.3/simanager-0.0.3.tar.gz

---
+++ packages/simanager/simanager-0.0.3.tar.gz/simanager-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simanager",
+    version="0.0.3",
+    author="Carlo Emilio Montanari",
+    author_email="carlo.emilio.montanari@cern.ch",
+    description="A Python package for managing simulations locally, on HTCondor and on Slurm, with some specific elements that are good in a CERNy environment.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carlidel/simanager",
+    packages=find_packages(),
+    install_requires=["numpy", "pyyaml"],
+    include_package_data=True,
+    # MIT license
+    license="MIT",
+)
