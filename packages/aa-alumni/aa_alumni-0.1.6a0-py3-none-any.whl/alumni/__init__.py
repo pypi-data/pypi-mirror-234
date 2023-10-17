44897c975d8d601a380359263d32a2bcf3546544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:07:04 +0000

    Add package aa-alumni/0.1.6a0/aa_alumni-0.1.6a0-py3-none-any.whl

---
+++ packages/aa-alumni/aa_alumni-0.1.6a0-py3-none-any.whl/alumni/__init__.py
@@ -0,0 +1,5 @@
+"""
+Integration with Alliance Auths State System, creates and maintains an Alumni State for past members of an Alliance and/or Corporation
+"""
+__version__ = "0.1.6a"
+__title__ = "Alumni"
