8cb77d56277c4ef8f0e226d32ca6609127072ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:08:00 +0000

    Add package chapter-marker/1.0.3/chapter_marker-1.0.3-py3-none-any.whl

---
+++ packages/chapter-marker/chapter_marker-1.0.3-py3-none-any.whl/chapter_marker/bgt_get_titles.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+""" usage: bgt-get-titles [options] [SHOW]
+
+options:
+    --out=FILE          file to output the titles to (default is stdout)
+    --apikey=KEY        The etherpad APIKEY
+
+requires either PAD_APIKEY to be set  or --apikey to be supplied
+
+returns the id of the next bgt show
+"""
+
+import os
+import sys
+import urllib.request
+
+import requests
+from docopt import docopt
+
+url = "https://etherpad.euer.krebsco.de/api/1.2.15/getText?apikey={}&padID={}"
+
+
+def current_show():
+    url = "https://pad.binaergewitter.de/"
+    ret = urllib.request.urlopen(url)
+    return ret.geturl().split("/")[-1]
+
+
+def main():
+    args = docopt(__doc__)
+    apikey = os.environ.get("PAD_APIKEY", args["--apikey"])
+    show = args["SHOW"]
+    out = args["--out"]
+
+    if not apikey:
+        print(__doc__)
+        sys.exit(1)
+    if not show:
+        show = current_show()
+    try:
+        ret = requests.get(url.format(apikey, show)).json()["data"]["text"]
+    except:  # noqa E722
+        # way to complicated to do right:
+        # https://stackoverflow.com/questions/16511337
+        print("for url:" + url.format(apikey, show))
+        print(f"response: {requests.get(url.format(apikey,show))}")
+    header = True
+
+    titles = []
+    for line in ret.split("\n"):
+        if line == "---":
+            header = False
+        if not header and line.startswith("## "):
+            titles.append(line.replace("## ", "").strip())
+
+    if not out:
+        print("\n".join(titles))
+    else:
+        with open(out, "w+") as f:
+            print("\n".join(titles), file=f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/chapter-marker/chapter_marker-1.0.3-py3-none-any.whl/chapter_marker/bgt_replace_text.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+""" usage: bgt-get-titles [options] [SHOW]
+
+options:
+    --apikey=KEY        The etherpad APIKEY
+
+requires either PAD_APIKEY to be set  or --apikey to be supplied
+
+returns the id of the next bgt show
+"""
+
+import os
+import sys
+import urllib.request
+
+import requests
+from docopt import docopt
+
+baseurl = "https://etherpad.euer.krebsco.de/api/1.2.15/{}?apikey={}&padID={}"
+
+
+def current_show():
+    url = "https://pad.binaergewitter.de/"
+    ret = urllib.request.urlopen(url)
+    return ret.geturl().split("/")[-1]
+
+
+def main():
+    args = docopt(__doc__)
+    apikey = os.environ.get("PAD_APIKEY", None)
+    if not apikey:
+        print("PAD_APIKEY is not set, falling back to --apikey")
+        apikey = args["--apikey"]
+
+    show = args["SHOW"]
+
+    if not apikey:
+        print(__doc__)
+        sys.exit(1)
+    if not show:
+        show = current_show()
+    try:
+        print(f"Fetching show {show}")
+        text = requests.get(baseurl.format("getText", apikey, show)).json()["data"]["text"]
+        print(f"replacing <SENDUNGSNUMMER> with {show.upper()}")
+        text = text.replace("<SENDUNGSNUMMER>", show.upper())
+        print("updating shownotes")
+        requests.post(baseurl.format("setText", apikey, show), data={"text": text})
+    except:  # noqa E722
+        print(f"for url: {baseurl.format(apikey, show)}")
+        print(f"response: {requests.get(baseurl.format(apikey,show))}")
+
+
+if __name__ == "__main__":
+    main()
