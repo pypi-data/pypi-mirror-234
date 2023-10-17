10de05cf96c5aab12fd16f616ce50d23c46b2887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:16:09 +0000

    Add package napari-wsi/0.1.4/napari_wsi-0.1.4-py3-none-any.whl

---
+++ packages/napari-wsi/napari_wsi-0.1.4-py3-none-any.whl/napari_wsi/widget.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from pathlib import Path
+
+from magicgui import magic_factory
+from napari.types import LayerDataTuple
+
+from . import read_rasterio, read_tifffile
+
+
+class WSIReaderBackend(Enum):
+    tifffile = "tifffile"
+    rasterio = "rasterio"
+
+
+DEFAULT_BACKEND = WSIReaderBackend.tifffile
+
+
+@magic_factory(
+    call_button="Load",
+    path={"label": "Path", "tooltip": "The file path."},
+    backend={"label": "Backend", "tooltip": "The backend for reading WSI data."},
+    series={"label": "Series", "tooltip": "The image series to read."},
+    split_rgb={"label": "Split RGB", "tooltip": "If set, split 3-channel images."},
+)
+def get_wsi_reader_widget(
+    path: Path,
+    backend: WSIReaderBackend = DEFAULT_BACKEND,
+    series: int = 0,
+    split_rgb: bool = False,
+) -> list[LayerDataTuple]:
+    if not path.is_file():
+        raise FileNotFoundError(path)
+
+    if backend == WSIReaderBackend.tifffile:
+        return read_tifffile(path, series=series, split_rgb=split_rgb)
+    if backend == WSIReaderBackend.rasterio:
+        return read_rasterio(path, split_rgb=split_rgb)
+
+    raise ValueError(f"Invalid backend {backend}.")
