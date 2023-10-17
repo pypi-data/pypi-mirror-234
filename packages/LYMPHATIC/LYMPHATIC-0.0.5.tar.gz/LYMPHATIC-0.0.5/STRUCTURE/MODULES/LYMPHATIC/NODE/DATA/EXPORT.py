b35c246f85dbd508d7b234ad52f6238205497ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:11:53 +0000

    Add package LYMPHATIC/0.0.5/LYMPHATIC-0.0.5.tar.gz

---
+++ packages/LYMPHATIC/LYMPHATIC-0.0.5.tar.gz/LYMPHATIC-0.0.5/STRUCTURE/MODULES/LYMPHATIC/NODE/DATA/EXPORT.py
@@ -0,0 +1,16 @@
+
+
+'''
+	rethinkdb dump 
+	[-c HOST:PORT] 
+	[-p] [--password-file FILENAME] 
+	[--tls-cert FILENAME] 
+	[-f FILE] 
+	[--clients NUM] 
+	[-e (DB | DB.TABLE)]
+'''
+
+'''
+	???
+		rethinkdb dump
+'''
