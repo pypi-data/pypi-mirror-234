5e1ebbdc0bee37bf30cafa36ec081001b1efbe88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 08:08:10 +0000

    Add package SilverStreamingApplicationWindchill/1.4/SilverStreamingApplicationWindchill-1.4.tar.gz

---
+++ packages/SilverStreamingApplicationWindchill/SilverStreamingApplicationWindchill-1.4.tar.gz/SilverStreamingApplicationWindchill-1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SilverStreamingApplicationWindchill',
+    version='1.4',
+    packages=['com.phida.main', 'com.phida.tests'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='workflow builder functinality for bronze layer added'
+)
