0ca5a551b43307d7c34ddeefeb682d20526733e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:12:16 +0000

    Add package slurminade/0.8.0/slurminade-0.8.0-py3-none-any.whl

---
+++ packages/slurminade/slurminade-0.8.0-py3-none-any.whl/slurminade/conf.py
@@ -0,0 +1,44 @@
+"""
+This file saves the default configuration for slurm.
+"""
+
+import json
+import os.path
+from pathlib import Path
+
+
+def _load_default_conf():
+    default_conf_file = os.path.join(Path.home(), ".slurminade_default.json")
+    try:
+        if os.path.isfile(default_conf_file):
+            with open(default_conf_file) as f:
+                return json.load(f)
+        else:
+            return {}
+    except Exception as e:
+        print(
+            f"slurminade could not open default configuration {default_conf_file}!\n{e!s}"
+        )
+    return {}
+
+
+__default_conf = _load_default_conf()
+
+
+def update_default_configuration(conf=None, **kwargs):
+    if conf:
+        __default_conf.update(conf)
+    if kwargs:
+        __default_conf.update(kwargs)
+
+
+def set_default_configuration(conf=None, **kwargs):
+    __default_conf = {}
+    update_default_configuration(conf, **kwargs)
+
+
+def _get_conf(conf=None):
+    conf = conf if conf else {}
+    conf_ = __default_conf.copy()
+    conf_.update(conf)
+    return conf_
