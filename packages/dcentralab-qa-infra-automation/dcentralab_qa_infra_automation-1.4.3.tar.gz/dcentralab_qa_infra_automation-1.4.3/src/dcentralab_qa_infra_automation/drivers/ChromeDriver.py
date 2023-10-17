017f9e119c61ced48c55ba548534ff3581834275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:10:02 +0000

    Add package dcentralab-qa-infra-automation/1.4.3/dcentralab_qa_infra_automation-1.4.3.tar.gz

---
+++ packages/dcentralab-qa-infra-automation/dcentralab_qa_infra_automation-1.4.3.tar.gz/dcentralab_qa_infra_automation-1.4.3/src/dcentralab_qa_infra_automation/drivers/ChromeDriver.py
@@ -0,0 +1,47 @@
+import os
+
+import pytest
+from dcentralab_qa_infra_automation.drivers.HelperFunctions import addExtensionToChrome
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+"""
+init chrome driver, using ChromeDriverManager for chromeDriver installation
+
+@Author: Efrat Cohen
+@Date: 11.2022
+"""
+
+
+def get_chrome_service():
+    chrome_service = Service(
+        executable_path=ChromeDriverManager().install())
+    return chrome_service
+
+
+def initChromeDriver():
+    """
+    init chrome driver, using ChromeDriverManager for chromeDriver installation
+    :return: driver - driver instance
+    """
+
+    driver = webdriver.Chrome(service=get_chrome_service())
+    pytest.logger.info("start the chrome driver with options")
+    return driver
+
+
+def initChromeDriverWithExtension():
+    """
+    init chrome driver with CRX extension, using ChromeDriverManager for chromeDriver installation
+    :return: driver - driver instance
+    """
+    options = ChromeOptions()
+    options.binary_location = os.get_exec_path()
+    pytest.logger.info("add extension to chrome")
+    options.add_extension(addExtensionToChrome())
+    pytest.logger.info("start the chrome driver with options")
+    chrome_service = get_chrome_service()
+    driver = webdriver.Chrome(options=options, service=chrome_service)
+    return driver
