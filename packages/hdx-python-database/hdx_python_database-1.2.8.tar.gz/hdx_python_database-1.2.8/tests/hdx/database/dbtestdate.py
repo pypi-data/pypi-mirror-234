a1d07271f8d4276849a03ab4a8a841236d71a4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 09:10:17 +0000

    Add package hdx-python-database/1.2.8/hdx_python_database-1.2.8.tar.gz

---
+++ packages/hdx-python-database/hdx_python_database-1.2.8.tar.gz/hdx_python_database-1.2.8/tests/hdx/database/dbtestdate.py
@@ -0,0 +1,32 @@
+"""SQLAlchemy class representing DBTestDate row. Holds test data for dates.
+"""
+from datetime import datetime
+
+from sqlalchemy import select
+from sqlalchemy.orm import Mapped, mapped_column
+
+from hdx.database.no_timezone import Base
+from hdx.database.views import view
+
+
+class DBTestDate(Base):
+    """
+    test_date: Mapped[datetime] = mapped_column(primary_key=True)
+    """
+
+    test_date: Mapped[datetime] = mapped_column(primary_key=True)
+
+    def __repr__(self) -> str:
+        """String representation of DBTestDate row
+
+        Returns:
+            str: String representation of DBTestDate row
+        """
+        return f"<Test date={str(self.test_date)}>"
+
+
+date_view = view(
+    name="date_view",
+    metadata=Base.metadata,
+    selectable=select(*DBTestDate.__table__.columns),
+)
