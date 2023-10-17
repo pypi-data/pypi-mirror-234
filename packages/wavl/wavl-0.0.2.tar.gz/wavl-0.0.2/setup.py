55bfddc2adeacffe1d903dc41947cfa194ab6ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:06:39 +0000

    Add package wavl/0.0.2/wavl-0.0.2.tar.gz

---
+++ packages/wavl/wavl-0.0.2.tar.gz/wavl-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wavl",
+    version="0.0.2",
+    author="synterweyst",
+    author_email=os.getenv("EMAIL"), # dont worry about this
+    description="bestest",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://wavl.top",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
