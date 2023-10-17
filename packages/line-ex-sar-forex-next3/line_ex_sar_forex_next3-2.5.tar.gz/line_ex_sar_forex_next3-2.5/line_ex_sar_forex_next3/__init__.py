2adbe1af800ccfdb7223de3bca9a7e68916471ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:17:02 +0000

    Add package line-ex-sar-forex-next3/2.5/line_ex_sar_forex_next3-2.5.tar.gz

---
+++ packages/line-ex-sar-forex-next3/line_ex_sar_forex_next3-2.5.tar.gz/line_ex_sar_forex_next3-2.5/line_ex_sar_forex_next3/__init__.py
@@ -0,0 +1,1 @@
+from .line_ex_sar_forex_next3 import LINE
---
+++ packages/line-ex-sar-forex-next3/line_ex_sar_forex_next3-2.5.tar.gz/line_ex_sar_forex_next3-2.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (name='line_ex_sar_forex_next3' ,
+       version = '2.5' ,
+       description = 'line_ex_sar_forex_next3' ,
+       author = 'omid' ,
+       packages =['line_ex_sar_forex_next3'] ,
+       zip_safe = False
+       )
