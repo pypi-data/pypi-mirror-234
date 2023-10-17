49c94c7d2ba82fbb1942f5fd8f2ee4835e1bc8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 09:10:22 +0000

    Add package f-ck/0.0.10/f-ck-0.0.10.tar.gz

---
+++ packages/f-ck/f-ck-0.0.10.tar.gz/f-ck-0.0.10/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='f-ck',
+    version='0.0.10',
+    license='Apache 2.0',
+    author='Zeio Nara',
+    author_email='zeionara@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A tiny program for uncensoring russian texts',
+    data_files=[('assets', ['assets/profane-words.txt'])],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/zeionara/fuck',
+    project_urls={
+        'Documentation': 'https://github.com/zeionara/fuck#readme',
+        'Bug Reports': 'https://github.com/zeionara/fuck/issues',
+        'Source Code': 'https://github.com/zeionara/fuck'
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.11"
+    ],
+    install_requires = ['pymorphy3', 'tqdm']
+)
