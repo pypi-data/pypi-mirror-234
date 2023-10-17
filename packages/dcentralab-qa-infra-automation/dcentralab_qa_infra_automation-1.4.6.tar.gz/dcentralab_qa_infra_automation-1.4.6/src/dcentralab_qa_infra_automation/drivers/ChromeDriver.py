6867f73b60a2f4c8281ef834ba9a3f3a9b024a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:13:00 +0000

    Add package dcentralab-qa-infra-automation/1.4.6/dcentralab_qa_infra_automation-1.4.6.tar.gz

---
+++ packages/dcentralab-qa-infra-automation/dcentralab_qa_infra_automation-1.4.6.tar.gz/dcentralab_qa_infra_automation-1.4.6/src/dcentralab_qa_infra_automation/drivers/ChromeDriver.py
@@ -0,0 +1,41 @@
+import pytest
+from dcentralab_qa_infra_automation.drivers.HelperFunctions import addExtensionToChrome
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+"""
+init chrome driver, using ChromeDriverManager for chromeDriver installation
+
+@Author: Efrat Cohen
+@Date: 11.2022
+"""
+
+
+def get_chrome_service():
+    print(ChromeDriverManager().driver.get_browser_version_from_os())
+    chrome_service = Service(
+        executable_path=ChromeDriverManager().install())
+    return chrome_service
+
+
+def initChromeDriver():
+    """
+    init chrome driver, using ChromeDriverManager for chromeDriver installation
+    :return: driver - driver instance
+    """
+
+    driver = webdriver.Chrome(service=get_chrome_service())
+    pytest.logger.info("start the chrome driver with options")
+    return driver
+
+
+def initChromeDriverWithExtension():
+    """
+    init chrome driver with CRX extension, using ChromeDriverManager for chromeDriver installation
+    :return: driver - driver instance
+    """
+    options = webdriver.ChromeOptions()
+    options.add_extension(addExtensionToChrome())
+    driver = webdriver.Chrome(options=options)
+    return driver
