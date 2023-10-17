9df118de158c88d4203fcaf8bc25c1754f02e289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:15:42 +0000

    Add package numba-stats/1.4.1/numba_stats-1.4.1-py3-none-any.whl

---
+++ packages/numba-stats/numba_stats-1.4.1-py3-none-any.whl/numba_stats/expon.py
@@ -0,0 +1,98 @@
+"""
+Exponential distribution.
+
+See Also
+--------
+scipy.stats.expon: Scipy equivalent.
+"""
+import numpy as np
+from math import expm1 as _expm1, log1p as _log1p
+from ._util import _jit, _trans, _generate_wrappers, _prange, _rvs_jit, _seed
+
+_doc_par = """
+loc : float
+    Location of the mode.
+scale : float
+    Standard deviation.
+"""
+
+
+@_jit(-1)
+def _cdf1(z):
+    return -_expm1(-z)
+
+
+@_jit(-1)
+def _ppf1(p):
+    return -_log1p(-p)
+
+
+@_jit(2)
+def _logpdf(x, loc, scale):
+    z = _trans(x, loc, scale)
+    return -z - np.log(scale)
+
+
+@_jit(2)
+def _pdf(x, loc, scale):
+    """
+    Return probability density.
+
+    Parameters
+    ----------
+    x: ArrayLike
+        Random variate.
+    loc : float
+        Location of the mode.
+    scale : float
+        Standard deviation.
+
+    Returns
+    -------
+    ArrayLike
+        Function evaluated at x.
+    """
+    return np.exp(_logpdf(x, loc, scale))
+
+
+@_jit(2)
+def _cdf(x, loc, scale):
+    """
+    Return cumulative probability.
+
+    Parameters
+    ----------
+    x: ArrayLike
+        Random variate.
+    loc : float
+        Location of the mode.
+    scale : float
+        Standard deviation.
+
+    Returns
+    -------
+    ArrayLike
+        Function evaluated at x.
+    """
+    z = _trans(x, loc, scale)
+    for i in _prange(len(z)):
+        z[i] = _cdf1(z[i])
+    return z
+
+
+@_jit(2)
+def _ppf(p, loc, scale):
+    z = np.empty_like(p)
+    for i in _prange(len(z)):
+        z[i] = _ppf1(p[i])
+    return scale * z + loc
+
+
+@_rvs_jit(2)
+def _rvs(loc, scale, size, random_state):
+    _seed(random_state)
+    p = np.random.uniform(0, 1, size)
+    return _ppf(p, loc, scale)
+
+
+_generate_wrappers(globals())
