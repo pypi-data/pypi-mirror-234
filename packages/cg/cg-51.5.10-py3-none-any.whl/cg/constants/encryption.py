1bdadc4506cbb32b08e7b30522544a742b8da844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:13:37 +0000

    Add package cg/51.5.10/cg-51.5.10-py3-none-any.whl

---
+++ packages/cg/cg-51.5.10-py3-none-any.whl/cg/constants/encryption.py
@@ -0,0 +1,58 @@
+"""Constants specific for encryption"""
+from enum import Enum
+from typing import List
+
+from cg.utils.enums import ListEnum
+
+
+class CipherAlgorithm(str, Enum):
+    TRIPLE_DES: str = "3DES"
+    AES: str = "AES"
+    AES192: str = "AES192"
+    AES256: str = "AES256"
+    BLOWFISH: str = "BLOWFISH"
+    CAMELLIA128: str = "CAMELLIA128"
+    CAMELLIA192: str = "CAMELLIA192"
+    CAMELLIA256: str = "CAMELLIA256"
+    CAST5: str = "CAST5"
+    IDEA: str = "IDEA"
+    TWOFISH: str = "TWOFISH"
+
+
+class EncryptionUserID(str, Enum):
+    HASTA_USER_ID: str = '"Clinical Genomics"'
+
+
+class GPGParameters(ListEnum):
+    ASYMMETRIC_ENCRYPTION: List[str] = [
+        "--encrypt",
+        "--recipient",
+        EncryptionUserID.HASTA_USER_ID,
+    ]
+    ASYMMETRIC_DECRYPTION: list = [
+        "--decrypt",
+        "--batch",
+        "--cipher-algo",
+        CipherAlgorithm.AES256,
+        "--passphrase",
+        EncryptionUserID.HASTA_USER_ID,
+    ]
+    SYMMETRIC_ENCRYPTION: List[str] = [
+        "--symmetric",
+        "--cipher-algo",
+        CipherAlgorithm.AES256,
+        "--batch",
+        "--compress-algo",
+        "None",
+        "--passphrase-file",
+    ]
+    SYMMETRIC_DECRYPTION: List[str] = [
+        "--decrypt",
+        "--cipher-algo",
+        CipherAlgorithm.AES256,
+        "--batch",
+        "--passphrase-file",
+    ]
+    OUTPUT_PARAMETER: List[str] = [
+        "-o",
+    ]
