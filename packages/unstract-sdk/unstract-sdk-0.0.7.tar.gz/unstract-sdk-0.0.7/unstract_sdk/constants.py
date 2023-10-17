ec3b58013bd2a3783883922dffaf6bf5027b6143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 12:06:14 +0000

    Add package unstract-sdk/0.0.7/unstract-sdk-0.0.7.tar.gz

---
+++ packages/unstract-sdk/unstract-sdk-0.0.7.tar.gz/unstract-sdk-0.0.7/unstract_sdk/constants.py
@@ -0,0 +1,10 @@
+class PlatformServiceKeys:
+    PLATFORM_API_KEY = "PLATFORM_API_KEY"
+
+
+class DbConnectorKeys:
+    ID = "id"
+    PROJECT_ID = "project_id"
+    CONNECTOR_ID = "connector_id"
+    TOOL_INSTANCE_ID = "tool_instance_id"
+    CONNECTOR_METADATA = "connector_metadata"
