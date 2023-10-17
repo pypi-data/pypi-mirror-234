873678d1ed1425c0ad20fca5486627923c4d5f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:17 +0000

    Add package junk-dbt/2.0.0/junk-dbt-2.0.0.tar.gz

---
+++ packages/junk-dbt/junk-dbt-2.0.0.tar.gz/junk-dbt-2.0.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "junk-dbt"
+package_version = "2.0.0"
+description = "test-dbt"
+
+requires = ["requests"]
+
+extras = {
+    "v15": [
+        "dbt-core>=1.5,<1.6",
+    ],
+    "fifteen": [
+        "dbt-core>=1.5,<1.6",
+    ]
+
+}
+
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    extras_require=extras,
+)
