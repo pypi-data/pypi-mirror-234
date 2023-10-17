9c8234ff04aad03ef86694affae69eadd10b69f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:08:01 +0000

    Add package wsjtx-srv/0.6/wsjtx_srv-0.6.tar.gz

---
+++ packages/wsjtx-srv/wsjtx_srv-0.6.tar.gz/wsjtx_srv-0.6/README.rst
@@ -0,0 +1,52 @@
+wsjtx-srv: Library for WSJT-X server implementation
+===================================================
+
+:Author: Ralf Schlatterbeck <rsc@runtux.com>
+
+This implements a simple UDP server that binds to the WSJT-X_ UDP message
+protocol port. It also provides everything for parsing and/or generating
+WSJT-X_ telegrams.
+
+By default calling ``wsjtx-srv`` will provide a simple server that
+colors all callsigns not in the ADIF file for the current band. It uses
+the ADIF logfile from WSJT-X_ with a default path to that file. You can
+specify the correct path for your installation either via
+command-line (call ``wsjtx-srv`` with the ``--help`` option) or in the
+environment variable ``WBF_PATH``. It has also an implementation that
+looks up DXCC-entities in my log database, but only those that have been
+confirmed via LOTW.
+
+The implementation of ``wsjtx-srv`` should give a rough idea of how to use
+this in your own projects.
+
+There is a companion-program ``wbf`` standing for *worked before* that
+takes a number of callsigns on the command-line and tells you the worked
+before status.
+
+.. _WSJT-X: https://physics.princeton.edu/pulsar/k1jt/wsjtx.html
+
+Changes
+-------
+
+Version 0.5: Add pyproject.toml
+
+- Installation with new setuptools again possible
+
+Version 0.4: Move to setuptools
+
+- Seems previous releases had versioning problems with pypi
+
+Version 0.3: Small fixes
+
+- Compatibility with older protocol versions, thanks to Sampo Savolainen
+  for the patch
+- Fix band lookup if no QSO on band
+
+Version 0.2: Fix setup.py install_requires
+
+Version 0.1: Initial implementation
+
+- Implement serialization and deserialization of WSJT-X_ telegrams and a
+  simple server
+- First Release
+
---
+++ packages/wsjtx-srv/wsjtx_srv-0.6.tar.gz/wsjtx_srv-0.6/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# Copyright (C) 2021-23 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Reichergasse 131, A-3411 Weidling.
+# Web: http://www.runtux.com Email: office@runtux.com
+# All rights reserved
+# ****************************************************************************
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ****************************************************************************
+
+import sys
+from setuptools import setup
+sys.path.insert (1, '.')
+from wsjtx_srv import __version__
+
+with open ('README.rst') as f:
+    description = f.read ()
+
+license     = 'BSD License'
+rq          = '>=3.7'
+setup \
+    ( name             = "wsjtx_srv"
+    , version          = __version__
+    , description      = "Library implementation of WSJTX companion program"
+    , long_description = ''.join (description)
+    , long_description_content_type = 'text/x-rst'
+    , license          = license
+    , author           = "Ralf Schlatterbeck"
+    , author_email     = "rsc@runtux.com"
+    , install_requires = ['rsclib', 'hamradio']
+    , packages         = ['wsjtx_srv']
+    , platforms        = 'Any'
+    , url              = "https://github.com/schlatterbeck/wsjtx-srv"
+    , entry_points     = dict
+        ( console_scripts =
+            [ 'wsjtx-srv=wsjtx_srv.wsjtx:main'
+            , 'wbf=wsjtx_srv.wsjtx:wbf'
+            ]
+        )
+    , python_requires  = rq
+    , classifiers      = \
+        [ 'Development Status :: 5 - Production/Stable'
+        , 'License :: OSI Approved :: ' + license
+        , 'Operating System :: OS Independent'
+        , 'Programming Language :: Python'
+        , 'Intended Audience :: Developers'
+        , 'Programming Language :: Python :: 3.7'
+        , 'Programming Language :: Python :: 3.8'
+        , 'Programming Language :: Python :: 3.9'
+        , 'Programming Language :: Python :: 3.10'
+        , 'Programming Language :: Python :: 3.11'
+        ]
+    )
