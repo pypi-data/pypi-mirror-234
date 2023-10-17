9d5964501d3215021e018c11efbeae1a9730d079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 09:07:43 +0000

    Add package formant/1.140.31/formant-1.140.31.tar.gz

---
+++ packages/formant/formant-1.140.31.tar.gz/formant-1.140.31/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationTemplateListResponse, AnnotationTemplateTags, AnnotationType, AnnotationField, AnnotationFieldValuesRequest, ScopeFilterTypesItem, AnnotationFieldValue, AnnotationTags, UserParametersRolesItem, UserParameters, AnnotationFieldValueTags, AnnotationMetadata, AnnotationFieldType, ScopeFilter, AnnotationFieldValuesRequestTags, SheetParameters, AnnotationTemplate, Annotation, AnnotationStreamType, TagParameters, AnnotationFieldValuesResponse
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.140.31.tar.gz/formant-1.140.31/formant/sdk/cloud/v2/src/resources/views/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import ImageViewConfigurationMode, LocalizationViewConfiguration, Filter, ViewListResponse, PartialViewTags, Camera, View, ViewConfigurationType, TransformTreeViewConfiguration, LocationViewport, ViewLayoutType, ViewConfiguration, NumericViewConfiguration, PartialView, LocationViewConfiguration, LocationViewConfigurationBasemap, ViewTags, PointCloudViewConfiguration, PartialViewLayoutType, Location, ImageViewConfiguration, BitsetViewConfiguration, Vector3
+from formant.sdk.cloud.v2.formant_admin_api_client.api.view import view_controller_get_one, view_controller_get_all, view_controller_patch
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
