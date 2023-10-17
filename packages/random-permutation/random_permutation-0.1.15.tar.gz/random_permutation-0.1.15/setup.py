4ae36421a24339cc4a18e289d7ac133194144602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:09:44 +0000

    Add package random-permutation/0.1.15/random_permutation-0.1.15.tar.gz

---
+++ packages/random-permutation/random_permutation-0.1.15.tar.gz/random_permutation-0.1.15/setup.py
@@ -0,0 +1,28 @@
+"""
+This setup file is used to package the random_permutation module.
+The module is used for efficiently generating very large random permutations.
+"""
+
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(
+  name='random_permutation',
+  version='0.1.15',
+  author='Gary William Flake',
+  author_email='gary@flake.org',
+  description=
+  'A module for efficiently generating very large random permutations',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages=find_packages(
+    include=['random_permutation', 'random_permutation.*']),
+  install_requires=['cryptography'],
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+)
