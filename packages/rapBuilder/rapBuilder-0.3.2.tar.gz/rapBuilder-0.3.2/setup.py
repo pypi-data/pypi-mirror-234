fca3718964460a1be01711d8a9f8eb25d858de11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:15:32 +0000

    Add package rapBuilder/0.3.2/rapBuilder-0.3.2.tar.gz

---
+++ packages/rapBuilder/rapBuilder-0.3.2.tar.gz/rapBuilder-0.3.2/setup.py
@@ -0,0 +1,31 @@
+"""
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+@File : setup.py
+@Author : Zhiyue Chen
+@Time : 2023/8/24 14:31
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='rapBuilder',
+    version='0.3.2',
+    packages=find_packages(),
+    install_requires=[
+        'ViennaRNA==2.6.3',
+        'tqdm',
+        'scikit-learn==1.3.1'
+    ],
+    python_requires='>=3.10',
+    platforms='linux_x86_64',
+    author='mistyfield',
+    author_email='20301050198@fudan.edu',
+    description='find optimal RBS and build pRAP system',
+    url='https://gitlab.igem.org/2023/software-tools/fudan',
+    license='CC BY 4.0',
+    license_file='LICENSE',
+    project_urls={
+        'Wiki': r'https://2023.igem.wiki/fudan/software',
+        'Documentation': r'https://mistyfield.github.io/RAP-Docs/'
+    }
+)
