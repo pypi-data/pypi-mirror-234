e4154ddd24e682853f2c1f30b79a78c1ec8c2ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 01:29:12 +0000

    Add package html-to-dash/0.2.6/html_to_dash-0.2.6-py3-none-any.whl

---
+++ packages/html-to-dash/html_to_dash-0.2.6-py3-none-any.whl/html_to_dash/__init__.py
@@ -0,0 +1,2 @@
+from .main import parse_html, FormatParser
+
---
+++ packages/html-to-dash/html_to_dash-0.2.6-py3-none-any.whl/html_to_dash/helper.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+import textwrap
+from lxml import etree
+from typing import Union, Callable
+
+# logger
+logger = logging.getLogger("html-to-dash")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(message)s")
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+def etree_pretty(root, space=" "*4, func: Union[None, Callable] = None):
+    """
+    Indent both tags and text in etree._Element.
+    :param root: etree._Element
+    :param space: The whitespace to insert for each indentation.
+    :param func: Custom function added to beautify multi line strings in dash format.
+    """
+    for elem in root.iterdescendants():
+        if elem.text:
+            depth = int(elem.xpath("count(ancestor::*)")) + 1
+            space_mul = depth if func is None else func(depth)
+            # The strip needs to be on the periphery, the dedent will maintain the relative position of each row.
+            temp_text = textwrap.dedent(elem.text).strip()
+            elem.text = (
+                "\n"
+                + textwrap.indent(temp_text, prefix=space_mul * space)
+                + ("\n" + space_mul * space)
+            )
+    etree.indent(root, space=space)
