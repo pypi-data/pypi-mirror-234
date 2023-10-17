e79eabdde2761203c000106d108165f789ce6536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:15:48 +0000

    Add package hello-chp/0.0.21/hello_chp-0.0.21.tar.gz

---
+++ packages/hello-chp/hello_chp-0.0.21.tar.gz/hello_chp-0.0.21/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+URL = "https://github.com/Quantmetry/qolmat"
+DOWNLOAD_URL = "https://pypi.org/project/qolmat/#files"
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/Quantmetry/qolmat",
+    "Documentation": "https://qolmat.readthedocs.io/en/latest/",
+    "Source Code": "https://github.com/Quantmetry/qolmat",
+}
+VERSION = "0.0.21"
+
+setup(
+    name="hello_chp",
+    version=VERSION,
+    author="Grégoire Martinon, Vianney Taquet, Damien Hervault",
+    author_email="gmartignon@quantmetry.com",
+    description="A Quantmetry tutorial on how to publish an opensource python package.",
+    license="BSD",
+    keywords="example opensource tutorial",
+    url=URL,
+    packages=find_packages(),
+    install_requires=["numpy>=1.20"],
+    extras_require={
+        "tests": ["flake8", "mypy", "pytest-cov"],
+        "docs": ["sphinx", "sphinx-gallery", "sphinx_rtd_theme", "numpydoc"],
+    },
+    long_description=read("README.rst"),
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
