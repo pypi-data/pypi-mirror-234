6073f74957d4f9f9c16bdb54c790ee60b1c21c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:08:41 +0000

    Add package flask-unity/0.0.15/flask_unity-0.0.15-py2.py3-none-any.whl

---
+++ packages/flask-unity/flask_unity-0.0.15-py2.py3-none-any.whl/flask_unity/database.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+class DB:
+    """This class handle mostly database management stuffs"""
+
+    def __init__(self):
+        """init method for our database stuffs"""
+        pass
+
+    def make_migrations(self):
+        """make migrations for database"""
+        pass
+
+    def migrating_database(self):
+        """migrate database"""
+        pass
+
+    def update_db(self):
+        """update database"""
+        pass
+
+    def dump_db(self):
+        """dump database"""
+        pass
+
+    def cleaned_db(self):
+        """cleaned database"""
+        pass
+
+
+"""
+Command line shortcuts for database:
+    db_status
+    db_migrate
+    db_makemigrations
+    db_pulldata
+    db_pushdata
+    db_cleaned
+"""
