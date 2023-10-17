388d6f7a091b1596594e1c4d729ba80e0982b9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 05:08:40 +0000

    Add package dissect.shellitem/3.7.dev0/dissect.shellitem-3.7.dev0.tar.gz

---
+++ packages/dissect.shellitem/dissect.shellitem-3.7.dev0.tar.gz/dissect.shellitem-3.7.dev0/tests/conftest.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+import pytest
+
+
+def absolute_path(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+
+@pytest.fixture
+def xp_modified_remote_lnk_file():
+    return Path(absolute_path("data/modified_remote.file.xp.lnk"))
+
+
+@pytest.fixture
+def xp_remote_lnk_file():
+    return Path(absolute_path("data/remote.file.xp.lnk"))
+
+
+@pytest.fixture
+def xp_remote_lnk_dir():
+    return Path(absolute_path("data/remote.directory.xp.lnk"))
+
+
+@pytest.fixture
+def win7_local_lnk_dir():
+    return Path(absolute_path("data/local.directory.seven.lnk"))
+
+
+@pytest.fixture
+def win81_downloads_lnk_dir():
+    return Path(absolute_path("data/downloads.win81.lnk"))
