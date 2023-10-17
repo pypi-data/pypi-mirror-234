41e5866da6d09b18fddf92cb96469c191f1150ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 09:09:55 +0000

    Add package execdata/2.1.2/execdata-2.1.2.tar.gz

---
+++ packages/execdata/execdata-2.1.2.tar.gz/execdata-2.1.2/execdata/__init__.py
@@ -0,0 +1,16 @@
+'''
+Date         : 2022-10-25 15:44:41
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-10-11 16:33:19
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\__init__.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
+'''
+
+from execdata.templateproj import add_one, add_two
+from execdata.data_conversion import convint, convfloat
+from execdata.data_preprocess import filtered_value_list, drop_columns
+from execdata.standardization import encode, sep, split, strat_split
+from execdata.model_evaluate import model_evaluate, algo_accuracy, result_comparision
