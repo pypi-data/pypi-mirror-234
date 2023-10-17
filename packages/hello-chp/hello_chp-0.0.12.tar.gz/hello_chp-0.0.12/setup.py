f63990fbf30747cdd54c9622ffa8de99444decd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 06:17:15 +0000

    Add package hello-chp/0.0.12/hello_chp-0.0.12.tar.gz

---
+++ packages/hello-chp/hello_chp-0.0.12.tar.gz/hello_chp-0.0.12/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="hello_chp",
+    version="0.0.12",
+    author="Grégoire Martinon, Vianney Taquet, Damien Hervault",
+    author_email="gmartignon@quantmetry.com",
+    description="A Quantmetry tutorial on how to publish an opensource python package.",
+    license="BSD",
+    keywords="example opensource tutorial",
+    url="http://packages.python.org/how_to_opensource",
+    packages=['how_to_opensource'],
+    install_requires=["numpy>=1.20"],
+    extras_require={
+        "tests": ["flake8", "mypy", "pytest-cov"],
+        "docs": ["sphinx", "sphinx-gallery", "sphinx_rtd_theme", "numpydoc"]
+    },
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
