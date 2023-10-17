778dfe1185b3733103b59555682e4b5de21af2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 11:07:43 +0000

    Add package listmakerkpanger882/0.0.2/listmakerkpanger882-0.0.2.tar.gz

---
+++ packages/listmakerkpanger882/listmakerkpanger882-0.0.2.tar.gz/listmakerkpanger882-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'a test package for checking dependency resolvers'
+LONG_DESCRIPTION = 'This simple package creates a list. It is not intended for actual use, but for demontrating version compatibility issues.'
+
+# Setting up
+setup(
+    name="listmakerkpanger882",
+    version=VERSION,
+    author="EldritchToast",
+    author_email="<kpanger88@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'dependency','test', 'list'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
