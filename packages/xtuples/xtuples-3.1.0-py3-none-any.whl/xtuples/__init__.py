1958605713716464f09cadeafcf9042e71fc592e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 07:12:47 +0000

    Add package xtuples/3.1.0/xtuples-3.1.0-py3-none-any.whl

---
+++ packages/xtuples/xtuples-3.1.0-py3-none-any.whl/xtuples/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present Tom Williams <tomjrw@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .i import iTuple, iLazy, pipe
+from .n import nTuple
+from . import f
