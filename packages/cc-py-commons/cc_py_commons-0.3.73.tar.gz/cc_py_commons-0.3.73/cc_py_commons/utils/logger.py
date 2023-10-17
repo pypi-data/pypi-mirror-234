96013d9890ca91b36e52430f61cc7af629bd8bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 09:07:52 +0000

    Add package cc-py-commons/0.3.73/cc_py_commons-0.3.73.tar.gz

---
+++ packages/cc-py-commons/cc_py_commons-0.3.73.tar.gz/cc_py_commons-0.3.73/cc_py_commons/utils/logger.py
@@ -0,0 +1,34 @@
+import os, sys, json
+import logging
+import socket
+from logging import StreamHandler
+from logging.handlers import SysLogHandler
+
+from cc_py_commons.config.env import env_name, app_config
+
+class ContextFilter(logging.Filter):
+  hostname = socket.gethostname()
+
+  def filter(self, record):
+    record.hostname = ContextFilter.hostname
+    return True
+
+def _config_logger():
+  logger = logging.getLogger(app_config.LOG_APP_NAME)
+  logger.setLevel(app_config.LOG_LEVEL)
+
+  contextFilter = ContextFilter()
+  logger.addFilter(contextFilter)
+  formatter = logging.Formatter('{}: %(levelname)s %(message)s'.format(app_config.LOG_APP_NAME),
+                            datefmt='%Y-%m-%d %I:%M:%S %p')
+
+  if env_name == 'local':
+    syslog = StreamHandler()
+  else:
+    syslog = SysLogHandler(address=('logs3.papertrailapp.com', 10024))
+
+  syslog.setFormatter(formatter)
+  logger.addHandler(syslog)
+  return logger
+
+logger = _config_logger()
