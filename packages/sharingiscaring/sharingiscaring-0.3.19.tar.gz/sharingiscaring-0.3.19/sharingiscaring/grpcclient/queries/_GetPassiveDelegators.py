891e7c53ef5e185a617cc8f7d8a5a24cbc22f17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-17 04:08:56 +0000

    Add package sharingiscaring/0.3.19/sharingiscaring-0.3.19.tar.gz

---
+++ packages/sharingiscaring/sharingiscaring-0.3.19.tar.gz/sharingiscaring-0.3.19/sharingiscaring/grpcclient/queries/_GetPassiveDelegators.py
@@ -0,0 +1,56 @@
+# ruff: noqa: F403, F405, E402
+from __future__ import annotations
+from sharingiscaring.GRPCClient.types_pb2 import *
+from sharingiscaring.enums import NET
+from enum import Enum
+from sharingiscaring.GRPCClient.queries._SharedConverters import (
+    Mixin as _SharedConverters,
+)
+from typing import Iterator
+from typing import TYPE_CHECKING
+import sys
+
+if TYPE_CHECKING:
+    from sharingiscaring.GRPCClient import GRPCClient
+from sharingiscaring.GRPCClient.CCD_Types import CCD_DelegatorInfo
+
+
+class Mixin(_SharedConverters):
+    def get_delegators_for_passive_delegation(
+        self: GRPCClient,
+        block_hash: str,
+        net: Enum = NET.MAINNET,
+    ) -> list[CCD_DelegatorInfo]:
+        result: list[CCD_DelegatorInfo] = []
+        blockHashInput = self.generate_block_hash_input_from(block_hash)
+
+        self.check_connection(net, sys._getframe().f_code.co_name)
+        if net == NET.MAINNET:
+            grpc_return_value: Iterator[
+                DelegatorInfo
+            ] = self.stub_mainnet.GetPassiveDelegators(request=blockHashInput)
+        else:
+            grpc_return_value: Iterator[
+                DelegatorInfo
+            ] = self.stub_testnet.GetPassiveDelegators(request=blockHashInput)
+
+        for delegator in list(grpc_return_value):
+            delegator_dict = {
+                "account": self.convertAccountAddress(delegator.account),
+                "stake": self.convertAmount(delegator.stake),
+            }
+            if delegator.pending_change:
+                if self.valueIsEmpty(delegator.pending_change):
+                    pass
+                else:
+                    delegator_dict.update(
+                        {
+                            "pending_change": self.convertPendingChange(
+                                delegator.pending_change
+                            )
+                        }
+                    )
+
+            result.append(CCD_DelegatorInfo(**delegator_dict))
+
+        return result
